597490262f05447ca51a1303e21d473a0b8d0bf0
Jon Skeet <jonskeet@google.com>
2016-03-03 12:11:12 +0000

    Prepare for public packages
    - Remove unnecessary AssemblyInfo files
    - Reduce other AssemblyInfo files to only what's required; dnx will build other values in
    - Remove unnecessary properties in project.json files for packages which we won't ship
    - Use an empty version string for "in-solution" version dependencies
    - Add appropriate properties to the Google.Storage.V1 project.json, to make it as close
      to shippable as we can right now.

--- src/Google.Storage.V1/Properties/AssemblyInfo.cs
+++ src/Google.Storage.V1/Properties/AssemblyInfo.cs
@@ -1,25 +1,5 @@
-﻿using System.Reflection;
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// Licensed under the Apache License Version 2.0.
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Google.Storage.V1")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Google.Storage.V1")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e8814844-5fa9-4f2b-aead-e7ce427de48a")]
 
 [assembly: InternalsVisibleTo("Google.Storage.V1.Tests,PublicKey=0024000004800000940000000602000000240000525341310004000001000100afab79952ee22215f12b4e09337e65509c943fbc22d7006bc371d581d0f0ebf0da5d8039aab2607fb68a138a5d80a71bc02b7ebf586dbe1f2493c0ab20423ababfd15ce74d2264a6b37745f3658f016abaad662182aaef634a60f1346fcc45343acab5b6781535a3134818e13fac895a6c106c0480e34bbb06cb123e5583d8d2")]
