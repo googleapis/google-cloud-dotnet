54f81cc3267e85c6a695d581aff2ac719a1a5069
Jon Skeet <skeet@pobox.com>
2016-07-11 16:59:10 +0100

    More structured myget build (#265)

--- myget.bat
+++ myget.bat
@@ -1,14 +1,47 @@
 REM This is all ugly... we'll want to revisit this as soon as possible
 call dnvm use 1.0.0-rc1-update1 -r clr
+IF ERRORLEVEL 1 goto error
 
 REM We've found some oddities when using our own feed.
 REM Basically, we want to fetch everything from nuget.org.
-rmdir /s /q %USERPROFILE%\.dnx\packages
+REM rmdir /s /q %USERPROFILE%\.dnx\packages
 
-call dnu restore src
+call dnu restore src test snippets
 IF ERRORLEVEL 1 EXIT /B 1
 
-REM We want to build and package everthing under src.
-REM We're not building any tests etc; that's what Travis is for.
-call dnu pack src\*
-IF ERRORLEVEL 1 EXIT /B 1
+for /D %%P in (src\Google*) do (
+  echo Building %%P
+  call dnu build --configuration %Configuration% %%P
+  IF ERRORLEVEL 1 goto error
+)
+
+for /D %%P in (snippets\Google*) do (
+  echo Building %%P
+  call dnu build --configuration %Configuration% %%P
+  IF ERRORLEVEL 1 goto error
+)
+
+REM We don't build/run the integration tests
+for /D %%P in (test\Google*.Tests) do (
+  echo Testing %%P
+  call dnu build --configuration %Configuration% %%P
+  IF ERRORLEVEL 1 goto error
+  dnx --configuration %Configuration% -p %%P test
+  IF ERRORLEVEL 1 goto error
+)
+
+for /D %%P in (src\Google*) do (
+  echo Packing %%P
+  call dnu pack --configuration %Configuration% %%P
+  IF ERRORLEVEL 1 goto error
+)
+
+echo Done!
+
+goto end
+
+:error
+echo Build failed due to error.
+exit /b 1
+
+:end
