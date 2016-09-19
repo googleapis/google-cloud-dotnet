aaf87b6919dfba648a0f1956973e48f53522e426
Jon Skeet <skeet@pobox.com>
2016-09-19 19:26:28 +0100

    Run coveralls upload only on merge

---
+++ coveralls.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Only push to coveralls when code is committed; secure variables
+# aren't decoded in pull requests.
+if [ -n "$COVERALLS_REPO_TOKEN" ]
+then
+  nuget install -OutputDirectory packages -Version 0.7.0 coveralls.net
+  packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i coverage/coverage.xml
+fi
