b0b2c45113b4870b9898ce6b525f12395297e82d
Jon Skeet <skeet@pobox.com>
2016-09-21 18:20:05 +0100

    Prepare monitoring/trace/error-reporting for 0.1.0
    This adds snippets and somewhat bare documentation.

---
+++ apis/Google.Devtools.Cloudtrace.V1/docs/index.md
@@ -0,0 +1,43 @@
+# Google.Devtools.Cloudtrace.V1
+
+`Google.Devtools.Cloudtrace.V1` is a .NET client
+library for the [Stackdriver Trace
+API](https://cloud.google.com/trace/).
+
+# Installation
+
+Install the `Google.Devtools.Cloudtrace.V1` package from our
+[early access MyGet
+feed](https://www.myget.org/gallery/google-dotnet-public).
+First onfigure your environment to include the appropriate NuGet feed
+URL:
+
+- NuGet v2: `https://www.myget.org/F/google-dotnet-public/`
+- NuGet v3: `https://www.myget.org/F/google-dotnet-public/api/v3/index.json`
+
+Next, add the package to your project in the normal way (for example
+by right-clicking on the project in Visual Studio and choosing
+"Manage NuGet Packages..."). Please ensure you enable pre-release
+packages (for example, in the Visual Studio NuGet user interface,
+check the "Include prerelease" box).
+
+# Authentication
+
+To authenticate all your API calls, first install and setup the
+[Google Cloud SDK](https://cloud.google.com/sdk/). After that is
+installed, run the following command in a Google Cloud SDK Shell:
+
+```sh
+> gcloud auth login
+```
+
+# Getting started
+
+The main entry point for the API is [TraceServiceClient](obj/api/Google.Devtools.Cloudtrace.V1.TraceServiceClient.yml).
+Create an instance using the static `Create` method, optionally specifying a service endpoint and settings.
+
+# Sample code
+
+## List traces
+
+[!code-cs[](obj/snippets/Google.Devtools.Cloudtrace.V1.TraceServiceClient.txt#ListTraces)]
---
+++ apis/Google.Monitoring.V3/Google.Monitoring.V3.Snippets/GroupServiceClientSnippets.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Google.Monitoring.V3
+{
+    [Collection(nameof(MonitoringFixture))]
+    public class GroupServiceClientSnippets
+    {
+        private readonly MonitoringFixture _fixture;
+
+        public GroupServiceClientSnippets(MonitoringFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void ListGroups()
+        {
+            string projectId = _fixture.ProjectId;
+
+            // Snippet: ListGroups
+            GroupServiceClient client = GroupServiceClient.Create();
+            string projectName = MetricServiceClient.FormatProjectName(projectId);
+            IPagedEnumerable<ListGroupsResponse, Group> groups = client.ListGroups(projectName, "", "", "");
+            foreach (Group group in groups.Take(10))
+            {
+                Console.WriteLine($"{group.Name}: {group.DisplayName}");
+            }
+            // End snippet
+        }
+    }
+}
