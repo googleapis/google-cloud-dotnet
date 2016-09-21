db4fa1ab2ed7ca951378f196e835769670e4806b
Jon Skeet <skeet@pobox.com>
2016-09-21 11:24:50 +0100

    Use relative paths for coverage (#391)

--- coveralls.sh
+++ coveralls.sh
@@ -5,5 +5,5 @@
 if [ -n "$COVERALLS_REPO_TOKEN" ]
 then
   nuget install -OutputDirectory packages -Version 0.7.0 coveralls.net
-  packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i coverage/coverage.xml
+  packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i coverage/coverage.xml --useRelativePaths
 fi
