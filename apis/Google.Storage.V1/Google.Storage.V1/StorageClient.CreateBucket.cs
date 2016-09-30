f5e5f4028bffd60c1dc4d66c132978fd3f247110
Jon Skeet <skeet@pobox.com>
2016-09-30 12:28:27 +0100

    Fix Storage documentation warnings

--- apis/Google.Storage.V1/Google.Storage.V1/StorageClient.CreateBucket.cs
+++ apis/Google.Storage.V1/Google.Storage.V1/StorageClient.CreateBucket.cs
@@ -21,22 +21,58 @@ namespace Google.Storage.V1
 {
     public abstract partial class StorageClient
     {
+        /// <summary>
+        /// Creates a storage bucket synchronously.
+        /// </summary>
+        /// <param name="projectId">The ID of the project to create the buckets in. Must not be null.</param>
+        /// <param name="bucket">The name of the bucket to create. Must not be null.</param>
+        /// <param name="options">Additional options for the operation. May be null, in which case appropriate
+        /// defaults will be used.</param>
+        /// <returns>The resource representation of the newly-created bucket.</returns>
         public virtual Bucket CreateBucket(string projectId, string bucket, CreateBucketOptions options = null)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Creates a storage bucket asynchronously.
+        /// </summary>
+        /// <param name="projectId">The ID of the project to create the buckets in. Must not be null.</param>
+        /// <param name="bucket">The name of the bucket to create. Must not be null.</param>
+        /// <param name="options">Additional options for the operation. May be null, in which case appropriate
+        /// defaults will be used.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+        /// <returns>A task representing the asynchronous operation, with a result returning the
+        /// resource representation of the newly-created bucket.</returns>
         public virtual Task<Bucket> CreateBucketAsync(string projectId, string bucket, CreateBucketOptions options = null,
             CancellationToken cancellationToken = default(CancellationToken))
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Creates a storage bucket synchronously.
+        /// </summary>
+        /// <param name="projectId">The ID of the project to create the buckets in. Must not be null.</param>
+        /// <param name="bucket">The resource representation of the bucket to create. Must not be null, and must have a name.</param>
+        /// <param name="options">Additional options for the operation. May be null, in which case appropriate
+        /// defaults will be used.</param>
+        /// <returns>The resource representation of the newly-created bucket.</returns>
         public virtual Bucket CreateBucket(string projectId, Bucket bucket, CreateBucketOptions options = null)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Creates a storage bucket asynchronously.
+        /// </summary>
+        /// <param name="projectId">The ID of the project to create the buckets in. Must not be null.</param>
+        /// <param name="bucket">The resource representation of the bucket to create. Must not be null, and must have a name.</param>
+        /// <param name="options">Additional options for the operation. May be null, in which case appropriate
+        /// defaults will be used.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+        /// <returns>A task representing the asynchronous operation, with a result returning the
+        /// resource representation of the newly-created bucket.</returns>
         public virtual Task<Bucket> CreateBucketAsync(string projectId, Bucket bucket, CreateBucketOptions options = null,
             CancellationToken cancellationToken = default(CancellationToken))
         {
