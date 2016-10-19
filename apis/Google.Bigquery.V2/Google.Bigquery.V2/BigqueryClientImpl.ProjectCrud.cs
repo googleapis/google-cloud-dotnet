83057d42248efb16d2fb39b7d02c4b21ee293b5e
Jon Skeet <skeet@pobox.com>
2016-10-19 07:39:03 +0100

    Add a ListProjects operation.
    It's slightly odd to have this operation in the API, as it's more
    of a Cloud aspect than BigQuery specific - but it's part of the
    exposed REST API, and other libraries expose it, so we're doing so
    for consistency.

---
+++ apis/Google.Bigquery.V2/Google.Bigquery.V2/BigqueryClientImpl.ProjectCrud.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax.Rest;
+using Google.Apis.Bigquery.v2.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using static Google.Apis.Bigquery.v2.ProjectsResource;
+
+namespace Google.Bigquery.V2
+{
+    public partial class BigqueryClientImpl
+    {
+        private sealed class ProjectsPageManager : IPageManager<ListRequest, ProjectList, CloudProject>
+        {
+            private readonly BigqueryClient _client;
+
+            internal ProjectsPageManager(BigqueryClient client)
+            {
+                _client = client;
+            }
+
+            public string GetNextPageToken(ProjectList response) => response.NextPageToken;
+            public IEnumerable<CloudProject> GetResources(ProjectList response) => response.Projects?.Select(resource => new CloudProject(_client, resource));
+            public void SetPageSize(ListRequest request, int pageSize) => request.MaxResults = pageSize;
+            public void SetPageToken(ListRequest request, string pageToken) => request.PageToken = pageToken;
+        }
+
+        /// <inheritdoc />
+        public override IPagedEnumerable<ProjectList, CloudProject> ListProjects(ListProjectsOptions options = null)
+        {
+            var pageManager = new ProjectsPageManager(this);
+            return new PagedEnumerable<ListRequest, ProjectList, CloudProject>(
+                () => CreateListProjectsRequest(options),
+                pageManager);
+        }
+
+        /// <inheritdoc />
+        private ListRequest CreateListProjectsRequest(ListProjectsOptions options)
+        {
+            var request = Service.Projects.List();
+            options?.ModifyRequest(request);
+            return request;
+        }
+    }
+}
