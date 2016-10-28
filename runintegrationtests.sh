7050c58886afd2d51981cada0a0b07897f28d4b2
Jon Skeet <skeet@pobox.com>
2016-10-28 08:36:46 +0100

    Integration test updates
    - New --continue option for runintegrationtests.sh, to continue from where it left off
    - Generate a results.xml file so it's easy to find out which tests are slow
    - Make Google.Storage.V1.Snippets work in core

--- runintegrationtests.sh
+++ runintegrationtests.sh
@@ -3,8 +3,10 @@
 # Assumption: everything has been built already.
 
 set -e
+PROGRESS_FILE=`realpath integrationprogress.txt`
 
-[[ $(dotnet --info | grep "OS Platform" | grep -c Windows) -ne 0 ]] && OS=Windows || OS=Linux
+[[ "$1" == "--continue" ]] || rm $PROGRESS_FILE
+touch $PROGRESS_FILE
 
 cd apis
 
@@ -13,8 +15,12 @@ for testdir in */*.IntegrationTests */*.Snippets
 do
   if [[ "$testdir" =~ (Metadata|AspNet|Log4Net) ]]
   then
-    echo Skipping $testdir
+    echo "Skipping $testdir; test not supported yet."
+  elif echo "$testdir" | grep --quiet -F -f $PROGRESS_FILE
+  then
+    echo "Skipping $testdir; test already run"
   else
-    dotnet test -f netcoreapp1.0 $DOTNET_TEST_ARGS $testdir
+    dotnet test -c Release --no-build -f netcoreapp1.0 $DOTNET_TEST_ARGS $testdir -xml $testdir/results.xml
+    echo "$testdir" >> $PROGRESS_FILE
   fi
 done
