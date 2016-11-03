2a853cdd7a2e485e8033591dd408af1b25d2bfd4
Jon Skeet <skeet@pobox.com>
2016-11-03 07:19:39 +0000

    Add unit tests for Async calls
    Basically these mirror the tests we have for sync calls

--- apis/Google.Bigquery.V2/Google.Bigquery.V2.Tests/BigqueryClientImplTest.cs
+++ apis/Google.Bigquery.V2/Google.Bigquery.V2.Tests/BigqueryClientImplTest.cs
@@ -14,6 +14,7 @@
 
 using Google.Apis.Bigquery.v2.Data;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 using static Google.Apis.Bigquery.v2.Data.DatasetList;
 using static Google.Apis.Bigquery.v2.Data.JobList;
@@ -201,6 +202,188 @@ namespace Google.Bigquery.V2.Tests
                 service.Jobs.Cancel(projectId, jobId),
                 new JobCancelResponse { Job = new Job { JobReference = reference } });
             var result = client.CancelJob(reference);
+            Assert.Equal(projectId, result.Reference.ProjectId);
+            Assert.Equal(jobId, result.Reference.JobId);
+        }
+
+        [Fact]
+        public async Task ListProjectsAsyncAsync()
+        {
+            var projectId = "project";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var reference = client.GetProjectReference(projectId);
+            service.ExpectRequest(
+                service.Projects.List(),
+                new ProjectList { Projects = new[] { new ProjectsData { ProjectReference = reference } } });
+            var result = client.ListProjectsAsync();
+            var dataset = await result.Single();
+            Assert.Equal(projectId, dataset.Reference.ProjectId);
+        }
+
+        [Fact]
+        public async Task CreateDatasetAsync()
+        {
+            var projectId = "project";
+            var datasetId = "dataset";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var reference = client.GetDatasetReference(projectId, datasetId);
+            service.ExpectRequest(
+                service.Datasets.Insert(new Dataset { DatasetReference = reference }, projectId),
+                new Dataset { DatasetReference = reference });
+            var result = await client.CreateDatasetAsync(reference);
+            Assert.Equal(projectId, result.Reference.ProjectId);
+            Assert.Equal(datasetId, result.Reference.DatasetId);
+        }
+
+        [Fact]
+        public async Task GetDatasetAsync()
+        {
+            var projectId = "project";
+            var datasetId = "dataset";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var reference = client.GetDatasetReference(projectId, datasetId);
+            service.ExpectRequest(
+                service.Datasets.Get(projectId, datasetId),
+                new Dataset { DatasetReference = reference });
+            var result = await client.GetDatasetAsync(reference);
+            Assert.Equal(projectId, result.Reference.ProjectId);
+            Assert.Equal(datasetId, result.Reference.DatasetId);
+        }
+
+        [Fact]
+        public async Task ListDatasetsAsync()
+        {
+            var projectId = "project";
+            var datasetId = "dataset";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var projectReference = client.GetProjectReference(projectId);
+            var datasetReference = client.GetDatasetReference(projectId, datasetId);
+            service.ExpectRequest(
+                service.Datasets.List(projectId),
+                new DatasetList { Datasets = new[] { new DatasetsData { DatasetReference = datasetReference } } });
+            var result = client.ListDatasetsAsync(projectReference);
+            var dataset = await result.Single();
+            Assert.Equal(projectId, dataset.Reference.ProjectId);
+            Assert.Equal(datasetId, dataset.Reference.DatasetId);
+        }
+
+        [Fact]
+        public async Task DeleteDatasetAsync()
+        {
+            var projectId = "project";
+            var datasetId = "dataset";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var reference = client.GetDatasetReference(projectId, datasetId);
+            service.ExpectRequest(
+                service.Datasets.Delete(projectId, datasetId),
+                "OK");
+            await client.DeleteDatasetAsync(reference);
+        }
+
+        [Fact]
+        public async Task CreateTableAsync()
+        {
+            var projectId = "project";
+            var datasetId = "dataset";
+            var tableId = "table";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var reference = client.GetTableReference(projectId, datasetId, tableId);
+            var schema = new TableSchemaBuilder { { "column", BigqueryDbType.Integer } }.Build();
+            service.ExpectRequest(
+                service.Tables.Insert(new Table { TableReference = reference, Schema = schema }, projectId, datasetId),
+                new Table { TableReference = reference, Schema = schema });
+            var result = await client.CreateTableAsync(reference, schema);
+            Assert.Equal(projectId, result.Reference.ProjectId);
+            Assert.Equal(datasetId, result.Reference.DatasetId);
+            Assert.Equal(tableId, result.Reference.TableId);
+            Assert.Equal("column", result.Schema.Fields[0].Name);
+        }
+
+        [Fact]
+        public async Task GetTableAsync()
+        {
+            var projectId = "project";
+            var datasetId = "dataset";
+            var tableId = "table";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var reference = client.GetTableReference(projectId, datasetId, tableId);
+            service.ExpectRequest(
+                service.Tables.Get(projectId, datasetId, tableId),
+                new Table { TableReference = reference });
+            var result = await client.GetTableAsync(reference);
+            Assert.Equal(projectId, result.Reference.ProjectId);
+            Assert.Equal(datasetId, result.Reference.DatasetId);
+            Assert.Equal(tableId, result.Reference.TableId);
+        }
+
+        [Fact]
+        public async Task DeleteTableAsync()
+        {
+            var projectId = "project";
+            var datasetId = "dataset";
+            var tableId = "table";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var reference = client.GetTableReference(projectId, datasetId, tableId);
+            service.ExpectRequest(
+                service.Tables.Delete(projectId, datasetId, tableId),
+                "OK");
+            await client.DeleteTableAsync(reference);
+        }
+
+        [Fact]
+        public async Task GetJobAsync()
+        {
+            var projectId = "project";
+            var jobId = "job";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var reference = client.GetJobReference(projectId, jobId);
+            service.ExpectRequest(
+                service.Jobs.Get(projectId, jobId),
+                new Job { JobReference = reference });
+            var result = await client.GetJobAsync(reference);
+            Assert.Equal(projectId, result.Reference.ProjectId);
+            Assert.Equal(jobId, result.Reference.JobId);
+        }
+
+        [Fact]
+        public async Task ListJobsAsync()
+        {
+            var projectId = "project";
+            var jobId = "job";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var projectReference = client.GetProjectReference(projectId);
+            var jobReference = client.GetJobReference(projectId, jobId);
+            service.ExpectRequest(
+                service.Jobs.List(projectId),
+                new JobList { Jobs = new[] { new JobsData { JobReference = jobReference } } });
+            var result = client.ListJobsAsync(projectReference);
+            var job = await result.Single();
+            Assert.Equal(projectId, job.Reference.ProjectId);
+            Assert.Equal(jobId, job.Reference.JobId);
+        }
+
+        [Fact]
+        public async Task CancelJobAsync()
+        {
+            var projectId = "project";
+            var jobId = "job";
+            var service = new FakeBigqueryService();
+            var client = new BigqueryClientImpl(projectId, service);
+            var reference = client.GetJobReference(projectId, jobId);
+            service.ExpectRequest(
+                service.Jobs.Cancel(projectId, jobId),
+                new JobCancelResponse { Job = new Job { JobReference = reference } });
+            var result = await client.CancelJobAsync(reference);
             Assert.Equal(projectId, result.Reference.ProjectId);
             Assert.Equal(jobId, result.Reference.JobId);
         }
