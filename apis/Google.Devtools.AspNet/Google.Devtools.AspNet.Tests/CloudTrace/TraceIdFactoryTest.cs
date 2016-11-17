ce62ca1623be6835bff8a8fc341bd818a912d0e3
GOOGLE\talarico <talarico@google.com>
2016-11-17 15:54:47 -0800

    Remove unneeded using

--- apis/Google.Devtools.AspNet/Google.Devtools.AspNet.Tests/CloudTrace/TraceIdFactoryTest.cs
+++ apis/Google.Devtools.AspNet/Google.Devtools.AspNet.Tests/CloudTrace/TraceIdFactoryTest.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using Moq;
 using System;
 using System.Linq;
 using Xunit;
