13970a66cf9e8b2b44d7138fe9e1c881c30cba20
GOOGLE\talarico <talarico@google.com>
2016-12-06 15:41:23 -0800

    Revert "Rename the 'Google.Devtools.AspNet' to 'Google.Cloud.Diagnostics.AspNet' and remove 'Cloud' in classes."
    This reverts commit 0f842ac5facc115fee855a502102d3b38ed9f040.

---
+++ apis/Google.Devtools.AspNet/Google.Devtools.AspNet/CloudErrorReporting/CloudErrorReportingExceptionLogger.cs
@@ -0,0 +1,193 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using Google.Cloud.ErrorReporting.V1Beta1;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+
+namespace Google.Devtools.AspNet
+{
+    /// <summary>
+    ///  Google Cloud Error Reporting <see cref="ExceptionLogger"/>.
+    /// </summary>
+    /// 
+    /// <example>
+    /// <code>
+    /// public static void Register(HttpConfiguration config)
+    /// {
+    ///   // Add a catch all for the uncaught exceptions.
+    ///   string projectId = "[Google Cloud Platform project ID]";
+    ///   string serviceName = "[Name of service]";
+    ///   string version = "[Version of service]";
+    ///   // Add a catch all for the uncaught exceptions.
+    ///   config.Services.Add(typeof(IExceptionLogger),
+    ///       CloudErrorReportingExceptionLogger.Create(projectId, serviceName, version));
+    /// }
+    /// </code>
+    /// </example>
+    /// 
+    /// <remarks>
+    /// Reports unhandled exceptions to Google Cloud Error Reporting.
+    /// Docs: https://cloud.google.com/error-reporting/docs/
+    /// </remarks>
+    public sealed class CloudErrorReportingExceptionLogger : ExceptionLogger
+    {
+        // The formated Google Cloud Platform project id.
+        private readonly string _projectResourceName;
+
+        // The service context in which this error has occurred.
+        // See: https://cloud.google.com/error-reporting/reference/rest/v1beta1/projects.events#ServiceContext
+        private readonly ServiceContext _serviceContext;
+
+        private readonly Task<ReportErrorsServiceClient> _clientTask;
+
+        /// <summary>
+        /// Creates an instance of <see cref="CloudErrorReportingExceptionLogger"/>
+        /// </summary>
+        /// <param name="clientTask">The Error Reporting client.</param>
+        /// <param name="projectId">The Google Cloud Platform project ID.</param>
+        /// <param name="serviceName">An identifier of the service, such as the name of the executable or job.</param>
+        /// <param name="version">Represents the source code version that the developer provided.</param> 
+        public static CloudErrorReportingExceptionLogger Create(
+            ReportErrorsServiceClient client, string projectId, string serviceName, string version) =>
+                new CloudErrorReportingExceptionLogger(Task.FromResult(client), projectId, serviceName, version);
+
+        /// <summary>
+        /// Creates an instance of <see cref="CloudErrorReportingExceptionLogger"/>
+        /// </summary>
+        /// <param name="clientTask">The Error Reporting client.</param>
+        /// <param name="projectId">The Google Cloud Platform project ID.</param>
+        /// <param name="serviceName">An identifier of the service, such as the name of the executable or job.</param>
+        /// <param name="version">Represents the source code version that the developer provided.</param> 
+        public static CloudErrorReportingExceptionLogger Create(
+            Task<ReportErrorsServiceClient> clientTask, string projectId, string serviceName, string version) =>
+                new CloudErrorReportingExceptionLogger(clientTask, projectId, serviceName, version);
+
+        /// <summary>
+        /// Creates an instance of <see cref="CloudErrorReportingExceptionLogger"/> using credentials as
+        /// defined by <see cref="GoogleCredential.GetApplicationDefaultAsync"/>.
+        /// </summary>
+        /// <param name="projectId">The Google Cloud Platform project ID.</param>
+        /// <param name="serviceName">An identifier of the service, such as the name of the executable or job.</param>
+        /// <param name="version">Represents the source code version that the developer provided.</param> 
+        public static CloudErrorReportingExceptionLogger Create(string projectId, string serviceName, string version) =>
+            new CloudErrorReportingExceptionLogger(ReportErrorsServiceClient.CreateAsync(), projectId, serviceName, version);
+
+        private CloudErrorReportingExceptionLogger(
+            Task<ReportErrorsServiceClient> clientTask, string projectId, string serviceName, string version) : base()
+        {
+            _clientTask = GaxPreconditions.CheckNotNull(clientTask, nameof(clientTask));
+            _projectResourceName = new ProjectName(GaxPreconditions.CheckNotNull(projectId, nameof(projectId))).ToString();
+
+            _serviceContext = new ServiceContext
+            {
+                Service = GaxPreconditions.CheckNotNull(serviceName, nameof(serviceName)),
+                Version = GaxPreconditions.CheckNotNull(version, nameof(version)),
+            };
+        }
+
+        /// <inheritdoc />
+        public override async Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
+        {
+            ReportedErrorEvent errorEvent = CreateReportRequest(context);
+            ReportErrorsServiceClient client = await _clientTask;
+            await client.ReportErrorEventAsync(_projectResourceName, errorEvent);
+        }
+
+        /// <inheritdoc />
+        public override void Log(ExceptionLoggerContext context)
+        {
+            ReportedErrorEvent errorEvent = CreateReportRequest(context);
+            // If the client task has faulted this will throw when accessing 'Result'
+            _clientTask.Result.ReportErrorEvent(_projectResourceName, errorEvent);
+        }
+
+        /// <inheritdoc />
+        public override bool ShouldLog(ExceptionLoggerContext context)
+        {
+            return context?.Exception != null;
+        }
+
+        /// <summary>
+        /// Gets information about the HTTP request and response when the exception occured 
+        /// and populates a <see cref="HttpRequestContext"/> object.
+        /// </summary>
+        private HttpRequestContext CreateHttpRequestContext(ExceptionLoggerContext exceptionLoggerContext)
+        {
+            ExceptionContext exceptionContext = exceptionLoggerContext.ExceptionContext;
+            HttpRequestMessage requestMessage = exceptionContext?.Request;
+            HttpResponseMessage responseMessage = exceptionContext?.Response;
+
+            return new HttpRequestContext()
+            {
+                Method = requestMessage?.Method?.ToString() ?? "",
+                Url = requestMessage?.RequestUri?.ToString() ?? "",
+                UserAgent = requestMessage?.Headers?.UserAgent?.ToString() ?? "",
+                ResponseStatusCode = (int) (responseMessage?.StatusCode ?? 0),
+            };
+        }
+
+        /// <summary>
+        /// Gets information about the source location where the exception occured 
+        /// and populates a <see cref="SourceLocation"/> object.
+        /// </summary>
+        private SourceLocation CreateSourceLocation(ExceptionLoggerContext exceptionLoggerContext)
+        {
+            Exception ex = exceptionLoggerContext?.Exception;
+            if (ex == null)
+            {
+                return new SourceLocation();
+            }
+
+            StackTrace stackTrace = new StackTrace(ex, true);
+            if (stackTrace.FrameCount == 0)
+            {
+                return new SourceLocation();
+            }
+
+            StackFrame frame = stackTrace.GetFrame(0);
+            return new SourceLocation()
+            {
+                FilePath = frame.GetFileName() ?? "",
+                LineNumber = frame.GetFileLineNumber(),
+                FunctionName = frame.GetMethod()?.Name ?? "",
+            };
+        }
+
+        /// <summary>
+        /// Gets infromation about the exception that occured and populates
+        /// a <see cref="ReportedErrorEvent"/> object.
+        /// </summary>
+        private ReportedErrorEvent CreateReportRequest(ExceptionLoggerContext context)
+        {
+            ErrorContext errorContext = new ErrorContext()
+            {
+                HttpRequest = CreateHttpRequestContext(context),
+                ReportLocation = CreateSourceLocation(context)
+            };
+
+            return new ReportedErrorEvent()
+            {
+                Message = context.Exception.ToString() ?? "",
+                Context = errorContext,
+                ServiceContext = _serviceContext,
+            };
+        }
+    }
+}
---
+++ apis/Google.Devtools.AspNet/Google.Devtools.AspNet/CloudTrace/TraceHeaderContext.cs
@@ -0,0 +1,102 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using System;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Google.Devtools.AspNet
+{
+    /// <summary>
+    /// Context from the Cloud Trace Header.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// A trace can be forced by passing information along in the trace header ("X-Cloud-Trace-Context").
+    /// The trace id, parent span id and whether or not to trace can be set.
+    /// See: https://cloud.google.com/trace/docs/faq#how_do_i_force_a_request_to_be_traced
+    /// </remarks>
+    internal sealed class TraceHeaderContext
+    {
+        /// <summary>An TraceHeaderContext representing no information or invalid information from a header.</summary>
+        private static readonly TraceHeaderContext InvalidTraceHeaderContext =
+            new TraceHeaderContext(null, null, false);
+
+        /// <summary>The trace header.</summary>
+        internal const string TraceHeader = "X-Cloud-Trace-Context";
+
+        /// <summary>
+        /// A regex to match the trace header. 
+        /// - ([A-Fa-f0-9]{32}): The trace id, a 32 character hex value.
+        /// - ([0-9]+): The span id, a 64 bit integer.
+        /// - (o=([0-3])): The trace mask, 1-3 denote it should be traced.
+        /// </summary>
+        internal static readonly Regex TraceHeaderRegex =
+            new Regex(@"^([A-Fa-f0-9]{32})/([0-9]+);(o=([0-3]))?$", RegexOptions.Compiled);
+
+        /// <summary>Gets the trace id or null if none is available.</summary>
+        public string TraceId { get; }
+
+        /// <summary>Gets the span id or null if none is available.</summary>
+        public ulong? SpanId { get; }
+
+        /// <summary>True if the request should be traced.</summary>
+        public bool ShouldTrace { get; }
+
+        /// <summary>
+        /// Create a <see cref="TraceHeaderContext"/> from an <see cref="HttpRequest"/>. 
+        /// </summary>
+        public static TraceHeaderContext FromRequest(HttpRequest request)
+        {
+            GaxPreconditions.CheckNotNull(request, nameof(request));
+            return FromWrapper(new HttpRequestWrapper(request));
+        }
+
+        internal static TraceHeaderContext FromWrapper(HttpRequestWrapper wrapper)
+        {
+            GaxPreconditions.CheckNotNull(wrapper, nameof(wrapper));
+            string header = wrapper.Headers.Get(TraceHeader);
+
+            if (header == null)
+            {
+                return InvalidTraceHeaderContext;
+            }
+
+            Match match = TraceHeaderRegex.Match(header);
+            if (!match.Success)
+            {
+                return InvalidTraceHeaderContext;
+            }
+
+            string traceId = match.Groups[1].Value;
+            ulong spanId;
+            if (!ulong.TryParse(match.Groups[2].Value, out spanId))
+            {
+                return InvalidTraceHeaderContext;
+            }
+            bool hasMask = match.Groups.Count > 4 && match.Groups[4].Success;
+            int traceMask = hasMask ? Convert.ToInt32(match.Groups[4].Value) : 0;
+            bool shouldTrace = traceMask > 0;
+            return new TraceHeaderContext(traceId, spanId, shouldTrace);
+        }
+
+        internal TraceHeaderContext(string traceId, ulong? spanId, bool shouldTrace)
+        {
+            TraceId = traceId;
+            SpanId = spanId;
+            ShouldTrace = shouldTrace;
+        }
+    }
+}
