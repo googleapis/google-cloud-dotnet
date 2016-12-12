3d2344cbbf77ca8bc3bcbd28a58ba9d9cc8fdef3
Jon Skeet <skeet@pobox.com>
2016-12-12 14:55:53 +0000

    Revamp of build scripts
    - Allow non-API packages to be specified, e.g.
      tagrelease.sh Google.Logging.V2 Google.Logging.Type
    - Autodetect version number from project.json
    - Copy nuget packages after build into releasebuild/nuget
    - Remove pushrelease.sh as less relevant given the above change

--- tagrelease.sh
+++ tagrelease.sh
@@ -9,9 +9,9 @@
 
 set -e
 
-if [ -z "$1" -o -z "$2" ]
+if [ -z "$1" ]
 then
-  echo Please specify API name and version
+  echo "Please specify API name and (optionally) package within API"
   exit 1
 fi
 
@@ -22,24 +22,20 @@ then
 fi
 
 api=$1
-version=$2
+package=$2
+
+[[ "$2" == "" ]] && \
+  package="$1" || \
+  package="$2"
 
 # Do everything from the repository root for sanity.
 cd $(dirname $0)
 
-# Check that the API exists. Some libraries
-# don't exist in an api/api directory; allow an override with --force
-# for now.
-# TODO: Allow X/Y format, e.g. Google.Logging.V2/Google.Logging.Type
-if [ ! -d apis/$api ]
+# Check that the API exists.
+if [ ! -f apis/$api/$package/project.json ]
 then
-  if [[ "$3" == "--force" ]]
-  then
-    echo Can\'t find apis/$api\, but --force specified.
-  else
-    echo Can\'t find apis/$api\; check for typos
-    exit 1
-  fi
+  echo "Can't find apis/$api/$package/project.json; check for typos"
+  exit 1
 fi
 
 # Check that our master is up-to-date with upstream.
@@ -49,16 +45,21 @@ git fetch -v --dry-run upstream 2>&1 \
   | grep -E '\[up to date\]\s+master\s+->\s+upstream/master' > /dev/null \
   || (echo Please update your local git repo before creating a release; exit 1)
 
-echo Creating release for API $api version $version
+version=`grep -E '"version": "[0-9]+\.[0-9]+\.[0-9]+(-[a-z0-9]+)?-\*",' apis/$api/$package/project.json \
+  | sed -E 's/"version": "//g' \
+  | sed 's/-\*",//g' \
+  | sed 's/ //g'`
+
+echo "Creating release for package $package version $version"
 
 commit=`git rev-parse master`
 [[ $version =~ ^(0\..*)|(.*-.*)$ ]] && prerelease="true" || prerelease="false"
 
 api_json=`cat << EndOfJson
 {
-  "tag_name": "$api-$version",
+  "tag_name": "$package-$version",
   "target_commitish": "$commit",
-  "name": "$version release of $api",
+  "name": "$version release of $apackage",
   "body": "",
   "draft": false,
   "prerelease": $prerelease
@@ -68,9 +69,4 @@ EndOfJson`
 # TODO: Work out what to do with the output of this
 curl --data "$api_json" https://api.github.com/repos/GoogleCloudPlatform/google-cloud-dotnet/releases?access_token=$github_access_token
 
-echo Created release $api-$version.
-
-# Plan:
-# Accept API name and version, e.g. tagrelease.sh Google.Pubsub.V1 1.0.0-beta03
-# Check that the local repo is up to date, using git fetch -v --dry-run upstream
-# Check http://www.barrykooij.com/create-github-releases-via-command-line/
+echo Created release $package-$version.
