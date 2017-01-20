7b1c6a7a293dcb94d78cf5d4980d627f497b2794
GOOGLE\talarico <talarico@google.com>
2017-01-19 16:32:37 -0800

    Unit tests for asp.net core trace

---
+++ apis/Google.Cloud.Diagnostics.AspNetCore/Google.Cloud.Diagnostics.AspNetCore.Tests/Trace/CloudTraceMiddlewareTest.cs
@@ -0,0 +1,66 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Diagnostics.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Google.Cloud.Diagnostics.AspNetCore.Tests.Trace
+{
+    public class CloudTraceMiddlewareTest
+    {
+        [Fact]
+        public async Task Invoke_Trace()
+        {
+            var context = new DefaultHttpContext();
+            var request = new DefaultHttpRequest(context);
+            request.Path = new PathString("/api/trace");
+
+            var delegateMock = new Mock<RequestDelegate>();
+            delegateMock.Setup(d => d(context)).Returns(Task.CompletedTask);
+
+            var tracerMock = new Mock<IManagedTracer>();
+            tracerMock.Setup(t => t.GetCurrentTraceId()).Returns("trace-id");
+            tracerMock.Setup(t => t.StartSpan(request.Path, null));
+            tracerMock.Setup(t => t.AnnotateSpan(It.IsAny<Dictionary<string, string>>()));
+            tracerMock.Setup(t => t.EndSpan());
+
+            var middleware = new CloudTraceMiddleware(delegateMock.Object);
+            await middleware.Invoke(context, tracerMock.Object);
+
+            delegateMock.VerifyAll();
+            tracerMock.VerifyAll();
+        }
+
+        [Fact]
+        public async Task Invoke_NoTrace()
+        {
+            var context = new DefaultHttpContext();
+            var delegateMock = new Mock<RequestDelegate>();
+            var tracerMock = new Mock<IManagedTracer>();
+            tracerMock.Setup(t => t.GetCurrentTraceId()).Returns((string) null);
+
+            var middleware = new CloudTraceMiddleware(delegateMock.Object);
+            await middleware.Invoke(context, tracerMock.Object);
+
+            delegateMock.Verify(d => d(context), Times.Once());
+            tracerMock.Verify(t => t.StartSpan(It.IsAny<string>(), null), Times.Never());
+            tracerMock.Verify(t => t.EndSpan(), Times.Never());
+        }
+    }
+}
---
+++ apis/Google.Cloud.Diagnostics.AspNetCore/Google.Cloud.Diagnostics.AspNetCore.Tests/Trace/LabelsTest.cs
@@ -0,0 +1,60 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Xunit;
+
+using LabelsCommon = Google.Cloud.Diagnostics.Common.Labels;
+
+namespace Google.Cloud.Diagnostics.AspNetCore.Tests.Trace
+{
+    public class LabelsTest
+    {
+        [Fact]
+        public void AgentLabel()
+        {
+            var labels = Labels.AgentLabel;
+            Assert.Single(labels);
+            Assert.Contains(LabelsCommon.AgentName, labels[LabelsCommon.Agent]);
+            Assert.Contains(LabelsCommon.GetAgentVersion(typeof(Labels)), labels[LabelsCommon.Agent]);
+        }
+
+        [Fact]
+        public void FromDefaultHttpRequest()
+        {
+            var request = new DefaultHttpRequest(new DefaultHttpContext());
+            request.ContentLength = 123;
+            request.Host = new HostString("google.com");
+            request.Method = "PUT";
+
+            var labels = Labels.FromDefaultHttpRequest(request);
+            Assert.Equal(3, labels.Count);
+            Assert.Equal("123", labels[LabelsCommon.HttpRequestSize]);
+            Assert.Equal("google.com", labels[LabelsCommon.HttpHost]);
+            Assert.Equal("PUT", labels[LabelsCommon.HttpMethod]);
+        }
+
+        [Fact]
+        public void FromDefaultHttpResponse()
+        {
+            var response = new DefaultHttpResponse(new DefaultHttpContext());
+            response.StatusCode = 404;
+
+            var labels = Labels.FromDefaultHttpResponse(response);
+            Assert.Equal(1, labels.Count);
+            Assert.Equal("404", labels[LabelsCommon.HttpStatusCode]);
+        }
+    }
+}
