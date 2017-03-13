79b2f0c6c4055f3dc466898694fc2e06a20fcd29
Jon Skeet <skeet@pobox.com>
2017-03-13 22:20:53 +0000

    More Datastore tests
    - Test that all the virtual methods in DatastoreTransaction throw
    - Test of the lookup logic (which makes repeated calls where necessary)

---
+++ apis/Google.Cloud.Datastore.V1/Google.Cloud.Datastore.V1.Tests/DatastoreTransactionTest.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.ClientTesting;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace Google.Cloud.Datastore.V1.Tests
+{
+    public class DatastoreTransactionTest : AbstractClientTester<DatastoreTransaction, DatastoreTransactionTest.DerivedDatastoreTransaction>
+    {
+        public class DerivedDatastoreTransaction : DatastoreTransaction
+        {
+        }
+
+        public static IEnumerable<object[]> NotImplementedMethods => AllInstanceMethods;
+
+        [Theory]
+        [MemberData(nameof(NotImplementedMethods))]
+        public void NotImplementedMethodsThrow(MethodInfo method)
+        {
+            AssertNotImplemented(method);
+        }
+
+        protected override object GetArgument(ParameterInfo parameter)
+        {
+            if (parameter.ParameterType == typeof(Entity))
+            {
+                return new Entity();
+            }
+            if (parameter.ParameterType == typeof(Key))
+            {
+                return new Key();
+            }
+            return base.GetArgument(parameter);
+        }
+    }
+}
