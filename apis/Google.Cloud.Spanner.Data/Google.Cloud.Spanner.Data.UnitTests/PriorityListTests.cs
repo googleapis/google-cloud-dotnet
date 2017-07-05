5ffad1e247c9e53832e4e07816bc4383b4780e79
benwu <benwu@google.com>
2017-07-05 16:34:44 -0700

    PriorityList unit tests.

---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.UnitTests/PriorityListTests.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Cloud.Spanner.V1;
+using Xunit;
+
+namespace Google.Cloud.Spanner.Data.Tests
+{
+    public class PriorityListTests
+    {
+        [Fact]
+        public void CanAddMultipleOfSamePriority()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+            TestItem item1, item2;
+            priorityList.Add(item1 = new TestItem(5));
+            priorityList.Add(item2 = new TestItem(5));
+            Assert.Equal(2, priorityList.Count);
+            AssertExist(priorityList, item1, item2);
+        }
+
+        [Fact]
+        public void DeleteRemovesCorrectItem()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+            TestItem toRemove;
+            priorityList.Add(toRemove = new TestItem(5));
+            priorityList.Add(new TestItem(5));
+            priorityList.Remove(toRemove);
+            Assert.Equal(1, priorityList.Count);
+            AssertNotExist(priorityList, toRemove);
+        }
+
+        [Fact]
+        public void DeleteRemovesOutOfSyncItem()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+            TestItem toRemove;
+            priorityList.Add(toRemove = new TestItem(5));
+            priorityList.Add(new TestItem(5));
+            toRemove.Priority = 4;
+
+            priorityList.Remove(toRemove);
+            Assert.Equal(1, priorityList.Count);
+            AssertNotExist(priorityList, toRemove);
+        }
+
+        [Fact]
+        public void DeleteNonExistentItemNoops()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+            priorityList.Add(new TestItem(1));
+            priorityList.Add(new TestItem(2));
+            priorityList.Remove(new TestItem(1));
+
+            Assert.Equal(2, priorityList.Count);
+        }
+
+        [Fact]
+        public void DeleteRemovesEventHook()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+            TestItem toRemove;
+            priorityList.Add(toRemove = new TestItem(5));
+            priorityList.Add(new TestItem(5));
+
+            Assert.True(toRemove.HasEventHooks);
+            priorityList.Remove(toRemove);
+            Assert.False(toRemove.HasEventHooks);
+        }
+
+        [Fact]
+        public void DoubleAddNoops()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+            TestItem itemToAdd;
+            priorityList.Add(itemToAdd = new TestItem(1));
+            priorityList.Add(itemToAdd);
+
+            Assert.Equal(1, priorityList.Count);
+            priorityList.Remove(itemToAdd);
+            //Lets make sure the double add didnt result in additional event hooks.
+            Assert.False(itemToAdd.HasEventHooks);
+        }
+
+        [Fact]
+        public void MultipleAddRemovesStaySorted()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+            TestItem item1, item2, item3, item4, item5;
+
+            priorityList.Add(item1 = new TestItem(1));
+            priorityList.Add(item2 = new TestItem(5));
+            priorityList.Add(item3 = new TestItem(3));
+            priorityList.Add(item4 = new TestItem(5));
+            priorityList.Add(item5 = new TestItem(6));
+
+            AssertExist(priorityList, item1, item2, item3, item4, item5);
+            AssertSorted(priorityList);
+
+            priorityList.Remove(item3);
+            AssertNotExist(priorityList, item3);
+            AssertSorted(priorityList);
+
+            priorityList.Remove(item4);
+            AssertNotExist(priorityList, item4);
+            AssertExist(priorityList, item2);
+            AssertSorted(priorityList);
+
+            priorityList.Add(item4);
+            AssertExist(priorityList, item4);
+            AssertSorted(priorityList);
+            Assert.Equal(4, priorityList.Count);
+        }
+
+        [Fact]
+        public void GetTop()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+
+            priorityList.Add(new TestItem(3));
+            priorityList.Add(new TestItem(11));
+            priorityList.Add(new TestItem(2));
+            priorityList.Add(new TestItem(0));
+            priorityList.Add(new TestItem(6));
+
+            AssertSorted(priorityList);
+
+            Assert.Equal(0, priorityList.GetTop().Priority);
+        }
+
+        [Fact]
+        public void RemoveLast()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+            TestItem itemToRemove;
+
+            priorityList.Add(new TestItem(3));
+            priorityList.Add(itemToRemove = new TestItem(11));
+            priorityList.Add(new TestItem(2));
+            priorityList.Add(new TestItem(0));
+            priorityList.Add(new TestItem(6));
+
+            priorityList.RemoveLast();
+            AssertSorted(priorityList);
+            AssertNotExist(priorityList, itemToRemove);
+        }
+
+        [Fact]
+        public void PriorityChangeResortsList()
+        {
+            PriorityList<TestItem> priorityList = new PriorityList<TestItem>();
+            TestItem changingItem;
+
+            priorityList.Add(new TestItem(3));
+            priorityList.Add(new TestItem(11));
+            priorityList.Add(changingItem = new TestItem(2));
+            priorityList.Add(new TestItem(0));
+            priorityList.Add(new TestItem(6));
+            AssertSorted(priorityList);
+            changingItem.Priority = 20;
+            AssertNotSorted(priorityList);
+            changingItem.FirePriorityChanged();
+            AssertSorted(priorityList);
+        }
+
+        private void AssertExist(PriorityList<TestItem> priorityList, params TestItem[] testItems)
+        {
+            foreach (var testItem in testItems)
+            {
+                Assert.True(priorityList.TryFindLinear(x => ReferenceEquals(x, testItem), out _));
+            }
+        }
+
+        private void AssertNotExist(PriorityList<TestItem> priorityList, params TestItem[] testItems)
+        {
+            foreach (var testItem in testItems)
+            {
+                Assert.False(priorityList.TryFindLinear(x => ReferenceEquals(x, testItem), out _));
+            }
+        }
+
+        private void AssertSorted(PriorityList<TestItem> priorityList)
+        {
+            Assert.True(IsSorted(priorityList));
+        }
+        private void AssertNotSorted(PriorityList<TestItem> priorityList)
+        {
+            Assert.False(IsSorted(priorityList));
+        }
+        private bool IsSorted(PriorityList<TestItem> priorityList)
+        {
+            int lastPriority = 0;
+            foreach (var testItem in priorityList.GetSnapshot())
+            {
+                if (lastPriority > testItem.Priority)
+                {
+                    return false;
+                }
+                lastPriority = testItem.Priority;
+            }
+            return true;
+        }
+
+        private class TestItem : IPriorityListItem<TestItem>
+        {
+            public TestItem(int priority)
+            {
+                Priority = priority;
+            }
+
+            public int Priority { get; set; }
+
+            //For test purposes, we do this manually.
+            //Normally this is done automatically, but we do it manually
+            //to test prioritylist behavior when the item is temporarily
+            //in a transition state.
+            public void FirePriorityChanged()
+            {
+                PriorityChanged?.Invoke(this, EventArgs.Empty);
+            }
+
+            /// <inheritdoc />
+            public int CompareTo(TestItem other)
+            {
+                return Comparer<int>.Default.Compare(Priority, other.Priority);
+            }
+
+            public bool HasEventHooks => PriorityChanged?.GetInvocationList().Any() ?? false;
+
+            /// <inheritdoc />
+            public event EventHandler<EventArgs> PriorityChanged;
+        }
+    }
+}
--- apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data/AssemblyInfo.cs
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data/AssemblyInfo.cs
@@ -14,10 +14,30 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Google.Cloud.Spanner.Data.IntegrationTests, PublicKey=" 
+[assembly: InternalsVisibleTo("Google.Cloud.Spanner.Data.IntegrationTests, PublicKey="
                               + "0024000004800000940000000602000000240000525341310004000001000100afab79952ee222"
                               + "15f12b4e09337e65509c943fbc22d7006bc371d581d0f0ebf0da5d8039aab2607fb68a138a5d80"
                               + "a71bc02b7ebf586dbe1f2493c0ab20423ababfd15ce74d2264a6b37745f3658f016abaad662182"
                               + "aaef634a60f1346fcc45343acab5b6781535a3134818e13fac895a6c106c0480e34bbb06cb123e"
                               + "5583d8d2"
 )]
+
+[assembly: InternalsVisibleTo("Google.Cloud.Spanner.Data.Tests, PublicKey="
+                              + "0024000004800000940000000602000000240000525341310004000001000100afab79952ee222"
+                              + "15f12b4e09337e65509c943fbc22d7006bc371d581d0f0ebf0da5d8039aab2607fb68a138a5d80"
+                              + "a71bc02b7ebf586dbe1f2493c0ab20423ababfd15ce74d2264a6b37745f3658f016abaad662182"
+                              + "aaef634a60f1346fcc45343acab5b6781535a3134818e13fac895a6c106c0480e34bbb06cb123e"
+                              + "5583d8d2"
+)]
+
+[assembly: InternalsVisibleTo("Castle.Core, PublicKey=002400000480000094000000060200000024000052534131000400000"
+                              + "100010077F5E87030DADCCCE6902C6ADAB7A987BD69CB5819991531F560785EACFC89B6FCDDF6B"
+                              + "B2A00743A7194E454C0273447FC6EEC36474BA8E5A3823147D214298E4F9A631B1AFEE1A51FFEA"
+                              + "E4672D498F14B000E3D321453CDD8AC064DE7E1CF4D222B7E81F54D4FD46725370D702A05B4873"
+                              + "8CC29D09228F1AA722AE1A9CA02FB")]
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=00240000048000009400000006020000002400005253"
+                              + "41310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa561"
+                              + "04f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191"
+                              + "195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9"
+                              + "291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
