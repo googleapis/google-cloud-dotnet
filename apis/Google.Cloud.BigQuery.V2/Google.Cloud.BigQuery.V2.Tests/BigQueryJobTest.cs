0f6dad2a91c97e3a89c0aa2841cca6cae48cfca5
Jon Skeet <jonskeet@google.com>
2017-08-09 11:17:13 +0100

    Improve BigQuery unit tests
    - More equivalence testing for BigQueryClient
    - Array and date/time/dateTime testing for BigQueryRow
    - Trivial tests for BigQueryPage
    - Validation tests for BigQueryInsertRow
    - Validate behavior of calling GetQueryResults on a dry-run query
      (with code change)
    - Add missing overload for UploadJson
    - Validate properties aren't implemented in BigQueryClient
    - (A few other bits and pieces)
    
    At this point I think that's all the low-hanging fruit. Most of the
    uncovered code would require mocking out the service. (Feasible, but
    fairly painful.)

---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.Tests/BigQueryJobTest.cs
@@ -0,0 +1,127 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Apis.Bigquery.v2;
+using Google.Apis.Bigquery.v2.Data;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Google.Cloud.BigQuery.V2.Tests
+{
+    public class BigQueryJobTest
+    {
+        [Fact]
+        public void Properties()
+        {
+            var resource = new Job
+            {
+                JobReference = new JobReference { ProjectId = "project", JobId = "job" },
+                Status = new JobStatus { State = "RUNNING" },
+                Statistics = new JobStatistics { CreationTime = 1000L }
+            };
+            var job = new BigQueryJob(new SimpleClient(), resource);
+            Assert.Same(resource, job.Resource);
+            Assert.Same(resource.JobReference, job.Reference);
+            Assert.Same(resource.Statistics, job.Statistics);
+            Assert.Same(resource.Status, job.Status);
+            Assert.Equal(JobState.Running, job.State);
+        }
+
+        [Fact]
+        public void ThrowOnAnyError_WithErrors()
+        {
+            var resource = new Job
+            {
+                JobReference = new JobReference { ProjectId = "project", JobId = "job" },
+                Status = new JobStatus
+                {
+                    State = "completed",
+                    Errors = new[] { new ErrorProto { Message = "Error 1" }, new ErrorProto { Message = "Error 2" } }
+                }
+            };
+            var job = new BigQueryJob(new SimpleClient(), resource);
+            var exception = Assert.Throws<GoogleApiException>(() => job.ThrowOnAnyError());
+            var requestError = exception.Error;
+            Assert.Equal(new[] { "Error 1", "Error 2" }, requestError.Errors.Select(e => e.Message));
+        }
+
+        [Fact]
+        public void ThrowOnAnyError_NoErrors()
+        {
+            var resource = new Job
+            {
+                Status = new JobStatus { State = "completed" },
+            };
+            var job = new BigQueryJob(new SimpleClient(), resource);
+            job.ThrowOnAnyError();
+        }
+
+        [Fact]
+        public async Task GetQueryResults_NonQuery()
+        {
+            var resource = new Job
+            {
+                JobReference = new JobReference { ProjectId = "project", JobId = "job" },
+                Configuration = new JobConfiguration
+                {
+                    Copy = new JobConfigurationTableCopy { }
+                }
+            };
+            var job = new BigQueryJob(new SimpleClient(), resource);
+            Assert.Throws<InvalidOperationException>(() => job.GetQueryResults());
+            await Assert.ThrowsAsync<InvalidOperationException>(() => job.GetQueryResultsAsync());
+        }
+
+        [Fact]
+        public async Task GetQueryResults_NoDestinationTable()
+        {
+            var resource = new Job
+            {
+                JobReference = new JobReference { ProjectId = "project", JobId = "job" },
+                Configuration = new JobConfiguration
+                {
+                    DryRun = true,
+                    Query = new JobConfigurationQuery { } // No destination table
+                }
+            };
+            var job = new BigQueryJob(new SimpleClient(), resource);
+            Assert.Throws<InvalidOperationException>(() => job.GetQueryResults());
+            await Assert.ThrowsAsync<InvalidOperationException>(() => job.GetQueryResultsAsync());
+        }
+
+        [Fact]
+        public async Task GetQueryResults_NoJobReference()
+        {
+            var resource = new Job
+            {
+                Configuration = new JobConfiguration
+                {
+                    DryRun = true,
+                    Query = new JobConfigurationQuery { DestinationTable = new TableReference { ProjectId = "project", DatasetId = "dataset", TableId = "table" } }
+                }
+            };
+            var job = new BigQueryJob(new SimpleClient(), resource);
+            Assert.Throws<InvalidOperationException>(() => job.GetQueryResults());
+            await Assert.ThrowsAsync<InvalidOperationException>(() => job.GetQueryResultsAsync());
+        }
+
+        // ThrowOnAnyError uses the client service name. This is simpler than mocking that.
+        private class SimpleClient : BigQueryClient
+        {
+            public override BigqueryService Service { get; } = new FakeBigqueryService();
+        }
+    }
+}
---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.Tests/BigQueryPageTest.cs
@@ -0,0 +1,57 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using Google.Apis.Bigquery.v2.Data;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Google.Cloud.BigQuery.V2.Tests
+{
+    public class BigQueryPageTest
+    {
+        [Fact]
+        public void PublicConstructor()
+        {
+            var rows = new List<BigQueryRow>();
+            var schema = new TableSchema();
+            var jobReference = new JobReference { ProjectId = "project", JobId = "job" };
+            var tableReference = new TableReference { ProjectId = "project", DatasetId = "dataset", TableId = "table" };
+            var nextPageToken = "token";
+            var page = new BigQueryPage(rows, schema, jobReference, tableReference, nextPageToken);
+            Assert.Same(rows, page.Rows);
+            Assert.Same(schema, page.Schema);
+            Assert.Same(jobReference, page.JobReference);
+            Assert.Same(tableReference, page.TableReference);
+            Assert.Same(nextPageToken, page.NextPageToken);
+        }
+
+        [Fact]
+        public void InternalConstructor()
+        {
+            var nextPageToken = "token";
+            var schema = new TableSchema();
+            var row = new BigQueryRow(new TableRow(), schema);
+            var rawPage = new Page<BigQueryRow>(new List<BigQueryRow> { row }, nextPageToken);
+            var jobReference = new JobReference { ProjectId = "project", JobId = "job" };
+            var tableReference = new TableReference { ProjectId = "project", DatasetId = "dataset", TableId = "table" };
+            var page = new BigQueryPage(rawPage, schema, jobReference, tableReference);
+            Assert.Equal(new[] { row }, page.Rows);
+            Assert.Same(schema, page.Schema);
+            Assert.Same(jobReference, page.JobReference);
+            Assert.Same(tableReference, page.TableReference);
+            Assert.Same(nextPageToken, page.NextPageToken);
+        }
+    }
+}
