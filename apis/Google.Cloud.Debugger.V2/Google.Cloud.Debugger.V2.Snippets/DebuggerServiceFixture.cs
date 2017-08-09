7d010b4cfb8d41d7450e9ec12b0f58d6f21849eb
GOOGLE\talarico <talarico@google.com>
2017-08-09 10:27:56 -0700

    Add a Controller Snippet

---
+++ apis/Google.Cloud.Debugger.V2/Google.Cloud.Debugger.V2.Snippets/DebuggerServiceFixture.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2017, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Xunit;
+
+namespace Google.Cloud.Debugger.V2.Snippets
+{
+    /// <summary>
+    /// Fixture which is set up at the start of the test run, and torn down at the end.
+    /// The Google Cloud Project name is fetched from the TEST_PROJECT environment variable.
+    /// </summary>
+    [CollectionDefinition(nameof(DebuggerServiceFixture))]
+    public sealed class DebuggerServiceFixture : IDisposable, ICollectionFixture<DebuggerServiceFixture>
+    {
+        private const string ProjectEnvironmentVariable = "TEST_PROJECT";
+
+        public string ProjectId { get; }
+
+        public DebuggerServiceFixture()
+        {
+            ProjectId = Environment.GetEnvironmentVariable(ProjectEnvironmentVariable);
+            if (string.IsNullOrEmpty(ProjectId))
+            {
+                throw new InvalidOperationException(
+                    $"Please set the {ProjectEnvironmentVariable} environment variable before running tests");
+            }
+        }
+
+        public void Dispose()
+        {
+            // No-op
+        }
+    }
+}
