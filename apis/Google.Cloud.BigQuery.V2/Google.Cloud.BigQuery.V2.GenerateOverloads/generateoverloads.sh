e9c0dcc40d704be28110ed33e9af3066c6a374f0
Jon Skeet <jonskeet@google.com>
2017-08-15 10:00:58 +0100

    Generate GetQueryResults
    The changes here are due to having to find wording that works for both the sync and async versions

--- apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.GenerateOverloads/generateoverloads.sh
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.GenerateOverloads/generateoverloads.sh
@@ -6,3 +6,4 @@ dotnet run -- Methods/DatasetCrud.xml $CLIENT.DatasetCrud.cs
 dotnet run -- Methods/DatasetLabels.xml $CLIENT.DatasetLabels.cs
 dotnet run -- Methods/InsertData.xml $CLIENT.InsertData.cs
 dotnet run -- Methods/JobCrud.xml $CLIENT.JobCrud.cs
+dotnet run -- Methods/Queries.xml $CLIENT.Queries.cs
