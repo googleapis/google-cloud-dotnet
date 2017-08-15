698c18c9d31ef15bdde1eef4234dc064a4c49899
Jon Skeet <jonskeet@google.com>
2017-08-15 08:21:49 +0100

    Generate InsertData methods

--- apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.GenerateOverloads/generateoverloads.sh
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.GenerateOverloads/generateoverloads.sh
@@ -3,5 +3,5 @@
 declare -r CLIENT=../Google.Cloud.BigQuery.V2/BigQueryClient
 
 dotnet run -- Methods/DatasetCrud.xml $CLIENT.DatasetCrud.cs
-
 dotnet run -- Methods/DatasetLabels.xml $CLIENT.DatasetLabels.cs
+dotnet run -- Methods/InsertData.xml $CLIENT.InsertData.cs
