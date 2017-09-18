60364aa0b2f5e40ab81188aabb72c5d7a3a6c46e
Jon Skeet <jonskeet@google.com>
2017-09-18 11:51:06 +0100

    Fix release process
    The argument to buildrelease.sh is now a commit hash. The script
    clones the repo as normal and checks out the specified commit, then
    finds all the tags for that commit, failing if there aren't any. The
    set of projects to build is the set of tags, truncating each tag at
    "-" (as the part after "-" is the version number in the tag).
    
    We now only build, pack and document the specified projects.

--- buildrelease.sh
+++ buildrelease.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
 
-# Script to perform a release build for all APIs, based on
-# the tag for a single API version.
-# Sample: buildrelease.sh Google.Pubsub.V1 1.0.0-beta05
+# Script to perform a release build for all APIs tagged at
+# a specific commit. A single argument is required: the commit to use.
 
 # The repo is cloned into a fresh "releasebuild" directory.
 
 set -e
 
-if [ -z "$1" -o -z "$2" ]
+if [ -z "$1" ]
 then
-  echo Please specify API name and version
+  echo "Please specify a commit hash"
   exit 1
 fi
 
-tag=$1-$2
-
-# TODO: We don't really want a regex here... just a match.
-git fetch -v --dry-run --tags upstream 2>&1 \
-  | grep -e $tag > /dev/null \
-  || (echo "Tag $tag appears not to exist. Did you run tagrelease.sh?"; exit 1)
+commit=$1
 
 # Do everything from the repository root for sanity.
 cd $(dirname $0)
@@ -28,20 +22,38 @@ rm -rf releasebuild
 git clone https://github.com/GoogleCloudPlatform/google-cloud-dotnet.git releasebuild -c core.autocrlf=input
 cd releasebuild
 export CI=true # Forces SourceLink in the main build.
-git checkout $tag
-./build.sh
-dotnet pack AllProjects.sln --no-build -o $PWD/nuget -c Release
+git checkout $commit
 
-# Remove any tools packages (e.g. the analyzer)
-rm -f nuget/Google.Cloud.Tools.*
+# Turn the multi-line output of git tag --points-at into space-separated list of projects
+projects=$(git tag --points-at $commit | sed 's/-.*//g' | awk -vORS=\  '{print $1}' | sed 's/ $//')
+
+if [ -z "$projects" ]
+then
+  echo "No tags found for commit $commit"
+  exit 1
+fi
+
+./build.sh $projects
+
+for project in $projects
+do
+  # Don't pack the whole solution - just the project. (Avoids packing dependent
+  # projects such as Google.LongRunning.)
+  dotnet pack --no-build --no-restore -o $PWD/nuget -c Release apis/$project/$project
+done
 
 # TODO: Make builddocs.sh cope with being run from any directory.
 cd docs
-./builddocs.sh
+./builddocs.sh root $projects
 
-echo "Release build complete. Next steps:"
+echo "Release build complete for the following projects:"
+for project in $projects
+do
+  echo "- ${project}"
+done
+echo "Next steps:"
 echo "- Upload new docs to gh-pages branch"
 echo "- Push packages to nuget:"
 echo "  - cd releasebuild/nuget"
 echo "  - Remove any packages you don't want to push"
-echo "  - for pkg in *.nupkg; do nuget push -Source nuget.org -ApiKey API_KEY_HERE \$pkg; done"
+echo "  - for pkg in *.nupkg; do dotnet nuget push -s https://api.nuget.org/v3/index.json -k API_KEY_HERE \$pkg; done"
