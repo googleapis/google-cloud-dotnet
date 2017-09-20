cab5b0ad4b9992027934edbb883c004a1cb04c40
Jon Skeet <jonskeet@google.com>
2017-09-20 10:49:55 +0100

    Fix Translation integration tests
    This is just a matter of adjusting the expected output.

--- apis/Google.Cloud.Translation.V2/Google.Cloud.Translation.V2.IntegrationTests/AdvancedTranslationClientTest.cs
+++ apis/Google.Cloud.Translation.V2/Google.Cloud.Translation.V2.IntegrationTests/AdvancedTranslationClientTest.cs
@@ -33,7 +33,7 @@ namespace Google.Cloud.Translation.V2.IntegrationTests
         {
             var client = AdvancedTranslationClient.Create();
             var translation = client.TranslateText(LargeText, LanguageCodes.French);
-            Assert.StartsWith("Lorsque, au cours des", translation.TranslatedText);
+            Assert.StartsWith("Quand, au cours des", translation.TranslatedText);
         }
 
         [Fact]
