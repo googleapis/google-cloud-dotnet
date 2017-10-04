beae1d4b704b55ed4ce4815b593e537a6b029314
Jon Skeet <jonskeet@google.com>
2017-10-04 15:06:27 +0100

    Datatypes for locations, blobs and timestamps
    This avoids the user needing to worry about the Protobuf types. (In particular, the immutability afforded this way is handy.)

---
+++ apis/Google.Cloud.Firestore.Data/Google.Cloud.Firestore.Data.Tests/GeoPointTest.cs
@@ -0,0 +1,122 @@
+﻿// Copyright 2017, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Type;
+using System;
+using System.Globalization;
+using Xunit;
+
+namespace Google.Cloud.Firestore.Data.Tests
+{
+    public class GeoPointTest
+    {
+        [Fact]
+        public void ToProto()
+        {
+            var point = new GeoPoint(15, 10);
+            var expected = new LatLng { Latitude = 15, Longitude = 10 };
+            Assert.Equal(expected, point.ToProto());
+        }
+
+        [Fact]
+        public void FromProto()
+        {
+            var proto = new LatLng { Latitude = 15, Longitude = 10 };
+            var expected = new GeoPoint(15, 10);
+            Assert.Equal(expected, GeoPoint.FromProto(proto));
+        }
+
+        [Theory]
+        [InlineData(-90.1, 0)]
+        [InlineData(90.1, 0)]
+        [InlineData(0, -180.1)]
+        [InlineData(0, 180.1)]
+        public void Construction_Invalid(double latitude, double longitude)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new GeoPoint(latitude, longitude));
+        }
+
+        [Theory]
+        [InlineData(-90, -180)]
+        [InlineData(90, 180)]
+        [InlineData(1.5, 2.5)]
+        public void Construction_Valid(double latitude, double longitude)
+        {
+            var point = new GeoPoint(latitude, longitude);
+            Assert.Equal(latitude, point.Latitude);
+            Assert.Equal(longitude, point.Longitude);
+        }
+
+        [Fact]
+        public void Equality()
+        {
+            GeoPoint g1 = new GeoPoint(1.5, 2.5);
+            // Equal
+            GeoPoint g2 = new GeoPoint(1.5, 2.5);
+            // Difference in latitude
+            GeoPoint g3 = new GeoPoint(2.5, 2.5);
+            // Difference in longitude
+            GeoPoint g4 = new GeoPoint(1.5, 3.5);
+
+            // Equals(GeoPoint)
+            Assert.True(g1.Equals(g2));
+            Assert.False(g1.Equals(g3));
+            Assert.False(g1.Equals(g4));
+
+            // Equals(object)
+            Assert.True(g1.Equals((object) g2));
+            Assert.False(g1.Equals((object) g3));
+            Assert.False(g1.Equals((object) g4));
+
+            Assert.True(g1 == g2);
+            Assert.False(g1 == g3);
+            Assert.False(g1 == g4);
+            Assert.False(g1 == null);
+
+            Assert.False(g1 != g2);
+            Assert.True(g1 != g3);
+            Assert.True(g1 != g4);
+            Assert.True(g1 != null);
+
+            Assert.Equal(g1.GetHashCode(), g2.GetHashCode());
+            Assert.NotEqual(g1.GetHashCode(), g3.GetHashCode());
+            Assert.NotEqual(g1.GetHashCode(), g4.GetHashCode());
+        }
+
+        [Fact]
+        public void DefaultValueIsOrigin()
+        {
+            GeoPoint point = default;
+            Assert.Equal(0, point.Latitude);
+            Assert.Equal(0, point.Longitude);
+        }
+
+        [Fact]
+        public void Formatting()
+        {
+            var point = new GeoPoint(3.5, -12.25);
+            CultureInfo savedCulture = CultureInfo.CurrentCulture;
+            try
+            {
+                CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+                Assert.Equal("GeoPoint: Latitude=3.5; Longitude=-12.25", point.ToString());
+            }
+            finally
+            {
+                CultureInfo.DefaultThreadCurrentUICulture = savedCulture;
+            }
+
+        }
+    }
+}
---
+++ apis/Google.Cloud.Firestore.Data/Google.Cloud.Firestore.Data.Tests/TimestampTest.cs
@@ -0,0 +1,193 @@
+﻿// Copyright 2017, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Xunit;
+using wkt = Google.Protobuf.WellKnownTypes;
+
+namespace Google.Cloud.Firestore.Data.Tests
+{
+    public class TimestampTest
+    {
+        [Fact]
+        public void Equality()
+        {
+            Timestamp t1 = new Timestamp(1, 2);
+            // Equal
+            Timestamp t2 = new Timestamp(1, 2);
+            // Difference in seconds
+            Timestamp t3 = new Timestamp(2, 2);
+            // Difference in nanoseconds
+            Timestamp t4 = new Timestamp(2, 3);
+
+            // Equals(Timestamp)
+            Assert.True(t1.Equals(t2));
+            Assert.False(t1.Equals(t3));
+            Assert.False(t1.Equals(t4));
+
+            // Equals(object)
+            Assert.True(t1.Equals((object) t2));
+            Assert.False(t1.Equals((object) t3));
+            Assert.False(t1.Equals((object) t4));
+
+            Assert.True(t1 == t2);
+            Assert.False(t1 == t3);
+            Assert.False(t1 == t4);
+
+            Assert.False(t1 != t2);
+            Assert.True(t1 != t3);
+
+            Assert.Equal(t1.GetHashCode(), t2.GetHashCode());
+            Assert.NotEqual(t1.GetHashCode(), t3.GetHashCode());
+            Assert.NotEqual(t1.GetHashCode(), t4.GetHashCode());
+        }
+
+        [Fact]
+        public void DefaultValueIsUnixEpoch()
+        {
+            Timestamp timestamp = default;
+            Assert.Equal(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc), timestamp.ToDateTime());
+        }
+
+        [Fact]
+        public void Comparisons()
+        {
+            Timestamp t1 = new Timestamp(1, 2);
+            Timestamp t2 = new Timestamp(1, 2); 
+            Timestamp t3 = new Timestamp(2, 2);
+            Timestamp t4 = new Timestamp(2, 3);
+
+            AssertComparisonAndReverse(t1, t2, 0);
+            AssertComparisonAndReverse(t1, t3, -1);
+            AssertComparisonAndReverse(t1, t4, -1);
+
+            void AssertComparisonAndReverse(Timestamp x, Timestamp y, int expectedSign)
+            {
+                AssertComparison(x, y, expectedSign);
+                AssertComparison(y, x, -expectedSign);
+            }
+
+            void AssertComparison(Timestamp x, Timestamp y, int expectedSign)
+            {
+                Assert.Equal(expectedSign, Math.Sign(x.CompareTo(y)));
+                Assert.Equal(expectedSign, Math.Sign(x.CompareTo((object) y)));
+
+                Assert.True((x == y) == (expectedSign == 0));
+                Assert.True((x != y) == (expectedSign != 0));
+                Assert.True((x <= y) == (expectedSign <= 0));
+                Assert.True((x < y) == (expectedSign < 0));
+                Assert.True((x >= y) == (expectedSign >= 0));
+                Assert.True((x > y) == (expectedSign > 0));
+            }
+        }
+
+        [Fact]
+        public void FromProto()
+        {
+            var direct = new Timestamp(1, 100);
+            var fromProto = Timestamp.FromProto(new wkt::Timestamp { Seconds = 1, Nanos = 100 });
+            Assert.Equal(direct, fromProto);
+        }
+
+        [Fact]
+        public void ToProto()
+        {
+            var timestamp = new Timestamp(1, 100);
+            var proto = new wkt::Timestamp { Seconds = 1, Nanos = 100 };
+            Assert.Equal(proto, timestamp.ToProto());
+        }
+
+        [Fact]
+        public void ToDateTimeAndDateTimeOffset()
+        {
+            var timestamp = new Timestamp(1, 100);
+            var expected = new DateTime(1970, 1, 1, 0, 0, 1, DateTimeKind.Utc).AddTicks(1);
+            Assert.Equal(expected, timestamp.ToDateTime());
+            Assert.Equal((DateTimeOffset) expected, timestamp.ToDateTimeOffset());
+        }
+
+        [Fact]
+        public void FromDateTime()
+        {
+            // 1 tick = 100ns
+            var timestamp = Timestamp.FromDateTime(new DateTime(1970, 1, 1, 0, 0, 1, DateTimeKind.Utc).AddTicks(1));
+            var expected = new Timestamp(1, 100);
+            Assert.Equal(expected, timestamp);
+        }
+
+        [Fact]
+        public void FromDateTimeOffset()
+        {
+            var timestamp = Timestamp.FromDateTimeOffset(new DateTimeOffset(1970, 1, 1, 0, 0, 1, TimeSpan.Zero).AddTicks(1));
+            var expected = new Timestamp(1, 100);
+            Assert.Equal(expected, timestamp);
+        }
+
+        [Theory]
+        [InlineData(Timestamp.MinSeconds - 1, 0)]
+        [InlineData(Timestamp.MaxSeconds + 1, 0)]
+        [InlineData(0, -1)]
+        [InlineData(0, 1_000_000_000)]
+        public void Construction_Invalid(long seconds, int nanoseconds)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Timestamp(seconds, nanoseconds));
+        }
+
+        [Theory]
+        [InlineData(Timestamp.MinSeconds, 0, "Timestamp: 0001-01-01T00:00:00Z")]
+        [InlineData(Timestamp.MaxSeconds, 999_999_999, "Timestamp: 9999-12-31T23:59:59.999999999Z")]
+        [InlineData(0, 0, "Timestamp: 1970-01-01T00:00:00Z")]
+        // We truncate trailing zeroes at seconds, milliseconds, and microseconds
+        [InlineData(0, 100_000_000, "Timestamp: 1970-01-01T00:00:00.100Z")]
+        [InlineData(0, 120_000_000, "Timestamp: 1970-01-01T00:00:00.120Z")]
+        [InlineData(0, 123_000_000, "Timestamp: 1970-01-01T00:00:00.123Z")]
+        [InlineData(0, 123_400_000, "Timestamp: 1970-01-01T00:00:00.123400Z")]
+        [InlineData(0, 123_450_000, "Timestamp: 1970-01-01T00:00:00.123450Z")]
+        [InlineData(0, 123_456_000, "Timestamp: 1970-01-01T00:00:00.123456Z")]
+        [InlineData(0, 123_456_700, "Timestamp: 1970-01-01T00:00:00.123456700Z")]
+        [InlineData(0, 123_456_780, "Timestamp: 1970-01-01T00:00:00.123456780Z")]
+        [InlineData(0, 123_456_789, "Timestamp: 1970-01-01T00:00:00.123456789Z")]
+        [InlineData(-1, 0, "Timestamp: 1969-12-31T23:59:59Z")]
+        public void Formatting(long seconds, int nanoseconds, string expected)
+        {
+            var timestamp = new Timestamp(seconds, nanoseconds);
+            Assert.Equal(expected, timestamp.ToString());
+        }
+
+        [Fact]
+        public void FromProtoOrNull_Null()
+        {
+            Assert.Null(Timestamp.FromProtoOrNull(null));
+        }
+
+        [Fact]
+        public void FromProtoOrNull_NonNull()
+        {
+            var proto = new wkt::Timestamp { Seconds = 1, Nanos = 100 };
+            var timestamp = Timestamp.FromProtoOrNull(proto).Value;
+            var expected = new Timestamp(1, 100);
+            Assert.Equal(expected, timestamp);
+        }
+
+        [Fact]
+        public void GetCurrentTimestamp()
+        {
+            var now = DateTime.UtcNow;
+            // Assume the system clock doesn't change by more than 5 seconds in the course of the test.
+            var timestamp = Timestamp.GetCurrentTimestamp();
+            var absDifferenceSeconds = Math.Abs((timestamp.ToDateTime() - now).TotalSeconds);
+            Assert.True(absDifferenceSeconds < 5);
+        }
+    }
+}
