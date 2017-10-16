8165623fd236f3725315c9b36e5f81346ab0c07a
Mike Dour <evildour@users.noreply.github.com>
2017-10-16 13:21:52 -0400

    BigtableVersion[Range] wrapper types (#1547)
    * BigtableVersion[Range] wrapper types
    
    * Address PR comments
    
    * Address PR comments
    
    * Address PR comments

---
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.Tests/BigtableVersionRangeTest.cs
@@ -0,0 +1,121 @@
+﻿// Copyright 2017 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Google.Cloud.Bigtable.V2.Tests
+{
+    public class BigtableVersionRangeTest
+    {
+        [Fact]
+        public void Equality()
+        {
+            var control = new BigtableVersionRange(0, 10);
+            var equal = new[]
+            {
+                new BigtableVersionRange(0, 10),
+                new BigtableVersionRange(new BigtableVersion(0), new BigtableVersion(10)),
+                new BigtableVersionRange(
+                    BigtableVersion.UnixEpoch,
+                    BigtableVersion.UnixEpoch + TimeSpan.FromTicks(100))
+            };
+            var unequal = new[]
+            {
+                new BigtableVersionRange(null, 10),
+                new BigtableVersionRange(null, new BigtableVersion(10)),
+                new BigtableVersionRange(null, BigtableVersion.UnixEpoch + TimeSpan.FromTicks(100)),
+                new BigtableVersionRange(0, null),
+                new BigtableVersionRange(new BigtableVersion(0), null),
+                new BigtableVersionRange(BigtableVersion.UnixEpoch, null),
+                new BigtableVersionRange(1, 10),
+                new BigtableVersionRange(0, 11)
+            };
+            ComparisonTester.AssertEqual(control, equal, unequal);
+        }
+
+        [Fact]
+        public void EqualityNullStart()
+        {
+            var control = new BigtableVersionRange(null, 10);
+            var equal = new[]
+            {
+                new BigtableVersionRange(null, 10),
+                new BigtableVersionRange(null, new BigtableVersion(10)),
+                new BigtableVersionRange(null, BigtableVersion.UnixEpoch + TimeSpan.FromTicks(100))
+            };
+            var unequal = new[]
+            {
+                new BigtableVersionRange(0, 10),
+                new BigtableVersionRange(new BigtableVersion(0), new BigtableVersion(10)),
+                new BigtableVersionRange(
+                    BigtableVersion.UnixEpoch,
+                    BigtableVersion.UnixEpoch + TimeSpan.FromTicks(100)),
+                new BigtableVersionRange(null, 11)
+            };
+            ComparisonTester.AssertEqual(control, equal, unequal);
+        }
+
+        [Fact]
+        public void EqualityNullEnd()
+        {
+            var control = new BigtableVersionRange(0, null);
+            var equal = new[]
+            {
+                new BigtableVersionRange(0, null),
+                new BigtableVersionRange(new BigtableVersion(0), null),
+                new BigtableVersionRange(BigtableVersion.UnixEpoch, null)
+            };
+            var unequal = new[]
+            {
+                new BigtableVersionRange(0, 10),
+                new BigtableVersionRange(new BigtableVersion(0), new BigtableVersion(10)),
+                new BigtableVersionRange(
+                    BigtableVersion.UnixEpoch,
+                    BigtableVersion.UnixEpoch + TimeSpan.FromTicks(100)),
+                new BigtableVersionRange(1, null)
+            };
+            ComparisonTester.AssertEqual(control, equal, unequal);
+        }
+
+        [Theory]
+        [InlineData("BigtableVersionRange: Start=0; End=1", 0, 1)]
+        [InlineData("BigtableVersionRange: Start=1; End=10", 1, 10)]
+        [InlineData("BigtableVersionRange: Start=(none); End=10", null, 10)]
+        [InlineData("BigtableVersionRange: Start=12345; End=(none)", 12345, null)]
+        public void Formatting(string expectedText, long? start, long? end)
+        {
+            var version = new BigtableVersionRange(start, end);
+            Assert.Equal(expectedText, version.ToString());
+        }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 10)]
+        [InlineData(null, 10)]
+        [InlineData(12345, null)]
+        public void ToTimestampRange(long? start, long? end)
+        {
+            var range = new BigtableVersionRange(start, end);
+            var expected = new TimestampRange
+            {
+                StartTimestampMicros = start ?? 0,
+                EndTimestampMicros = end ?? 0
+            };
+            Assert.Equal(expected, range.ToTimestampRange());
+        }
+    }
+}
---
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.Tests/BigtableVersionTest.cs
@@ -0,0 +1,123 @@
+﻿// Copyright 2017 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xunit;
+
+namespace Google.Cloud.Bigtable.V2.Tests
+{
+    public class BigtableVersionTest
+    {
+        [Fact]
+        public void Comparisons()
+        {
+            var control = new BigtableVersion(5);
+            var less = new[]
+            {
+                new BigtableVersion(0),
+                new BigtableVersion(4)
+            };
+            var equal = new[]
+            {
+                new BigtableVersion(5)
+            };
+            var greater = new[]
+            {
+                new BigtableVersion(6),
+                new BigtableVersion(-1),
+                new BigtableVersion(long.MaxValue)
+            };
+            ComparisonTester.AssertCompare(control, less, equal, greater);
+            ComparisonTester.AssertCompareOperators(control, less, equal, greater);
+        }
+
+        [Fact]
+        public void MicrosScaleFromDateTime()
+        {
+            var utcNow = DateTime.UtcNow;
+            var v1 = new BigtableVersion(utcNow);
+            var v2 = new BigtableVersion(utcNow + TimeSpan.FromSeconds(1));
+            var diff = v2.Value - v1.Value;
+            Assert.Equal(1_000_000, diff);
+        }
+
+        [Fact]
+        public void MicrosScaleToDateTime()
+        {
+            var utcNow = DateTime.UtcNow;
+            var v1 = new BigtableVersion(12345);
+            var v2 = new BigtableVersion(12345 + 1_000_000);
+            var timespanDiff = v2.ToDateTime() - v1.ToDateTime();
+            Assert.Equal(TimeSpan.FromSeconds(1), timespanDiff);
+        }
+
+        [Fact]
+        public void NegativeOneIsUtcNow()
+        {
+            var negativeOne = new BigtableVersion(-1);
+            var utcNow = new BigtableVersion(DateTime.UtcNow);
+
+            // Verify they are not more than 1 second apart
+            Assert.True(utcNow.Value - negativeOne.Value < 1_000_000);
+        }
+
+        [Fact]
+        public void RoundTripDateTime()
+        {
+            var dateTime = DateTime.UtcNow;
+            var version = new BigtableVersion(dateTime);
+
+            // BigtableVersion stores micros, which is 10s of ticks, so truncate the ticks.
+            var expected = new DateTime(dateTime.Ticks - (dateTime.Ticks % 10), DateTimeKind.Utc);
+            Assert.Equal(expected, version.ToDateTime());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(long.MaxValue - 1)]
+        [InlineData(long.MaxValue)]
+        public void RoundTripLong(long value)
+        {
+            var version = new BigtableVersion(value);
+            Assert.Equal(value, version.Value);
+        }
+
+        [Theory]
+        [InlineData("BigtableVersion: 1507852800000000", "2017-10-13")]
+        public void FormattingDateTime(string expectedText, string dateTimeText)
+        {
+            var dateTime = DateTime.ParseExact(
+                dateTimeText,
+                "yyyy-MM-dd",
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal);
+            var version = new BigtableVersion(dateTime);
+            Assert.Equal(expectedText, version.ToString());
+        }
+
+        [Theory]
+        [InlineData("BigtableVersion: 0", 0)]
+        [InlineData("BigtableVersion: 1", 1)]
+        [InlineData("BigtableVersion: 9223372036854775807", long.MaxValue)]
+        public void FormattingLong(string expectedText, long versionValue)
+        {
+            var version = new BigtableVersion(versionValue);
+            Assert.Equal(expectedText, version.ToString());
+        }
+    }
+}
