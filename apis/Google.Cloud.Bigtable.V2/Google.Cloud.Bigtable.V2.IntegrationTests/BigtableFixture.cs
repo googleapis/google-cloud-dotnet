2dafbc61561be9a8080a9fb56798299d70455b45
Mike Dour <evildour@users.noreply.github.com>
2017-10-23 16:51:43 +0000

    Mutations factory methods and MutateRow API (#1565)
    * Mutations factory methods and MutateRow API
    
    * Address PR comments
    
    * Address PR comments
    
    * Rebase and regenerate projects
    
    * Use dummy client for unit tests

---
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.IntegrationTests/BigtableFixture.cs
@@ -0,0 +1,140 @@
+﻿// Copyright 2017, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax.Grpc;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Bigtable.Admin.V2;
+using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Google.Cloud.Bigtable.V2.IntegrationTests
+{
+    [CollectionDefinition(nameof(BigtableFixture))]
+    public class BigtableFixture : IDisposable, ICollectionFixture<BigtableFixture>
+    {
+        private const string EmulatorEnvironmentVariable = "BIGTABLE_EMULATOR_HOST";
+        private const string TestProjectEnvironmentVariable = "TEST_PROJECT";
+
+        public const string ColumnFamily1 = "cf1";
+        public const string OtherColumnFamily = "test_data";
+
+        public ProjectName ProjectName { get; private set; }
+        public InstanceName DefaultInstanceName { get; private set; }
+        public TableName DefaultTableName { get; private set; }
+
+        public BigtableInstanceAdminClient DefaultInstanceAdminClient { get; private set; }
+        public BigtableTableAdminClient DefaultTableAdminClient { get; private set; }
+        public BigtableClient DefaultTableClient { get; private set; }
+
+        public Channel EmulatorChannel { get; }
+
+        public BigtableFixture()
+        {
+            string emulatorHost = Environment.GetEnvironmentVariable(EmulatorEnvironmentVariable);
+
+            string projectId;
+            if (!string.IsNullOrEmpty(emulatorHost))
+            {
+                projectId = "emulator-test-project";
+                EmulatorChannel = new Channel(emulatorHost, ChannelCredentials.Insecure);
+            }
+            else
+            {
+                projectId = Environment.GetEnvironmentVariable(TestProjectEnvironmentVariable);
+                if (string.IsNullOrEmpty(projectId))
+                {
+                    throw new InvalidOperationException(
+                        $"Please set either the {EmulatorEnvironmentVariable} or {TestProjectEnvironmentVariable} environment variable before running tests");
+                }
+            }
+
+            ProjectName = new ProjectName(projectId);
+
+            Task.Run(InitBigtableInstanceAndTable).Wait();
+        }
+
+        private async Task InitBigtableInstanceAndTable()
+        {
+            DefaultInstanceName = new InstanceName(ProjectName.ProjectId, "i" + Guid.NewGuid().ToString("N"));
+
+            if (EmulatorChannel == null)
+            {
+                DefaultInstanceAdminClient = BigtableInstanceAdminClient.Create();
+                DefaultTableAdminClient = BigtableTableAdminClient.Create();
+                DefaultTableClient = BigtableClient.Create();
+
+                // Cluster ID must be max 30 characters
+                string clusterId = DefaultInstanceName.InstanceId.Substring(0, 22) + "-cluster";
+
+                var instanceOperation = await DefaultInstanceAdminClient.CreateInstanceAsync(new CreateInstanceRequest
+                {
+                    Parent = ProjectName.ToString(),
+                    InstanceId = DefaultInstanceName.InstanceId,
+                    Instance = new Instance
+                    {
+                        DisplayName = "Integration tests instance",
+                        Type = Instance.Types.Type.Development
+                    },
+                    Clusters =
+                    {
+                        {
+                            clusterId,
+                            new Cluster
+                            {
+                                ClusterName = new ClusterName(ProjectName.ProjectId, DefaultInstanceName.InstanceId, clusterId),
+                                DefaultStorageType = StorageType.Hdd,
+                                LocationAsLocationName = new LocationName(ProjectName.ProjectId, "us-central1-c")
+                            }
+                        }
+                    }
+                });
+                await instanceOperation.PollUntilCompletedAsync();
+            }
+            else
+            {
+                DefaultTableAdminClient = BigtableTableAdminClient.Create(EmulatorChannel);
+                DefaultTableClient = BigtableClient.Create(EmulatorChannel);
+            }
+            
+            DefaultTableName = new TableName(ProjectName.ProjectId, DefaultInstanceName.InstanceId, "default-table");
+            await DefaultTableAdminClient.CreateTableAsync(
+                DefaultInstanceName,
+                DefaultTableName.TableId,
+                new Table
+                {
+                    Granularity = Table.Types.TimestampGranularity.Millis,
+                    ColumnFamilies =
+                    {
+                        { ColumnFamily1, new ColumnFamily { GcRule = new GcRule { MaxNumVersions = 3 } } },
+                        { OtherColumnFamily, new ColumnFamily { GcRule = new GcRule { MaxNumVersions = 3 } } }
+                    }
+                });
+        }
+
+        public void Dispose()
+        {
+            if (DefaultTableAdminClient != null && DefaultTableName != null)
+            {
+                DefaultTableAdminClient.DeleteTable(new DeleteTableRequest { Name = DefaultTableName.ToString() });
+            }
+
+            if (DefaultInstanceAdminClient != null && DefaultInstanceName != null)
+            {
+                DefaultInstanceAdminClient.DeleteInstance(DefaultInstanceName);
+            }
+        }
+    }
+}
--- apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2/BigtableClientPartial.cs
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2/BigtableClientPartial.cs
@@ -12,10 +12,197 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Api.Gax;
 using Google.Api.Gax.Grpc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Google.Cloud.Bigtable.V2
 {
+    public partial class BigtableClient
+    {
+        /// <summary>
+        /// Mutates a row atomically. Cells already present in the row are left
+        /// unchanged unless explicitly changed by <paramref name="mutations"/>.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Note that string is implicitly convertible to <see cref="BigtableByteString"/>, so <paramref name="rowKey"/> can
+        /// be specified using a string as well and its UTF-8 representations will be used.
+        /// </para>
+        /// <para>
+        /// This method simply delegates to <see cref="MutateRow(MutateRowRequest, CallSettings)"/>.
+        /// </para>
+        /// </remarks>
+        /// <param name="tableName">
+        /// The unique name of the table to which the mutation should be applied. Must not be null.
+        /// </param>
+        /// <param name="rowKey">
+        /// The key of the row to which the mutation should be applied. Must not be empty.
+        /// </param>
+        /// <param name="mutations">
+        /// Changes to be atomically applied to the specified row. Entries are applied
+        /// in order, meaning that earlier mutations can be masked by later ones.
+        /// Must contain at least one entry and at most 100000. Must not be null, or contain null
+        /// elements.
+        /// </param>
+        /// <param name="callSettings">
+        /// If not null, applies overrides to this RPC call.
+        /// </param>
+        /// <returns>
+        /// The response from mutating the row.
+        /// </returns>
+        public virtual MutateRowResponse MutateRow(
+            TableName tableName,
+            BigtableByteString rowKey,
+            IEnumerable<Mutation> mutations,
+            CallSettings callSettings = null) =>
+            MutateRow(
+                CreateMutateRowRequest(tableName, rowKey, mutations),
+                callSettings);
+
+        /// <summary>
+        /// Mutates a row atomically. Cells already present in the row are left
+        /// unchanged unless explicitly changed by <paramref name="mutations"/>.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Note that string is implicitly convertible to <see cref="BigtableByteString"/>, so <paramref name="rowKey"/> can
+        /// be specified using a string as well and its UTF-8 representations will be used.
+        /// </para>
+        /// <para>
+        /// This method simply delegates to <see cref="MutateRow(MutateRowRequest, CallSettings)"/>.
+        /// </para>
+        /// </remarks>
+        /// <param name="tableName">
+        /// The unique name of the table to which the mutation should be applied. Must not be null.
+        /// </param>
+        /// <param name="rowKey">
+        /// The key of the row to which the mutation should be applied. Must not be empty.
+        /// </param>
+        /// <param name="mutations">
+        /// Changes to be atomically applied to the specified row. Entries are applied
+        /// in order, meaning that earlier mutations can be masked by later ones.
+        /// Must contain at least one entry and at most 100000. Must not be null, or contain null
+        /// elements.
+        /// </param>
+        /// <returns>
+        /// The response from mutating the row.
+        /// </returns>
+        public virtual MutateRowResponse MutateRow(
+            TableName tableName,
+            BigtableByteString rowKey,
+            params Mutation[] mutations) =>
+            MutateRow(
+                CreateMutateRowRequest(tableName, rowKey, mutations));
+
+        /// <summary>
+        /// Asynchronously mutates a row atomically. Cells already present in the row are left
+        /// unchanged unless explicitly changed by <paramref name="mutations"/>.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Note that string is implicitly convertible to <see cref="BigtableByteString"/>, so <paramref name="rowKey"/> can
+        /// be specified using a string as well and its UTF-8 representations will be used.
+        /// </para>
+        /// <para>
+        /// This method simply delegates to <see cref="MutateRowAsync(MutateRowRequest, CallSettings)"/>.
+        /// </para>
+        /// </remarks>
+        /// <param name="tableName">
+        /// The unique name of the table to which the mutation should be applied. Must not be null.
+        /// </param>
+        /// <param name="rowKey">
+        /// The key of the row to which the mutation should be applied. Must not be empty.
+        /// </param>
+        /// <param name="mutations">
+        /// Changes to be atomically applied to the specified row. Entries are applied
+        /// in order, meaning that earlier mutations can be masked by later ones.
+        /// Must contain at least one entry and at most 100000. Must not be null, or contain null
+        /// elements.
+        /// </param>
+        /// <param name="callSettings">
+        /// If not null, applies overrides to this RPC call.
+        /// </param>
+        /// <returns>
+        /// The response from mutating the row.
+        /// </returns>
+        public virtual Task<MutateRowResponse> MutateRowAsync(
+            TableName tableName,
+            BigtableByteString rowKey,
+            IEnumerable<Mutation> mutations,
+            CallSettings callSettings = null) =>
+            MutateRowAsync(
+                CreateMutateRowRequest(tableName, rowKey, mutations),
+                callSettings);
+
+        /// <summary>
+        /// Asynchronously mutates a row atomically. Cells already present in the row are left
+        /// unchanged unless explicitly changed by <paramref name="mutations"/>.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Note that string is implicitly convertible to <see cref="BigtableByteString"/>, so <paramref name="rowKey"/> can
+        /// be specified using a string as well and its UTF-8 representations will be used.
+        /// </para>
+        /// <para>
+        /// This method simply delegates to <see cref="MutateRowAsync(MutateRowRequest, CallSettings)"/>.
+        /// </para>
+        /// </remarks>
+        /// <param name="tableName">
+        /// The unique name of the table to which the mutation should be applied. Must not be null.
+        /// </param>
+        /// <param name="rowKey">
+        /// The key of the row to which the mutation should be applied. Must not be empty.
+        /// </param>
+        /// <param name="mutations">
+        /// Changes to be atomically applied to the specified row. Entries are applied
+        /// in order, meaning that earlier mutations can be masked by later ones.
+        /// Must contain at least one entry and at most 100000. Must not be null, or contain null
+        /// elements.
+        /// </param>
+        /// <param name="cancellationToken">A cancellation token to monitor for the asynchronous operation.</param>
+        /// <returns>
+        /// The response from mutating the row.
+        /// </returns>
+        public virtual Task<MutateRowResponse> MutateRowAsync(
+            TableName tableName,
+            BigtableByteString rowKey,
+            IEnumerable<Mutation> mutations,
+            CancellationToken cancellationToken) =>
+            MutateRowAsync(
+                CreateMutateRowRequest(tableName, rowKey, mutations),
+                CallSettings.FromCancellationToken(cancellationToken));
+
+        private static MutateRowRequest CreateMutateRowRequest(
+            TableName tableName,
+            BigtableByteString rowKey,
+            IEnumerable<Mutation> mutations)
+        {
+            GaxPreconditions.CheckNotNull(tableName, nameof(tableName));
+            GaxPreconditions.CheckArgument(rowKey.Length != 0, nameof(rowKey), "The row key must not empty");
+            GaxPreconditions.CheckNotNull(mutations, nameof(mutations));
+            var mutationsChecked = mutations.Select(mutation =>
+            {
+                GaxPreconditions.CheckArgument(mutation != null, nameof(mutations), "Entries must not be null");
+                return mutation;
+            });
+
+            var request = new MutateRowRequest
+            {
+                TableName = tableName.ToString(),
+                RowKey = rowKey,
+                Mutations = { mutationsChecked }
+            };
+
+            GaxPreconditions.CheckArgument(
+                request.Mutations.Count != 0, nameof(mutations), "There must be at least one entry.");
+            return request;
+        }
+    }
+
     public partial class BigtableClientImpl
     {
         private const string ResourcePrefixHeader = "google-cloud-resource-prefix";
