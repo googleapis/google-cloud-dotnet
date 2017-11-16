23db3792bfcc1a021481be3f7b58b01cbdbcf209
Jon Skeet <jonskeet@google.com>
2017-11-16 12:24:20 +0000

    Simple throughput measurement tool for storage
    We can use this to investigate #1480.

---
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.ThroughputTool/Program.cs
@@ -0,0 +1,90 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.IO;
+
+namespace Google.Cloud.Storage.V1.ThroughputTool
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Configuration configuration = null;
+            try
+            {
+                configuration = Configuration.FromCommandLineArguments(args);
+                CreateFile(configuration);
+                RunUpload(configuration);
+                RunDownload(configuration);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if (configuration != null)
+                {
+                    File.Delete(configuration.LocalFile);
+                }
+            }
+        }
+
+        private static void CreateFile(Configuration configuration)
+        {
+            var random = new Random();
+            long bytesLeft = configuration.SizeBytes;
+            byte[] buffer = new byte[1024 * 1024];
+            using (var output = File.Create(configuration.LocalFile))
+            {
+                while (bytesLeft > 0)
+                {
+                    random.NextBytes(buffer);
+                    int count = (int) Math.Min(bytesLeft, buffer.Length);
+                    output.Write(buffer, 0, count);
+                    bytesLeft -= count;
+                }
+            }
+        }
+
+        private static void RunUpload(Configuration configuration)
+        {
+            var client = StorageClient.Create();
+            Console.WriteLine("Uploading");
+            using (var input = File.OpenRead(configuration.LocalFile))
+            {
+                using (var reporter = new ProgressReporter())
+                {
+                    client.UploadObject(configuration.Bucket, configuration.ObjectName, "application/binary", input, progress: reporter);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static void RunDownload(Configuration configuration)
+        {
+            var client = StorageClient.Create();
+            Console.WriteLine("Downloading");
+            using (var output = new NullStream())
+            {
+                using (var reporter = new ProgressReporter())
+                {
+                    client.DownloadObject(configuration.Bucket, configuration.ObjectName, output, progress: reporter);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.ThroughputTool/ProgressReporter.cs
@@ -0,0 +1,73 @@
+﻿// Copyright 2017 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Apis.Download;
+using Google.Apis.Upload;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Cloud.Storage.V1.ThroughputTool
+{
+    internal sealed class ProgressReporter : IDisposable, IProgress<IDownloadProgress>, IProgress<IUploadProgress>
+    {
+        private static readonly TimeSpan s_reportPeriod = TimeSpan.FromSeconds(5);
+
+        private readonly Stopwatch _stopwatch;
+        private readonly TaskCompletionSource<int> _tcs;
+        private readonly Thread _reportingThread;
+        private long _bytesProcessed;
+
+        internal ProgressReporter()
+        {
+            _stopwatch = Stopwatch.StartNew();
+            _tcs = new TaskCompletionSource<int>();
+            _reportingThread = new Thread(ReportPeriodically);
+            _reportingThread.Start();
+        }
+
+        private void ReportPeriodically()
+        {
+            Console.WriteLine("Timestamp  Elapsed         MB     Mbps");
+            while (true)
+            {
+                if (_tcs.Task.Wait(s_reportPeriod))
+                {
+                    return;
+                }
+                DateTime now = DateTime.UtcNow;
+                TimeSpan elapsed = _stopwatch.Elapsed;
+                double totalSeconds = elapsed.TotalSeconds;
+                long bytesProcessed = Interlocked.Read(ref _bytesProcessed);
+                double megabytesProcessed = bytesProcessed / (1024.0 * 1024.0);
+                double megabitsProcessed = megabytesProcessed * 8;
+                double? mbps = totalSeconds < 0.1 ? default(double?) : megabitsProcessed / totalSeconds;
+                Console.WriteLine($"{now:HH:mm:ss}   {elapsed:hh':'mm':'ss}  {megabytesProcessed,8:####0.00}   {mbps,6:##0.00}");
+            }
+        }
+
+        public void Dispose()
+        {
+            _tcs.TrySetResult(0);
+            _reportingThread.Join();
+        }
+
+        public void Report(IUploadProgress value) =>
+            Interlocked.Exchange(ref _bytesProcessed, value.BytesSent);
+
+        public void Report(IDownloadProgress value) =>
+            Interlocked.Exchange(ref _bytesProcessed, value.BytesDownloaded);
+    }
+}
