4cd683962b9e7a87f0257c74a448226404640c2c
Jon Skeet <jonskeet@google.com>
2017-12-08 08:43:52 +0000

    Add Kubernetes Engine smoke tests

---
+++ apis/Google.Cloud.Container.V1/Google.Cloud.Container.V1.SmokeTests/ClusterManagerSmokeTest.g.cs
@@ -0,0 +1,60 @@
+// Copyright 2017, Google LLC All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+using Google.Api.Gax;
+using Google.Api.Gax.Grpc;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Cloud.Container.V1.SmokeTests
+{
+    public class ClusterManagerSmokeTest
+    {
+        public static int Main(string[] args)
+        {
+            // Read projectId from args
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Project ID must be passed as first argument.");
+                Console.WriteLine();
+                return 1;
+            }
+            string projectId = args[0];
+
+            // Create client
+            ClusterManagerClient client = ClusterManagerClient.Create();
+
+            // Initialize request argument(s)
+            string projectId2 = projectId;
+            string zone = "us-central1-a";
+
+            // Call API method
+            ListClustersResponse response = client.ListClusters(projectId2, zone);
+            // Show the result
+            Console.WriteLine(response);
+
+            // Success
+            Console.WriteLine("Smoke test passed OK");
+            return 0;
+        }
+    }
+}
