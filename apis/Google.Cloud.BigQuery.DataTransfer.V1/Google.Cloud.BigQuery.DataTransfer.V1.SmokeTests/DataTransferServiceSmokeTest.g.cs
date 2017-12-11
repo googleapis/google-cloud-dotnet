00523c21b640cf60e509c972b6947b8927a1fb62
Jon Skeet <jonskeet@google.com>
2017-12-11 11:20:40 +0000

    Add smoke tests for BigQuery.DataTransfer and Trace v2

---
+++ apis/Google.Cloud.BigQuery.DataTransfer.V1/Google.Cloud.BigQuery.DataTransfer.V1.SmokeTests/DataTransferServiceSmokeTest.g.cs
@@ -0,0 +1,53 @@
+// Copyright 2017, Google LLC All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+using Google.Api.Gax;
+using Google.Api.Gax.Grpc;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Google.Cloud.BigQuery.DataTransfer.V1.SmokeTests
+{
+    public class DataTransferServiceSmokeTest
+    {
+        public static int Main(string[] args)
+        {
+            // Create client
+            DataTransferServiceClient client = DataTransferServiceClient.Create();
+
+            // Initialize request argument(s)
+            ParentNameOneof parent = ParentNameOneof.From(new LocationName("[PROJECT]", "[LOCATION]"));
+
+            // Call API method
+            PagedEnumerable<ListDataSourcesResponse, DataSource> pagedResponse = client.ListDataSources(parent);
+            // Show the result
+            foreach (var item in pagedResponse)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Success
+            Console.WriteLine("Smoke test passed OK");
+            return 0;
+        }
+    }
+}
