627ceb5816843410ad7e2d8dc35bba2d4dcfe215
Mike Dour <evildour@users.noreply.github.com>
2017-12-11 10:58:47 -0500

    [Bigtable] Ensure lexicographical order of families in row merger (#1770)
    * Ensure lexicographical order of families in row merger
    
    * Address PR comments

---
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.Tests/MockReadRowsStream.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2017 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Grpc.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Google.Cloud.Bigtable.V2.Tests
+{
+    public class MockReadRowsStream : BigtableClient.ReadRowsStream
+    {
+        private IAsyncEnumerator<ReadRowsResponse> _responseStream;
+
+        public MockReadRowsStream(params ReadRowsResponse[] responses) =>
+            _responseStream = responses.ToAsyncEnumerable().GetEnumerator();
+
+        public override AsyncServerStreamingCall<ReadRowsResponse> GrpcCall => null;
+        public override IAsyncEnumerator<ReadRowsResponse> ResponseStream => _responseStream;
+    }
+}
