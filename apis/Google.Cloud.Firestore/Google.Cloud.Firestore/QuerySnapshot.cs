451e7aeb244af19382b2e38a9eda48508bf21cf4
Jon Skeet <jonskeet@google.com>
2018-02-02 10:00:30 +0000

    Make QuerySnapshot implement IReadOnlyList<DocumentSnapshot>
    Fixes #1884.
    
    For some reason dotCover doesn't pick up the fact that I'm testing
    the indexer, but it's not worth looking into that right now.

--- apis/Google.Cloud.Firestore/Google.Cloud.Firestore/QuerySnapshot.cs
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore/QuerySnapshot.cs
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Google.Cloud.Firestore
@@ -18,7 +20,7 @@ namespace Google.Cloud.Firestore
     /// <summary>
     /// An immutable snapshot of complete query results.
     /// </summary>
-    public sealed class QuerySnapshot
+    public sealed class QuerySnapshot : IReadOnlyList<DocumentSnapshot>
     {
         internal QuerySnapshot(Query query, IReadOnlyList<DocumentSnapshot> documents, Timestamp readTime)
         {
@@ -41,5 +43,25 @@ namespace Google.Cloud.Firestore
         /// The documents in the snapshot.
         /// </summary>
         public IReadOnlyList<DocumentSnapshot> Documents { get; }
+
+        /// <summary>
+        /// Returns the document snapshot with the specified index within this query snapshot.
+        /// </summary>
+        /// <param name="index">The index of the document to return.</param>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="index"/> is less than 0, or greater than or equal to <see cref="Count"/>.</exception>
+        /// <returns>The document snapshot with the specified index within this query snapshot.</returns>
+        public DocumentSnapshot this[int index] => Documents[index];
+
+        /// <summary>
+        /// Returns the number of documents in this query snapshot.
+        /// </summary>
+        /// <value>The number of documents in this query snapshot.</value>
+        public int Count => Documents.Count;
+
+        /// <inheritdoc />
+        public IEnumerator<DocumentSnapshot> GetEnumerator() => Documents.GetEnumerator();
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
