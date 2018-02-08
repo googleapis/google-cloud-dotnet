23c87e8623e87c36949f0aaacd126eb740b795b8
Jon Skeet <jonskeet@google.com>
2018-02-08 11:46:39 +0000

    GetBigQueryServiceAccountEmail implementation and tests

---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.IntegrationTests/ServiceAccountTest.cs
@@ -0,0 +1,34 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Xunit;
+
+namespace Google.Cloud.BigQuery.V2.IntegrationTests
+{
+    [Collection(nameof(BigQueryFixture))]
+    public class ServiceAccountTest
+    {
+        private readonly BigQueryFixture _fixture;
+
+        public ServiceAccountTest(BigQueryFixture fixture) => _fixture = fixture;
+
+        [Fact]
+        public void GetBigQueryServiceAccountEmail()
+        {
+            var client = BigQueryClient.Create(_fixture.ProjectId);
+            string serviceEmail = client.GetBigQueryServiceAccountEmail();
+            Assert.Contains("gserviceaccount.com", serviceEmail);
+        }
+    }
+}
---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryClientImpl.ServiceAccount.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using Google.Apis.Bigquery.v2.Data;
+using System.Threading;
+using System.Threading.Tasks;
+using static Google.Apis.Bigquery.v2.ProjectsResource;
+
+namespace Google.Cloud.BigQuery.V2
+{
+    public partial class BigQueryClientImpl
+    {
+        /// <inheritdoc />
+        public override string GetBigQueryServiceAccountEmail(ProjectReference projectReference, GetBigQueryServiceAccountEmailOptions options = null) =>
+            CreateGetServiceAccountEmailRequest(projectReference, options).Execute().Email;
+
+        /// <inheritdoc />
+        public override async Task<string> GetBigQueryServiceAccountEmailAsync(ProjectReference projectReference, GetBigQueryServiceAccountEmailOptions options = null, CancellationToken cancellationToken = default)
+        {
+            var request = CreateGetServiceAccountEmailRequest(projectReference, options);
+            var response = await request.ExecuteAsync(cancellationToken).ConfigureAwait(false);
+            return response.Email;
+        }
+
+        private GetServiceAccountRequest CreateGetServiceAccountEmailRequest(ProjectReference projectReference, GetBigQueryServiceAccountEmailOptions options)
+        {
+            GaxPreconditions.CheckNotNull(projectReference, nameof(projectReference));
+
+            var request = Service.Projects.GetServiceAccount(projectReference.ProjectId);
+            request.ModifyRequest += _versionHeaderAction;
+            options?.ModifyRequest(request);
+            RetryHandler.MarkAsRetriable(request);
+            return request;
+        }
+    }
+}
