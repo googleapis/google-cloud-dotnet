f89c526e2357e45d820da01ebcb43fbc505882e2
Jon Skeet <jonskeet@google.com>
2018-02-08 16:19:22 +0000

    Make generateapis.sh use toolversions.sh
    One fewer place to specify version numbers...

--- toolversions.sh
+++ toolversions.sh
@@ -1,23 +1,27 @@
 # This is intended to be imported using the "source" function from
 # any scripts that use tools.
 
+# TODO: Make all of this work under Linux too, if it's useful
+
 declare -r REPO_ROOT=$(realpath $(dirname ${BASH_SOURCE}))
+declare -r TOOL_PACKAGES=$REPO_ROOT/packages
 
 declare -r DOCFX_VERSION=2.28.2
 declare -r DOTCOVER_VERSION=2017.1.20170613.162720 
 declare -r REPORTGENERATOR_VERSION=2.4.5.0
 declare -r PROTOC_VERSION=3.4.0
+declare -r GRPC_VERSION=1.7.0
 
 # Variables to use to invoke the tools themselves
 
-declare -r DOCFX=$REPO_ROOT/packages/docfx.$DOCFX_VERSION/docfx.exe
+declare -r DOCFX=$TOOL_PACKAGES/docfx.$DOCFX_VERSION/docfx.exe
 declare -r FIND=/usr/bin/find
-declare -r DOTCOVER=$REPO_ROOT/packages/JetBrains.dotCover.CommandLineTools.$DOTCOVER_VERSION/tools/dotCover.exe
-declare -r REPORTGENERATOR=$REPO_ROOT/packages/ReportGenerator.$REPORTGENERATOR_VERSION/tools/ReportGenerator.exe
+declare -r DOTCOVER=$TOOL_PACKAGES/JetBrains.dotCover.CommandLineTools.$DOTCOVER_VERSION/tools/dotCover.exe
+declare -r REPORTGENERATOR=$TOOL_PACKAGES/ReportGenerator.$REPORTGENERATOR_VERSION/tools/ReportGenerator.exe
 
-declare -r PROTOBUF_TOOLS_ROOT=$REPO_ROOT/packages/Google.Protobuf.Tools.$PROTOC_VERSION
-# TODO: Make this work under Linux to
+declare -r PROTOBUF_TOOLS_ROOT=$TOOL_PACKAGES/Google.Protobuf.Tools.$PROTOC_VERSION
 declare -r PROTOC=$PROTOBUF_TOOLS_ROOT/tools/windows_x64/protoc.exe
+declare -r GRPC_PLUGIN=packages/Grpc.Tools.$GRPC_VERSION/tools/windows_x64/grpc_csharp_plugin.exe
 
 # Use an appropriate version of nuget... preferring
 # first an existing NUGET variable, then NuGet, then
@@ -36,23 +40,27 @@ fi
 # when required. (They handle the case where the tool is already installed.)
 
 install_dotcover() {
-  $NUGET install -Verbosity quiet -OutputDirectory $REPO_ROOT/packages -Version $DOTCOVER_VERSION JetBrains.dotCover.CommandLineTools
+  $NUGET install -Verbosity quiet -OutputDirectory $TOOL_PACKAGES -Version $DOTCOVER_VERSION JetBrains.dotCover.CommandLineTools
 }
 
 install_reportgenerator() {
-  $NUGET install -Verbosity quiet -OutputDirectory $REPO_ROOT/packages -Version $REPORTGENERATOR_VERSION ReportGenerator
+  $NUGET install -Verbosity quiet -OutputDirectory $TOOL_PACKAGES -Version $REPORTGENERATOR_VERSION ReportGenerator
 }
 
 install_protoc() {
-  $NUGET install -Verbosity quiet -OutputDirectory $REPO_ROOT/packages -Version $PROTOC_VERSION Google.Protobuf.Tools
+  $NUGET install -Verbosity quiet -OutputDirectory $TOOL_PACKAGES -Version $PROTOC_VERSION Google.Protobuf.Tools
+}
+
+install_grpc() {
+  $NUGET install -Verbosity quiet -OutputDirectory $TOOL_PACKAGES -Version $GRPC_VERSION Grpc.Tools
 }
 
 install_docfx() {
   if [[ ! -f $DOCFX ]]
   then
     (echo "Fetching docfx v${DOCFX_VERSION}";
-     mkdir -p $REPO_ROOT/packages;
-     cd $REPO_ROOT/packages;
+     mkdir -p $TOOL_PACKAGES;
+     cd $TOOL_PACKAGES;
      mkdir docfx.$DOCFX_VERSION;
      cd docfx.$DOCFX_VERSION;
      curl -sSL https://github.com/dotnet/docfx/releases/download/v${DOCFX_VERSION}/docfx.zip -o tmp.zip;
