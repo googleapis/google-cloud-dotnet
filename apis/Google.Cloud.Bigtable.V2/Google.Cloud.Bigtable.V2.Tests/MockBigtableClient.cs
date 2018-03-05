2eabe485a18098c79b04e16cbe0bd04ec2dfb577
Mike Dour <mdour@google.com>
2018-03-05 09:19:22 -0500

    Use Moq for MutateRows retry tests

--- apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.Tests/MockBigtableClient.cs
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.Tests/MockBigtableClient.cs
@@ -12,30 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Google.Api.Gax;
 using Google.Api.Gax.Grpc;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Google.Cloud.Bigtable.V2.Tests
 {
     public class RequestMadeException : Exception { }
 
-    // TODO: See if Moq would be possible/easier rather than hand-rolling.
     public class MockBigtableClient : BigtableClient
     {
-        private readonly MockBigtableServiceApiClient _client = new MockBigtableServiceApiClient();
-
-        /// <summary>
-        /// Adds a stream that will be returned on a subsequent <see cref="MutateRows"/> call. Added streams will be returned in FIFO order.
-        /// </summary>
-        public void AddMutateRowsStream(BigtableServiceApiClient.MutateRowsStream mockMutateRowsStream) => _client.AddMutateRowsStream(mockMutateRowsStream);
-
-        protected override BigtableServiceApiClient GetUnderlyingClient() => _client;
-
         public override CheckAndMutateRowResponse CheckAndMutateRow(CheckAndMutateRowRequest request, CallSettings callSettings = null) =>
             throw new RequestMadeException();
         public override Task<CheckAndMutateRowResponse> CheckAndMutateRowAsync(CheckAndMutateRowRequest request, CallSettings callSettings = null) =>
@@ -45,9 +31,9 @@ namespace Google.Cloud.Bigtable.V2.Tests
         public override Task<MutateRowResponse> MutateRowAsync(MutateRowRequest request, CallSettings callSettings = null) =>
             throw new RequestMadeException();
         public override MutateRowsResponse MutateRows(MutateRowsRequest request, CallSettings callSettings = null) =>
-            Task.Run(() => MutateRowsAsync(request, callSettings)).ResultWithUnwrappedExceptions();
+            throw new RequestMadeException();
         public override Task<MutateRowsResponse> MutateRowsAsync(MutateRowsRequest request, CallSettings callSettings = null) =>
-            ConvertResult(request, callSettings, GetUnderlyingClient().MutateRows(request, callSettings));
+            throw new RequestMadeException();
         public override ReadModifyWriteRowResponse ReadModifyWriteRow(ReadModifyWriteRowRequest request, CallSettings callSettings = null) =>
             throw new RequestMadeException();
         public override Task<ReadModifyWriteRowResponse> ReadModifyWriteRowAsync(ReadModifyWriteRowRequest request, CallSettings callSettings = null) =>
@@ -56,15 +42,5 @@ namespace Google.Cloud.Bigtable.V2.Tests
             throw new RequestMadeException();
         public override BigtableServiceApiClient.SampleRowKeysStream SampleRowKeys(SampleRowKeysRequest request, CallSettings callSettings = null) =>
             throw new RequestMadeException();
-
-        public class MockBigtableServiceApiClient : BigtableServiceApiClient
-        {
-            private readonly Queue<MutateRowsStream> _mockMutateRowsStreams = new Queue<MutateRowsStream>();
-
-            public void AddMutateRowsStream(MutateRowsStream mockMutateRowsStream) => _mockMutateRowsStreams.Enqueue(mockMutateRowsStream);
-
-            public override MutateRowsStream MutateRows(MutateRowsRequest request, CallSettings callSettings = null) =>
-                _mockMutateRowsStreams.Count != 0 ? _mockMutateRowsStreams.Dequeue() : throw new RequestMadeException();
-        }
     }
 }
