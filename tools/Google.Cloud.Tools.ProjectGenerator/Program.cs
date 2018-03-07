6b9a407d1d38d6a258d1230f2b09c6130898c115
Jon Skeet <jonskeet@google.com>
2018-03-07 16:18:30 +0000

    Update default versions of gRPC and GAX

--- tools/Google.Cloud.Tools.ProjectGenerator/Program.cs
+++ tools/Google.Cloud.Tools.ProjectGenerator/Program.cs
@@ -45,8 +45,8 @@ namespace Google.Cloud.Tools.ProjectGenerator
         private const string ProjectVersionValue = "project";
         private const string DefaultVersionValue = "default";
         private const string GrpcPackage = "Grpc.Core";
-        private const string DefaultGaxVersion = "2.2.1";
-        private const string GrpcVersion = "1.8.0";
+        private const string DefaultGaxVersion = "2.3.0";
+        private const string GrpcVersion = "1.10.0";
         private static readonly Dictionary<string, string> DefaultPackageVersions = new Dictionary<string, string>
         {
             { "Google.Api.Gax", DefaultGaxVersion },
