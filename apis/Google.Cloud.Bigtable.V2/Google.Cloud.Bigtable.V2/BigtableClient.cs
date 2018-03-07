189577196e4940cb409521030a71e4b749ae75f0
Alex <alex@qlogic.io>
2018-03-07 11:35:12 -0500

    Minor correction to documentation

--- apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2/BigtableClient.cs
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2/BigtableClient.cs
@@ -253,8 +253,9 @@ namespace Google.Cloud.Bigtable.V2
             /// <summary>
             /// Instantiate with the specified settings.
             /// </summary>
-            /// <param name="clientCount">Optional. If not set will default to number of processors.
-            /// The number of <see cref="BigtableServiceApiClient"/>s to create and use within a <see cref="BigtableClient"/> instance.</param>
+            /// <param name="clientCount">Optional.
+            /// The number of <see cref="BigtableServiceApiClient"/>s to create and use within a <see cref="BigtableClient"/> instance.
+            /// If not set will default to number of processors.</param>
             /// <param name="bigtableServiceApiSettings">Optional. The settings to use when creating <see cref="BigtableServiceApiClient"/> instances.</param>
             /// <param name="credentials">Optional. Credentials to use when creating <see cref="BigtableServiceApiClient"/> instances.</param>
             /// <param name="serviceEndpoint">Optional.
