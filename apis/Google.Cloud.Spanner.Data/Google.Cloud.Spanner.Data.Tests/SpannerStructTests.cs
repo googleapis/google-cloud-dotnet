cedc18662563efc9169ef32a2048cf4a38232428
Jon Skeet <jonskeet@google.com>
2018-04-20 16:44:09 +0100

    Introduce SpannerStruct
    This is currently unused: it's basically just a list of fields.

---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/SpannerStructTests.cs
@@ -0,0 +1,72 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Google.Cloud.Spanner.Data.Tests
+{
+    public class SpannerStructTests
+    {
+        [Fact]
+        public void NullNameCoalescing()
+        {
+            var value = new SpannerStruct
+            {
+                { null, SpannerDbType.String, "foo" },
+                new SpannerStruct.Field(null, SpannerDbType.String, "bar")
+            };
+            Assert.Equal("", value[0].Name);
+            Assert.Equal("", value[1].Name);
+        }
+
+        [Fact]
+        public void NullProtection()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SpannerStruct.Field("name", null, "value"));
+            var value = new SpannerStruct();
+            Assert.Throws<ArgumentNullException>(() => value.Add("name", null, "value"));
+            Assert.Throws<ArgumentNullException>(() => value.Add(null));
+        }
+
+        [Fact]
+        public void InterfaceAccess()
+        {
+            var value = new SpannerStruct
+            {
+                { "f", SpannerDbType.String, null },
+                { "g", SpannerDbType.Int64, 100L },
+            };
+            var fields = value.ToList();
+            Assert.Same(fields[0], value[0]);
+            Assert.Same(fields[1], value[1]);
+            Assert.Equal(2, fields.Count);
+            Assert.Throws<ArgumentOutOfRangeException>(() => fields[2]);
+        }
+
+        [Fact]
+        public void SimpleProperties()
+        {
+            var value = new SpannerStruct
+            {
+                { "f", SpannerDbType.String, "value" }
+            };
+            var field = value[0];
+            Assert.Equal("f", field.Name);
+            Assert.Equal(SpannerDbType.String, field.Type);
+            Assert.Equal("value", field.Value);
+        }
+    }
+}
---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data/SpannerStruct.cs
@@ -0,0 +1,108 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Google.Cloud.Spanner.Data
+{
+    // TODO: Indexer by string?
+    // TODO: Convenient view for accessing values instead of fields?
+    // TODO: Remove or modify fields?
+    // TODO: Hoist the nested Field type to a top-level type (SpannerStructField)?
+
+    /// <summary>
+    /// A struct parameter or returned value.
+    /// </summary>
+    /// <remarks>
+    /// A Spanner struct isn't exactly the same as a Google Protobuf struct:
+    /// <list type="bullet">
+    ///   <item><description>Field names are optional</description></item>
+    ///   <item><description>Field names do not have to be unique</description></item>
+    /// </list>
+    /// </remarks>
+    public sealed class SpannerStruct : IReadOnlyList<SpannerStruct.Field>
+    {
+        private readonly IList<Field> _fields = new List<Field>();
+
+        /// <summary>
+        /// Returns the number of fields currently in the struct.
+        /// </summary>
+        public int Count => _fields.Count;
+
+        /// <summary>
+        /// Returns the field at the given index.
+        /// </summary>
+        /// <param name="index">The index of the field to return</param>
+        /// <returns>The field at the given index</returns>
+        public Field this[int index] => _fields[index];
+
+        /// <inheritdoc />
+        public IEnumerator<Field> GetEnumerator() => _fields.GetEnumerator();
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <summary>
+        /// Adds a new field to the struct with the given name, type and value.
+        /// </summary>
+        /// <param name="name">The name of the field. May be null, and does not need to be unique within the struct.
+        /// If the value is null, the <see cref="Field"/> value added will have an empty string for the name.</param>
+        /// <param name="type">The type of the field. Must not be null.</param>
+        /// <param name="value">The value of the field. May be null.</param>
+        public void Add(string name, SpannerDbType type, object value) =>
+            _fields.Add(new Field(name, type, value));
+
+        /// <summary>
+        /// Adds a new field to the struct.
+        /// </summary>
+        /// <param name="field">The field to add. Must not be null.</param>
+        public void Add(Field field) => _fields.Add(GaxPreconditions.CheckNotNull(field, nameof(field)));
+
+        /// <summary>
+        /// A field within a struct.
+        /// </summary>
+        public class Field
+        {
+            /// <summary>
+            /// Name of the field in the struct; will never be null, but may be empty.
+            /// </summary>
+            public string Name { get; }
+
+            /// <summary>
+            /// Type of the field. Never null.
+            /// </summary>
+            public SpannerDbType Type { get; }
+
+            /// <summary>
+            /// Value of the field. May be null.
+            /// </summary>
+            public object Value { get; }
+
+            /// <summary>
+            /// Constructs a struct field with the given name, type and value.
+            /// </summary>
+            /// <param name="name">Name of the field. May be null, which will be converted into an empty string.</param>
+            /// <param name="type">Type of the field. Must not be null.</param>
+            /// <param name="value">Value of the field. May be null.</param>
+            public Field(string name, SpannerDbType type, object value)
+            {
+                Name = name ?? "";
+                Type = GaxPreconditions.CheckNotNull(type, nameof(type));
+                Value = value;
+            }
+        }
+    }
+}
