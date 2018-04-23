b065e3595367bdf7b91a9135ab3f76cea1960e95
Jon Skeet <jonskeet@google.com>
2018-04-23 17:03:32 +0100

    Introduce integration tests for SpannerStruct parameters (as opposed to returned structs)

---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.IntegrationTests/StructParameterTests.cs
@@ -0,0 +1,304 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Google.Cloud.Spanner.Data.IntegrationTests
+{
+    [Collection(nameof(TestDatabaseFixture))]
+    public class StructParameterTests
+    {
+        private readonly TestDatabaseFixture _fixture;
+
+        public StructParameterTests(TestDatabaseFixture fixture) => _fixture = fixture;
+
+        [Fact]
+        public async Task SimpleFieldAccess()
+        {
+            var structParam = new SpannerStruct
+            {
+                { "threadf", SpannerDbType.Int64, 1 },
+                { "userf", SpannerDbType.String, "bob" }
+            };
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT @struct_param.userf, @p4"))
+                {
+                    cmd.Parameters.Add("struct_param", structParam.GetSpannerDbType(), structParam);
+                    cmd.Parameters.Add("p4", SpannerDbType.Int64, 10);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        Assert.True(await reader.ReadAsync());
+                        Assert.Equal("bob", reader.GetString(0));
+                        Assert.Equal(10L, reader.GetInt64(1));
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task SimpleFieldAccess_NullStructValue()
+        {
+            var structSchema = new SpannerStruct
+            {
+                { "threadf", SpannerDbType.Int64, 1 },
+                { "userf", SpannerDbType.String, "bob" }
+            }.GetSpannerDbType();
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT @struct_param.userf"))
+                {
+                    cmd.Parameters.Add("struct_param", structSchema, null);
+                    cmd.Parameters.Add("p4", SpannerDbType.Int64, 10);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        Assert.True(await reader.ReadAsync());
+                        Assert.True(reader.IsDBNull(0));
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task NestedStructFieldAccess()
+        {
+            var nestedStruct = new SpannerStruct
+            {
+                { "nestedf", SpannerDbType.String, "bob" }
+            };
+            var outerStruct = new SpannerStruct
+            {
+                { "structf", nestedStruct.GetSpannerDbType(), nestedStruct }
+            };
+
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT @struct_param.structf.nestedf"))
+                {
+                    cmd.Parameters.Add("struct_param", outerStruct.GetSpannerDbType(), outerStruct);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        Assert.True(await reader.ReadAsync());
+                        Assert.Equal("bob", reader.GetString(0));
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task NestedStructFieldAccess_NullStructValue()
+        {
+            var nestedStruct = new SpannerStruct
+            {
+                { "nestedf", SpannerDbType.String, "bob" }
+            };
+            var outerStruct = new SpannerStruct
+            {
+                { "structf", nestedStruct.GetSpannerDbType(), nestedStruct }
+            };
+
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT @struct_param.structf.nestedf"))
+                {
+                    cmd.Parameters.Add("struct_param", outerStruct.GetSpannerDbType(), null);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        Assert.True(await reader.ReadAsync());
+                        Assert.True(reader.IsDBNull(0));
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task EmptyStruct()
+        {
+            var emptyStruct = new SpannerStruct();
+
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT @struct_param IS NULL"))
+                {
+                    cmd.Parameters.Add("struct_param", emptyStruct.GetSpannerDbType(), emptyStruct);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        Assert.True(await reader.ReadAsync());
+                        Assert.False(reader.GetBoolean(0));
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task EmptyStruct_NullValue()
+        {
+            var emptyStruct = new SpannerStruct();
+
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT @struct_param IS NULL"))
+                {
+                    cmd.Parameters.Add("struct_param", emptyStruct.GetSpannerDbType(), null);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        Assert.True(await reader.ReadAsync());
+                        Assert.True(reader.GetBoolean(0));
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task NullFieldValue()
+        {
+            var structValue = new SpannerStruct
+            {
+                { "f1", SpannerDbType.Int64, null }
+            };
+
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT @struct_param.f1"))
+                {
+                    cmd.Parameters.Add("struct_param", structValue.GetSpannerDbType(), structValue);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        Assert.True(await reader.ReadAsync());
+                        Assert.True(reader.IsDBNull(0));
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task ServerStructEquality()
+        {
+            var structValue = new SpannerStruct
+            {
+                { "threadf", SpannerDbType.Int64, 1 },
+                { "userf", SpannerDbType.String, "bob" }
+            };
+
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT @struct_param = STRUCT < threadf INT64, userf STRING>(1,\"bob\")"))
+                {
+                    cmd.Parameters.Add("struct_param", structValue.GetSpannerDbType(), structValue);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        Assert.True(await reader.ReadAsync());
+                        Assert.True(reader.GetBoolean(0));
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task NullnessCheck()
+        {
+            var structValue = new SpannerStruct
+            {
+                { "threadf", SpannerDbType.Int64, 1 },
+                { "userf", SpannerDbType.String, "bob" }
+            };
+
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT @struct_arr_param IS NULL"))
+                {
+                    cmd.Parameters.Add(
+                        "struct_arr_param",
+                        SpannerDbType.ArrayOf(structValue.GetSpannerDbType()),
+                        new[] { structValue });
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        Assert.True(await reader.ReadAsync());
+                        Assert.False(reader.GetBoolean(0));
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task NullArrayOfStructField()
+        {
+            var nestedStruct = new SpannerStruct
+            {
+                { "threadid", SpannerDbType.Int64, null }
+            };
+            var structValue = new SpannerStruct
+            {
+                { "intf", SpannerDbType.Int64, 1 },
+                { "arraysf", SpannerDbType.ArrayOf(nestedStruct.GetSpannerDbType()), null }
+            };
+
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT a.threadid FROM UNNEST(@struct_param.arraysf) a"))
+                {
+                    cmd.Parameters.Add("struct_param", structValue.GetSpannerDbType(), structValue);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        // No rows
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task NullArrayOfStruct()
+        {
+            var structValue = new SpannerStruct
+            {
+                { "threadid", SpannerDbType.Int64, null }
+            };
+
+            using (var connection = await _fixture.GetTestDatabaseConnectionAsync())
+            {
+                using (var cmd = connection.CreateSelectCommand("SELECT a.threadid FROM UNNEST(@struct_arr_param) a"))
+                {
+                    cmd.Parameters.Add("struct_arr_param", SpannerDbType.ArrayOf(structValue.GetSpannerDbType()), null);
+
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        // No rows
+                        Assert.False(await reader.ReadAsync());
+                    }
+                }
+            }
+        }
+    }
+}
