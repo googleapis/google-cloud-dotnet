eb50bf0242e38dfd6eecbcdd3acc0a3242837c63
Mike Dour <evildour@users.noreply.github.com>
2018-04-24 14:49:44 -0400

    Fix our code fix providers, which were no longer working. (#2107)
    It looks like newer versions of VS are not loading code fix providers compiled against older Roslyn versions even though the associated analyzers are loaded and used.

--- tools/Google.Cloud.Tools.Analyzers/DefaultLiteralCodeFixProvider.cs
+++ tools/Google.Cloud.Tools.Analyzers/DefaultLiteralCodeFixProvider.cs
@@ -17,8 +17,6 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Editing;
-using Microsoft.CodeAnalysis.Semantics;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
