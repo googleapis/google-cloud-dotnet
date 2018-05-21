cb4ad609a0fba9003225d53df19e7376e07b956f
Jon Skeet <jonskeet@google.com>
2018-05-21 16:21:13 +0100

    Common infrastructure to be used by Spanner tests
    This will eventually be used by both the integration tests and the snippets, to remove redundancy.

---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.CommonTesting/SpannerAssert.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Xunit;
+
+namespace Google.Cloud.Spanner.Data.CommonTesting
+{
+    /// <summary>
+    /// Common assertions for Spanner tests.
+    /// </summary>
+    public static class SpannerAssert
+    {
+        /// <summary>
+        /// Asserts that the given exception was due to a timeout. Sometimes gRPC
+        /// will return an exception with a status code of Unavailable when it's really
+        /// a timeout. This appears to depend on which combination of tests is being run,
+        /// so may well be around how the underlying gRPC channels are being used. We test
+        /// in a slightly looser way than might be expected to work around this.
+        /// </summary>
+        public static void IsTimeout(SpannerException exception)
+        {
+            // TODO: Investigate the cause of this further.
+            var code = exception.ErrorCode;
+            Assert.True(code == ErrorCode.DeadlineExceeded || code == ErrorCode.Unavailable,
+                $"Expected code of DeadlineExceeded or Unavailable; was {code}");
+            Assert.False(exception.IsTransientSpannerFault());
+        }
+    }
+}
---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.CommonTesting/SpannerTableFixture.cs
@@ -0,0 +1,63 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.ClientTesting;
+using System;
+using System.Threading;
+
+namespace Google.Cloud.Spanner.Data.CommonTesting
+{
+    /// <summary>
+    /// Base classes for test fixtures for tables.
+    /// </summary>
+    public abstract class SpannerTableFixture : SpannerFixtureBase
+    {
+        public string TableName { get; }
+
+        public SpannerTableFixture(string tableName)
+        {
+            TableName = tableName;
+            if (Database.Fresh)
+            {
+                FileLogger.Log($"Creating table {TableName}");
+                CreateTable();
+            }
+            FileLogger.Log($"Populating table {TableName}");
+            PopulateTable(Database.Fresh);
+            FileLogger.Log($"Table populated");
+
+        }
+
+        /// <summary>
+        /// Creates the table. This method is only called when a new database has been created.
+        /// </summary>
+        protected abstract void CreateTable();
+
+        /// <summary>
+        /// Optional operation; populates the table, potentially doing different things based on whether
+        /// the database is new or not.
+        /// </summary>
+        protected virtual void PopulateTable(bool fresh)
+        {
+        }
+
+        protected void ExecuteDdl(string ddl)
+        {
+            using (var connection = GetConnection())
+            {
+                connection.CreateDdlCommand(ddl).ExecuteNonQuery();
+            }
+        }
+    }
+}
