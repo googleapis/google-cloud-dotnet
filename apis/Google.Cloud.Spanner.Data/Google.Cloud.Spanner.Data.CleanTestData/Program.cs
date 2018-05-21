55c0a0ccd02d9d9e8d9fa640a1c7c915a508dcb8
Jon Skeet <jonskeet@google.com>
2018-05-21 16:17:37 +0100

    Add a new console app to clean up test databases
    The changes elsewhere in tests (in coming commits) mean we can't reliably know
    when we're done with a test - and it's always possible for a machine to reboot or whatever
    without cleaning up anyway. This has the additional advantage of allowing a user to look at
    the database after the tests have run.

---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.CleanTestData/Program.cs
@@ -0,0 +1,49 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Spanner.Admin.Database.V1;
+using Google.Cloud.Spanner.Common.V1;
+using System;
+using System.Linq;
+
+namespace Google.Cloud.Spanner.Data.CleanTestData
+{
+    public class Program
+    {
+        private static int Main(string[] args)
+        {
+            if (args.Length == 0 || args.Length > 2)
+            {
+                Console.WriteLine("Arguments: <project ID> [instance ID]");
+                Console.WriteLine("A default instance ID of 'spannerintegration' will be used if not specified.");
+                return 1;
+            }
+            string projectId = args[0];
+            string instanceId = args.Length == 2 ? args[1] : "spannerintegration";
+            var client = DatabaseAdminClient.Create();
+            var instanceName = new InstanceName(projectId, instanceId);
+            var databases = client.ListDatabases(instanceName).ToList();
+            foreach (var database in databases)
+            {
+                var name = DatabaseName.Parse(database.Name);
+                if (name.DatabaseId.StartsWith("testdb"))
+                {
+                    Console.WriteLine($"Dropping {name.DatabaseId}");
+                    client.DropDatabase(name);
+                }
+            }
+            return 0;
+        }
+    }
+}
