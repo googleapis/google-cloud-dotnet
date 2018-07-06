0e41295ff000d9ed6fbe78229b8e855228de7dc1
Jon Skeet <jonskeet@google.com>
2018-07-06 08:01:05 +0100

    Create a sample listing all the global key rings for a project
    This also serves as a smoke test for the API, effectively.

---
+++ apis/Google.Cloud.Kms.V1/Google.Cloud.Kms.V1.Snippets/KeyManagementServiceClientSnippets.cs
@@ -0,0 +1,45 @@
+﻿// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.ClientTesting;
+using System;
+using Xunit;
+
+namespace Google.Cloud.Kms.V1.Snippets
+{
+    [Collection(nameof(KeyManagementServiceFixture))]
+    [SnippetOutputCollector]
+    public class KeyManagementServiceClientSnippets
+    {
+        private readonly KeyManagementServiceFixture _fixture;
+
+        public KeyManagementServiceClientSnippets(KeyManagementServiceFixture fixture) =>
+            _fixture = fixture;
+
+        [Fact]
+        public void ListGlobalKeyRings()
+        {
+            string projectId = _fixture.ProjectId;
+
+            // Sample: ListGlobalKeyRings
+            KeyManagementServiceClient client = KeyManagementServiceClient.Create();
+            LocationName globalLocation = new LocationName(projectId, "global");
+            foreach (KeyRing keyRing in client.ListKeyRings(globalLocation))
+            {
+                Console.WriteLine(keyRing.Name);
+            }
+            // End sample
+        }
+    }
+}
--- apis/Google.Cloud.Kms.V1/docs/index.md
+++ apis/Google.Cloud.Kms.V1/docs/index.md
@@ -13,3 +13,9 @@
 {{client-classes}}
 
 {{client-construction}}
+
+# Sample code
+
+This example lists all the key rings in the "global" location for a specific project.
+
+[!code-cs[](obj/snippets/Google.Cloud.Kms.V1.KeyManagementServiceClient.txt#ListGlobalKeyRings)]
