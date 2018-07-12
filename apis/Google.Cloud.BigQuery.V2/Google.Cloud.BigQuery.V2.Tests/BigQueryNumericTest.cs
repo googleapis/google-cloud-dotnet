ce20f5f03718a59457db5964a5ae3eb8b6a13386
Jon Skeet <jonskeet@google.com>
2018-07-12 11:31:31 +0100

    More tests and completed implementation of BigQueryNumeric
    Local benchmark results (mean):
    
    Parse_Huge: 5,998.24ns
    Parse_Medium: 1,196.38ns
    Parse_Integer: 828.04ns
    FromDecimal: 466.05ns
    DecimalConversion: 480.09ns
    UInt64Conversion: 113.65ns
    Int64Conversion: 113.54ns
    ToString_Huge: 577.06ns
    ToString_Medium: 241.70ns
    ToString_Integer: 235.72ns
    ToDecimal_Medium: 1,067.89ns
    ToDecimal_Integer: 959.10ns
    Addition: 124.04ns
    Subtraction: 137.71ns
    UnaryNegation: 46.13ns
    
    While more performance work would be good, I think it's good enough to merge into master already, assuming that's okay with the BigQuery team.

--- apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.Tests/BigQueryNumericTest.cs
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.Tests/BigQueryNumericTest.cs
@@ -12,14 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Cloud.ClientTesting;
 using System;
 using System.Globalization;
+using System.Linq;
 using Xunit;
 
 namespace Google.Cloud.BigQuery.V2.Tests
 {
     public class BigQueryNumericTest
     {
+        private const string MaxText = "99999999999999999999999999999.999999999";
+        private const string MinText = "-99999999999999999999999999999.999999999";
+        private const string EpsilonText = "0.000000001";
+        private const string NegativeEpsilonText = "-0.000000001";
+
         [Theory, CombinatorialData]
         public void ConversionFromDecimal(
             [CombinatorialValues(
@@ -36,11 +43,61 @@ namespace Google.Cloud.BigQuery.V2.Tests
                 text = "-" + text;
             }
             decimal parsed = decimal.Parse(text, CultureInfo.InvariantCulture);
-            BigQueryNumeric numeric = (BigQueryNumeric) parsed;
+            BigQueryNumeric numeric = (BigQueryNumeric)parsed;
             Assert.Equal(text, numeric.ToString());
         }
 
-        // TODO: Conversions from decimal that are lossy.
+        [Theory]
+        [InlineData("1.123456789123", "1.123456789")]
+        [InlineData("-1.123456789123", "-1.123456789")]
+        [InlineData("0.000000000000001", "0")]
+        [InlineData("-0.000000000000001", "0")]
+        public void ConversionFromDecimal_Lossy(string decimalText, string expectedText)
+        {
+            decimal input = decimal.Parse(decimalText, CultureInfo.InvariantCulture);
+            BigQueryNumeric expected = BigQueryNumeric.Parse(expectedText);
+
+            BigQueryNumeric conversionOutput = (BigQueryNumeric)input;
+            BigQueryNumeric fromDecimalOutput = BigQueryNumeric.FromDecimal(input, LossOfPrecisionHandling.Truncate);
+
+            Assert.Equal(expected, conversionOutput);
+            Assert.Equal(expected, fromDecimalOutput);
+            Assert.Throws<ArgumentException>(() => BigQueryNumeric.FromDecimal(input, LossOfPrecisionHandling.Throw));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(-1)]
+        [InlineData(int.MinValue)]
+        [InlineData(int.MaxValue)]
+        public void ConversionFromInt32(int input)
+        {
+            BigQueryNumeric value = input;
+            Assert.Equal(input.ToString(CultureInfo.InvariantCulture), value.ToString());
+        }
+
+        [Theory]
+        [InlineData(0L)]
+        [InlineData(1L)]
+        [InlineData(-1L)]
+        [InlineData(long.MinValue)]
+        [InlineData(long.MaxValue)]
+        public void ConversionFromInt64(long input)
+        {
+            BigQueryNumeric value = input;
+            Assert.Equal(input.ToString(CultureInfo.InvariantCulture), value.ToString());
+        }
+
+        [Theory]
+        [InlineData(0UL)]
+        [InlineData(1UL)]
+        [InlineData(ulong.MaxValue)]
+        public void ConversionFromUInt64(ulong input)
+        {
+            BigQueryNumeric value = input;
+            Assert.Equal(input.ToString(CultureInfo.InvariantCulture), value.ToString());
+        }
 
         [Theory, CombinatorialData]
         public void ToDecimal_Precise(
@@ -84,12 +141,15 @@ namespace Google.Cloud.BigQuery.V2.Tests
             if (negate)
             {
                 text = "-" + text;
-            }            
+            }
             BigQueryNumeric numeric = BigQueryNumeric.Parse(text);
             // Decimal.Parse will silently lose precision
             decimal expected = decimal.Parse(text, CultureInfo.InvariantCulture);
             decimal actual = numeric.ToDecimal(LossOfPrecisionHandling.Truncate);
             Assert.Equal(expected, actual);
+            // Conversion via the explicit conversion should do the same thing
+            decimal actual2 = (decimal) numeric;
+            Assert.Equal(expected, actual2);
         }
 
         [Theory, CombinatorialData]
@@ -113,7 +173,8 @@ namespace Google.Cloud.BigQuery.V2.Tests
             [CombinatorialValues(
                 "123456789012345678901234567.890123456",
                 "79228162514264337593543950335.000000001",
-                "99999999999999999999999999999.999999999",
+                MaxText,
+                EpsilonText,
                 "5",
                 "50",
                 "50.01",
@@ -130,6 +191,191 @@ namespace Google.Cloud.BigQuery.V2.Tests
             }
             BigQueryNumeric numeric = BigQueryNumeric.Parse(text);
             Assert.Equal(text, numeric.ToString());
+
+            // Check that TryParse works too
+            Assert.True(BigQueryNumeric.TryParse(text, out var numeric2));
+            Assert.Equal(numeric, numeric2);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("-")]
+        [InlineData("+")]
+        [InlineData("non-digits")]
+        [InlineData("   ")]
+        [InlineData(" 0")]
+        [InlineData("0 ")]
+        [InlineData(" 0 ")]
+        // Overflow
+        [InlineData("100000000000000000000000000000")]
+        [InlineData("-100000000000000000000000000000")]
+        // Different parsing path but leading to overflow
+        [InlineData("100000000000000000000000000000.0")]
+        [InlineData("-100000000000000000000000000000.0")]
+        // Things we may wish to make valid later
+        [InlineData("+5")]
+        [InlineData(".5")]
+        [InlineData("-.5")]
+        [InlineData("+.5")]
+        [InlineData("1e6")]
+        [InlineData("1e-6")]
+        public void TryParse_Invalid(string text)
+        {
+            Assert.False(BigQueryNumeric.TryParse(text, out var value));
+            Assert.Equal(BigQueryNumeric.Zero, value);
+        }
+
+        [Fact]
+        public void ZeroIsDefaultValue()
+        {
+            Assert.Equal("0", default(BigQueryNumeric).ToString());
+        }
+
+        [Fact]
+        public void Constants()
+        {
+            Assert.Equal(MaxText, BigQueryNumeric.MaxValue.ToString());
+            Assert.Equal(MinText, BigQueryNumeric.MinValue.ToString());
+            Assert.Equal(EpsilonText, BigQueryNumeric.Epsilon.ToString());
+            Assert.Equal("0", BigQueryNumeric.Zero.ToString());
+        }
+
+        [Theory]
+        [InlineData(MaxText, new[] { MaxText }, new[] { "99999999999999999999999999999.999999998", "99999999999999999999999999999.999999998" })]
+        [InlineData("1", new[] { "1.0", "1.00", "01" }, new[] { "-1", "0.999999999", "1.000000001" })]
+        [InlineData("0", new[] { "0.0", "-0", "-0.0" }, new[] { "0.000000001", "-0.000000001" })]
+        public void Equality(string controlText, string[] equalText, string[] unequalText)
+        {
+            BigQueryNumeric control = BigQueryNumeric.Parse(controlText);
+            BigQueryNumeric[] equal = equalText.Select(BigQueryNumeric.Parse).ToArray();
+            BigQueryNumeric[] unequal = unequalText.Select(BigQueryNumeric.Parse).ToArray();
+            EqualityTester.AssertEqual(control, equal, unequal);
+            EqualityTester.AssertEqualityOperators(control, equal, unequal);
+        }
+
+        [Theory]
+        [InlineData("0", EpsilonText)]
+        [InlineData(NegativeEpsilonText, "0")]
+        [InlineData("-5", "2")]
+        [InlineData("1.0", "1.1")]
+        [InlineData("99999999999999999999999999999.99999998", MaxText)]
+        [InlineData(MinText, "-99999999999999999999999999999.99999998")]
+        public void CompareTo_Unequal(string smallerText, string biggerText)
+        {
+            var smaller = BigQueryNumeric.Parse(smallerText);
+            var bigger = BigQueryNumeric.Parse(biggerText);
+            Assert.InRange(smaller.CompareTo(bigger), int.MinValue, -1);
+            Assert.InRange(bigger.CompareTo(smaller), 1, int.MaxValue);
+            Assert.InRange(((IComparable)smaller).CompareTo(bigger), int.MinValue, -1);
+            Assert.InRange(((IComparable)bigger).CompareTo(smaller), 1, int.MaxValue);
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("1")]
+        [InlineData("-1")]
+        [InlineData(MaxText)]
+        [InlineData(MinText)]
+        [InlineData(EpsilonText)]
+        public void CompareTo_Equal(string text)
+        {
+            var value = BigQueryNumeric.Parse(text);
+            Assert.Equal(0, value.CompareTo(value));
+            Assert.Equal(0, (IComparable) value.CompareTo(value));
+        }
+
+        [Fact]
+        public void CompareTo_NegativeZero()
+        {
+            // These really are the same value... we don't differentiate.
+            var regularZero = BigQueryNumeric.Zero;
+            var negativeZero = BigQueryNumeric.Parse("-0");
+            Assert.Equal(0, regularZero.CompareTo(negativeZero));
+            Assert.Equal(0, (IComparable) regularZero.CompareTo(negativeZero));
+            Assert.Equal(0, negativeZero.CompareTo(regularZero));
+            Assert.Equal(0, (IComparable) negativeZero.CompareTo(regularZero));
+        }
+
+        [Fact]
+        public void CompareTo_Null()
+        {
+            IComparable minValue = BigQueryNumeric.MinValue;
+            Assert.InRange(minValue.CompareTo(null), 1, int.MaxValue);
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("1")]
+        [InlineData("1.1")]
+        [InlineData(MaxText)]
+        public void UnaryNegation(string text)
+        {
+            var positive = BigQueryNumeric.Parse(text);
+            var negative = BigQueryNumeric.Parse("-" + text);
+            Assert.Equal(negative, -positive);
+            Assert.Equal(positive, -negative);
+        }
+
+        [Theory]
+        [InlineData("0")]
+        [InlineData("1")]
+        [InlineData("1.1")]
+        [InlineData("-1.1")]
+        [InlineData(MaxText)]
+        public void UnaryPlus(string text)
+        {
+            var value = BigQueryNumeric.Parse(text);
+            Assert.Equal(value, +value);
+        }
+
+        [Theory]
+        [InlineData("2", "3", "5")]
+        [InlineData("1", "0.1", "1.1")]
+        [InlineData("1", "-0.1", "0.9")]
+        [InlineData("-1", "0.1", "-0.9")]
+        public void Addition_Valid(string leftText, string rightText, string expectedText)
+        {
+            var left = BigQueryNumeric.Parse(leftText);
+            var right = BigQueryNumeric.Parse(rightText);
+            var expected = BigQueryNumeric.Parse(expectedText);
+            Assert.Equal(expected, left + right);
+        }
+
+        [Theory]
+        [InlineData("2", "3", "-1")]
+        [InlineData("1", "0.1", "0.9")]
+        [InlineData("1", "-0.1", "1.1")]
+        [InlineData("-1", "0.1", "-1.1")]
+        public void Subtraction_Valid(string leftText, string rightText, string expectedText)
+        {
+            var left = BigQueryNumeric.Parse(leftText);
+            var right = BigQueryNumeric.Parse(rightText);
+            var expected = BigQueryNumeric.Parse(expectedText);
+            Assert.Equal(expected, left - right);
+        }
+
+        [Theory]
+        [InlineData("50000000000000000000000000000", "50000000000000000000000000000")]
+        [InlineData("-50000000000000000000000000000", "-50000000000000000000000000000")]
+        [InlineData(MaxText, EpsilonText)]
+        [InlineData(MinText, NegativeEpsilonText)]
+        public void Addition_Overflow(string leftText, string rightText)
+        {
+            var left = BigQueryNumeric.Parse(leftText);
+            var right = BigQueryNumeric.Parse(rightText);
+            Assert.Throws<OverflowException>(() => left + right);
+        }
+
+        [Theory]
+        [InlineData("50000000000000000000000000000", "-50000000000000000000000000000")]
+        [InlineData("-50000000000000000000000000000", "50000000000000000000000000000")]
+        [InlineData(MaxText, NegativeEpsilonText)]
+        [InlineData(MinText, EpsilonText)]
+        public void Subtraction_Overflow(string leftText, string rightText)
+        {
+            var left = BigQueryNumeric.Parse(leftText);
+            var right = BigQueryNumeric.Parse(rightText);
+            Assert.Throws<OverflowException>(() => left - right);
         }
     }
 }
