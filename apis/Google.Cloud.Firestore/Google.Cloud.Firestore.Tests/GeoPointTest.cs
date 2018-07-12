606914f67ceccfcc5880f387790e179ef840a7fa
Jon Skeet <jonskeet@google.com>
2018-07-12 09:22:58 +0100

    Move EqualityTester to common testing project
    This requires changing the project target from netstandard1.3 to netstandard1.6 (for TypeInfo.GetMethod).
    That should be fine, as this is only used by test projects, all of which target netcoreapp rather than netstandard.

--- apis/Google.Cloud.Firestore/Google.Cloud.Firestore.Tests/GeoPointTest.cs
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore.Tests/GeoPointTest.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Cloud.ClientTesting;
 using Google.Type;
 using System;
 using System.Globalization;
