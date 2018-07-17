cb0c50dd4067863cfa0708c702c578e8ab34eee3
Jon Skeet <jonskeet@google.com>
2018-07-17 08:29:09 +0100

    Add manual code to Speech v1p1beta1
    This is mostly copied from Speech.V1 with the namespaces changed.

---
+++ apis/Google.Cloud.Speech.V1P1Beta1/Google.Cloud.Speech.V1P1Beta1/RecognitionAudioPartial.cs
@@ -0,0 +1,177 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using Google.Protobuf;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Google.Cloud.Speech.V1P1Beta1
+{
+    public partial class RecognitionAudio
+    {
+        private static readonly HttpClient s_defaultHttpClient = new HttpClient();
+
+        /// <summary>
+        /// Constructs a <see cref="RecognitionAudio"/> with a <see cref="Uri"/> property referring to a Google Cloud
+        /// Storage URI.
+        /// </summary>
+        /// <param name="storageUri">A Google Cloud Storage URI, of the form <c>gs://bucket-name/object-name</c>. Must not be null.</param>
+        /// <returns>The newly created RecognitionAudio.</returns>
+        public static RecognitionAudio FromStorageUri(string storageUri)
+        {
+            GaxPreconditions.CheckNotNull(storageUri, nameof(storageUri));
+            // TODO: Validate that it looks like gs://bucket/object
+            return new RecognitionAudio { Uri = storageUri };
+        }
+
+        /// <summary>
+        /// Asynchronously constructs a <see cref="RecognitionAudio"/> by downloading data from the given URI.
+        /// </summary>
+        /// <param name="uri">The URI to fetch. Must not be null.</param>
+        /// <param name="httpClient">The <see cref="HttpClient"/> to use to fetch the image, or
+        /// <c>null</c> to use a default client.</param>
+        /// <returns>A task representing the asynchronous operation. The result will be the newly created RecognitionAudio.</returns>
+        public async static Task<RecognitionAudio> FetchFromUriAsync(Uri uri, HttpClient httpClient = null)
+        {
+            GaxPreconditions.CheckNotNull(uri, nameof(uri));
+            httpClient = httpClient ?? s_defaultHttpClient;
+            var bytes = await httpClient.GetByteArrayAsync(uri).ConfigureAwait(false);
+            return FromBytes(bytes);
+        }
+
+        /// <summary>
+        /// Asynchronously constructs a <see cref="RecognitionAudio"/> by downloading data from the given URI.
+        /// </summary>
+        /// <param name="uri">The URI to fetch. Must not be null.</param>
+        /// <param name="httpClient">The <see cref="HttpClient"/> to use to fetch the image, or
+        /// <c>null</c> to use a default client.</param>
+        /// <returns>A task representing the asynchronous operation. The result will be the newly created RecognitionAudio.</returns>
+        public async static Task<RecognitionAudio> FetchFromUriAsync(string uri, HttpClient httpClient = null)
+        {
+            GaxPreconditions.CheckNotNull(uri, nameof(uri));
+            httpClient = httpClient ?? s_defaultHttpClient;
+            var bytes = await httpClient.GetByteArrayAsync(uri).ConfigureAwait(false);
+            return FromBytes(bytes);
+        }
+
+        // TODO: Find better synchronous HTTP fetching?
+
+        /// <summary>
+        /// Constructs a <see cref="RecognitionAudio"/> by downloading data from the given URI.
+        /// </summary>
+        /// <param name="uri">The URI to fetch. Must not be null.</param>
+        /// <param name="httpClient">The <see cref="HttpClient"/> to use to fetch the image, or
+        /// <c>null</c> to use a default client.</param>
+        /// <returns>The newly created RecognitionAudio.</returns>
+        public static RecognitionAudio FetchFromUri(string uri, HttpClient httpClient = null)
+        {
+            GaxPreconditions.CheckNotNull(uri, nameof(uri));
+            return Task.Run(() => FetchFromUriAsync(uri, httpClient)).ResultWithUnwrappedExceptions();
+        }
+
+        /// <summary>
+        /// Constructs a <see cref="RecognitionAudio"/> by downloading data from the given URI.
+        /// </summary>
+        /// <param name="uri">The URI to fetch. Must not be null.</param>
+        /// <param name="httpClient">The <see cref="HttpClient"/> to use to fetch the image, or
+        /// <c>null</c> to use a default client.</param>
+        /// <returns>The newly created RecognitionAudio.</returns>
+        public static RecognitionAudio FetchFromUri(Uri uri, HttpClient httpClient = null)
+        {
+            GaxPreconditions.CheckNotNull(uri, nameof(uri));
+            return Task.Run(() => FetchFromUriAsync(uri, httpClient)).ResultWithUnwrappedExceptions();
+        }
+
+        /// <summary>
+        /// Constructs a <see cref="RecognitionAudio"/> by loading data from the given file path.
+        /// </summary>
+        /// <param name="path">The file path to load RecognitionAudio data from. Must not be null.</param>
+        /// <returns>The newly created RecognitionAudio.</returns>
+        public static RecognitionAudio FromFile(string path)
+        {
+            GaxPreconditions.CheckNotNull(path, nameof(path));
+            return FromBytes(File.ReadAllBytes(path));
+        }
+
+        /// <summary>
+        /// Asynchronously constructs a <see cref="RecognitionAudio"/> by loading data from the given file path.
+        /// </summary>
+        /// <param name="path">The file path to load RecognitionAudio data from. Must not be null.</param>
+        /// <returns>The newly created RecognitionAudio.</returns>
+        public static Task<RecognitionAudio> FromFileAsync(string path) =>
+            // We don't want any file system access to occur synchronously, including opening.
+            // The ConfigureAwait(false) inside is unnecessary but harmless.
+            Task.Run(async () =>
+            {
+                GaxPreconditions.CheckNotNull(path, nameof(path));
+                using (var input = File.OpenRead(path))
+                {
+                    return await FromStreamAsync(input).ConfigureAwait(false);
+                }
+            });
+
+        /// <summary>
+        /// Constructs a <see cref="RecognitionAudio"/> by loading data from the given stream.
+        /// </summary>
+        /// <param name="stream">The stream to load RecognitionAudio data from. Must not be null.</param>
+        /// <returns>The newly created RecognitionAudio.</returns>
+        public static RecognitionAudio FromStream(Stream stream)
+        {
+            GaxPreconditions.CheckNotNull(stream, nameof(stream));
+            return new RecognitionAudio { Content = ByteString.FromStream(stream) };
+        }
+
+        /// <summary>
+        /// Asynchronously constructs a <see cref="RecognitionAudio"/> by loading data from the given stream.
+        /// </summary>
+        /// <param name="stream">The stream to load RecognitionAudio data from. Must not be null.</param>
+        /// <returns>The newly created RecognitionAudio.</returns>
+        public static async Task<RecognitionAudio> FromStreamAsync(Stream stream)
+        {
+            GaxPreconditions.CheckNotNull(stream, nameof(stream));
+            return new RecognitionAudio { Content = await ByteString.FromStreamAsync(stream).ConfigureAwait(false) };
+        }
+
+        /// <summary>
+        /// Constructs a <see cref="RecognitionAudio"/> from the given byte array.
+        /// </summary>
+        /// <remarks>This method copies the data from the byte array; modifications to <paramref name="bytes"/>
+        /// after this method returns will not be reflected in the RecognitionAudio.</remarks>
+        /// <param name="bytes">The bytes representing the raw RecognitionAudio data.</param>
+        /// <returns>The newly created RecognitionAudio.</returns>
+        public static RecognitionAudio FromBytes(byte[] bytes)
+        {
+            GaxPreconditions.CheckNotNull(bytes, nameof(bytes));
+            return new RecognitionAudio { Content = ByteString.CopyFrom(bytes) };
+        }
+
+        /// <summary>
+        /// Constructs a <see cref="RecognitionAudio"/> from a section of the given byte array.
+        /// </summary>
+        /// <remarks>This method copies the data from the byte array; modifications to <paramref name="bytes"/>
+        /// after this method returns will not be reflected in the RecognitionAudio.</remarks>
+        /// <param name="bytes">The bytes representing the raw RecognitionAudio data.</param>
+        /// <param name="offset">The offset into the byte array of the start of the data to include in the RecognitionAudio.</param>
+        /// <param name="count">The number of bytes to include in the RecognitionAudio.</param>
+        /// <returns>The newly created RecognitionAudio.</returns>
+        public static RecognitionAudio FromBytes(byte[] bytes, int offset, int count)
+        {
+            GaxPreconditions.CheckNotNull(bytes, nameof(bytes));
+            return new RecognitionAudio { Content = ByteString.CopyFrom(bytes, offset, count) };
+        }
+    }
+}
--- apis/Google.Cloud.Speech.V1P1Beta1/docs/index.md
+++ apis/Google.Cloud.Speech.V1P1Beta1/docs/index.md
@@ -10,6 +10,43 @@
 
 # Getting started
 
-{{client-classes}}
+The simplest option is to use the synchronous, one-shot API as shown
+below in the sample code. More complex scenarios are considered further down this page.
 
-{{client-construction}}
+Note that the audio data should be mono rather than stereo, and the
+format needs to be explicitly specified in the request.
+
+# Sample code
+
+## Constructing a RecognitionAudio object
+
+There are various factory methods on the
+[RecognitionAudio](obj/api/Google.Cloud.Speech.V1P1Beta1.RecognitionAudio.yml) class to allow
+instances to be constructed from files, streams, byte arrays and URIs.
+
+[!code-cs[](obj/snippets/Google.Cloud.Speech.V1P1Beta1.RecognitionAudio.txt#FactoryMethods)]
+
+## Detect speech in a single file
+
+[!code-cs[](obj/snippets/Google.Cloud.Speech.V1P1Beta1.SpeechClient.txt#Recognize)]
+
+# Immediate, long-running and streaming operations
+
+The underlying RPC API contains three modes of operation.
+
+The simplest is via the Recognize method. You make a single
+request, and get a single response with the result of the analysis.
+
+The LongRunningRecognize method still requires all of the audio data to be
+passed in a single request, but the response from the RPC is a
+Google.LongRunning.Operation, representing an operation which could
+take some time to complete. It contains a token which can be used to
+retrieve the results later - you can think of it as a more
+persistent and remote `Task<T>` to a first approximation.
+
+Finally, the RPC API supports StreamingRecognize, which is a
+bidirectional streaming API: the client makes a number of requests,
+and the server emits a number of responses. This enables a
+conversation to be transcribed in near real time, for example,
+without the client needing to split it into chunks for single
+operations.
