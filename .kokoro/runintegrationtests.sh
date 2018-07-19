96e2a9ad61ca204e1147dba6ac7d23f90cade7c1
Ian Talarico <talarico@rodete-desktop-imager.corp.google.com>
2018-07-19 15:39:50 -0700

    Change how to 'cd' into the proper directory for scripts

--- .kokoro/runintegrationtests.sh
+++ .kokoro/runintegrationtests.sh
@@ -2,15 +2,19 @@
 
 set -e
 
+SCRIPT=$(readlink -f "$0")
+ROOT_DIR=$(dirname "$SCRIPT")
+
+cd $ROOT_DIR
+cd ..
+
 export GOOGLE_APPLICATION_CREDENTIALS="$KOKORO_KEYSTORE_DIR/73609_cloud-sharp-jenkins-compute-service-account"
 export REQUESTER_PAYS_CREDENTIALS="$KOKORO_KEYSTORE_DIR/73609_gcloud-devel-service-account"
-
-cd ..
 
 # Build the libraries and run unit tests.
 ./build.sh
 
 # Allow each integration test 3 chances to pass.
-./runintegrationtests.sh || true
-./runintegrationtests.sh --retry || true
-./runintegrationtests.sh --retry
+#./runintegrationtests.sh || true
+#./runintegrationtests.sh --retry || true
+#./runintegrationtests.sh --retry
