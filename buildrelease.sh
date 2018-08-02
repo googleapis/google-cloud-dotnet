a2091f3600b3dccdb7b9810e0ab9ea1f1cadbc71
GOOGLE\talarico <talarico@google.com>
2018-08-02 08:44:28 -0700

    Allow retries of integraion tests during a release.
    Integration tests can be flakey and we should allow retries.

--- buildrelease.sh
+++ buildrelease.sh
@@ -65,9 +65,13 @@ fi
 
 ./build.sh $projects
 
+# Retry integration tests up to 3 times as they can sometimes
+# be a little flakey.
 if [[ "$run_tests" = true ]]
 then
-  ./runintegrationtests.sh $projects
+  ./runintegrationtests.sh $projects || true
+  ./runintegrationtests.sh --retry || true
+  ./runintegrationtests.sh --retry
 fi
 
 if [[ "$rebuild_docs" = false ]]
