41d4ff62926880c7750f8973a6139564c026ccf6
Jon Skeet <jonskeet@google.com>
2018-10-09 14:47:17 +0100

    Use the dotCover dotnet CLI tool for coverage
    This commit is entirely manual; the next one is purely generated
    
    Fixes #2569.

--- tools/Google.Cloud.Tools.ProjectGenerator/Program.cs
+++ tools/Google.Cloud.Tools.ProjectGenerator/Program.cs
@@ -461,6 +461,10 @@ namespace Google.Cloud.Tools.ProjectGenerator
                 );
             string project = Path.GetFileName(directory);
             var dependenciesElement = CreateDependenciesElement(project, dependencies, api.IsReleaseVersion, testProject: true, apiNames: apiNames);
+            // Include dotCover CLI tool for dotnet
+            dependenciesElement.Add(new XElement("DotNetCliToolReference",
+                new XAttribute("Include", "JetBrains.dotCover.CommandLineTools"),
+                new XAttribute("Version", "2018.2.0")));
             // Allow test projects to use dynamic...
             dependenciesElement.Add(new XElement("Reference",
                 new XAttribute("Condition", "'$(TargetFramework)' == 'net452'"),
@@ -477,9 +481,6 @@ namespace Google.Cloud.Tools.ProjectGenerator
             {
                 return;
             }
-            var targetExecutable = new XElement("TargetExecutable", "/Program Files/dotnet/dotnet.exe");
-            var targetArguments = new XElement("TargetArguments",
-                $"test --no-build -c Release");
             var filters = new XElement("Filters", new XElement("IncludeFilters",
                 new XElement("FilterEntry", new XElement("ModuleMask", api.Id)),
                 // Allow tests to contribute coverage to project dependencies, but not package dependencies
@@ -494,14 +495,10 @@ namespace Google.Cloud.Tools.ProjectGenerator
                 new XElement("AttributeFilterEntry", "System.Diagnostics.DebuggerNonUserCodeAttribute")
             );
             var output = new XElement("Output", $"../../../coverage/{Path.GetFileName(directory)}.dvcr");
-            var workingDir = new XElement("TargetWorkingDir", ".");
 
             var doc = new XElement("CoverageParams",
-                targetExecutable,
-                targetArguments,
                 filters,
                 attributeFilters,
-                workingDir,
                 output
             );
 
