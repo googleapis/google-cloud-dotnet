ba4f0b66a28f9d726410fcee19fb907b0c1157ae
Jon Skeet <jonskeet@google.com>
2018-10-11 17:09:58 +0100

    Proportional implementation of RetrySettings.IJitter
    The RandomJitter is *uniformly* random across the whole range; this has a little more nuance

---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/V1/ProportionalRandomJitterTests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Google.Cloud.Spanner.V1.Tests
+{
+    public class ProportionalRandomJitterTests
+    {
+        [Fact]
+        public void ValuesWithinRange()
+        {
+            // We might as well use the example from the docs...
+            double randomProportion = 0.1;
+            TimeSpan delay = TimeSpan.FromMinutes(60);
+
+            var jitter = new ProportionalRandomJitter(randomProportion);
+
+            var delays = Enumerable.Range(0, 10000)
+                .Select(_ => jitter.GetDelay(delay))
+                .ToList();
+            var min = delays.Min();
+            var max = delays.Max();
+
+            // We don't mind if rounding reduces the lowest value to just under 54 minutes
+            // With 10,000 iterations there should be at least one value of about 54 minutes
+            Assert.InRange(min, TimeSpan.FromMinutes(53.9), TimeSpan.FromMinutes(54.9));
+
+            // With 10,000 iterations there should be at least one value of about 59 minutes
+            // We should never go above 60 minutes.
+            Assert.InRange(max, TimeSpan.FromMinutes(59), TimeSpan.FromMinutes(60));
+        }
+    }
+}
---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/V1/README.md
@@ -0,0 +1,12 @@
+﻿This directory contains unit tests for classes in Google.Cloud.Spanner.V1.
+Tests should be in the Google.Cloud.Spanner.V1.Tests namespace.
+
+While it's slightly odd that they're here rather than under Google.Cloud.Spanner.V1,
+it makes things significantly simpler:
+
+- It means we can have all our common test infrastructure in Google.Cloud.Spanner.Data
+- It means fewer test projects to keep track of
+- It means we can work in the Google.Cloud.Spanner.Data solution but still modify the tests appropriately
+
+Realistically, we do most development in the Google.Cloud.Spanner.Data solution, so this
+is a pragmatic choice. It doesn't affect the production code at all.
