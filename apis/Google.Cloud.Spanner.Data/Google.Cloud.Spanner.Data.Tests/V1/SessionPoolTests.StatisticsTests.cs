76a006c757513ad8c8fc309d37e6e83003a02e28
Jon Skeet <jonskeet@google.com>
2018-10-17 16:55:35 +0100

    Statistics for session pools
    This should all be trivial
    The SessionPoolTest.cs is just an empty partial class for the moment, to allow grouping. It wil be populated eventually :)

---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/V1/SessionPoolTests.StatisticsTests.cs
@@ -0,0 +1,73 @@
+﻿// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Spanner.Common.V1;
+using Xunit;
+using static Google.Cloud.Spanner.V1.PoolRewrite.SessionPool;
+
+namespace Google.Cloud.Spanner.V1.PoolRewrite.Tests
+{
+    public partial class SessionPoolTests
+    {
+        public sealed class StatisticsTests
+        {
+            [Fact]
+            public void AllProperties()
+            {
+                var db1Stats = new DatabaseStatistics(
+                    databaseName: new DatabaseName("project", "instance", "db1"),
+                    activeSessionCount: 4,
+                    readPoolCount: 5,
+                    readWritePoolCount: 10,
+                    inFlightCreationCount: 3,
+                    pendingAcquisitionCount: 0,
+                    readWriteTransactionRequests: 100L,
+                    readWriteTransactionRequestsPrewarmed: 95L,
+                    healthy: true,
+                    shutdown: false);
+                var db2Stats = new DatabaseStatistics(
+                    databaseName: new DatabaseName("project", "instance", "db2"),
+                    activeSessionCount: 8,
+                    readPoolCount: 7,
+                    readWritePoolCount: 3,
+                    inFlightCreationCount: 2,
+                    pendingAcquisitionCount: 0,
+                    readWriteTransactionRequests: 200L,
+                    readWriteTransactionRequestsPrewarmed: 195L,
+                    healthy: true,
+                    shutdown: false);
+                var db3Stats = new DatabaseStatistics(
+                    databaseName: new DatabaseName("project", "instance", "db3"),
+                    activeSessionCount: 3,
+                    readPoolCount: 0,
+                    readWritePoolCount: 0,
+                    inFlightCreationCount: 16,
+                    pendingAcquisitionCount: 3,
+                    readWriteTransactionRequests: 500L,
+                    readWriteTransactionRequestsPrewarmed: 500L,
+                    healthy: true,
+                    shutdown: false);
+                var allStats = new[] { db1Stats, db2Stats, db3Stats };
+                var statistics = new Statistics(allStats);
+
+                Assert.Equal(allStats, statistics.PerDatabaseStatistics);
+                Assert.Equal(15, statistics.TotalActiveSessionCount);
+                Assert.Equal(12, statistics.TotalReadPoolCount);
+                Assert.Equal(13, statistics.TotalReadWritePoolCount);
+                Assert.Equal(21, statistics.TotalInFlightCreationCount);
+                Assert.Equal(3, statistics.TotalPendingAcquisitionCount);
+            }
+        }
+    }
+}
---
+++ apis/Google.Cloud.Spanner.V1/Google.Cloud.Spanner.V1/PoolRewrite/SessionPool.DatabaseStatistics.cs
@@ -0,0 +1,116 @@
+﻿// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+using Google.Cloud.Spanner.Common.V1;
+
+namespace Google.Cloud.Spanner.V1.PoolRewrite
+{
+    public partial class SessionPool
+    {
+        /// <summary>
+        /// A snapshot of statistics for one database within a <see cref="SessionPool"/>.
+        /// </summary>
+        public sealed class DatabaseStatistics
+        {
+            /// <summary>
+            /// The database this set of statistics corresponds to.
+            /// </summary>
+            public DatabaseName DatabaseName { get; }
+
+            /// <summary>
+            /// The number of read-only sessions in the pool.
+            /// </summary>
+            public int ReadPoolCount { get; }
+
+            /// <summary>
+            /// The number of read/write sessions in the pool.
+            /// </summary>
+            public int ReadWritePoolCount { get; }
+
+            /// <summary>
+            /// The number of active sessions.
+            /// </summary>
+            public int ActiveSessionCount { get; }
+
+            /// <summary>
+            /// The number of session creation (or refresh, or transaction creation) requests in flight.
+            /// </summary>
+            public int InFlightCreationCount { get; }
+
+            /// <summary>
+            /// The number of client calls awaiting sessions.
+            /// </summary>
+            public int PendingAcquisitionCount { get; }
+
+            /// <summary>
+            /// The number of times a read/write transaction was requested.
+            /// </summary>
+            public long ReadWriteTransactionRequests { get; }
+
+            /// <summary>
+            /// The number of times a read/write transaction was satisfied with a pre-warmed transaction.
+            /// </summary>
+            public long ReadWriteTransactionRequestsPrewarmed { get; }
+
+            /// <summary>
+            /// Whether the pool is healthy or not.
+            /// </summary>
+            public bool Healthy { get; }
+
+            /// <summary>
+            /// Whether the pool has been shut down or not. (This indicates the start
+            /// of shutdown, not necessarily the end.)
+            /// </summary>
+            public bool Shutdown { get; }
+
+            /// <summary>
+            /// Constructs a new instance.
+            /// </summary>
+            internal DatabaseStatistics(
+                DatabaseName databaseName,
+                int activeSessionCount,
+                int readPoolCount,
+                int readWritePoolCount,
+                int inFlightCreationCount,
+                int pendingAcquisitionCount,
+                long readWriteTransactionRequests,
+                long readWriteTransactionRequestsPrewarmed,
+                bool healthy,
+                bool shutdown)
+            {
+                DatabaseName = databaseName;
+                ActiveSessionCount = activeSessionCount;
+                ReadPoolCount = readPoolCount;
+                ReadWritePoolCount = readWritePoolCount;
+                InFlightCreationCount = inFlightCreationCount;
+                PendingAcquisitionCount = pendingAcquisitionCount;
+                ReadWriteTransactionRequests = readWriteTransactionRequests;
+                ReadWriteTransactionRequestsPrewarmed = readWriteTransactionRequestsPrewarmed;
+                Healthy = healthy;
+                Shutdown = shutdown;
+            }
+
+            /// <inheritdoc />
+            public override string ToString()
+            {
+                string status = Shutdown ? "Shutdown"
+                    : Healthy ? "Healthy"
+                    : "Unhealthy";
+                // TODO: Use multiple lines? There are definite pros and cons both ways.
+                return $"Database: {DatabaseName}; Active: {ActiveSessionCount}; Read pool: {ReadPoolCount}; Write pool: {ReadWritePoolCount}; In-flight creation: {InFlightCreationCount}; Pending acquisitions: {PendingAcquisitionCount}; Status: {status}; Read/write prewarming: {ReadWriteTransactionRequestsPrewarmed}/{ReadWriteTransactionRequests}";
+            }
+        }
+    }
+}
