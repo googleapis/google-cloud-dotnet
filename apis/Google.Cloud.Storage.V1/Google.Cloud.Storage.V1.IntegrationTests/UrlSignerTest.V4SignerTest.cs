f24bb248eac19a03f8283c3f4f8be5a4f1b58255
Jon Skeet <jonskeet@google.com>
2018-11-22 17:40:15 +0000

    First pass at V4 signing implementation
    This is definitely not ready for merging into master:
    
    - We ignore the headers entirely
    - The "get bucket" test fails
    - We're currently defaulting the expiry to an hour, and almost certainly shouldn't (we should probably reject it)
    
    However, it's a good starting point.

---
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.IntegrationTests/UrlSignerTest.V4SignerTest.cs
@@ -0,0 +1,108 @@
+// Copyright 2018 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.ClientTesting;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Google.Cloud.Storage.V1.IntegrationTests
+{
+    public partial class UrlSignerTest
+    {
+        [Collection(nameof(StorageFixture))]
+        public class V4SignerTest
+        {
+            private readonly StorageFixture _fixture;
+
+            private UrlSigner Signer => _fixture.UrlSigner.WithSigningVersion(SigningVersion.V4);
+
+            public V4SignerTest(StorageFixture fixture)
+            {
+                _fixture = fixture;
+                _fixture.RegisterDelayTests(this);
+            }
+
+            private static string GetTestName([CallerMemberName] string methodName = null) => $"{nameof(V4SignerTest)}.{methodName}";
+
+            [Fact]
+            public async Task DeleteTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void DeleteTest_InitDelayTest() => DeleteTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task GetTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void GetTest_InitDelayTest() => GetTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task GetBucketTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void GetBucketTest_InitDelayTest() => GetBucketTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task GetObjectWithSpacesTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void GetObjectWithSpacesTest_InitDelayTest() => GetObjectWithSpacesTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task GetWithCustomerSuppliedEncryptionKeysTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void GetWithCustomerSuppliedEncryptionKeysTest_InitDelayTest() => GetWithCustomerSuppliedEncryptionKeysTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task GetNoExpirationTest()
+            {
+                var url = Signer.Sign(_fixture.ReadBucket, _fixture.SmallObject, expiration: null);
+
+                // Verify that the URL works.
+                var response = await _fixture.HttpClient.GetAsync(url);
+                var result = await response.Content.ReadAsByteArrayAsync();
+                AssertContentEqual(_fixture.SmallContent, result);
+            }
+
+            [Fact]
+            public async Task GetWithCustomHeadersTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void GetWithCustomHeadersTest_InitDelayTest() => GetWithCustomHeadersTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task HeadTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void HeadTest_InitDelayTest() => HeadTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task HeadWithGetMethodSignedURLTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void HeadWithGetMethodSignedURLTest_InitDelayTest() => HeadWithGetMethodSignedURLTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task PutTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void PutTest_InitDelayTest() => PutTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task PutWithCustomerSuppliedEncryptionKeysTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void PutWithCustomerSuppliedEncryptionKeysTest_InitDelayTest() => PutWithCustomerSuppliedEncryptionKeysTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task PutWithCustomHeadersTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void PutWithCustomHeadersTest_InitDelayTest() => PutWithCustomHeadersTest_Common(_fixture, Signer);
+
+
+            [Fact]
+            public async Task ResumableUploadTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void ResumableUploadTest_InitDelayTest() => ResumableUploadTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task ResumableUploadResumeTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void ResumableUploadResumeTest_InitDelayTest() => ResumableUploadResumeTest_Common(_fixture, Signer);
+
+            [Fact]
+            public async Task ResumableUploadWithCustomerSuppliedEncryptionKeysTest() => await _fixture.FinishDelayTest(GetTestName());
+            private void ResumableUploadWithCustomerSuppliedEncryptionKeysTest_InitDelayTest() => ResumableUploadWithCustomerSuppliedEncryptionKeysTest_Common(_fixture, Signer);
+        }
+    }
+}
--- apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1/UrlSigner.V4Signer.cs
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1/UrlSigner.V4Signer.cs
@@ -12,9 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Api.Gax;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -24,6 +30,9 @@ namespace Google.Cloud.Storage.V1
     {
         private sealed class V4Signer : ISigner
         {
+            // Note: It's irritating to have to convert from base64 to bytes and then to hex, but we can't change the IBlobSigner implementation
+            // and ServiceAccountCredential.CreateSignature returns base64 anyway.
+
             public string Sign(
                 string bucket,
                 string objectName,
@@ -34,10 +43,14 @@ namespace Google.Cloud.Storage.V1
                 IBlobSigner blobSigner,
                 IClock clock)
             {
-                throw new NotImplementedException();
+                var state = new SigningState(bucket, objectName, expiration, requestMethod, requestHeaders, contentHeaders, blobSigner, clock);
+                var base64Signature = blobSigner.CreateSignature(state.blobToSign);
+                var rawSignature = Convert.FromBase64String(base64Signature);
+                var hexSignature = FormatHex(rawSignature);
+                return state.GetResult(hexSignature);
             }
 
-            public Task<string> SignAsync(
+            public async Task<string> SignAsync(
                 string bucket,
                 string objectName,
                 DateTimeOffset? expiration,
@@ -48,7 +61,118 @@ namespace Google.Cloud.Storage.V1
                 IClock clock,
                 CancellationToken cancellationToken)
             {
-                throw new NotImplementedException();
+                var state = new SigningState(bucket, objectName, expiration, requestMethod, requestHeaders, contentHeaders, blobSigner, clock);
+                var base64Signature = await blobSigner.CreateSignatureAsync(state.blobToSign, cancellationToken).ConfigureAwait(false);
+                var rawSignature = Convert.FromBase64String(base64Signature);
+                var hexSignature = FormatHex(rawSignature);
+                return state.GetResult(hexSignature);
+            }
+
+            /// <summary>
+            /// State which needs to be carried between the "pre-signing" stage and "post-signing" stages
+            /// of the implementation.
+            /// </summary>
+            private struct SigningState
+            {
+                private string resourcePath;
+                private List<string> queryParameters;
+                internal byte[] blobToSign;
+
+                internal SigningState(
+                    string bucket,
+                    string objectName,
+                    DateTimeOffset? expiration,
+                    HttpMethod requestMethod,
+                    Dictionary<string, IEnumerable<string>> requestHeaders,
+                    Dictionary<string, IEnumerable<string>> contentHeaders,
+                    IBlobSigner blobSigner,
+                    IClock clock)
+                {
+                    StorageClientImpl.ValidateBucketName(bucket);
+
+                    bool isResumableUpload = false;
+                    if (requestMethod == null)
+                    {
+                        requestMethod = HttpMethod.Get;
+                    }
+                    else if (requestMethod == ResumableHttpMethod)
+                    {
+                        isResumableUpload = true;
+                        requestMethod = HttpMethod.Post;
+                    }
+
+                    var now = clock.GetCurrentDateTimeUtc();
+                    var timestamp = now.ToString("yyyyMMdd'T'HHmmss'Z'", CultureInfo.InvariantCulture);
+                    var datestamp = now.ToString("yyyyMMdd", CultureInfo.InvariantCulture);
+                    // FIXME: Default expiry?
+                    string expirySeconds = ((int?)(expiration - now)?.TotalSeconds)?.ToString(CultureInfo.InvariantCulture) ?? "3600";
+
+                    string clientEmail = blobSigner.Id;
+                    string credentialScope = $"{datestamp}/auto/gcs/goog4_request";
+                    string credential = WebUtility.UrlEncode($"{blobSigner.Id}/{credentialScope}");
+
+                    // FIXME: Use requestHeaders and contentHeaders
+                    var headers = new SortedDictionary<string, string>();
+                    headers["host"] = "storage.googleapis.com";
+
+                    var canonicalHeaderBuilder = new StringBuilder();
+                    foreach (var pair in headers)
+                    {
+                        canonicalHeaderBuilder.Append($"{pair.Key}:{pair.Value}\n");
+                    }
+
+                    var canonicalHeaders = canonicalHeaderBuilder.ToString().ToLowerInvariant();
+                    var signedHeaders = string.Join(";", headers.Keys.Select(k => k.ToLowerInvariant()));
+
+                    queryParameters = new List<string>
+                    {
+                        "X-Goog-Algorithm=GOOG4-RSA-SHA256",
+                        $"X-Goog-Credential={credential}",
+                        $"X-Goog-Date={timestamp}",
+                        $"X-Goog-Expires={expirySeconds}",
+                        $"X-Goog-SignedHeaders={signedHeaders}"
+                    };
+                    if (isResumableUpload)
+                    {
+                        queryParameters.Insert(4, "X-Goog-Resumable=Start");
+                    }
+
+                    var canonicalQueryString = string.Join("&", queryParameters);
+                    resourcePath = $"/{bucket}";
+                    if (objectName != null)
+                    {
+                        resourcePath += $"/{Uri.EscapeDataString(objectName)}";
+                    }
+
+                    var canonicalRequest = $"{requestMethod}\n{resourcePath}\n{canonicalQueryString}\n{canonicalHeaders}\n{signedHeaders}\nUNSIGNED-PAYLOAD";
+                    string hashHex;
+                    using (var sha256 = SHA256.Create())
+                    {
+                        hashHex = FormatHex(sha256.ComputeHash(Encoding.UTF8.GetBytes(canonicalRequest)));
+                    }
+
+                    blobToSign = Encoding.UTF8.GetBytes($"GOOG4-RSA-SHA256\n{timestamp}\n{credentialScope}\n{hashHex}");
+                }
+
+                internal string GetResult(string signature)
+                {
+                    queryParameters.Add($"x-goog-signature={WebUtility.UrlEncode(signature)}");
+                    return $"{StorageHost}{resourcePath}?{string.Join("&", queryParameters)}";
+                }
+            }
+
+            private const string HexCharacters = "0123456789abcdef";
+            private static string FormatHex(byte[] bytes)
+            {
+                // Could just use BitConverter, but it's inefficient to create multiple strings and
+                // easy to do it ourselves instead.
+                char[] chars = new char[bytes.Length * 2];
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    chars[i * 2] = HexCharacters[bytes[i] >> 4];
+                    chars[i * 2 + 1] = HexCharacters[bytes[i] & 0xf];
+                }
+                return new string(chars);
             }
         }
     }
