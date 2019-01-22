9bfa04ea02c0e208ed56a947a0c0aaa4c4e63ea1
Jon Skeet <jonskeet@google.com>
2019-01-22 08:00:11 +0000

    Perform case-insensitive checking on image labels
    While we could just update to use Plant, Flower etc, it's simpler to be a bit looser with the comparison.

--- apis/Google.Cloud.Vision.V1/Google.Cloud.Vision.V1.Snippets/ImageAnnotatorClientSnippets.cs
+++ apis/Google.Cloud.Vision.V1/Google.Cloud.Vision.V1.Snippets/ImageAnnotatorClientSnippets.cs
@@ -207,9 +207,9 @@ namespace Google.Cloud.Vision.V1.Snippets
 
             // Not exhaustive, but let's check certain basic labels.
             var descriptions = labels.Select(l => l.Description).ToList();
-            Assert.Contains("flower", descriptions);
-            Assert.Contains("plant", descriptions);
-            Assert.Contains("vase", descriptions);
+            Assert.Contains("flower", descriptions, StringComparer.OrdinalIgnoreCase);
+            Assert.Contains("plant", descriptions, StringComparer.OrdinalIgnoreCase);
+            Assert.Contains("vase", descriptions, StringComparer.OrdinalIgnoreCase);
         }
 
         // See-also: DetectLabels(*, *, *, *)
