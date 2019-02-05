865107cd9bebf5b21eb065c5fd6026c3dd24107c
Jon Skeet <jonskeet@google.com>
2019-02-05 15:00:23 +0000

    Create snippet (nearly) for IRM
    This does prove that the API works, but has a few workarounds we'd want to fix before making it a real sample.

---
+++ apis/Google.Cloud.Irm.V1Alpha2/Google.Cloud.Irm.V1Alpha2.Snippets/IncidentServiceClientSnippets.cs
@@ -0,0 +1,67 @@
+﻿// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax.ResourceNames;
+using Google.Cloud.ClientTesting;
+using Google.Protobuf.WellKnownTypes;
+using System;
+using Xunit;
+using static Google.Cloud.Irm.V1Alpha2.Incident.Types;
+
+namespace Google.Cloud.Irm.V1Alpha2.Snippets
+{
+    [Collection(nameof(IrmFixture))]
+    [SnippetOutputCollector]
+    public class IncidentServiceClientSnippets
+    {
+        private readonly IrmFixture _fixture;
+
+        public IncidentServiceClientSnippets(IrmFixture fixture) => _fixture = fixture;
+
+        [Fact]
+        public void CreateAndUpdateIncident()
+        {
+            string projectId = _fixture.ProjectId;
+            IncidentServiceClient client = IncidentServiceClient.Create();
+            Incident incident = new Incident
+            {
+                Severity = Severity.Medium,
+                Stage = Stage.Detected,
+                Title = "Something is wrong"
+            };
+            ProjectName parent = new ProjectName(projectId);
+
+            // Create the initial incident
+            incident = client.CreateIncident(incident, parent.ToString());
+
+            // TODO: Simplify this:
+            // - (Config issue) We should be able to use IncidentName everywhere.
+            // - (Service change) We should be able to use the name from the created incident.
+            string incidentId = IncidentName.Parse(incident.Name).IncidentId;
+            string fullIncidentName = new IncidentName(projectId, incidentId).ToString();
+
+            // Resolve it
+            Incident patch = new Incident
+            {
+                Name = fullIncidentName,
+                Stage = Stage.Resolved
+            };
+            client.UpdateIncident(patch, new FieldMask { Paths = { "stage" } });
+
+            // Fetch and display
+            Incident fetched = client.GetIncident(fullIncidentName);
+            Console.WriteLine(fetched);
+        }
+    }
+}
