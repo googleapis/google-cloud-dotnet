8a0f023e3b41a6928cf56f55314efcc1470db70e
Jon Skeet <jonskeet@google.com>
2019-02-15 10:28:37 +0000

    Fix nested array chunking in Spanner results

--- apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/V1/ReliableStreamReaderTests.cs
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/V1/ReliableStreamReaderTests.cs
@@ -51,6 +51,82 @@ namespace Google.Cloud.Spanner.V1.Tests
             await AssertValues(reader, "a", "xy", "c");
         }
 
+        [Fact]
+        public async Task NestedArrayChunkSplitting()
+        {
+            // One string field, then an array-of-struct field, where the struct has two string fields.
+            var metadata = new ResultSetMetadata
+            {
+                RowType = new StructType
+                {
+                    Fields =
+                    {
+                        new StructType.Types.Field { Name = "f1", Type = new V1.Type { Code = TypeCode.String } },
+                        new StructType.Types.Field
+                        {
+                            Name = "f2", Type = new V1.Type
+                            {
+                                Code = TypeCode.Array,
+                                ArrayElementType = new V1.Type
+                                {
+                                    Code = TypeCode.Struct,
+                                    StructType = new StructType
+                                    {
+                                        Fields =
+                                        {
+                                            new StructType.Types.Field { Name = "f2-1", Type = new V1.Type { Code = TypeCode.String } },
+                                            new StructType.Types.Field { Name = "f2-2", Type = new V1.Type { Code = TypeCode.String } }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+                    }
+                }
+            };
+            // The second row splits within the nested array
+            var results = new[]
+            {
+                new PartialResultSet
+                {
+                    ChunkedValue = true,
+                    Values =
+                    {
+                        Value.ForString("f1-v1"),
+                        Value.ForList(Value.ForList(Value.ForString("f2-1-1-v1"), Value.ForString("f2-2-1-v2"))),
+                        Value.ForString("f1-v2"),
+                        Value.ForList(
+                            Value.ForList(Value.ForString("f2-1-2-v1"), Value.ForString("f2-2-2-v2")),
+                            Value.ForList(Value.ForString("f2-1-3-v1"), Value.ForString("f2-2-3-v2"))
+                        ),
+                    }
+                },
+                new PartialResultSet
+                {
+                    Values =
+                    {
+                        Value.ForList(
+                            Value.ForList(new Value[0]),
+                            Value.ForList(Value.ForString("f2-1-4-v1"), Value.ForString("f2-2-4-v2"))
+                        )
+                    }
+                }
+            };
+            var expectedValues = new[]
+            {
+                Value.ForString("f1-v1"),
+                Value.ForList(Value.ForList(Value.ForString("f2-1-1-v1"), Value.ForString("f2-2-1-v2"))),
+                Value.ForString("f1-v2"),
+                Value.ForList(
+                    Value.ForList(Value.ForString("f2-1-2-v1"), Value.ForString("f2-2-2-v2")),
+                    Value.ForList(Value.ForString("f2-1-3-v1"), Value.ForString("f2-2-3-v2")),
+                    Value.ForList(Value.ForString("f2-1-4-v1"), Value.ForString("f2-2-4-v2"))
+                )
+            };
+            var reader = CreateReader(results, metadata);
+            await AssertValues(reader, expectedValues);
+        }
+
         private static async Task AssertValues(ReliableStreamReader reader, params string[] expectedValues)
         {
             var actual = new List<string>();
@@ -59,6 +135,17 @@ namespace Google.Cloud.Spanner.V1.Tests
             {
                 Assert.Equal(Value.KindOneofCase.StringValue, value.KindCase);
                 actual.Add(value.StringValue);
+            }
+            Assert.Equal(expectedValues, actual.ToArray());
+        }
+
+        private static async Task AssertValues(ReliableStreamReader reader, params Value[] expectedValues)
+        {
+            var actual = new List<Value>();
+            Value value;
+            while ((value = await reader.NextAsync(default)) != null)
+            {
+                actual.Add(value);
             }
             Assert.Equal(expectedValues, actual.ToArray());
         }
