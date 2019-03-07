2a4f21712c5f3b9bbf313a909a708982e59e533a
Chris Bacon <chris@dunelm.org.uk>
2019-03-07 15:37:10 +0000

    [DO NOT MERGE] Very first micro-generator run

--- apis/Google.Cloud.Vision.V1/Google.Cloud.Vision.V1.Snippets/ImageAnnotatorClientSnippets.g.cs
+++ apis/Google.Cloud.Vision.V1/Google.Cloud.Vision.V1.Snippets/ImageAnnotatorClientSnippets.g.cs
@@ -1,77 +1,46 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-// Generated code. DO NOT EDIT!
-
-namespace Google.Cloud.Vision.V1.Snippets
+namespace Google.Cloud.Vision.V1
 {
-    using Google.Api.Gax;
-    using Google.Api.Gax.Grpc;
-    using apis = Google.Cloud.Vision.V1;
     using Google.LongRunning;
-    using Google.Protobuf;
-    using Google.Protobuf.WellKnownTypes;
-    using Grpc.Core;
-    using System;
-    using System.Collections;
     using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Linq;
-    using System.Threading;
     using System.Threading.Tasks;
 
-    /// <summary>Generated snippets</summary>
-    public class GeneratedImageAnnotatorClientSnippets
+    /// <summary>Generated snippets.</summary>
+    public sealed class GeneratedImageAnnotatorSnippets
     {
-        /// <summary>Snippet for BatchAnnotateImagesAsync</summary>
-        public async Task BatchAnnotateImagesAsync()
-        {
-            // Snippet: BatchAnnotateImagesAsync(IEnumerable<AnnotateImageRequest>,CallSettings)
-            // Additional: BatchAnnotateImagesAsync(IEnumerable<AnnotateImageRequest>,CancellationToken)
-            // Create client
-            ImageAnnotatorClient imageAnnotatorClient = await ImageAnnotatorClient.CreateAsync();
-            // Initialize request argument(s)
-            IEnumerable<AnnotateImageRequest> requests = new List<AnnotateImageRequest>();
-            // Make the request
-            BatchAnnotateImagesResponse response = await imageAnnotatorClient.BatchAnnotateImagesAsync(requests);
-            // End snippet
-        }
-
         /// <summary>Snippet for BatchAnnotateImages</summary>
-        public void BatchAnnotateImages()
+        public void BatchAnnotateImages_RequestObject()
         {
-            // Snippet: BatchAnnotateImages(IEnumerable<AnnotateImageRequest>,CallSettings)
+            // Snippet: BatchAnnotateImages(BatchAnnotateImagesRequest, CallSettings)
             // Create client
             ImageAnnotatorClient imageAnnotatorClient = ImageAnnotatorClient.Create();
             // Initialize request argument(s)
-            IEnumerable<AnnotateImageRequest> requests = new List<AnnotateImageRequest>();
+            BatchAnnotateImagesRequest request = new BatchAnnotateImagesRequest
+            {
+                Requests =
+                {
+                    new AnnotateImageRequest(),
+                },
+            };
             // Make the request
-            BatchAnnotateImagesResponse response = imageAnnotatorClient.BatchAnnotateImages(requests);
+            BatchAnnotateImagesResponse response = imageAnnotatorClient.BatchAnnotateImages(request);
             // End snippet
         }
 
         /// <summary>Snippet for BatchAnnotateImagesAsync</summary>
         public async Task BatchAnnotateImagesAsync_RequestObject()
         {
-            // Snippet: BatchAnnotateImagesAsync(BatchAnnotateImagesRequest,CallSettings)
-            // Additional: BatchAnnotateImagesAsync(BatchAnnotateImagesRequest,CancellationToken)
+            // Snippet: BatchAnnotateImagesAsync(BatchAnnotateImagesRequest, CallSettings)
+            // Additional: BatchAnnotateImagesAsync(BatchAnnotateImagesRequest, CancellationToken)
             // Create client
             ImageAnnotatorClient imageAnnotatorClient = await ImageAnnotatorClient.CreateAsync();
             // Initialize request argument(s)
             BatchAnnotateImagesRequest request = new BatchAnnotateImagesRequest
             {
-                Requests = { },
+                Requests =
+                {
+                    new AnnotateImageRequest(),
+                },
             };
             // Make the request
             BatchAnnotateImagesResponse response = await imageAnnotatorClient.BatchAnnotateImagesAsync(request);
@@ -79,77 +48,64 @@ namespace Google.Cloud.Vision.V1.Snippets
         }
 
         /// <summary>Snippet for BatchAnnotateImages</summary>
-        public void BatchAnnotateImages_RequestObject()
+        public void BatchAnnotateImages()
         {
-            // Snippet: BatchAnnotateImages(BatchAnnotateImagesRequest,CallSettings)
+            // Snippet: BatchAnnotateImages(IEnumerable, CallSettings)
             // Create client
             ImageAnnotatorClient imageAnnotatorClient = ImageAnnotatorClient.Create();
             // Initialize request argument(s)
-            BatchAnnotateImagesRequest request = new BatchAnnotateImagesRequest
+            IEnumerable<AnnotateImageRequest> requests = new AnnotateImageRequest[]
             {
-                Requests = { },
+                new AnnotateImageRequest(),
             };
             // Make the request
-            BatchAnnotateImagesResponse response = imageAnnotatorClient.BatchAnnotateImages(request);
+            BatchAnnotateImagesResponse response = imageAnnotatorClient.BatchAnnotateImages(requests);
             // End snippet
         }
 
-        /// <summary>Snippet for AsyncBatchAnnotateFilesAsync</summary>
-        public async Task AsyncBatchAnnotateFilesAsync()
+        /// <summary>Snippet for BatchAnnotateImagesAsync</summary>
+        public async Task BatchAnnotateImagesAsync()
         {
-            // Snippet: AsyncBatchAnnotateFilesAsync(IEnumerable<AsyncAnnotateFileRequest>,CallSettings)
-            // Additional: AsyncBatchAnnotateFilesAsync(IEnumerable<AsyncAnnotateFileRequest>,CancellationToken)
+            // Snippet: BatchAnnotateImagesAsync(IEnumerable, CallSettings)
+            // Additional: BatchAnnotateImagesAsync(IEnumerable, CancellationToken)
             // Create client
             ImageAnnotatorClient imageAnnotatorClient = await ImageAnnotatorClient.CreateAsync();
             // Initialize request argument(s)
-            IEnumerable<AsyncAnnotateFileRequest> requests = new List<AsyncAnnotateFileRequest>();
-            // Make the request
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> response =
-                await imageAnnotatorClient.AsyncBatchAnnotateFilesAsync(requests);
-
-            // Poll until the returned long-running operation is complete
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> completedResponse =
-                await response.PollUntilCompletedAsync();
-            // Retrieve the operation result
-            AsyncBatchAnnotateFilesResponse result = completedResponse.Result;
-
-            // Or get the name of the operation
-            string operationName = response.Name;
-            // This name can be stored, then the long-running operation retrieved later by name
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> retrievedResponse =
-                await imageAnnotatorClient.PollOnceAsyncBatchAnnotateFilesAsync(operationName);
-            // Check if the retrieved long-running operation has completed
-            if (retrievedResponse.IsCompleted)
+            IEnumerable<AnnotateImageRequest> requests = new AnnotateImageRequest[]
             {
-                // If it has completed, then access the result
-                AsyncBatchAnnotateFilesResponse retrievedResult = retrievedResponse.Result;
-            }
+                new AnnotateImageRequest(),
+            };
+            // Make the request
+            BatchAnnotateImagesResponse response = await imageAnnotatorClient.BatchAnnotateImagesAsync(requests);
             // End snippet
         }
 
         /// <summary>Snippet for AsyncBatchAnnotateFiles</summary>
-        public void AsyncBatchAnnotateFiles()
+        public void AsyncBatchAnnotateFiles_RequestObject()
         {
-            // Snippet: AsyncBatchAnnotateFiles(IEnumerable<AsyncAnnotateFileRequest>,CallSettings)
+            // Snippet: AsyncBatchAnnotateFiles(AsyncBatchAnnotateFilesRequest, CallSettings)
             // Create client
             ImageAnnotatorClient imageAnnotatorClient = ImageAnnotatorClient.Create();
             // Initialize request argument(s)
-            IEnumerable<AsyncAnnotateFileRequest> requests = new List<AsyncAnnotateFileRequest>();
+            AsyncBatchAnnotateFilesRequest request = new AsyncBatchAnnotateFilesRequest
+            {
+                Requests =
+                {
+                    new AsyncAnnotateFileRequest(),
+                },
+            };
             // Make the request
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> response =
-                imageAnnotatorClient.AsyncBatchAnnotateFiles(requests);
+            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> response = imageAnnotatorClient.AsyncBatchAnnotateFiles(request);
 
             // Poll until the returned long-running operation is complete
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> completedResponse =
-                response.PollUntilCompleted();
+            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> completedResponse = response.PollUntilCompleted();
             // Retrieve the operation result
             AsyncBatchAnnotateFilesResponse result = completedResponse.Result;
 
             // Or get the name of the operation
             string operationName = response.Name;
             // This name can be stored, then the long-running operation retrieved later by name
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> retrievedResponse =
-                imageAnnotatorClient.PollOnceAsyncBatchAnnotateFiles(operationName);
+            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> retrievedResponse = imageAnnotatorClient.PollOnceAsyncBatchAnnotateFiles(operationName);
             // Check if the retrieved long-running operation has completed
             if (retrievedResponse.IsCompleted)
             {
@@ -162,29 +118,30 @@ namespace Google.Cloud.Vision.V1.Snippets
         /// <summary>Snippet for AsyncBatchAnnotateFilesAsync</summary>
         public async Task AsyncBatchAnnotateFilesAsync_RequestObject()
         {
-            // Snippet: AsyncBatchAnnotateFilesAsync(AsyncBatchAnnotateFilesRequest,CallSettings)
+            // Snippet: AsyncBatchAnnotateFilesAsync(AsyncBatchAnnotateFilesRequest, CallSettings)
+            // Additional: AsyncBatchAnnotateFilesAsync(AsyncBatchAnnotateFilesRequest, CancellationToken)
             // Create client
             ImageAnnotatorClient imageAnnotatorClient = await ImageAnnotatorClient.CreateAsync();
             // Initialize request argument(s)
             AsyncBatchAnnotateFilesRequest request = new AsyncBatchAnnotateFilesRequest
             {
-                Requests = { },
+                Requests =
+                {
+                    new AsyncAnnotateFileRequest(),
+                },
             };
             // Make the request
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> response =
-                await imageAnnotatorClient.AsyncBatchAnnotateFilesAsync(request);
+            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> response = await imageAnnotatorClient.AsyncBatchAnnotateFilesAsync(request);
 
             // Poll until the returned long-running operation is complete
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> completedResponse =
-                await response.PollUntilCompletedAsync();
+            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> completedResponse = await response.PollUntilCompletedAsync();
             // Retrieve the operation result
             AsyncBatchAnnotateFilesResponse result = completedResponse.Result;
 
             // Or get the name of the operation
             string operationName = response.Name;
             // This name can be stored, then the long-running operation retrieved later by name
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> retrievedResponse =
-                await imageAnnotatorClient.PollOnceAsyncBatchAnnotateFilesAsync(operationName);
+            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> retrievedResponse = await imageAnnotatorClient.PollOnceAsyncBatchAnnotateFilesAsync(operationName);
             // Check if the retrieved long-running operation has completed
             if (retrievedResponse.IsCompleted)
             {
@@ -193,41 +150,5 @@ namespace Google.Cloud.Vision.V1.Snippets
             }
             // End snippet
         }
-
-        /// <summary>Snippet for AsyncBatchAnnotateFiles</summary>
-        public void AsyncBatchAnnotateFiles_RequestObject()
-        {
-            // Snippet: AsyncBatchAnnotateFiles(AsyncBatchAnnotateFilesRequest,CallSettings)
-            // Create client
-            ImageAnnotatorClient imageAnnotatorClient = ImageAnnotatorClient.Create();
-            // Initialize request argument(s)
-            AsyncBatchAnnotateFilesRequest request = new AsyncBatchAnnotateFilesRequest
-            {
-                Requests = { },
-            };
-            // Make the request
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> response =
-                imageAnnotatorClient.AsyncBatchAnnotateFiles(request);
-
-            // Poll until the returned long-running operation is complete
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> completedResponse =
-                response.PollUntilCompleted();
-            // Retrieve the operation result
-            AsyncBatchAnnotateFilesResponse result = completedResponse.Result;
-
-            // Or get the name of the operation
-            string operationName = response.Name;
-            // This name can be stored, then the long-running operation retrieved later by name
-            Operation<AsyncBatchAnnotateFilesResponse, OperationMetadata> retrievedResponse =
-                imageAnnotatorClient.PollOnceAsyncBatchAnnotateFiles(operationName);
-            // Check if the retrieved long-running operation has completed
-            if (retrievedResponse.IsCompleted)
-            {
-                // If it has completed, then access the result
-                AsyncBatchAnnotateFilesResponse retrievedResult = retrievedResponse.Result;
-            }
-            // End snippet
-        }
-
     }
 }
--- apis/Google.Cloud.Vision.V1/Google.Cloud.Vision.V1.Tests/ImageAnnotatorClientTest.g.cs
+++ apis/Google.Cloud.Vision.V1/Google.Cloud.Vision.V1.Tests/ImageAnnotatorClientTest.g.cs
@@ -1,116 +1,122 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+using gaxgrpc = Google.Api.Gax.Grpc;
+using lro = Google.LongRunning;
+using grpccore = Grpc.Core;
+using moq = Moq;
+using st = System.Threading;
+using stt = System.Threading.Tasks;
+using xunit = Xunit;
 
-// Generated code. DO NOT EDIT!
-
-namespace Google.Cloud.Vision.V1.Tests
+namespace Google.Cloud.Vision.V1
 {
-    using Google.Api.Gax;
-    using Google.Api.Gax.Grpc;
-    using apis = Google.Cloud.Vision.V1;
-    using Google.LongRunning;
-    using Google.Protobuf.WellKnownTypes;
-    using Grpc.Core;
-    using Moq;
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Xunit;
-
-    /// <summary>Generated unit tests</summary>
-    public class GeneratedImageAnnotatorClientTest
+    /// <summary>Generated unit tests.</summary>
+    public sealed class GeneratedImageAnnotatorTest
     {
-        [Fact]
-        public void BatchAnnotateImages()
+        [xunit::FactAttribute]
+        public void BatchAnnotateImagesRequestObject()
         {
-            Mock<ImageAnnotator.ImageAnnotatorClient> mockGrpcClient = new Mock<ImageAnnotator.ImageAnnotatorClient>(MockBehavior.Strict);
-            mockGrpcClient.Setup(x => x.CreateOperationsClient())
-                .Returns(new Mock<Operations.OperationsClient>().Object);
-            BatchAnnotateImagesRequest expectedRequest = new BatchAnnotateImagesRequest
-            {
-                Requests = { },
-            };
-            BatchAnnotateImagesResponse expectedResponse = new BatchAnnotateImagesResponse();
-            mockGrpcClient.Setup(x => x.BatchAnnotateImages(expectedRequest, It.IsAny<CallOptions>()))
-                .Returns(expectedResponse);
-            ImageAnnotatorClient client = new ImageAnnotatorClientImpl(mockGrpcClient.Object, null);
-            IEnumerable<AnnotateImageRequest> requests = new List<AnnotateImageRequest>();
-            BatchAnnotateImagesResponse response = client.BatchAnnotateImages(requests);
-            Assert.Same(expectedResponse, response);
-            mockGrpcClient.VerifyAll();
-        }
-
-        [Fact]
-        public async Task BatchAnnotateImagesAsync()
-        {
-            Mock<ImageAnnotator.ImageAnnotatorClient> mockGrpcClient = new Mock<ImageAnnotator.ImageAnnotatorClient>(MockBehavior.Strict);
-            mockGrpcClient.Setup(x => x.CreateOperationsClient())
-                .Returns(new Mock<Operations.OperationsClient>().Object);
-            BatchAnnotateImagesRequest expectedRequest = new BatchAnnotateImagesRequest
-            {
-                Requests = { },
-            };
-            BatchAnnotateImagesResponse expectedResponse = new BatchAnnotateImagesResponse();
-            mockGrpcClient.Setup(x => x.BatchAnnotateImagesAsync(expectedRequest, It.IsAny<CallOptions>()))
-                .Returns(new Grpc.Core.AsyncUnaryCall<BatchAnnotateImagesResponse>(Task.FromResult(expectedResponse), null, null, null, null));
-            ImageAnnotatorClient client = new ImageAnnotatorClientImpl(mockGrpcClient.Object, null);
-            IEnumerable<AnnotateImageRequest> requests = new List<AnnotateImageRequest>();
-            BatchAnnotateImagesResponse response = await client.BatchAnnotateImagesAsync(requests);
-            Assert.Same(expectedResponse, response);
-            mockGrpcClient.VerifyAll();
-        }
-
-        [Fact]
-        public void BatchAnnotateImages2()
-        {
-            Mock<ImageAnnotator.ImageAnnotatorClient> mockGrpcClient = new Mock<ImageAnnotator.ImageAnnotatorClient>(MockBehavior.Strict);
-            mockGrpcClient.Setup(x => x.CreateOperationsClient())
-                .Returns(new Mock<Operations.OperationsClient>().Object);
+            moq::Mock<ImageAnnotator.ImageAnnotatorClient> mockGrpcClient = new moq::Mock<ImageAnnotator.ImageAnnotatorClient>(moq::MockBehavior.Strict);
+            mockGrpcClient.Setup(x => x.CreateOperationsClient()).Returns(new moq::Mock<lro::Operations.OperationsClient>().Object);
             BatchAnnotateImagesRequest request = new BatchAnnotateImagesRequest
             {
-                Requests = { },
+                Requests =
+                {
+                    new AnnotateImageRequest(),
+                },
             };
-            BatchAnnotateImagesResponse expectedResponse = new BatchAnnotateImagesResponse();
-            mockGrpcClient.Setup(x => x.BatchAnnotateImages(request, It.IsAny<CallOptions>()))
-                .Returns(expectedResponse);
+            BatchAnnotateImagesResponse expectedResponse = new BatchAnnotateImagesResponse
+            {
+                Responses =
+                {
+                    new AnnotateImageResponse(),
+                },
+            };
+            mockGrpcClient.Setup(x => x.BatchAnnotateImages(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(expectedResponse);
             ImageAnnotatorClient client = new ImageAnnotatorClientImpl(mockGrpcClient.Object, null);
             BatchAnnotateImagesResponse response = client.BatchAnnotateImages(request);
-            Assert.Same(expectedResponse, response);
+            xunit::Assert.Same(expectedResponse, response);
             mockGrpcClient.VerifyAll();
         }
 
-        [Fact]
-        public async Task BatchAnnotateImagesAsync2()
+        [xunit::FactAttribute]
+        public async stt::Task BatchAnnotateImagesRequestObjectAsync()
         {
-            Mock<ImageAnnotator.ImageAnnotatorClient> mockGrpcClient = new Mock<ImageAnnotator.ImageAnnotatorClient>(MockBehavior.Strict);
-            mockGrpcClient.Setup(x => x.CreateOperationsClient())
-                .Returns(new Mock<Operations.OperationsClient>().Object);
+            moq::Mock<ImageAnnotator.ImageAnnotatorClient> mockGrpcClient = new moq::Mock<ImageAnnotator.ImageAnnotatorClient>(moq::MockBehavior.Strict);
+            mockGrpcClient.Setup(x => x.CreateOperationsClient()).Returns(new moq::Mock<lro::Operations.OperationsClient>().Object);
             BatchAnnotateImagesRequest request = new BatchAnnotateImagesRequest
             {
-                Requests = { },
+                Requests =
+                {
+                    new AnnotateImageRequest(),
+                },
             };
-            BatchAnnotateImagesResponse expectedResponse = new BatchAnnotateImagesResponse();
-            mockGrpcClient.Setup(x => x.BatchAnnotateImagesAsync(request, It.IsAny<CallOptions>()))
-                .Returns(new Grpc.Core.AsyncUnaryCall<BatchAnnotateImagesResponse>(Task.FromResult(expectedResponse), null, null, null, null));
+            BatchAnnotateImagesResponse expectedResponse = new BatchAnnotateImagesResponse
+            {
+                Responses =
+                {
+                    new AnnotateImageResponse(),
+                },
+            };
+            mockGrpcClient.Setup(x => x.BatchAnnotateImagesAsync(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(new grpccore::AsyncUnaryCall<BatchAnnotateImagesResponse>(stt::Task.FromResult(expectedResponse), null, null, null, null));
             ImageAnnotatorClient client = new ImageAnnotatorClientImpl(mockGrpcClient.Object, null);
-            BatchAnnotateImagesResponse response = await client.BatchAnnotateImagesAsync(request);
-            Assert.Same(expectedResponse, response);
+            BatchAnnotateImagesResponse responseCallSettings = await client.BatchAnnotateImagesAsync(request, gaxgrpc::CallSettings.FromCancellationToken(st::CancellationToken.None));
+            xunit::Assert.Same(expectedResponse, responseCallSettings);
+            BatchAnnotateImagesResponse responseCancellationToken = await client.BatchAnnotateImagesAsync(request, st::CancellationToken.None);
+            xunit::Assert.Same(expectedResponse, responseCancellationToken);
             mockGrpcClient.VerifyAll();
         }
 
+        [xunit::FactAttribute]
+        public void BatchAnnotateImages()
+        {
+            moq::Mock<ImageAnnotator.ImageAnnotatorClient> mockGrpcClient = new moq::Mock<ImageAnnotator.ImageAnnotatorClient>(moq::MockBehavior.Strict);
+            mockGrpcClient.Setup(x => x.CreateOperationsClient()).Returns(new moq::Mock<lro::Operations.OperationsClient>().Object);
+            BatchAnnotateImagesRequest request = new BatchAnnotateImagesRequest
+            {
+                Requests =
+                {
+                    new AnnotateImageRequest(),
+                },
+            };
+            BatchAnnotateImagesResponse expectedResponse = new BatchAnnotateImagesResponse
+            {
+                Responses =
+                {
+                    new AnnotateImageResponse(),
+                },
+            };
+            mockGrpcClient.Setup(x => x.BatchAnnotateImages(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(expectedResponse);
+            ImageAnnotatorClient client = new ImageAnnotatorClientImpl(mockGrpcClient.Object, null);
+            BatchAnnotateImagesResponse response = client.BatchAnnotateImages(request.Requests);
+            xunit::Assert.Same(expectedResponse, response);
+            mockGrpcClient.VerifyAll();
+        }
+
+        [xunit::FactAttribute]
+        public async stt::Task BatchAnnotateImagesAsync()
+        {
+            moq::Mock<ImageAnnotator.ImageAnnotatorClient> mockGrpcClient = new moq::Mock<ImageAnnotator.ImageAnnotatorClient>(moq::MockBehavior.Strict);
+            mockGrpcClient.Setup(x => x.CreateOperationsClient()).Returns(new moq::Mock<lro::Operations.OperationsClient>().Object);
+            BatchAnnotateImagesRequest request = new BatchAnnotateImagesRequest
+            {
+                Requests =
+                {
+                    new AnnotateImageRequest(),
+                },
+            };
+            BatchAnnotateImagesResponse expectedResponse = new BatchAnnotateImagesResponse
+            {
+                Responses =
+                {
+                    new AnnotateImageResponse(),
+                },
+            };
+            mockGrpcClient.Setup(x => x.BatchAnnotateImagesAsync(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(new grpccore::AsyncUnaryCall<BatchAnnotateImagesResponse>(stt::Task.FromResult(expectedResponse), null, null, null, null));
+            ImageAnnotatorClient client = new ImageAnnotatorClientImpl(mockGrpcClient.Object, null);
+            BatchAnnotateImagesResponse responseCallSettings = await client.BatchAnnotateImagesAsync(request.Requests, gaxgrpc::CallSettings.FromCancellationToken(st::CancellationToken.None));
+            xunit::Assert.Same(expectedResponse, responseCallSettings);
+            BatchAnnotateImagesResponse responseCancellationToken = await client.BatchAnnotateImagesAsync(request.Requests, st::CancellationToken.None);
+            xunit::Assert.Same(expectedResponse, responseCancellationToken);
+            mockGrpcClient.VerifyAll();
+        }
     }
 }
