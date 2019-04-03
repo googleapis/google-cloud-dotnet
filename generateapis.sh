b4c06bc4fefe4103e29f28ef476e8936873a8bd1
Jon Skeet <jonskeet@google.com>
2019-04-03 18:05:29 +0100

    Add Recaptcha Enterprise into API generation

--- generateapis.sh
+++ generateapis.sh
@@ -231,6 +231,7 @@ generate_api Google.Cloud.OsLogin.V1 google/cloud/oslogin/v1 oslogin_v1.yaml
 generate_api Google.Cloud.OsLogin.V1Beta google/cloud/oslogin/v1beta oslogin_v1beta.yaml
 generate_api Google.Cloud.PhishingProtection.V1Beta1 google/cloud/phishingprotection/v1beta1 phishingprotection.yaml
 generate_api Google.Cloud.PubSub.V1 google/pubsub/v1 pubsub.yaml
+generate_api Google.Cloud.RecaptchaEnterprise.V1Beta1 google/cloud/recaptchaenterprise/v1beta1 recaptchaenterprise_v1beta1.yaml
 generate_api Google.Cloud.Redis.V1 google/cloud/redis/v1 redis_v1.yaml
 generate_api Google.Cloud.Redis.V1Beta1 google/cloud/redis/v1beta1 redis_v1beta1.yaml
 generate_api Google.Cloud.Scheduler.V1 google/cloud/scheduler/v1 cloudscheduler_v1.yaml
