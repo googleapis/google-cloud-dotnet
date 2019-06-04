5580ff72a6fc6b261eb9bbce21ad1f158146c0e7
Jon Skeet <jonskeet@google.com>
2019-06-04 11:27:44 +0100

    Regenerate Vision V1 - comments only

--- apis/Google.Cloud.Vision.V1/Google.Cloud.Vision.V1/ProductSearch.cs
+++ apis/Google.Cloud.Vision.V1/Google.Cloud.Vision.V1/ProductSearch.cs
@@ -140,7 +140,10 @@ namespace Google.Cloud.Vision.V1 {
     /// The list of product categories to search in. Currently, we only consider
     /// the first category, and either "homegoods-v2", "apparel-v2", or "toys-v2"
     /// should be specified. The legacy categories "homegoods", "apparel", and
-    /// "toys" are still supported, but these should not be used for new products.
+    /// "toys" are still supported but will be deprecated. For new products, please
+    /// use "homegoods-v2", "apparel-v2", or "toys-v2" for better product search
+    /// accuracy. It is recommended to migrate existing products to these
+    /// categories as well.
     /// </summary>
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     public pbc::RepeatedField<string> ProductCategories {
