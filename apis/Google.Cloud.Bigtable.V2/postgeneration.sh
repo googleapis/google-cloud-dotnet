4fd92833ed4cfeb9378de0a89c9490b6b30efa16
Jon Skeet <jonskeet@google.com>
2019-07-02 15:30:05 +0100

    Run Bigtable postgeneration script on .NET Core even on Linux
    This appears to work now... I can't remember exactly what didn't work before.

--- apis/Google.Cloud.Bigtable.V2/postgeneration.sh
+++ apis/Google.Cloud.Bigtable.V2/postgeneration.sh
@@ -4,15 +4,7 @@ set -e
 
 sed -i -r -f BigtableServiceApiClient.sed Google.Cloud.Bigtable.V2/BigtableServiceApiClient.cs
 
-case "$OSTYPE" in
-  win* | msys* | cygwin*)
-    dotnet run -p Google.Cloud.Bigtable.V2.GenerateClient \
-      Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.csproj \
-      BigtableServiceApiClient \
-      BigtableClient
-    ;;
-  *)
-    echo "Skipping BigtableClient generation; currently limited to Windows"
-    ;;
-esac
-
+dotnet run -p Google.Cloud.Bigtable.V2.GenerateClient \
+  Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.csproj \
+  BigtableServiceApiClient \
+  BigtableClient
