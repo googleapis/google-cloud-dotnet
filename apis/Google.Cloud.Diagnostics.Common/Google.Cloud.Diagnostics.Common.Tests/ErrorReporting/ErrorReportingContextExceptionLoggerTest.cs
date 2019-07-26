9e1fa0a0654557d702963d9bad5246b7d66e7890
Jon Skeet <jonskeet@google.com>
2019-07-26 12:57:27 +0100

    Remove mocking from error reporting test
    This commit will make it a lot easier to diagnose failures.

--- apis/Google.Cloud.Diagnostics.Common/Google.Cloud.Diagnostics.Common.Tests/ErrorReporting/ErrorReportingContextExceptionLoggerTest.cs
+++ apis/Google.Cloud.Diagnostics.Common/Google.Cloud.Diagnostics.Common.Tests/ErrorReporting/ErrorReportingContextExceptionLoggerTest.cs
@@ -15,8 +15,8 @@
 using Google.Cloud.ClientTesting;
 using Google.Cloud.Logging.V2;
 using Google.Protobuf.WellKnownTypes;
-using Moq;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -40,88 +40,86 @@ namespace Google.Cloud.Diagnostics.Common.Tests
         public void Log()
         {
             var options = ErrorReportingOptions.Create(
-                EventTarget.ForLogging("pid", loggingClient: new Mock<LoggingServiceV2Client>().Object));
-            var mockConsumer = new Mock<IConsumer<LogEntry>>();
-            mockConsumer.Setup(c => c.Receive(IsContext(_method, _uri, _userAgent, options)));
-
+                EventTarget.ForLogging("pid", loggingClient: new ThrowingLoggingClient()));
+            var consumer = new FakeConsumer();
+            
             IContextExceptionLogger logger = new ErrorReportingContextExceptionLogger(
-                mockConsumer.Object, _service, _version, options);
-
+                consumer, _service, _version, options);
             logger.Log(CreateException(), new FakeContextWrapper());
 
-            mockConsumer.VerifyAll();
+            ValidateSingleEntry(consumer, _method, _uri, _userAgent, options);
         }
 
         [Fact]
         public void Log_Simple()
         {
             var options = ErrorReportingOptions.Create(
-                EventTarget.ForLogging("pid", loggingClient: new Mock<LoggingServiceV2Client>().Object));
-            var mockConsumer = new Mock<IConsumer<LogEntry>>();
-            mockConsumer.Setup(c => c.Receive(IsContext("", "", "", options)));
+                EventTarget.ForLogging("pid", loggingClient: new ThrowingLoggingClient()));
+            var consumer = new FakeConsumer();
 
             IContextExceptionLogger logger = new ErrorReportingContextExceptionLogger(
-                 mockConsumer.Object, _service, _version, options);
-
+                 consumer, _service, _version, options);
             logger.Log(CreateException(), new EmptyContextWrapper());
 
-            mockConsumer.VerifyAll();
+            ValidateSingleEntry(consumer, "", "", "", options);
         }
 
         [Fact]
         public async Task LogAsync()
         {
             var options = ErrorReportingOptions.Create(
-                EventTarget.ForLogging("pid", loggingClient: new Mock<LoggingServiceV2Client>().Object));
-            var mockConsumer = new Mock<IConsumer<LogEntry>>();
-            mockConsumer.Setup(c => c.ReceiveAsync(
-                IsContext(_method, _uri, _userAgent, options), It.IsAny<CancellationToken>()))
-                .Returns(Task.FromResult(true));
+                EventTarget.ForLogging("pid", loggingClient: new ThrowingLoggingClient()));
+            var consumer = new FakeConsumer();
 
             IContextExceptionLogger logger = new ErrorReportingContextExceptionLogger(
-                mockConsumer.Object, _service, _version, options);
-
+                consumer, _service, _version, options);
             await logger.LogAsync(CreateException(), new FakeContextWrapper());
 
-            mockConsumer.VerifyAll();
+            ValidateSingleEntry(consumer, _method, _uri, _userAgent, options);
         }
 
-        internal IEnumerable<LogEntry> IsContext(
-            string method, string uri, string userAgent, ErrorReportingOptions options)
+        private void ValidateSingleEntry(FakeConsumer consumer, string method, string uri, string userAgent, ErrorReportingOptions options)
         {
-            return Match.Create<IEnumerable<LogEntry>>(enumerable => {
-                var e = enumerable.Single();
-                var eventTarget = options.EventTarget;
-                
-                var json = e.JsonPayload?.Fields;
-                var message = json["message"].StringValue;
-                var context = json["context"]?.StructValue?.Fields;
-                var httpRequest = context["httpRequest"]?.StructValue?.Fields;
-                var methodVal = httpRequest["method"].StringValue;
-                var urlVal = httpRequest["url"].StringValue;
-                var userAgentVal = httpRequest["userAgent"].StringValue;
-                var reportLocation = context["reportLocation"]?.StructValue?.Fields;
-                var filePathVal = reportLocation["filePath"].StringValue;
-                var lineNumberVal = reportLocation["lineNumber"].NumberValue;
-                var functionNameVal = reportLocation["functionName"].StringValue;
-                var serviceContext = json["serviceContext"]?.StructValue?.Fields;
-                var serviceVal = serviceContext["service"].StringValue;
-                var versionVal = serviceContext["version"].StringValue;
+            var entries = consumer.Entries.ToList();
+            if (entries.Count == 1 && Matches(entries[0], method, uri, userAgent, options))
+            {
+                return;
+            }
+            Assert.True(false, $"Expected single matching entry. Received:\n{string.Join("\n", entries)}");
+        }
 
-                return e.LogName == eventTarget.LogTarget.GetFullLogName(eventTarget.LogName) &&
-                    e.Timestamp.Seconds <= Timestamp.FromDateTime(DateTime.UtcNow).Seconds &&
-                    e.Resource == eventTarget.MonitoredResource &&
-                    e.Severity == Logging.Type.LogSeverity.Error &&
-                    message.Contains(_exceptionMessage) &&
-                    method.Equals(methodVal) &&
-                    uri.Equals(urlVal) &&
-                    userAgent.Equals(userAgentVal) &&
-                    (!_isWindows || lineNumberVal > 0) &&
-                    (!_isWindows || !string.IsNullOrEmpty(filePathVal)) &&
-                    nameof(CreateException).Equals(functionNameVal) &&
-                    _service.Equals(serviceVal) &&
-                    _version.Equals(versionVal);
-            });
+        private bool Matches(LogEntry entry, string method, string uri, string userAgent, ErrorReportingOptions options)
+        {
+            var json = entry.JsonPayload?.Fields;
+            var eventTarget = options.EventTarget;
+            
+            var message = json["message"].StringValue;
+            var context = json["context"]?.StructValue?.Fields;
+            var httpRequest = context["httpRequest"]?.StructValue?.Fields;
+            var methodVal = httpRequest["method"].StringValue;
+            var urlVal = httpRequest["url"].StringValue;
+            var userAgentVal = httpRequest["userAgent"].StringValue;
+            var reportLocation = context["reportLocation"]?.StructValue?.Fields;
+            var filePathVal = reportLocation["filePath"].StringValue;
+            var lineNumberVal = reportLocation["lineNumber"].NumberValue;
+            var functionNameVal = reportLocation["functionName"].StringValue;
+            var serviceContext = json["serviceContext"]?.StructValue?.Fields;
+            var serviceVal = serviceContext["service"].StringValue;
+            var versionVal = serviceContext["version"].StringValue;
+
+            return entry.LogName == eventTarget.LogTarget.GetFullLogName(eventTarget.LogName) &&
+                entry.Timestamp.Seconds <= Timestamp.FromDateTime(DateTime.UtcNow).Seconds &&
+                entry.Resource == eventTarget.MonitoredResource &&
+                entry.Severity == Logging.Type.LogSeverity.Error &&
+                message.Contains(_exceptionMessage) &&
+                method.Equals(methodVal) &&
+                uri.Equals(urlVal) &&
+                userAgent.Equals(userAgentVal) &&
+                (!_isWindows || lineNumberVal > 0) &&
+                (!_isWindows || !string.IsNullOrEmpty(filePathVal)) &&
+                nameof(CreateException).Equals(functionNameVal) &&
+                _service.Equals(serviceVal) &&
+                _version.Equals(versionVal);
         }
 
         /// <summary>Create a thrown exception with message.</summary>
@@ -149,6 +147,36 @@ namespace Google.Cloud.Diagnostics.Common.Tests
             string IContextWrapper.GetHttpMethod() => null;
             string IContextWrapper.GetUri() => null;
             string IContextWrapper.GetUserAgent() => null;
+        }
+
+        private class FakeConsumer : IConsumer<LogEntry>
+        {
+            internal ConcurrentBag<LogEntry> Entries { get; } = new ConcurrentBag<LogEntry>();
+
+            public void Dispose()
+            {
+            }
+
+            public void Receive(IEnumerable<LogEntry> items)
+            {
+                foreach (var item in items)
+                {
+                    Entries.Add(item);
+                }
+            }
+
+            public Task ReceiveAsync(IEnumerable<LogEntry> items, CancellationToken cancellationToken = default)
+            {
+                Receive(items);
+                return Task.CompletedTask;
+            }
+        }
+
+        /// <summary>
+        /// Logging client that will just throw if any methods are called.
+        /// </summary>
+        private class ThrowingLoggingClient : LoggingServiceV2Client
+        {
         }
     }
 }
