894256c2e4a4da21b306c4997789bb5f98a3835f
Jon Skeet <jonskeet@google.com>
2019-08-12 14:20:38 +0100

    Prototype of changes for a client library using IAM
    (We don't have the relevant fields in the request proto yet, so we can't implement it fully.)

---
+++ apis/Google.Cloud.Bigtable.Admin.V2/Google.Cloud.Bigtable.Admin.V2.Tests/IamVersionTest.cs
@@ -0,0 +1,84 @@
+﻿// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Iam.V1;
+using Grpc.Core;
+using System;
+using Xunit;
+
+namespace Google.Cloud.Bigtable.Admin.V2.Tests
+{
+    public class IamVersionTest
+    {
+        [Theory]
+        [InlineData(null, 1, 1)]
+        [InlineData(null, null, null)]
+        [InlineData(3, 1, 1)]
+        [InlineData(3, null, 3)]
+        [InlineData(1, 3, 3)]
+        [InlineData(1, null, 1)]
+        public void GetIamPolicyWithVersions(int? settingsVersion, int? callerRequestVersion, int? expectedRpcVersion)
+        {
+            var settings = new BigtableInstanceAdminSettings { SupportedIamPolicyVersion = settingsVersion };
+            var callInvoker = new FakeCallInvoker();
+            
+            var client = new BigtableInstanceAdminClientBuilder
+            {
+                Settings = new BigtableInstanceAdminSettings { SupportedIamPolicyVersion = settingsVersion },
+                CallInvoker = callInvoker
+            }.Build();
+
+            var callerRequest = new GetIamPolicyRequest
+            {
+                // TODO: Specify the caller request version
+            };
+
+            client.GetIamPolicy(callerRequest);
+            var lastRequest = callInvoker.LastRequest;
+
+            // TODO: Assertions around the version in lastRequest            
+            if (callerRequestVersion != expectedRpcVersion)
+            {
+                Assert.NotSame(callerRequest, lastRequest);
+            }
+        }
+
+        private class FakeCallInvoker : CallInvoker
+        {
+            public GetIamPolicyRequest LastRequest { get; set; }
+            
+            public override AsyncClientStreamingCall<TRequest, TResponse> AsyncClientStreamingCall<TRequest, TResponse>(Method<TRequest, TResponse> method, string host, CallOptions options) =>
+                throw new NotImplementedException();
+
+            public override AsyncDuplexStreamingCall<TRequest, TResponse> AsyncDuplexStreamingCall<TRequest, TResponse>(Method<TRequest, TResponse> method, string host, CallOptions options) =>
+                throw new NotImplementedException();
+
+            public override AsyncServerStreamingCall<TResponse> AsyncServerStreamingCall<TRequest, TResponse>(Method<TRequest, TResponse> method, string host, CallOptions options, TRequest request) =>
+                throw new NotImplementedException();
+
+            public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(Method<TRequest, TResponse> method, string host, CallOptions options, TRequest request) =>
+                throw new NotImplementedException();
+
+            public override TResponse BlockingUnaryCall<TRequest, TResponse>(Method<TRequest, TResponse> method, string host, CallOptions options, TRequest request)
+            {
+                if (typeof(TRequest) == typeof(GetIamPolicyRequest))
+                {
+                    LastRequest = (GetIamPolicyRequest) (object) request;
+                    return (TResponse) Activator.CreateInstance(typeof(TResponse));
+                }
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ apis/Google.Cloud.Bigtable.Admin.V2/Google.Cloud.Bigtable.Admin.V2/BigtableInstanceAdminClientPartial.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Partial classes to support IAM policy versions
+
+using Google.Api.Gax.Grpc;
+using Google.Cloud.Iam.V1;
+
+namespace Google.Cloud.Bigtable.Admin.V2
+{
+    public partial class BigtableInstanceAdminSettings
+    {
+        /// <summary>
+        /// The version of IAM policies that the client will indicate that it supports in RPCs.
+        /// This defaults to a null value, meaning that requests will not be modified at all.
+        /// If a value is already present in a request, it is not overwritten.
+        /// </summary>
+        public int? SupportedIamPolicyVersion { get; set; }
+
+        partial void OnCopy(BigtableInstanceAdminSettings existing)
+        {
+            SupportedIamPolicyVersion = existing.SupportedIamPolicyVersion;
+        }
+    }
+
+    public partial class BigtableInstanceAdminClientImpl
+    {
+        private int? supportedIamPolicyVersion;
+
+        partial void OnConstruction(BigtableInstanceAdmin.BigtableInstanceAdminClient grpcClient, BigtableInstanceAdminSettings effectiveSettings, ClientHelper clientHelper)
+        {
+            supportedIamPolicyVersion = effectiveSettings.SupportedIamPolicyVersion;
+        }
+
+        partial void Modify_GetIamPolicyRequest(ref GetIamPolicyRequest request, ref CallSettings settings)
+        {
+            if (supportedIamPolicyVersion == null)
+            {
+                return;
+            }
+            // TODO: Set the appropriate field            
+        }
+    }
+}
