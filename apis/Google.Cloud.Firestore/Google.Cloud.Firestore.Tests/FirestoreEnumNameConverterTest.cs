68410d73d741ae953fa04a05a535fb7a616f9edc
Jon Skeet <jonskeet@google.com>
2019-08-27 06:21:16 +0100

    Promote the FirestoreEnumNameConverter to the public API
    This was previously just a proof-of-concept, but it's useful to put
    into the public API once. At the moment it's relatively crude, but
    we can add customization later if requested. In particular, I suspect we may want:
    
    - Case-insensitive comparisons
    - Customization of the name returned based on a custom attribute
    
    Fixes #2842

---
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore.Tests/FirestoreEnumNameConverterTest.cs
@@ -0,0 +1,83 @@
+﻿// Copyright 2019, Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Xunit;
+
+namespace Google.Cloud.Firestore.Tests
+{
+    // Note: General integration (with the rest of the framework) tests are in SerializationTestData.
+    public class FirestoreEnumNameConverterTest
+    {
+        private static readonly FirestoreEnumNameConverter<SampleEnum> s_converter = new FirestoreEnumNameConverter<SampleEnum>();
+
+        [Theory]
+        [InlineData("Foo", SampleEnum.Foo)]
+        [InlineData("Bar", SampleEnum.Bar)]
+        [InlineData("Bar2", SampleEnum.Bar)]
+        public void FromFirestore_Valid(string text, SampleEnum expectedResult) =>
+            Assert.Equal(expectedResult, s_converter.FromFirestore(text));
+
+        [Fact]
+        public void FromFirestore_UnknownValue() =>
+            Assert.Throws<ArgumentException>(() => s_converter.FromFirestore("Baz"));
+
+        [Fact]
+        public void FromFirestore_NonString() =>
+            Assert.Throws<InvalidCastException>(() => s_converter.FromFirestore(new object()));
+
+        [Fact]
+        public void FromFirestore_NumericString() =>
+            Assert.Throws<ArgumentException>(() => s_converter.FromFirestore("10"));
+
+        [Fact]
+        public void ToFirestore_UnknownValue() =>
+            Assert.Throws<ArgumentException>(() => s_converter.ToFirestore((SampleEnum) 15));
+
+        [Fact]
+        public void ToFirestore_Unique() =>
+            Assert.Equal(nameof(SampleEnum.Foo), s_converter.ToFirestore(SampleEnum.Foo));
+
+        [Fact]
+        public void ToFirestore_NotUnique()
+        {
+            string actual = (string) s_converter.ToFirestore(SampleEnum.Bar);
+            Assert.Contains(actual, new[] { nameof(SampleEnum.Bar), nameof(SampleEnum.Bar2) });
+        }
+
+        public enum SampleEnum
+        {
+            Foo = 10,
+            Bar = 20,
+            Bar2 = 20
+        }
+
+        [Fact]
+        public void EnumCanBePrivate()
+        {
+            var converter = new FirestoreEnumNameConverter<PrivateEnum>();
+            Assert.Equal(nameof(PrivateEnum.SampleValue), converter.ToFirestore(PrivateEnum.SampleValue));
+            Assert.Equal(PrivateEnum.SampleValue, converter.FromFirestore(nameof(PrivateEnum.SampleValue)));
+        }
+        
+        /// <summary>
+        /// SampleEnum has to be public so we can use it in a theory. We have a single test that
+        /// private enums work too.
+        /// </summary>
+        private enum PrivateEnum
+        {
+            SampleValue = 5
+        }
+    }
+}
---
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore/FirestoreEnumNameConverter.cs
@@ -0,0 +1,83 @@
+﻿// Copyright 2019, Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Google.Cloud.Firestore
+{
+    /// <summary>
+    /// Custom converter which uses enum value names instead of integer values as the Firestore representation.
+    /// This converter is not used by default; it must be configured in the same way as any other custom converter.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Currently this is always case-sensitive, with no customization of the names used. In future releases
+    /// we may introduce further ways to configure this converter, but the default behavior will remain the same.
+    /// </para>
+    /// <para>
+    /// When the same enum value has multiple names, no guarantee is made about which one is returned, although both
+    /// are accepted for conversion back to enum values. You are strongly encouraged not to use multiple names for the
+    /// same value.
+    /// </para>
+    /// </remarks>
+    /// <typeparam name="T">The enum type to convert</typeparam>
+    public sealed class FirestoreEnumNameConverter<T> : IFirestoreConverter<T>
+        where T : struct, Enum
+    {
+        // These dictionaries are static as they only depend on the type of T.
+        // There's no point in doing the reflection again each time a converter is constructed.
+        // If we introduce features like case-insensitivity or custom names, we may want to introduce instance
+        // dictionary fields as well.
+        private static readonly Dictionary<string, T> s_nameToValueMap;
+        private static readonly Dictionary<T, string> s_valueToNameMap;
+
+        static FirestoreEnumNameConverter()
+        {
+            s_nameToValueMap = new Dictionary<string, T>();
+            s_valueToNameMap = new Dictionary<T, string>();
+
+            foreach (var field in typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                var name = field.Name;
+                var value = (T) field.GetValue(null);
+                s_nameToValueMap[name] = value;
+                // This may overwrite an existing value, but that's okay. We explicitly don't
+                // make any guarantees as to which name is returned for a value with multiple names.
+                s_valueToNameMap[value] = name;
+            }
+        }
+        
+        /// <inheritdoc />
+        public T FromFirestore(object value) =>
+            s_nameToValueMap.TryGetValue((string) value, out var result) ?
+            result : throw new ArgumentException($"Unknown name '{value}' for enum {typeof(T).FullName}");
+
+        /// <summary>
+        /// Converts an enum value to its name.
+        /// </summary>
+        /// <remarks>
+        /// If multiple values in the enum map to the same integer, it is undefined which will be returned.
+        /// If the value is not a named enum element, an exception is thrown, even if the enum is
+        /// decorated with <see cref="FlagsAttribute"/>.
+        /// </remarks>
+        /// <param name="value">The value to convert.</param>
+        /// <returns>The name of the value.</returns>
+        /// <exception cref="ArgumentException">The given value is not a named value within the enum.</exception>
+        public object ToFirestore(T value) =>
+            s_valueToNameMap.TryGetValue(value, out var result) ?
+            result : throw new ArgumentException($"Unknown value '{value}' for enum {typeof(T).FullName}");
+    }
+}
