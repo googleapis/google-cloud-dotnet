78214e438a539b5617d803fa56f946a7608cc387
yoshi-automation <yoshi-automation@google.com>
2019-10-22 09:17:28 -0700

    Regenerate Google.Cloud.Scheduler.V1

--- apis/Google.Cloud.Scheduler.V1/Google.Cloud.Scheduler.V1/ResourceNames.cs
+++ apis/Google.Cloud.Scheduler.V1/Google.Cloud.Scheduler.V1/ResourceNames.cs
@@ -226,6 +226,105 @@ namespace Google.Cloud.Scheduler.V1
         public static bool operator !=(LocationName a, LocationName b) => !(a == b);
     }
 
+    /// <summary>
+    /// Resource name for the 'topic' resource.
+    /// </summary>
+    public sealed partial class TopicName : gax::IResourceName, sys::IEquatable<TopicName>
+    {
+        private static readonly gax::PathTemplate s_template = new gax::PathTemplate("projects/{project}/topics/{topic}");
+
+        /// <summary>
+        /// Parses the given topic resource name in string form into a new
+        /// <see cref="TopicName"/> instance.
+        /// </summary>
+        /// <param name="topicName">The topic resource name in string form. Must not be <c>null</c>.</param>
+        /// <returns>The parsed <see cref="TopicName"/> if successful.</returns>
+        public static TopicName Parse(string topicName)
+        {
+            gax::GaxPreconditions.CheckNotNull(topicName, nameof(topicName));
+            gax::TemplatedResourceName resourceName = s_template.ParseName(topicName);
+            return new TopicName(resourceName[0], resourceName[1]);
+        }
+
+        /// <summary>
+        /// Tries to parse the given topic resource name in string form into a new
+        /// <see cref="TopicName"/> instance.
+        /// </summary>
+        /// <remarks>
+        /// This method still throws <see cref="sys::ArgumentNullException"/> if <paramref name="topicName"/> is null,
+        /// as this would usually indicate a programming error rather than a data error.
+        /// </remarks>
+        /// <param name="topicName">The topic resource name in string form. Must not be <c>null</c>.</param>
+        /// <param name="result">When this method returns, the parsed <see cref="TopicName"/>,
+        /// or <c>null</c> if parsing fails.</param>
+        /// <returns><c>true</c> if the name was parsed successfully; <c>false</c> otherwise.</returns>
+        public static bool TryParse(string topicName, out TopicName result)
+        {
+            gax::GaxPreconditions.CheckNotNull(topicName, nameof(topicName));
+            gax::TemplatedResourceName resourceName;
+            if (s_template.TryParseName(topicName, out resourceName))
+            {
+                result = new TopicName(resourceName[0], resourceName[1]);
+                return true;
+            }
+            else
+            {
+                result = null;
+                return false;
+            }
+        }
+
+        /// <summary>Formats the IDs into the string representation of the <see cref="TopicName"/>.</summary>
+        /// <param name="projectId">The <c>project</c> ID. Must not be <c>null</c>.</param>
+        /// <param name="topicId">The <c>topic</c> ID. Must not be <c>null</c>.</param>
+        /// <returns>The string representation of the <see cref="TopicName"/>.</returns>
+        public static string Format(string projectId, string topicId) =>
+            s_template.Expand(gax::GaxPreconditions.CheckNotNull(projectId, nameof(projectId)), gax::GaxPreconditions.CheckNotNull(topicId, nameof(topicId)));
+
+        /// <summary>
+        /// Constructs a new instance of the <see cref="TopicName"/> resource name class
+        /// from its component parts.
+        /// </summary>
+        /// <param name="projectId">The project ID. Must not be <c>null</c>.</param>
+        /// <param name="topicId">The topic ID. Must not be <c>null</c>.</param>
+        public TopicName(string projectId, string topicId)
+        {
+            ProjectId = gax::GaxPreconditions.CheckNotNull(projectId, nameof(projectId));
+            TopicId = gax::GaxPreconditions.CheckNotNull(topicId, nameof(topicId));
+        }
+
+        /// <summary>
+        /// The project ID. Never <c>null</c>.
+        /// </summary>
+        public string ProjectId { get; }
+
+        /// <summary>
+        /// The topic ID. Never <c>null</c>.
+        /// </summary>
+        public string TopicId { get; }
+
+        /// <inheritdoc />
+        public gax::ResourceNameKind Kind => gax::ResourceNameKind.Simple;
+
+        /// <inheritdoc />
+        public override string ToString() => s_template.Expand(ProjectId, TopicId);
+
+        /// <inheritdoc />
+        public override int GetHashCode() => ToString().GetHashCode();
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => Equals(obj as TopicName);
+
+        /// <inheritdoc />
+        public bool Equals(TopicName other) => ToString() == other?.ToString();
+
+        /// <inheritdoc />
+        public static bool operator ==(TopicName a, TopicName b) => ReferenceEquals(a, b) || (a?.Equals(b) ?? false);
+
+        /// <inheritdoc />
+        public static bool operator !=(TopicName a, TopicName b) => !(a == b);
+    }
+
 
     public partial class CreateJobRequest
     {
@@ -301,6 +400,19 @@ namespace Google.Cloud.Scheduler.V1
         {
             get { return string.IsNullOrEmpty(Name) ? null : Google.Cloud.Scheduler.V1.JobName.Parse(Name); }
             set { Name = value != null ? value.ToString() : ""; }
+        }
+
+    }
+
+    public partial class PubsubTarget
+    {
+        /// <summary>
+        /// <see cref="Google.Cloud.Scheduler.V1.TopicName"/>-typed view over the <see cref="TopicName"/> resource name property.
+        /// </summary>
+        public Google.Cloud.Scheduler.V1.TopicName TopicNameAsTopicName
+        {
+            get { return string.IsNullOrEmpty(TopicName) ? null : Google.Cloud.Scheduler.V1.TopicName.Parse(TopicName); }
+            set { TopicName = value != null ? value.ToString() : ""; }
         }
 
     }
