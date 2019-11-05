26a556f66913938d8a7c56bc6fce911fcbf72694
Jon Skeet <jonskeet@google.com>
2019-11-05 13:56:42 +0000

    Implement Firestore emulator support to match Datastore
    Fixes #3397, but needs documentation.
    
    I suspect the emulator always actually starts on "::1" by default.
    We can address the IPv6 issue if and when we need to.

--- apis/Google.Cloud.Firestore/Google.Cloud.Firestore.IntegrationTests/FirestoreFixture.cs
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore.IntegrationTests/FirestoreFixture.cs
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Api.Gax.Grpc;
 using Google.Cloud.ClientTesting;
 using Google.Cloud.Firestore.IntegrationTests.Models;
+using Google.Cloud.Firestore.V1;
+using Grpc.Core;
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -63,7 +66,7 @@ namespace Google.Cloud.Firestore.IntegrationTests
             // Currently, only the default database is supported... so we create all our collections with a randomly-generated prefix.
             // When multiple databases are supported, we'll create a new one per test run.
             CollectionPrefix = IdGenerator.FromGuid(prefix: "test-");
-            FirestoreDb = FirestoreDb.Create(ProjectId);
+            FirestoreDb = new FirestoreDbBuilder { ProjectId = ProjectId, EmulatorDetection = EmulatorDetection.ProductionOrEmulator }.Build();
             NonQueryCollection = FirestoreDb.Collection(CollectionPrefix + "-non-query");
             HighScoreCollection = FirestoreDb.Collection(CollectionPrefix + "-high-scores");
             ArrayQueryCollection = FirestoreDb.Collection(CollectionPrefix + "-array-query");
---
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore/EmulatorDetection.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Google.Cloud.Firestore
+{
+    /// <summary>
+    /// Specifies how the use of the Firestore emulator is detected via the FIRESTORE_EMULATOR_HOST environment variable,
+    /// when used by <see cref="FirestoreDbBuilder"/>.
+    /// </summary>
+    public enum EmulatorDetection
+    {
+        /// <summary>
+        /// Ignore environment variables entirely.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Always connect to the production servers, but throw an exception if
+        /// the environment variable is set to suggest the emulator is expected.
+        /// </summary>
+        ProductionOnly = 1,
+
+        /// <summary>
+        /// Always connect to the emulator, throwing an exception if the environment
+        /// variable is configured.
+        /// </summary>
+        EmulatorOnly = 2,
+
+        /// <summary>
+        /// Connect to the emulator if the environment variable is present,
+        /// or production otherwise. This is a convenient option, but risks damage to
+        /// production databases or running up unexpected bills if tests are accidentally
+        /// run in production due to the environment variable being absent unexpectedly.
+        /// (Using separate projects for production and testing is a best practice for
+        /// preventing the first issue, but may be unrealistic for small or hobby projects.)
+        /// </summary>
+        ProductionOrEmulator = 3
+    }
+}
