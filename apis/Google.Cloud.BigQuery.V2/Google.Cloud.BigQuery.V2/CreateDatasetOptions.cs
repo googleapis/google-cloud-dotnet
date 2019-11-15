7b03b152d368161078f3debf26a14aef1bc6ab4a
Amanda Tarafa Mas <atarafamas@google.com>
2019-11-15 00:15:08 +0000

    CreateDatasetOptions
    CreateDatasetOptions nits.

--- apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/CreateDatasetOptions.cs
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/CreateDatasetOptions.cs
@@ -41,6 +41,14 @@ namespace Google.Cloud.BigQuery.V2
         public TimeSpan? DefaultTableExpiration { get; set; }
 
         /// <summary>
+        /// The default partition expiration for all partitioned tables created in the new dataset.
+        /// Partitioned tables will (by default) be automatically deleted this long after they are created,
+        /// unless otherwise specified, such as by <see cref="CreateTableOptions.Expiration"/>.
+        /// For partitioned tables, this values has priority over <see cref="DefaultTableExpiration"/>.
+        /// </summary>
+        public TimeSpan? DefaultPartitionExpiration { get; set; }
+
+        /// <summary>
         /// Location to create the dataset in, such as "EU" or "US". See
         /// the Dataset resource documentation for all possible values. If this is not set, the
         /// client's <see cref="BigQueryClient.DefaultLocation">default location</see> will be used
@@ -48,6 +56,14 @@ namespace Google.Cloud.BigQuery.V2
         /// </summary>
         /// <seealso cref="BigQueryClient.WithDefaultLocation(string)"/>
         public string Location { get; set; }
+
+        /// <summary>
+        /// The default encryption key for all tables in the dataset.
+        /// Once this property is set, all newly-created tables in the dataset
+        /// will have encryption key set to this value, unless otherwise specified,
+        /// such as by <see cref="CreateTableOptions.EncryptionConfiguration"/>.
+        /// </summary>
+        public EncryptionConfiguration DefaultEncryptionConfiguration { get; set; }
 
         internal void ModifyRequest(Dataset dataset, InsertRequest request)
         {
@@ -63,10 +79,17 @@ namespace Google.Cloud.BigQuery.V2
             {
                 dataset.Location = Location;
             }
-            
             if (DefaultTableExpiration != null)
             {
                 dataset.DefaultTableExpirationMs = (long) DefaultTableExpiration.Value.TotalMilliseconds;
+            }
+            if (DefaultPartitionExpiration != null)
+            {
+                dataset.DefaultPartitionExpirationMs = (long) DefaultPartitionExpiration.Value.TotalMilliseconds;
+            }
+            if (DefaultEncryptionConfiguration != null)
+            {
+                dataset.DefaultEncryptionConfiguration = DefaultEncryptionConfiguration;
             }
         }
     }
