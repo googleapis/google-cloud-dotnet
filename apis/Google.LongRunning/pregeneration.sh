43e878f2cbaa078c0ae81424f4581ec3781af6e0
Jon Skeet <jonskeet@google.com>
2019-12-12 16:15:08 +0000

    Move Google.LongRunning to the microgenerator
    This requires some method signatures (as per the patch) which will eventually be in GitHub.

--- apis/Google.LongRunning/pregeneration.sh
+++ apis/Google.LongRunning/pregeneration.sh
@@ -6,3 +6,7 @@ set -e
 sed -i \
   s/license-header-bsd-3-clause.txt/license-header-apache-2.0.txt/g \
   ../../googleapis/google/longrunning/longrunning_gapic.yaml
+
+# The patch file is relative to the root; it looks like patch files don't like to specify
+# parent directories.
+(cd ../..; git apply apis/Google.LongRunning/pregeneration.patch)
