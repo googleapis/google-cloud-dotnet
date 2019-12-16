198d09272710cc807767f97d079978e02f6b23a6
Jon Skeet <jonskeet@google.com>
2019-12-16 11:57:46 +0000

    Fix the post-generation fix-up for LRO
    (We want to revert changes to multiple files, so just check out the directory.)

--- apis/Google.LongRunning/postgeneration.sh
+++ apis/Google.LongRunning/postgeneration.sh
@@ -3,4 +3,4 @@
 set -e
 
 # Undo the changes made in pregeneration.sh and pregeneration.patch
-git -C ../../googleapis checkout google/longrunning/longrunning_gapic.yaml
+git -C ../../googleapis checkout google/longrunning
