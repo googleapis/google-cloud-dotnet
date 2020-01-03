91774fd99d5b05803e306856257e59d76ae6222a
Jon Skeet <jonskeet@google.com>
2020-01-03 14:38:11 +0000

    Regenerate Dataproc (with microgenerator)

---
+++ apis/Google.Cloud.Dataproc.V1/Google.Cloud.Dataproc.V1.Tests/AutoscalingPolicyServiceClientTest.g.cs
@@ -0,0 +1,207 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+using gaxgrpc = Google.Api.Gax.Grpc;
+using wkt = Google.Protobuf.WellKnownTypes;
+using grpccore = Grpc.Core;
+using moq = Moq;
+using st = System.Threading;
+using stt = System.Threading.Tasks;
+using xunit = Xunit;
+
+namespace Google.Cloud.Dataproc.V1.Tests
+{
+    /// <summary>Generated unit tests.</summary>
+    public sealed class GeneratedAutoscalingPolicyServiceClientTest
+    {
+        [xunit::FactAttribute]
+        public void CreateAutoscalingPolicyRequestObject()
+        {
+            moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient> mockGrpcClient = new moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient>(moq::MockBehavior.Strict);
+            CreateAutoscalingPolicyRequest request = new CreateAutoscalingPolicyRequest
+            {
+                ParentAsAutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+                Policy = new AutoscalingPolicy(),
+            };
+            AutoscalingPolicy expectedResponse = new AutoscalingPolicy
+            {
+                Id = "id74b70bb8",
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+                BasicAlgorithm = new BasicAutoscalingAlgorithm(),
+                WorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+                SecondaryWorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+            };
+            mockGrpcClient.Setup(x => x.CreateAutoscalingPolicy(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(expectedResponse);
+            AutoscalingPolicyServiceClient client = new AutoscalingPolicyServiceClientImpl(mockGrpcClient.Object, null);
+            AutoscalingPolicy response = client.CreateAutoscalingPolicy(request);
+            xunit::Assert.Same(expectedResponse, response);
+            mockGrpcClient.VerifyAll();
+        }
+
+        [xunit::FactAttribute]
+        public async stt::Task CreateAutoscalingPolicyRequestObjectAsync()
+        {
+            moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient> mockGrpcClient = new moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient>(moq::MockBehavior.Strict);
+            CreateAutoscalingPolicyRequest request = new CreateAutoscalingPolicyRequest
+            {
+                ParentAsAutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+                Policy = new AutoscalingPolicy(),
+            };
+            AutoscalingPolicy expectedResponse = new AutoscalingPolicy
+            {
+                Id = "id74b70bb8",
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+                BasicAlgorithm = new BasicAutoscalingAlgorithm(),
+                WorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+                SecondaryWorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+            };
+            mockGrpcClient.Setup(x => x.CreateAutoscalingPolicyAsync(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(new grpccore::AsyncUnaryCall<AutoscalingPolicy>(stt::Task.FromResult(expectedResponse), null, null, null, null));
+            AutoscalingPolicyServiceClient client = new AutoscalingPolicyServiceClientImpl(mockGrpcClient.Object, null);
+            AutoscalingPolicy responseCallSettings = await client.CreateAutoscalingPolicyAsync(request, gaxgrpc::CallSettings.FromCancellationToken(st::CancellationToken.None));
+            xunit::Assert.Same(expectedResponse, responseCallSettings);
+            AutoscalingPolicy responseCancellationToken = await client.CreateAutoscalingPolicyAsync(request, st::CancellationToken.None);
+            xunit::Assert.Same(expectedResponse, responseCancellationToken);
+            mockGrpcClient.VerifyAll();
+        }
+
+        [xunit::FactAttribute]
+        public void UpdateAutoscalingPolicyRequestObject()
+        {
+            moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient> mockGrpcClient = new moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient>(moq::MockBehavior.Strict);
+            UpdateAutoscalingPolicyRequest request = new UpdateAutoscalingPolicyRequest
+            {
+                Policy = new AutoscalingPolicy(),
+            };
+            AutoscalingPolicy expectedResponse = new AutoscalingPolicy
+            {
+                Id = "id74b70bb8",
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+                BasicAlgorithm = new BasicAutoscalingAlgorithm(),
+                WorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+                SecondaryWorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+            };
+            mockGrpcClient.Setup(x => x.UpdateAutoscalingPolicy(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(expectedResponse);
+            AutoscalingPolicyServiceClient client = new AutoscalingPolicyServiceClientImpl(mockGrpcClient.Object, null);
+            AutoscalingPolicy response = client.UpdateAutoscalingPolicy(request);
+            xunit::Assert.Same(expectedResponse, response);
+            mockGrpcClient.VerifyAll();
+        }
+
+        [xunit::FactAttribute]
+        public async stt::Task UpdateAutoscalingPolicyRequestObjectAsync()
+        {
+            moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient> mockGrpcClient = new moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient>(moq::MockBehavior.Strict);
+            UpdateAutoscalingPolicyRequest request = new UpdateAutoscalingPolicyRequest
+            {
+                Policy = new AutoscalingPolicy(),
+            };
+            AutoscalingPolicy expectedResponse = new AutoscalingPolicy
+            {
+                Id = "id74b70bb8",
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+                BasicAlgorithm = new BasicAutoscalingAlgorithm(),
+                WorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+                SecondaryWorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+            };
+            mockGrpcClient.Setup(x => x.UpdateAutoscalingPolicyAsync(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(new grpccore::AsyncUnaryCall<AutoscalingPolicy>(stt::Task.FromResult(expectedResponse), null, null, null, null));
+            AutoscalingPolicyServiceClient client = new AutoscalingPolicyServiceClientImpl(mockGrpcClient.Object, null);
+            AutoscalingPolicy responseCallSettings = await client.UpdateAutoscalingPolicyAsync(request, gaxgrpc::CallSettings.FromCancellationToken(st::CancellationToken.None));
+            xunit::Assert.Same(expectedResponse, responseCallSettings);
+            AutoscalingPolicy responseCancellationToken = await client.UpdateAutoscalingPolicyAsync(request, st::CancellationToken.None);
+            xunit::Assert.Same(expectedResponse, responseCancellationToken);
+            mockGrpcClient.VerifyAll();
+        }
+
+        [xunit::FactAttribute]
+        public void GetAutoscalingPolicyRequestObject()
+        {
+            moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient> mockGrpcClient = new moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient>(moq::MockBehavior.Strict);
+            GetAutoscalingPolicyRequest request = new GetAutoscalingPolicyRequest
+            {
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+            };
+            AutoscalingPolicy expectedResponse = new AutoscalingPolicy
+            {
+                Id = "id74b70bb8",
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+                BasicAlgorithm = new BasicAutoscalingAlgorithm(),
+                WorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+                SecondaryWorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+            };
+            mockGrpcClient.Setup(x => x.GetAutoscalingPolicy(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(expectedResponse);
+            AutoscalingPolicyServiceClient client = new AutoscalingPolicyServiceClientImpl(mockGrpcClient.Object, null);
+            AutoscalingPolicy response = client.GetAutoscalingPolicy(request);
+            xunit::Assert.Same(expectedResponse, response);
+            mockGrpcClient.VerifyAll();
+        }
+
+        [xunit::FactAttribute]
+        public async stt::Task GetAutoscalingPolicyRequestObjectAsync()
+        {
+            moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient> mockGrpcClient = new moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient>(moq::MockBehavior.Strict);
+            GetAutoscalingPolicyRequest request = new GetAutoscalingPolicyRequest
+            {
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+            };
+            AutoscalingPolicy expectedResponse = new AutoscalingPolicy
+            {
+                Id = "id74b70bb8",
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+                BasicAlgorithm = new BasicAutoscalingAlgorithm(),
+                WorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+                SecondaryWorkerConfig = new InstanceGroupAutoscalingPolicyConfig(),
+            };
+            mockGrpcClient.Setup(x => x.GetAutoscalingPolicyAsync(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(new grpccore::AsyncUnaryCall<AutoscalingPolicy>(stt::Task.FromResult(expectedResponse), null, null, null, null));
+            AutoscalingPolicyServiceClient client = new AutoscalingPolicyServiceClientImpl(mockGrpcClient.Object, null);
+            AutoscalingPolicy responseCallSettings = await client.GetAutoscalingPolicyAsync(request, gaxgrpc::CallSettings.FromCancellationToken(st::CancellationToken.None));
+            xunit::Assert.Same(expectedResponse, responseCallSettings);
+            AutoscalingPolicy responseCancellationToken = await client.GetAutoscalingPolicyAsync(request, st::CancellationToken.None);
+            xunit::Assert.Same(expectedResponse, responseCancellationToken);
+            mockGrpcClient.VerifyAll();
+        }
+
+        [xunit::FactAttribute]
+        public void DeleteAutoscalingPolicyRequestObject()
+        {
+            moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient> mockGrpcClient = new moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient>(moq::MockBehavior.Strict);
+            DeleteAutoscalingPolicyRequest request = new DeleteAutoscalingPolicyRequest
+            {
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+            };
+            wkt::Empty expectedResponse = new wkt::Empty { };
+            mockGrpcClient.Setup(x => x.DeleteAutoscalingPolicy(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(expectedResponse);
+            AutoscalingPolicyServiceClient client = new AutoscalingPolicyServiceClientImpl(mockGrpcClient.Object, null);
+            client.DeleteAutoscalingPolicy(request);
+            mockGrpcClient.VerifyAll();
+        }
+
+        [xunit::FactAttribute]
+        public async stt::Task DeleteAutoscalingPolicyRequestObjectAsync()
+        {
+            moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient> mockGrpcClient = new moq::Mock<AutoscalingPolicyService.AutoscalingPolicyServiceClient>(moq::MockBehavior.Strict);
+            DeleteAutoscalingPolicyRequest request = new DeleteAutoscalingPolicyRequest
+            {
+                AutoscalingPolicyName = AutoscalingPolicyName.FromProjectRegionAutoscalingPolicy("[PROJECT]", "[REGION]", "[AUTOSCALING_POLICY]"),
+            };
+            wkt::Empty expectedResponse = new wkt::Empty { };
+            mockGrpcClient.Setup(x => x.DeleteAutoscalingPolicyAsync(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(new grpccore::AsyncUnaryCall<wkt::Empty>(stt::Task.FromResult(expectedResponse), null, null, null, null));
+            AutoscalingPolicyServiceClient client = new AutoscalingPolicyServiceClientImpl(mockGrpcClient.Object, null);
+            await client.DeleteAutoscalingPolicyAsync(request, gaxgrpc::CallSettings.FromCancellationToken(st::CancellationToken.None));
+            await client.DeleteAutoscalingPolicyAsync(request, st::CancellationToken.None);
+            mockGrpcClient.VerifyAll();
+        }
+    }
+}
--- apis/Google.Cloud.Dataproc.V1/Google.Cloud.Dataproc.V1.Tests/ClusterControllerClientTest.g.cs
+++ apis/Google.Cloud.Dataproc.V1/Google.Cloud.Dataproc.V1.Tests/ClusterControllerClientTest.g.cs
@@ -1,4 +1,4 @@
-// Copyright 2019 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,135 +14,173 @@
 
 // Generated code. DO NOT EDIT!
 
+using gaxgrpc = Google.Api.Gax.Grpc;
+using lro = Google.LongRunning;
+using grpccore = Grpc.Core;
+using moq = Moq;
+using st = System.Threading;
+using stt = System.Threading.Tasks;
+using xunit = Xunit;
+
 namespace Google.Cloud.Dataproc.V1.Tests
 {
-    using Google.Api.Gax;
-    using Google.Api.Gax.Grpc;
-    using apis = Google.Cloud.Dataproc.V1;
-    using Google.LongRunning;
-    using Google.Protobuf.WellKnownTypes;
-    using Grpc.Core;
-    using Moq;
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Collections.ObjectModel;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Xunit;
-
-    /// <summary>Generated unit tests</summary>
-    public class GeneratedClusterControllerClientTest
+    /// <summary>Generated unit tests.</summary>
+    public sealed class GeneratedClusterControllerClientTest
     {
-        [Fact]
-        public void GetCluster()
+        [xunit::FactAttribute]
+        public void GetClusterRequestObject()
         {
-            Mock<ClusterController.ClusterControllerClient> mockGrpcClient = new Mock<ClusterController.ClusterControllerClient>(MockBehavior.Strict);
-            mockGrpcClient.Setup(x => x.CreateOperationsClient())
-                .Returns(new Mock<Operations.OperationsClient>().Object);
-            GetClusterRequest expectedRequest = new GetClusterRequest
-            {
-                ProjectId = "projectId-1969970175",
-                Region = "region-934795532",
-                ClusterName = "clusterName-1018081872",
-            };
-            Cluster expectedResponse = new Cluster
-            {
-                ProjectId = "projectId2939242356",
-                ClusterName = "clusterName2875867491",
-                ClusterUuid = "clusterUuid-1017854240",
-            };
-            mockGrpcClient.Setup(x => x.GetCluster(expectedRequest, It.IsAny<CallOptions>()))
-                .Returns(expectedResponse);
-            ClusterControllerClient client = new ClusterControllerClientImpl(mockGrpcClient.Object, null);
-            string projectId = "projectId-1969970175";
-            string region = "region-934795532";
-            string clusterName = "clusterName-1018081872";
-            Cluster response = client.GetCluster(projectId, region, clusterName);
-            Assert.Same(expectedResponse, response);
-            mockGrpcClient.VerifyAll();
-        }
-
-        [Fact]
-        public async Task GetClusterAsync()
-        {
-            Mock<ClusterController.ClusterControllerClient> mockGrpcClient = new Mock<ClusterController.ClusterControllerClient>(MockBehavior.Strict);
-            mockGrpcClient.Setup(x => x.CreateOperationsClient())
-                .Returns(new Mock<Operations.OperationsClient>().Object);
-            GetClusterRequest expectedRequest = new GetClusterRequest
-            {
-                ProjectId = "projectId-1969970175",
-                Region = "region-934795532",
-                ClusterName = "clusterName-1018081872",
-            };
-            Cluster expectedResponse = new Cluster
-            {
-                ProjectId = "projectId2939242356",
-                ClusterName = "clusterName2875867491",
-                ClusterUuid = "clusterUuid-1017854240",
-            };
-            mockGrpcClient.Setup(x => x.GetClusterAsync(expectedRequest, It.IsAny<CallOptions>()))
-                .Returns(new Grpc.Core.AsyncUnaryCall<Cluster>(Task.FromResult(expectedResponse), null, null, null, null));
-            ClusterControllerClient client = new ClusterControllerClientImpl(mockGrpcClient.Object, null);
-            string projectId = "projectId-1969970175";
-            string region = "region-934795532";
-            string clusterName = "clusterName-1018081872";
-            Cluster response = await client.GetClusterAsync(projectId, region, clusterName);
-            Assert.Same(expectedResponse, response);
-            mockGrpcClient.VerifyAll();
-        }
-
-        [Fact]
-        public void GetCluster2()
-        {
-            Mock<ClusterController.ClusterControllerClient> mockGrpcClient = new Mock<ClusterController.ClusterControllerClient>(MockBehavior.Strict);
-            mockGrpcClient.Setup(x => x.CreateOperationsClient())
-                .Returns(new Mock<Operations.OperationsClient>().Object);
+            moq::Mock<ClusterController.ClusterControllerClient> mockGrpcClient = new moq::Mock<ClusterController.ClusterControllerClient>(moq::MockBehavior.Strict);
+            mockGrpcClient.Setup(x => x.CreateOperationsClient()).Returns(new moq::Mock<lro::Operations.OperationsClient>().Object);
             GetClusterRequest request = new GetClusterRequest
             {
-                ProjectId = "projectId-1969970175",
-                Region = "region-934795532",
-                ClusterName = "clusterName-1018081872",
+                ProjectId = "project_id43ad98b0",
+                ClusterName = "cluster_name88432a5e",
+                Region = "regionedb20d96",
             };
             Cluster expectedResponse = new Cluster
             {
-                ProjectId = "projectId2939242356",
-                ClusterName = "clusterName2875867491",
-                ClusterUuid = "clusterUuid-1017854240",
+                ProjectId = "project_id43ad98b0",
+                ClusterName = "cluster_name88432a5e",
+                Config = new ClusterConfig(),
+                Status = new ClusterStatus(),
+                ClusterUuid = "cluster_uuid256bc378",
+                StatusHistory =
+                {
+                    new ClusterStatus(),
+                },
+                Labels =
+                {
+                    {
+                        "key8a0b6e3c",
+                        "value60c16320"
+                    },
+                },
+                Metrics = new ClusterMetrics(),
             };
-            mockGrpcClient.Setup(x => x.GetCluster(request, It.IsAny<CallOptions>()))
-                .Returns(expectedResponse);
+            mockGrpcClient.Setup(x => x.GetCluster(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(expectedResponse);
             ClusterControllerClient client = new ClusterControllerClientImpl(mockGrpcClient.Object, null);
             Cluster response = client.GetCluster(request);
-            Assert.Same(expectedResponse, response);
+            xunit::Assert.Same(expectedResponse, response);
             mockGrpcClient.VerifyAll();
         }
 
-        [Fact]
-        public async Task GetClusterAsync2()
+        [xunit::FactAttribute]
+        public async stt::Task GetClusterRequestObjectAsync()
         {
-            Mock<ClusterController.ClusterControllerClient> mockGrpcClient = new Mock<ClusterController.ClusterControllerClient>(MockBehavior.Strict);
-            mockGrpcClient.Setup(x => x.CreateOperationsClient())
-                .Returns(new Mock<Operations.OperationsClient>().Object);
+            moq::Mock<ClusterController.ClusterControllerClient> mockGrpcClient = new moq::Mock<ClusterController.ClusterControllerClient>(moq::MockBehavior.Strict);
+            mockGrpcClient.Setup(x => x.CreateOperationsClient()).Returns(new moq::Mock<lro::Operations.OperationsClient>().Object);
             GetClusterRequest request = new GetClusterRequest
             {
-                ProjectId = "projectId-1969970175",
-                Region = "region-934795532",
-                ClusterName = "clusterName-1018081872",
+                ProjectId = "project_id43ad98b0",
+                ClusterName = "cluster_name88432a5e",
+                Region = "regionedb20d96",
             };
             Cluster expectedResponse = new Cluster
             {
-                ProjectId = "projectId2939242356",
-                ClusterName = "clusterName2875867491",
-                ClusterUuid = "clusterUuid-1017854240",
+                ProjectId = "project_id43ad98b0",
+                ClusterName = "cluster_name88432a5e",
+                Config = new ClusterConfig(),
+                Status = new ClusterStatus(),
+                ClusterUuid = "cluster_uuid256bc378",
+                StatusHistory =
+                {
+                    new ClusterStatus(),
+                },
+                Labels =
+                {
+                    {
+                        "key8a0b6e3c",
+                        "value60c16320"
+                    },
+                },
+                Metrics = new ClusterMetrics(),
             };
-            mockGrpcClient.Setup(x => x.GetClusterAsync(request, It.IsAny<CallOptions>()))
-                .Returns(new Grpc.Core.AsyncUnaryCall<Cluster>(Task.FromResult(expectedResponse), null, null, null, null));
+            mockGrpcClient.Setup(x => x.GetClusterAsync(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(new grpccore::AsyncUnaryCall<Cluster>(stt::Task.FromResult(expectedResponse), null, null, null, null));
             ClusterControllerClient client = new ClusterControllerClientImpl(mockGrpcClient.Object, null);
-            Cluster response = await client.GetClusterAsync(request);
-            Assert.Same(expectedResponse, response);
+            Cluster responseCallSettings = await client.GetClusterAsync(request, gaxgrpc::CallSettings.FromCancellationToken(st::CancellationToken.None));
+            xunit::Assert.Same(expectedResponse, responseCallSettings);
+            Cluster responseCancellationToken = await client.GetClusterAsync(request, st::CancellationToken.None);
+            xunit::Assert.Same(expectedResponse, responseCancellationToken);
             mockGrpcClient.VerifyAll();
         }
 
+        [xunit::FactAttribute]
+        public void GetCluster()
+        {
+            moq::Mock<ClusterController.ClusterControllerClient> mockGrpcClient = new moq::Mock<ClusterController.ClusterControllerClient>(moq::MockBehavior.Strict);
+            mockGrpcClient.Setup(x => x.CreateOperationsClient()).Returns(new moq::Mock<lro::Operations.OperationsClient>().Object);
+            GetClusterRequest request = new GetClusterRequest
+            {
+                ProjectId = "project_id43ad98b0",
+                ClusterName = "cluster_name88432a5e",
+                Region = "regionedb20d96",
+            };
+            Cluster expectedResponse = new Cluster
+            {
+                ProjectId = "project_id43ad98b0",
+                ClusterName = "cluster_name88432a5e",
+                Config = new ClusterConfig(),
+                Status = new ClusterStatus(),
+                ClusterUuid = "cluster_uuid256bc378",
+                StatusHistory =
+                {
+                    new ClusterStatus(),
+                },
+                Labels =
+                {
+                    {
+                        "key8a0b6e3c",
+                        "value60c16320"
+                    },
+                },
+                Metrics = new ClusterMetrics(),
+            };
+            mockGrpcClient.Setup(x => x.GetCluster(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(expectedResponse);
+            ClusterControllerClient client = new ClusterControllerClientImpl(mockGrpcClient.Object, null);
+            Cluster response = client.GetCluster(request.ProjectId, request.Region, request.ClusterName);
+            xunit::Assert.Same(expectedResponse, response);
+            mockGrpcClient.VerifyAll();
+        }
+
+        [xunit::FactAttribute]
+        public async stt::Task GetClusterAsync()
+        {
+            moq::Mock<ClusterController.ClusterControllerClient> mockGrpcClient = new moq::Mock<ClusterController.ClusterControllerClient>(moq::MockBehavior.Strict);
+            mockGrpcClient.Setup(x => x.CreateOperationsClient()).Returns(new moq::Mock<lro::Operations.OperationsClient>().Object);
+            GetClusterRequest request = new GetClusterRequest
+            {
+                ProjectId = "project_id43ad98b0",
+                ClusterName = "cluster_name88432a5e",
+                Region = "regionedb20d96",
+            };
+            Cluster expectedResponse = new Cluster
+            {
+                ProjectId = "project_id43ad98b0",
+                ClusterName = "cluster_name88432a5e",
+                Config = new ClusterConfig(),
+                Status = new ClusterStatus(),
+                ClusterUuid = "cluster_uuid256bc378",
+                StatusHistory =
+                {
+                    new ClusterStatus(),
+                },
+                Labels =
+                {
+                    {
+                        "key8a0b6e3c",
+                        "value60c16320"
+                    },
+                },
+                Metrics = new ClusterMetrics(),
+            };
+            mockGrpcClient.Setup(x => x.GetClusterAsync(request, moq::It.IsAny<grpccore::CallOptions>())).Returns(new grpccore::AsyncUnaryCall<Cluster>(stt::Task.FromResult(expectedResponse), null, null, null, null));
+            ClusterControllerClient client = new ClusterControllerClientImpl(mockGrpcClient.Object, null);
+            Cluster responseCallSettings = await client.GetClusterAsync(request.ProjectId, request.Region, request.ClusterName, gaxgrpc::CallSettings.FromCancellationToken(st::CancellationToken.None));
+            xunit::Assert.Same(expectedResponse, responseCallSettings);
+            Cluster responseCancellationToken = await client.GetClusterAsync(request.ProjectId, request.Region, request.ClusterName, st::CancellationToken.None);
+            xunit::Assert.Same(expectedResponse, responseCancellationToken);
+            mockGrpcClient.VerifyAll();
+        }
     }
 }
