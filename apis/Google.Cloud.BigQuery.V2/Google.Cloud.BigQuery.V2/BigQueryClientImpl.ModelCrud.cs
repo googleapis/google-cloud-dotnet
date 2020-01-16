38e755a7fe3e67f4db45c338eb3bb16be4114856
Amanda Tarafa Mas <atarafamas@google.com>
2020-01-16 13:10:31 +0000

    Adds some Model CRUD operations and supporting types.

---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryClientImpl.ModelCrud.cs
@@ -0,0 +1,148 @@
+﻿// Copyright 2020 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using Google.Api.Gax.Rest;
+using Google.Apis.Bigquery.v2.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static Google.Apis.Bigquery.v2.ModelsResource;
+
+namespace Google.Cloud.BigQuery.V2
+{
+    public partial class BigQueryClientImpl
+    {
+        // Visible for testing
+        internal sealed class ModelPageManager : IPageManager<ListRequest, ListModelsResponse, BigQueryModel>
+        {
+            private readonly BigQueryClient _client;
+
+            internal ModelPageManager(BigQueryClient client)
+            {
+                _client = client;
+            }
+
+            public string GetNextPageToken(ListModelsResponse response) => response.NextPageToken;
+            public IEnumerable<BigQueryModel> GetResources(ListModelsResponse response) => response.Models?.Select(resource => new BigQueryModel(_client, resource));
+            public void SetPageSize(ListRequest request, int pageSize) => request.MaxResults = pageSize;
+            public void SetPageToken(ListRequest request, string pageToken) => request.PageToken = pageToken;
+        }
+
+        /// <inheritdoc />
+        public override BigQueryModel GetModel(ModelReference modelReference, GetModelOptions options = null)
+        {
+            var request = CreateGetModelRequest(modelReference, options);
+            var resource = request.Execute();
+            return new BigQueryModel(this, resource);
+        }
+
+        /// <inheritdoc />
+        public override async Task<BigQueryModel> GetModelAsync(ModelReference modelReference, GetModelOptions options = null, CancellationToken cancellationToken = default)
+        {
+            var request = CreateGetModelRequest(modelReference, options);
+            var resource = await request.ExecuteAsync().ConfigureAwait(false);
+            return new BigQueryModel(this, resource);
+        }
+
+        /// <inheritdoc />
+        public override void DeleteModel(ModelReference modelReference, DeleteModelOptions options = null)
+        {
+            var request = CreateDeleteModelRequest(modelReference, options);
+            request.Execute();
+        }
+
+        /// <inheritdoc />
+        public override async Task DeleteModelAsync(ModelReference modelReference, DeleteModelOptions options = null, CancellationToken cancellationToken = default)
+        {
+            var request = CreateDeleteModelRequest(modelReference, options);
+            await request.ExecuteAsync().ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public override BigQueryModel PatchModel(ModelReference modelReference, Model resource, PatchModelOptions options = null)
+        {
+            var request = CreatePatchModelRequest(modelReference, resource, options);
+            var model = request.Execute();
+            return new BigQueryModel(this, model);
+        }
+
+        /// <inheritdoc />
+        public override async Task<BigQueryModel> PatchModelAsync(ModelReference modelReference, Model resource, PatchModelOptions options = null, CancellationToken cancellationToken = default)
+        {
+            var request = CreatePatchModelRequest(modelReference, resource, options);
+            var model = await request.ExecuteAsync().ConfigureAwait(false);
+            return new BigQueryModel(this, model);
+        }
+
+        /// <inheritdoc />
+        public override PagedEnumerable<ListModelsResponse, BigQueryModel> ListModels(DatasetReference datasetReference, ListModelsOptions options = null)
+        {
+            GaxPreconditions.CheckNotNull(datasetReference, nameof(datasetReference));
+
+            var pageManager = new ModelPageManager(this);
+            return new RestPagedEnumerable<ListRequest, ListModelsResponse, BigQueryModel>(
+                () => CreateListModelsRequest(datasetReference, options),
+                pageManager);
+        }
+
+        /// <inheritdoc />
+        public override PagedAsyncEnumerable<ListModelsResponse, BigQueryModel> ListModelsAsync(DatasetReference datasetReference, ListModelsOptions options = null)
+        {
+            GaxPreconditions.CheckNotNull(datasetReference, nameof(datasetReference));
+
+            var pageManager = new ModelPageManager(this);
+            return new RestPagedAsyncEnumerable<ListRequest, ListModelsResponse, BigQueryModel>(
+                () => CreateListModelsRequest(datasetReference, options),
+                pageManager);
+        }
+
+        private GetRequest CreateGetModelRequest(ModelReference modelReference, GetModelOptions options)
+        {
+            GaxPreconditions.CheckNotNull(modelReference, nameof(modelReference));
+
+            var request = Service.Models.Get(modelReference.ProjectId, modelReference.DatasetId, modelReference.ModelId);
+            options?.ModifyRequest(request);
+            RetryHandler.MarkAsRetriable(request);
+            return request;
+        }
+
+        private DeleteRequest CreateDeleteModelRequest(ModelReference modelReference, DeleteModelOptions options)
+        {
+            GaxPreconditions.CheckNotNull(modelReference, nameof(modelReference));
+            var request = Service.Models.Delete(modelReference.ProjectId, modelReference.DatasetId, modelReference.ModelId);
+            options?.ModifyRequest(request);
+            return request;
+        }
+
+        private PatchRequest CreatePatchModelRequest(ModelReference modelReference, Model resource, PatchModelOptions options)
+        {
+            GaxPreconditions.CheckNotNull(modelReference, nameof(modelReference));
+            GaxPreconditions.CheckNotNull(resource, nameof(resource));
+            var request = Service.Models.Patch(resource, modelReference.ProjectId, modelReference.DatasetId, modelReference.ModelId);
+            options?.ModifyRequest(request);
+            RetryIfETagPresent(request, resource);
+            return request;
+        }
+
+        private ListRequest CreateListModelsRequest(DatasetReference datasetReference, ListModelsOptions options)
+        {
+            var request = Service.Models.List(datasetReference.ProjectId, datasetReference.DatasetId);
+            options?.ModifyRequest(request);
+            RetryHandler.MarkAsRetriable(request);
+            return request;
+        }
+    }
+}
---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryModel.cs
@@ -0,0 +1,65 @@
+﻿// Copyright 2020 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using Google.Apis.Bigquery.v2.Data;
+
+namespace Google.Cloud.BigQuery.V2
+{
+    /// <summary>
+    /// A model within a BigQuery dataset.
+    /// </summary>
+    /// <remarks>
+    /// This class wraps the underlying REST API resource and retains a reference to the original
+    /// client, allowing for simpler model-oriented operations.
+    /// </remarks>
+    public sealed class BigQueryModel
+    {
+        private readonly BigQueryClient _client;
+
+        /// <summary>
+        /// The underlying REST-ful resource for the model.
+        /// </summary>
+        /// <remarks>
+        /// The data within the resource may be incomplete, depending on how it was obtained.
+        /// (ListModels operations return less data than GetModel operations.)
+        /// </remarks>
+        public Model Resource { get; }
+
+        /// <summary>
+        /// The fully-qualified identifier for the model, as an object which can be used for other operations
+        /// within this API.
+        /// </summary>
+        /// <remarks>
+        /// The properties within the reference can be used to determine the project ID,
+        /// dataset ID and model ID components.
+        /// </remarks>
+        public ModelReference Reference => Resource.ModelReference;
+
+        /// <summary>
+        /// Constructs a new model.
+        /// </summary>
+        /// <remarks>
+        /// This is public to allow tests to construct instances for production code to consume;
+        /// production code should not normally construct instances itself.
+        /// </remarks>
+        /// <param name="client">The client to use for operations on the model. Must not be null.</param>
+        /// <param name="resource">The REST-ful resource representing the model. Must not be null.</param>
+        public BigQueryModel(BigQueryClient client, Model resource)
+        {
+            _client = GaxPreconditions.CheckNotNull(client, nameof(client));
+            Resource = GaxPreconditions.CheckNotNull(resource, nameof(resource));
+        }
+    }
+}
