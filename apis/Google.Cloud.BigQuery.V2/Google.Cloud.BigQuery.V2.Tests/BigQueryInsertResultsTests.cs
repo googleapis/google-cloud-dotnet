89bcc89eae0fbc19fab016fa82eba275cbf81cac
Amanda Tarafa Mas <atarafamas@google.com>
2020-01-28 15:03:24 +0000

    Adds BigQueryInsertResults which wraps the response obtained from insert attempts.

---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.Tests/BigQueryInsertResultsTests.cs
@@ -0,0 +1,352 @@
+﻿// Copyright 2020 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Apis.Bigquery.v2;
+using Google.Apis.Bigquery.v2.Data;
+using Google.Apis.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using static Google.Apis.Bigquery.v2.Data.TableDataInsertAllResponse;
+
+namespace Google.Cloud.BigQuery.V2.Tests
+{
+    public class BigQueryInsertResultsTests
+    {
+        private readonly BigQueryClient _client;
+
+        public BigQueryInsertResultsTests()
+        {
+            _client = new DerivedBigQueryClient();
+        }
+
+        public static IEnumerable<object[]> NotNullableConstructorParametersData
+        {
+            get
+            {
+                yield return new object[] { null, new InsertOptions(), new List<BigQueryInsertRow>().AsReadOnly(), new TableDataInsertAllResponse() };
+                yield return new object[] { new DerivedBigQueryClient(), new InsertOptions(), null, new TableDataInsertAllResponse() };
+                yield return new object[] { new DerivedBigQueryClient(), new InsertOptions(), new List<BigQueryInsertRow>().AsReadOnly(), null };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(NotNullableConstructorParametersData))]
+        public void Constructor_NullParameters_Fail(
+            BigQueryClient client, InsertOptions options, IReadOnlyList<BigQueryInsertRow> rows, TableDataInsertAllResponse response) =>
+            Assert.Throws<ArgumentNullException>(() => new BigQueryInsertResults(client, options, rows, response));
+
+        [Fact]
+        public void Constructor_NullOptions() =>
+            Assert.NotNull(new BigQueryInsertResults(_client, null, new List<BigQueryInsertRow>(), new TableDataInsertAllResponse()));
+
+        public static IEnumerable<object[]> NoErrorsData
+        {
+            get
+            {
+                yield return new object[] { new TableDataInsertAllResponse() };
+                yield return new object[] { new TableDataInsertAllResponse { InsertErrors = new List<InsertErrorsData>() } };
+                yield return new object[] { new TableDataInsertAllResponse { InsertErrors = new List<InsertErrorsData> { null, null, null } } };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(NoErrorsData))]
+        public void AllRowsInserted(TableDataInsertAllResponse response)
+        {
+            BigQueryInsertResults results = new BigQueryInsertResults(
+                _client, 
+                new InsertOptions(), 
+                new List<BigQueryInsertRow> { new BigQueryInsertRow() },
+                response);
+
+            Assert.Equal(BigQueryInsertStatus.AllRowsInserted, results.Status);
+            Assert.Equal(0, results.OriginalRowsWithErrors);
+            Assert.Equal(1, results.InsertAttemptRowCount);
+            Assert.Empty(results.Errors);
+            Assert.Same(results, results.ThrowOnNoneInserted());
+            Assert.Same(results, results.ThrowOnNotAllInserted());
+            Assert.Same(results, results.ThrowOnAnyError());
+        }
+
+        [Fact]
+        public void SomeRowsInserted()
+        {
+            ErrorProto row1Error1 = new ErrorProto { Location = "field_1", Reason = "reason_1", Message = "message_1" };
+            ErrorProto row1Error2 = new ErrorProto { Location = "field_2", Message = "message_2" };
+            InsertErrorsData row1List1 = new InsertErrorsData { Index = 1, Errors = new List<ErrorProto> { row1Error1, row1Error2 } };
+
+            ErrorProto row5Error1 = new ErrorProto { Location = "field_3", Reason = "reason_3" };
+            InsertErrorsData row5List1 = new InsertErrorsData { Index = 5, Errors = new List<ErrorProto> { row5Error1 } };
+
+            InsertErrorsData row6List1 = new InsertErrorsData { Index = 6 };
+
+            ErrorProto row1Error3 = new ErrorProto { Location = "field_4", Reason = "reason_4", Message = "message_4" };
+            InsertErrorsData row1List2 = new InsertErrorsData { Index = 1, Errors = new List<ErrorProto> { row1Error3 } };
+
+            TableDataInsertAllResponse response = new TableDataInsertAllResponse
+            {
+                InsertErrors = new List<InsertErrorsData>
+                {
+                    row1List1,
+                    row5List1,
+                    row6List1,
+                    row1List2
+                }
+            };
+
+            IReadOnlyList<BigQueryInsertRow> rows = Enumerable.Range(0, 8).Select(_ => new BigQueryInsertRow()).ToList().AsReadOnly();
+
+            List<BigQueryInsertRowErrors> expectedInsertRowErrors = new List<BigQueryInsertRowErrors>
+            {
+                new BigQueryInsertRowErrors(rows[1], new List<InsertErrorsData> { row1List1, row1List2 }),
+                new BigQueryInsertRowErrors(rows[5], new List<InsertErrorsData> { row5List1 }),
+                new BigQueryInsertRowErrors(rows[6], new List<InsertErrorsData> { row6List1 }),
+            };
+            List<SingleError> expectedSingleErrors = expectedInsertRowErrors.SelectMany(rowError => rowError).ToList();
+
+            BigQueryInsertResults results = new BigQueryInsertResults(
+                _client,
+                new InsertOptions { SkipInvalidRows = true },
+                rows,
+                response);
+
+            Assert.Equal(BigQueryInsertStatus.SomeRowsInserted, results.Status);
+            Assert.Equal(3, results.OriginalRowsWithErrors);
+            Assert.Equal(8, results.InsertAttemptRowCount);
+            Assert.Equal(expectedInsertRowErrors, results.Errors.ToList(), new BigQueryInsertRowErrorEqualityComparer());
+            Assert.Same(results, results.ThrowOnNoneInserted());
+            AssertException(results.ThrowOnNotAllInserted, expectedSingleErrors, BigQueryInsertStatus.SomeRowsInserted);
+            AssertException(results.ThrowOnAnyError, expectedSingleErrors, BigQueryInsertStatus.SomeRowsInserted);
+        }
+
+        [Fact]
+        public void NoRowsInserted_NoSkip()
+        {
+            ErrorProto row1Error1 = new ErrorProto { Location = "field_1", Reason = "reason_1", Message = "message_1" };
+            ErrorProto row1Error2 = new ErrorProto { Location = "field_2", Message = "message_2" };
+            InsertErrorsData row1List1 = new InsertErrorsData { Index = 1, Errors = new List<ErrorProto> { row1Error1, row1Error2 } };
+
+            ErrorProto row5Error1 = new ErrorProto { Location = "field_3", Reason = "reason_3" };
+            InsertErrorsData row5List1 = new InsertErrorsData { Index = 5, Errors = new List<ErrorProto> { row5Error1 } };
+
+            InsertErrorsData row6List1 = new InsertErrorsData { Index = 6 };
+
+            ErrorProto row1Error3 = new ErrorProto { Location = "field_4", Reason = "reason_4", Message = "message_4" };
+            InsertErrorsData row1List2 = new InsertErrorsData { Index = 1, Errors = new List<ErrorProto> { row1Error3 } };
+
+            TableDataInsertAllResponse response = new TableDataInsertAllResponse
+            {
+                InsertErrors = new List<InsertErrorsData>
+                {
+                    row1List1,
+                    row5List1,
+                    row6List1,
+                    row1List2
+                }
+            };
+
+            IReadOnlyList<BigQueryInsertRow> rows = Enumerable.Range(0, 8).Select(_ => new BigQueryInsertRow()).ToList().AsReadOnly();
+
+            // We don't need to have errors for all rows to know that no rows were inserted.
+            // If SkipInvalidRows is not set to true, then even if only one row has errors, no rows are inserted.
+            List<BigQueryInsertRowErrors> expectedInsertRowErrors = new List<BigQueryInsertRowErrors>
+            {
+                new BigQueryInsertRowErrors(rows[1], new List<InsertErrorsData> { row1List1, row1List2 }),
+                new BigQueryInsertRowErrors(rows[5], new List<InsertErrorsData> { row5List1 }),
+                new BigQueryInsertRowErrors(rows[6], new List<InsertErrorsData> { row6List1 }),
+            };
+            List<SingleError> expectedSingleErrors = expectedInsertRowErrors.SelectMany(rowError => rowError).ToList();
+
+            BigQueryInsertResults results = new BigQueryInsertResults(
+                _client,
+                new InsertOptions(),
+                rows,
+                response);
+
+            Assert.Equal(BigQueryInsertStatus.NoRowsInserted, results.Status);
+            Assert.Equal(3, results.OriginalRowsWithErrors);
+            Assert.Equal(8, results.InsertAttemptRowCount);
+            Assert.Equal(expectedInsertRowErrors, results.Errors.ToList(), new BigQueryInsertRowErrorEqualityComparer());
+            AssertException(results.ThrowOnNoneInserted, expectedSingleErrors, BigQueryInsertStatus.NoRowsInserted);
+            AssertException(results.ThrowOnNotAllInserted, expectedSingleErrors, BigQueryInsertStatus.NoRowsInserted);
+            AssertException(results.ThrowOnAnyError, expectedSingleErrors, BigQueryInsertStatus.NoRowsInserted);
+        }
+
+        [Fact]
+        public void NoRowsInserted_AllInvalid()
+        {
+            ErrorProto row0Error1 = new ErrorProto { Location = "field_1", Reason = "reason_1", Message = "message_1" };
+            ErrorProto row0Error2 = new ErrorProto { Location = "field_2", Message = "message_2" };
+            InsertErrorsData row0List1 = new InsertErrorsData { Index = 0, Errors = new List<ErrorProto> { row0Error1, row0Error2 } };
+
+            ErrorProto row1Error1 = new ErrorProto { Location = "field_3", Reason = "reason_3" };
+            InsertErrorsData row1List1 = new InsertErrorsData { Index = 1, Errors = new List<ErrorProto> { row1Error1 } };
+
+            InsertErrorsData row2List1 = new InsertErrorsData { Index = 2 };
+
+            ErrorProto row0Error3 = new ErrorProto { Location = "field_4", Reason = "reason_4", Message = "message_4" };
+            InsertErrorsData row0List2 = new InsertErrorsData { Index = 0, Errors = new List<ErrorProto> { row0Error3 } };
+
+            TableDataInsertAllResponse response = new TableDataInsertAllResponse
+            {
+                InsertErrors = new List<InsertErrorsData>
+                {
+                    row0List1,
+                    row1List1,
+                    row2List1,
+                    row0List2
+                }
+            };
+
+            IReadOnlyList<BigQueryInsertRow> rows = Enumerable.Range(0, 3).Select(_ => new BigQueryInsertRow()).ToList().AsReadOnly();
+
+            List<BigQueryInsertRowErrors> expectedInsertRowErrors = new List<BigQueryInsertRowErrors>
+            {
+                new BigQueryInsertRowErrors(rows[0], new List<InsertErrorsData> { row0List1, row0List2 }),
+                new BigQueryInsertRowErrors(rows[1], new List<InsertErrorsData> { row1List1 }),
+                new BigQueryInsertRowErrors(rows[2], new List<InsertErrorsData> { row2List1 }),
+            };
+            List<SingleError> expectedSingleErrors = expectedInsertRowErrors.SelectMany(rowError => rowError).ToList();
+
+            BigQueryInsertResults results = new BigQueryInsertResults(
+                _client,
+                new InsertOptions { SkipInvalidRows = true },
+                rows,
+                response);
+
+            Assert.Equal(BigQueryInsertStatus.NoRowsInserted, results.Status);
+            Assert.Equal(3, results.OriginalRowsWithErrors);
+            Assert.Equal(3, results.InsertAttemptRowCount);
+            Assert.Equal(expectedInsertRowErrors, results.Errors.ToList(), new BigQueryInsertRowErrorEqualityComparer());
+            AssertException(results.ThrowOnNoneInserted, expectedSingleErrors, BigQueryInsertStatus.NoRowsInserted);
+            AssertException(results.ThrowOnNotAllInserted, expectedSingleErrors, BigQueryInsertStatus.NoRowsInserted);
+            AssertException(results.ThrowOnAnyError, expectedSingleErrors, BigQueryInsertStatus.NoRowsInserted);
+        }
+
+        [Fact]
+        public void UnexpectedResponse()
+        {
+            ErrorProto row1Error1 = new ErrorProto { Location = "field_1", Reason = "reason_1", Message = "message_1" };
+            ErrorProto row1Error2 = new ErrorProto { Location = "field_2", Message = "message_2" };
+            InsertErrorsData row1List1 = new InsertErrorsData { Index = 1, Errors = new List<ErrorProto> { row1Error1, row1Error2 } };
+
+            ErrorProto row5Error1 = new ErrorProto { Location = "field_3", Reason = "reason_3" };
+            InsertErrorsData row5List1 = new InsertErrorsData { Index = 5, Errors = new List<ErrorProto> { row5Error1 } };
+
+            InsertErrorsData row6List1 = new InsertErrorsData { Index = 6 };
+
+            ErrorProto row1Error3 = new ErrorProto { Location = "field_4", Reason = "reason_4", Message = "message_4" };
+            InsertErrorsData row1List2 = new InsertErrorsData { Index = 1, Errors = new List<ErrorProto> { row1Error3 } };
+
+            // Row 11, which we don't have
+            ErrorProto row11Error1 = new ErrorProto { Location = "field_5", Reason = "reason_5", Message = "message_5" };
+            InsertErrorsData row11List1 = new InsertErrorsData { Index = 11, Errors = new List<ErrorProto> { row11Error1 } };
+
+            // No row index specified
+            ErrorProto rowNullError1 = new ErrorProto { Location = "field_6", Reason = "reason_6", Message = "message_6" };
+            InsertErrorsData rowNullList1 = new InsertErrorsData { Index = null, Errors = new List<ErrorProto> { rowNullError1 } };
+
+            TableDataInsertAllResponse response = new TableDataInsertAllResponse
+            {
+                InsertErrors = new List<InsertErrorsData>
+                {
+                    row1List1,
+                    row5List1,
+                    null, // Ignored
+                    row6List1,
+                    row11List1,
+                    row1List2,
+                    rowNullList1
+                }
+            };
+
+            IReadOnlyList<BigQueryInsertRow> rows = Enumerable.Range(0, 8).Select(_ => new BigQueryInsertRow()).ToList().AsReadOnly();
+
+            List<BigQueryInsertRowErrors> expectedInsertRowErrors = new List<BigQueryInsertRowErrors>
+            {
+                new BigQueryInsertRowErrors(rows[1], new List<InsertErrorsData> { row1List1, row1List2 }),
+                new BigQueryInsertRowErrors(rows[5], new List<InsertErrorsData> { row5List1 }),
+                new BigQueryInsertRowErrors(rows[6], new List<InsertErrorsData> { row6List1 }),
+                new BigQueryInsertRowErrors(null, new List<InsertErrorsData> { row11List1 }),
+                new BigQueryInsertRowErrors(null, new List<InsertErrorsData> { rowNullList1 }),
+            };
+            List<SingleError> expectedSingleErrors = expectedInsertRowErrors.SelectMany(rowError => rowError).ToList();
+
+            BigQueryInsertResults results = new BigQueryInsertResults(
+                _client,
+                new InsertOptions { SkipInvalidRows = true },
+                rows,
+                response);
+
+            Assert.Equal(BigQueryInsertStatus.SomeRowsInserted, results.Status);
+            Assert.Equal(3, results.OriginalRowsWithErrors);
+            Assert.Equal(8, results.InsertAttemptRowCount);
+            Assert.Equal(expectedInsertRowErrors, results.Errors.ToList(), new BigQueryInsertRowErrorEqualityComparer());
+            Assert.Same(results, results.ThrowOnNoneInserted());
+            AssertException(results.ThrowOnNotAllInserted, expectedSingleErrors, BigQueryInsertStatus.SomeRowsInserted);
+            AssertException(results.ThrowOnAnyError, expectedSingleErrors, BigQueryInsertStatus.SomeRowsInserted);
+        }
+
+        private void AssertException(Func<BigQueryInsertResults> throwing, List<SingleError> expectedErrors, BigQueryInsertStatus expectedStatus)
+        {
+            GoogleApiException exception = Assert.Throws<GoogleApiException>(throwing);
+            Assert.Equal("fake_service_name", exception.ServiceName);
+            Assert.Contains(expectedStatus.ToString(), exception.Message);
+            Assert.Equal(expectedErrors, exception.Error.Errors, new SingleErrorEqualityComparer());
+        }
+
+        public class DerivedBigQueryClient : BigQueryClient
+        {
+            private readonly BigqueryService _service = new DerivedBigQueryService();
+            public override BigqueryService Service => _service;
+        }
+
+        public class DerivedBigQueryService : BigqueryService
+        {
+            public override string Name => "fake_service_name";
+        }
+
+        public class BigQueryInsertRowErrorEqualityComparer : IEqualityComparer<BigQueryInsertRowErrors>
+        {
+            public bool Equals(BigQueryInsertRowErrors x, BigQueryInsertRowErrors y)
+            {
+                if (x == y)
+                {
+                    return true;
+                }
+                if (x == null || y == null)
+                {
+                    return false;
+                }
+
+                return x.OriginalRowIndex == y.OriginalRowIndex &&
+                    // This is good enough for the purposes of this test.
+                    x.OriginalRow == y.OriginalRow &&
+                    x.SequenceEqual(y, new SingleErrorEqualityComparer());
+            }
+
+            public int GetHashCode(BigQueryInsertRowErrors obj) => throw new NotImplementedException();
+        }
+
+        public class SingleErrorEqualityComparer : IEqualityComparer<SingleError>
+        {
+            public bool Equals(SingleError x, SingleError y) =>
+                string.Equals(x?.ToString(), y?.ToString(), StringComparison.InvariantCulture);
+
+            public int GetHashCode(SingleError obj) => throw new NotImplementedException();
+        }
+    }
+}
---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryInsertStatus.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 2020 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Google.Cloud.BigQuery.V2
+{
+    /// <summary>
+    /// Specifies the status of an insert attempt depending on the validity of the rows.
+    /// Note that the validity of a row is determined by whether <see cref="InsertOptions.AllowUnknownFields"/>
+    /// is set to true or not.
+    /// If <see cref="InsertOptions.AllowUnknownFields"/> is set to true, then rows containing fields
+    /// that do not match any on the destination table will, otherwise, be considered valid rows.
+    /// All matching fields in these rows will be inserted, while unmatching fields will be silently skipped.
+    /// If <see cref="InsertOptions.AllowUnknownFields"/> is not set to true, then rows containing fields
+    /// that do not match any on the destination table will be considered invalid and won't be inserted.
+    /// </summary>
+    public enum BigQueryInsertStatus
+    {
+        /// <summary>
+        /// All rows were inserted successfully.
+        /// This only happens if all rows are valid rows.
+        /// </summary>
+        AllRowsInserted,
+
+        /// <summary>
+        /// Some rows were inserted but not others. In that case the <see cref="BigQueryInsertResults"/>
+        /// returned by the insert operation will contain error information about the rows that were not inserted.
+        /// This happens only when <see cref="InsertOptions.SkipInvalidRows"/> is set to true and some, 
+        /// but not all of the rows, are invalid.
+        /// </summary>
+        SomeRowsInserted,
+
+        /// <summary>
+        /// No rows were inserted. In that case the <see cref="BigQueryInsertResults"/>
+        /// returned by the insert operation will contain error information about the invalid rows.
+        /// This can happen when:
+        /// <list type="bullet">
+        /// <item> No rows are valid.</item>
+        /// <item> <see cref="InsertOptions.SkipInvalidRows"/> is not set to true and at least
+        /// one row is invalid.</item>
+        /// </list>
+        /// </summary>
+        NoRowsInserted
+    }
+}
