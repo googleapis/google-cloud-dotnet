9a2f966db40745b25dc69510c4156b7490015a94
Amanda Tarafa Mas <atarafamas@google.com>
2020-01-28 15:03:24 +0000

    Adds BigQueryInsertResults which wraps the response obtained from insert attempts.

---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.Tests/BigQueryInsertRowErrorsTests.cs
@@ -0,0 +1,206 @@
+﻿// Copyright 2020 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Apis.Bigquery.v2.Data;
+using Google.Apis.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using static Google.Apis.Bigquery.v2.Data.TableDataInsertAllResponse;
+using static Google.Cloud.BigQuery.V2.Tests.BigQueryInsertResultsTests;
+
+namespace Google.Cloud.BigQuery.V2.Tests
+{
+    public class BigQueryInsertRowErrorsTests
+    {
+        [Fact]
+        public void Constructor_NullErrors()
+        {
+            Assert.Throws<ArgumentException>(() => new BigQueryInsertRowErrors(new BigQueryInsertRow(), null));
+        }
+
+        [Fact]
+        public void Constructor_EmptyErrors()
+        {
+            Assert.Throws<ArgumentException>(() => new BigQueryInsertRowErrors(
+                new BigQueryInsertRow(),
+                new List<InsertErrorsData>()));
+        }
+
+        [Fact]
+        public void Constructor_NullRow_NoErrorDetail()
+        {
+            List<InsertErrorsData> errors = new List<InsertErrorsData>
+            {
+                new InsertErrorsData { }
+            };
+
+            SingleError expected = new SingleError
+            {
+                Message = "Error in row unknown."
+            };
+
+            // This is allowed in case there's no information (index)
+            // to associate errors to a row.
+            BigQueryInsertRowErrors insertRowErrors = new BigQueryInsertRowErrors(null, errors);
+            Assert.Single(insertRowErrors);
+            Assert.Contains(expected, insertRowErrors, new SingleErrorEqualityComparer());
+            Assert.Null(insertRowErrors.OriginalRow);
+            Assert.Null(insertRowErrors.OriginalRowIndex);
+        }
+
+        [Fact]
+        public void Constructor_NullRow_ErrorDetail()
+        {
+            List<InsertErrorsData> errors = new List<InsertErrorsData>
+            {
+                new InsertErrorsData
+                {
+                    Index = null,
+                    Errors = new List<ErrorProto>
+                    {
+                        new ErrorProto
+                        {
+                            Reason = "Very bad reason",
+                            Message = "A very bad error occurred"
+                        }
+                    }
+                }
+            };
+
+            SingleError expected = new SingleError
+            {
+                Reason = "Very bad reason",
+                Message = "Error in row unknown. A very bad error occurred"
+            };
+
+            // This is allowed in case there's no information (index)
+            // to associate errors to a row.
+            BigQueryInsertRowErrors insertRowErrors = new BigQueryInsertRowErrors(null, errors);
+            Assert.Single(insertRowErrors);
+            Assert.Contains(expected, insertRowErrors, new SingleErrorEqualityComparer());
+            Assert.Null(insertRowErrors.OriginalRow);
+            Assert.Null(insertRowErrors.OriginalRowIndex);
+        }
+
+        [Fact]
+        public void OriginalRow()
+        {
+            List<InsertErrorsData> errors = new List<InsertErrorsData>
+            {
+                new InsertErrorsData
+                {
+                    Index = 1,
+                    Errors = new List<ErrorProto>
+                    {
+                        new ErrorProto
+                        {
+                            Reason = "Reason 1",
+                            Message = "A very bad error 1 occurred",
+                            Location = "field_1"
+                        },
+                    }
+                },
+            };
+
+            IList<SingleError> expected = new List<SingleError>
+            {
+                new SingleError
+                {
+                    Reason = "Reason 1",
+                    Message = "Error in row 1. A very bad error 1 occurred",
+                    Location = "field_1"
+                },
+            };
+
+            BigQueryInsertRow row = new BigQueryInsertRow();
+
+            BigQueryInsertRowErrors insertRowErrors = new BigQueryInsertRowErrors(row, errors);
+            Assert.Equal(expected, insertRowErrors.ToList(), new SingleErrorEqualityComparer());
+            Assert.Same(row, insertRowErrors.OriginalRow);
+            Assert.Equal(1, insertRowErrors.OriginalRowIndex);
+        }
+
+        [Fact]
+        public void GetEnumerator()
+        {
+            List<InsertErrorsData> errors = new List<InsertErrorsData>
+            {
+                new InsertErrorsData
+                {
+                    Index = 1,
+                    Errors = new List<ErrorProto>
+                    {
+                        new ErrorProto
+                        {
+                            Reason = "Reason 1",
+                            Message = "A very bad error 1 occurred",
+                            Location = "field_1"
+                        },
+                        new ErrorProto
+                        {
+                            Reason = "Reason 2",
+                            Location = "field_2"
+                        }
+                    }
+                },
+                new InsertErrorsData { },
+                new InsertErrorsData
+                {
+                    Index = 1,
+                    Errors = new List<ErrorProto>
+                    {
+                        new ErrorProto
+                        {
+                            Reason = "Reason 3",
+                            Message = "A very bad error 3 occurred",
+                            Location = "field_3"
+                        }
+                    }
+                },
+                null
+            };
+
+            IList<SingleError> expected = new List<SingleError>
+            {
+                new SingleError
+                {
+                    Reason = "Reason 1",
+                    Message = "Error in row 1. A very bad error 1 occurred",
+                    Location = "field_1"
+                },
+                new SingleError
+                {
+                    Reason = "Reason 2",
+                    Message = "Error in row 1. ",
+                    Location = "field_2"
+                },
+                new SingleError
+                {
+                    Reason = "Reason 3",
+                    Message = "Error in row 1. A very bad error 3 occurred",
+                    Location = "field_3"
+                }
+            };
+
+            // This is allowed in case there's no information (index)
+            // to associate errors to a row or the index obtained in the response is out of range (unlikely).
+            BigQueryInsertRowErrors insertRowErrors = new BigQueryInsertRowErrors(null, errors);
+            Assert.Equal(expected, insertRowErrors.ToList(), new SingleErrorEqualityComparer());
+            Assert.Null(insertRowErrors.OriginalRow);
+            Assert.Equal(1, insertRowErrors.OriginalRowIndex);
+        }
+    }
+}
---
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryInsertRowErrors.cs
@@ -0,0 +1,91 @@
+﻿// Copyright 2020 Google Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax;
+using Google.Apis.Requests;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using static Google.Apis.Bigquery.v2.Data.TableDataInsertAllResponse;
+
+namespace Google.Cloud.BigQuery.V2
+{
+    /// <summary>
+    /// Represents insert errors related to one row.
+    /// </summary>
+    public sealed class BigQueryInsertRowErrors : IEnumerable<SingleError>
+    {
+        private readonly IReadOnlyList<InsertErrorsData> _errors;
+
+        /// <summary>
+        /// The original row to which these errors are related to.
+        /// </summary>
+        /// <remarks>
+        /// Some errors obtained after an insert request might not contain enough
+        /// information so as to associate them with one row.
+        /// In such cases this may be null.
+        /// </remarks>
+        public BigQueryInsertRow OriginalRow { get; }
+
+        /// <summary>
+        /// The index of the row these errors relate to in the original
+        /// inser request. Might be unknown.
+        /// </summary>
+        public long? OriginalRowIndex => _errors[0].Index;
+
+        /// <summary>
+        /// Returns an iterator over the insert errors.
+        /// </summary>
+        /// <returns>An iterator over the insert errors.</returns>
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <summary>
+        /// Constructs a new set of insert errors related to one insert row.
+        /// </summary>
+        /// <param name="originalRow">The original row to which these errors are related to if known. May be null.</param>
+        /// <param name="errors">The errors information.</param>
+        public BigQueryInsertRowErrors(BigQueryInsertRow originalRow, IReadOnlyList<InsertErrorsData> errors)
+        {
+            GaxPreconditions.CheckArgument(errors?.Count > 0, nameof(errors), "Can't be null or empty");
+            _errors = errors;
+            OriginalRow = originalRow;
+        }
+
+        /// <summary>
+        /// Returns an interator over the insert errors.
+        /// </summary>
+        /// <returns>An iterator over the insert errors.</returns>
+        public IEnumerator<SingleError> GetEnumerator()
+        {
+            string messagePrefix = $"Error in row {OriginalRowIndex?.ToString() ?? "unknown"}.";
+
+            IEnumerable<SingleError> singleErrors = from error in _errors
+                                                    where error?.Errors != null
+                                                    from detail in error.Errors
+                                                    where detail != null
+                                                    select new SingleError
+                                                    {
+                                                        Location = detail.Location,
+                                                        Reason = detail.Reason,
+                                                        Message = $"{messagePrefix} {detail.Message ?? string.Empty}"
+                                                    };
+            if (!singleErrors.Any())
+            {
+                // No error details, just unknown errors potentially associated to a row.
+                return Enumerable.Repeat(new SingleError { Message = messagePrefix }, 1).GetEnumerator();
+            }
+            return singleErrors.GetEnumerator();
+        }
+    }
+}
