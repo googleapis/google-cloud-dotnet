e429a27b5cad604c9e98acc626dad113b1650618
Jon Skeet <jonskeet@google.com>
2020-01-30 15:01:19 +0000

    Fix Grafeas code modifier for Create method changes in generator
    We now *do* use a GrafeasClientBuilder, as it's used for everything "non-default".
    It should be fine to either provide a CallInvoker directly, or an endpoint... the default credentials would only work for GCP, but that's not too bad.

---
+++ apis/Grafeas.V1/Grafeas.V1/GrafeasClientBuilder.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Api.Gax.Grpc;
+using System;
+
+namespace Grafeas.V1
+{
+    // Partial class for GrafeasClientBuilder, to work around there being no default endpoint/scopes.
+    public partial class GrafeasClientBuilder
+    {
+        /// <inheritdoc />
+        protected override void Validate()
+        {
+            base.Validate();
+            // We can handle Endpoint + (credentials or scopes), or a CallInvoker
+            if (Endpoint is object && (ChannelCredentials is object || Scopes is object))
+            {
+                return;
+            }
+            if (CallInvoker is object)
+            {
+                return;
+            }
+            throw new InvalidOperationException($"Either Endpoint or CallInvoker must be set");
+        }
+
+        /// <inheritdoc />
+        protected override ChannelPool GetChannelPool() =>
+            throw new InvalidOperationException("GrafeasClient has no channel pool");
+
+        /// <inheritdoc />
+        protected override string GetDefaultEndpoint() =>
+            throw new InvalidOperationException("Grafeas has no default endpoint");
+    }
+}
