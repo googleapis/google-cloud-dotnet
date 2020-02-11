googleapis/google-cloud-dotnet

commit b33e8e350aa5088e23fa5ec7d1cb5e0c80ad0b1f

PR comments for ListRows

apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryClientImpl.Queries.cs
@@ -128,7 +128,9 @@ namespace Google.Cloud.BigQuery.V2
         public override PagedEnumerable<TableDataList, BigQueryRow> ListRows(TableReference tableReference, TableSchema schema = null, ListRowsOptions options = null)
         {
             GaxPreconditions.CheckNotNull(tableReference, nameof(tableReference));
-            var resultSchema = schema ?? GetSchema(tableReference);
+            // There's no way to specify fetching rows with no fields (that looks more like a dry run anyways).
+            // So, if the schema is empty, the whole rows will be fetch, so we need to get the whole schema.
+            var resultSchema = schema?.Fields?.Count > 0 ? schema : GetSchema(tableReference);
 
             var pageManager = new TableRowPageManager(resultSchema);
             return new RestPagedEnumerable<TabledataResource.ListRequest, TableDataList, BigQueryRow>(
@@ -143,7 +145,9 @@ namespace Google.Cloud.BigQuery.V2
             GaxPreconditions.CheckNotNull(tableReference, nameof(tableReference));
             // TODO: This is a synchronous call. We can't easily make this part asynchronous - we don't have a cancellation token, and we're returning
             // a non-task value. We could defer until the first MoveNext call, but that's tricky.
-            var resultSchema = schema ?? GetSchema(tableReference);
+            // There's no way to specify fetching rows with no fields (that looks more like a dry run anyways).
+            // So, if the schema is empty, the whole rows will be fetch, so we need to get the whole schema.
+            var resultSchema = schema?.Fields?.Count > 0 ? schema : GetSchema(tableReference);
 
             var pageManager = new TableRowPageManager(schema);
             return new RestPagedAsyncEnumerable<TabledataResource.ListRequest, TableDataList, BigQueryRow>(
@@ -188,10 +192,9 @@ namespace Google.Cloud.BigQuery.V2
         {
             var request = Service.Tabledata.List(tableReference.ProjectId, tableReference.DatasetId, tableReference.TableId);
             options?.ModifyRequest(request);
-            if (schema != null)
-            {
-                request.SelectedFields = schema.BuildSelectedFields();
-            }
+            // null and empty schemas are handled by BuildSelectedFields,
+            // but both values mean the same, and that is to return whole rows.
+            request.SelectedFields = schema.BuildSelectedFields();
             RetryHandler.MarkAsRetriable(request);
             return request;
         }

apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.IntegrationTests/QueryTest.cs
@@ -727,19 +727,40 @@ namespace Google.Cloud.BigQuery.V2.IntegrationTests
             {
                 SelectedFields = "age,gender,children.gender,children.age"
             };
+
+            // Obtain the table's partial schema.
             var table = client.GetTable(datasetId, tableId, options);
-            // Making sure we grab a row of a person with children.
-            var row = client.ListRows(datasetId, tableId, table.Schema).First(row => ((Dictionary<string, object>[])row["children"]).Length > 0);
+            // Use the partial schema to obtain partial rows.
+            var rows = client.ListRows(datasetId, tableId, table.Schema);
+            // Make sure we grab a row of a person with children for testing fields that should be present.
+            var row = rows.First(row => ((Dictionary<string, object>[])row["children"])?.Length > 0);
+
             // These should be present
             Assert.NotNull(row["age"]);
             Assert.NotNull(row["gender"]);
-            var children = (Dictionary<string, object>[])row["children"];
+            var children = row["children"] as Dictionary<string, object>[];
             Assert.NotNull(children);
             Assert.NotNull(children[0]["gender"]);
             Assert.NotNull(children[0]["age"]);
             // These shouldn't
             Assert.Throws<KeyNotFoundException>(() => row["fullName"]);
             Assert.Throws<KeyNotFoundException>(() => children[0]["name"]);
+        }
+
+        [Fact]
+        public void ListRows_EmptySchema()
+        {
+            var client = BigQueryClient.Create(_fixture.ProjectId);
+            string datasetId = _fixture.DatasetId;
+            string tableId = _fixture.PeopleTableId;
+
+            // Use an empty schema to obtain the rows.
+            // We should get full rows.
+            var rows = client.ListRows(datasetId, tableId, new TableSchema()).ToList();
+
+            // The People table has 7 top level fields. We should have all fields.
+            object dummy;
+            Assert.All(rows, row => dummy = row[6]);
         }
 
         private class TitleComparer : IEqualityComparer<BigQueryRow>

apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.Tests/TableSchemaExtensionsTest.cs
@@ -43,14 +43,21 @@ namespace Google.Cloud.BigQuery.V2.Tests
         }
 
         [Fact]
-        public void SelectedFields_EmptySchema()
+        public void BuildSelectedFields_NullSchema()
+        {
+            TableSchema schema = null;
+            Assert.Null(schema.BuildSelectedFields());
+        }
+
+        [Fact]
+        public void BuildSelectedFields_EmptySchema()
         {
             var schema = new TableSchema();
             Assert.Equal(string.Empty, schema.BuildSelectedFields());
         }
 
         [Fact]
-        public void SelectedFields_OneField()
+        public void BuildSelectedFields_OneField()
         {
             var schema = new TableSchema
             {
@@ -64,7 +71,7 @@ namespace Google.Cloud.BigQuery.V2.Tests
         }
 
         [Fact]
-        public void SelectedFields_OneLevel()
+        public void BuildSelectedFields_OneLevel()
         {
             var schema = new TableSchema
             {
@@ -79,7 +86,7 @@ namespace Google.Cloud.BigQuery.V2.Tests
         }
 
         [Fact]
-        public void SelectedFields_Nested()
+        public void BuildSelectedFields_Nested()
         {
             var schema = new TableSchema
             {

apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryClient.Queries.cs
@@ -226,7 +226,9 @@ namespace Google.Cloud.BigQuery.V2
         /// </remarks>
         /// <param name="datasetId">The dataset ID. Must not be null.</param>
         /// <param name="tableId">The table ID. Must not be null.</param>
-        /// <param name="schema">The schema to use when interpreting results. This may be null, in which case it will be fetched from
+        /// <param name="schema">The schema to use when interpreting results. If this is a partial schema, then partial rows
+        /// will be fetched. See <see cref="GetTableOptions.SelectedFields"/> for how to obtain a table's partial schema.
+        /// This may be null or empty (i.e. <see cref="TableSchema.Fields"/> null or empty), in which case it will be fetched from
         /// the table first.</param>
         /// <param name="options">The options for the operation. May be null, in which case defaults will be supplied.</param>
         /// <returns>The results of listing the rows within the table.</returns>
@@ -243,7 +245,9 @@ namespace Google.Cloud.BigQuery.V2
         /// or service failures can cause exceptions even after the first results have been returned.
         /// </remarks>
         /// <param name="tableReference">A fully-qualified identifier for the table. Must not be null.</param>
-        /// <param name="schema">The schema to use when interpreting results. This may be null, in which case it will be fetched from
+        /// <param name="schema">The schema to use when interpreting results. If this is a partial schema, then partial rows
+        /// will be fetched. See <see cref="GetTableOptions.SelectedFields"/> for how to obtain a table's partial schema.
+        /// This may be null or empty (i.e. <see cref="TableSchema.Fields"/> null or empty), in which case it will be fetched from
         /// the table first.</param>
         /// <param name="options">The options for the operation. May be null, in which case defaults will be supplied.</param>
         /// <returns>The results of listing the rows within the table.</returns>

apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/TableSchemaExtensions.cs
@@ -43,33 +43,52 @@ namespace Google.Cloud.BigQuery.V2
 
         internal static string BuildSelectedFields(this TableSchema schema)
         {
-            if (!(schema?.Fields?.Count > 0))
+            // Althouhg null and empty SelectedFields mean the same, that is to return
+            // whole rows, let's return null for a null schema and string.Empty for an
+            // empty schema, just in case these get diferentiated in the future. And also
+            // it's clearer.
+            if (schema == null)
+            {
+                return null;
+            }
+            if (!(schema.Fields?.Count > 0))
             {
                 return string.Empty;
             }
 
+            // This list will contain the paths to all terminal fields that we have found.
+            // In the end this list will contain all selected fields.
             var built = new List<string>();
-            var currentField = new List<string>();
-            BuildSelectedFields(schema.Fields, currentField, built);
+            // This list will contain the path to the field we are currently traversing.
+            // If it is a terminal field (no nested fields) then we build the path to it
+            // and backtrack to the previous level.
+            // If it is not terminal, then we just traverse (in depth) its nested fields.
+            var currentPath = new List<string>();
+            BuildSelectedFields(schema.Fields, currentPath, built);
             return string.Join(",", built);
         }
 
-        private static void BuildSelectedFields(IList<TableFieldSchema> fieldSchemas, IList<string> currentField, IList<string> built)
+        private static void BuildSelectedFields(IList<TableFieldSchema> fieldSchemas, IList<string> currentPath, IList<string> built)
         {
-            // Whatever we have in currentField is terminal.
-            // Build that an we are done.
             if (!(fieldSchemas?.Count > 0))
             {
-                built.Add(string.Join(".", currentField));
+                // Whatever we have in currentPath is terminal.
+                // Build that and we are done.
+                built.Add(string.Join(".", currentPath));
             }
             else
             {
-                int removeAtIndex = currentField.Count;
-                foreach(var field in fieldSchemas)
+                // For each field at this level, we'll add it to the current path
+                // and then go in depth into its possible nested fields.
+                // When we return from the recursive call, we can remove this field
+                // from the current path. It will always be at the last index, which
+                // will always be one more than what we have right now.
+                int removeAtIndex = currentPath.Count;
+                foreach (var field in fieldSchemas)
                 {
-                    currentField.Add(field.Name);
-                    BuildSelectedFields(field.Fields, currentField, built);
-                    currentField.RemoveAt(removeAtIndex);
+                    currentPath.Add(field.Name);
+                    BuildSelectedFields(field.Fields, currentPath, built);
+                    currentPath.RemoveAt(removeAtIndex);
                 }
             }
         }

commit 8f868e3c486bf97a2c64af8e36610decfad5d9ba

Fixes issue introduced in #4073.   * Moves ListRows tests to correct files.   * Adds testing for ListRowsAsync.

apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryClientImpl.Queries.cs
@@ -149,7 +149,7 @@ namespace Google.Cloud.BigQuery.V2
             // So, if the schema is empty, the whole rows will be fetch, so we need to get the whole schema.
             var resultSchema = schema?.Fields?.Count > 0 ? schema : GetSchema(tableReference);
 
-            var pageManager = new TableRowPageManager(schema);
+            var pageManager = new TableRowPageManager(resultSchema);
             return new RestPagedAsyncEnumerable<TabledataResource.ListRequest, TableDataList, BigQueryRow>(
                 // Pass the original schema, if it was null then the whole table will be fetch and we don't need to
                 // specify selected fields.

apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.IntegrationTests/ListRowsTest.cs
@@ -13,10 +13,12 @@
 // limitations under the License.
 
 
+using Google.Apis.Bigquery.v2.Data;
 using Google.Cloud.ClientTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Google.Cloud.BigQuery.V2.IntegrationTests
@@ -93,6 +95,70 @@ namespace Google.Cloud.BigQuery.V2.IntegrationTests
 
             Func<BigQueryRow, string> projection = r => $"{r["gameStarted"]:o} {r["player"]} {r["score"]}";
             Assert.Equal(rows1.Select(projection), rows2.Select(projection));
+        }
+
+        [Fact]
+        public async Task ListRows_PartialSchema()
+        {
+            var client = BigQueryClient.Create(_fixture.ProjectId);
+            string datasetId = _fixture.DatasetId;
+            string tableId = _fixture.PeopleTableId;
+            var options = new GetTableOptions
+            {
+                SelectedFields = "age,gender,children.gender,children.age"
+            };
+
+            // Obtain the table's partial schema.
+            var table = client.GetTable(datasetId, tableId, options);
+            // Use the partial schema to obtain partial rows.
+            // We are testing both the sync and async versions.
+            var rowsAsync = client.ListRowsAsync(datasetId, tableId, table.Schema);
+            var rows = client.ListRows(datasetId, tableId, table.Schema);
+            // Make sure we grab a row of a person with children for testing fields that should be present.
+            var rowAsyncTask = rowsAsync.FirstAsync(row => ((Dictionary<string, object>[])row["children"])?.Length > 0);
+            var rowSync = rows.First(row => ((Dictionary<string, object>[])row["children"])?.Length > 0);
+            var rowAsync = await rowAsyncTask.ConfigureAwait(false);
+
+            AssertPartialRow(rowSync);
+            AssertPartialRow(rowAsync);
+
+            void AssertPartialRow(BigQueryRow row)
+            {
+                // These should be present
+                Assert.NotNull(row["age"]);
+                Assert.NotNull(row["gender"]);
+                var children = row["children"] as Dictionary<string, object>[];
+                Assert.NotNull(children);
+                Assert.NotNull(children[0]["gender"]);
+                Assert.NotNull(children[0]["age"]);
+                // These shouldn't
+                Assert.Throws<KeyNotFoundException>(() => row["fullName"]);
+                Assert.Throws<KeyNotFoundException>(() => children[0]["name"]);
+            }
+        }
+
+        [Fact]
+        public async Task ListRows_EmptySchema()
+        {
+            var client = BigQueryClient.Create(_fixture.ProjectId);
+            string datasetId = _fixture.DatasetId;
+            string tableId = _fixture.PeopleTableId;
+
+            // Use an empty schema to obtain the rows.
+            // We should get full rows.
+            var rowsAsyncTask = client.ListRowsAsync(datasetId, tableId, new TableSchema()).ToListAsync();
+            var rowsSync = client.ListRows(datasetId, tableId, new TableSchema()).ToList();
+            var rowsAsync = await rowsAsyncTask.ConfigureAwait(false);
+
+            AssertFullRows(rowsSync);
+            AssertFullRows(rowsAsync);
+
+            void AssertFullRows(IList<BigQueryRow> rows)
+            {
+                // The People table has 7 top level fields. We should have all fields.
+                object dummy;
+                Assert.All(rows, row => dummy = row[6]);
+            }
         }
     }
 }

apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.IntegrationTests/QueryTest.cs
@@ -717,52 +717,6 @@ namespace Google.Cloud.BigQuery.V2.IntegrationTests
             Assert.Equal(DateTime.MinValue, (DateTime) row["x"]);
         }
 
-        [Fact]
-        public void ListRows_PartialSchema()
-        {
-            var client = BigQueryClient.Create(_fixture.ProjectId);
-            string datasetId = _fixture.DatasetId;
-            string tableId = _fixture.PeopleTableId;
-            var options = new GetTableOptions
-            {
-                SelectedFields = "age,gender,children.gender,children.age"
-            };
-
-            // Obtain the table's partial schema.
-            var table = client.GetTable(datasetId, tableId, options);
-            // Use the partial schema to obtain partial rows.
-            var rows = client.ListRows(datasetId, tableId, table.Schema);
-            // Make sure we grab a row of a person with children for testing fields that should be present.
-            var row = rows.First(row => ((Dictionary<string, object>[])row["children"])?.Length > 0);
-
-            // These should be present
-            Assert.NotNull(row["age"]);
-            Assert.NotNull(row["gender"]);
-            var children = row["children"] as Dictionary<string, object>[];
-            Assert.NotNull(children);
-            Assert.NotNull(children[0]["gender"]);
-            Assert.NotNull(children[0]["age"]);
-            // These shouldn't
-            Assert.Throws<KeyNotFoundException>(() => row["fullName"]);
-            Assert.Throws<KeyNotFoundException>(() => children[0]["name"]);
-        }
-
-        [Fact]
-        public void ListRows_EmptySchema()
-        {
-            var client = BigQueryClient.Create(_fixture.ProjectId);
-            string datasetId = _fixture.DatasetId;
-            string tableId = _fixture.PeopleTableId;
-
-            // Use an empty schema to obtain the rows.
-            // We should get full rows.
-            var rows = client.ListRows(datasetId, tableId, new TableSchema()).ToList();
-
-            // The People table has 7 top level fields. We should have all fields.
-            object dummy;
-            Assert.All(rows, row => dummy = row[6]);
-        }
-
         private class TitleComparer : IEqualityComparer<BigQueryRow>
         {
             public bool Equals(BigQueryRow x, BigQueryRow y) => (string)x["title"] == (string)y["title"];