3ef581b446511c4a73481822e4d53a7dba15a852
Jon Skeet <jonskeet@google.com>
2020-02-13 16:51:15 +0000

    Regenerate Container.V1 after the protos have been fixed to avoid "required deprecated" fields
    We now don't need the patch file.

--- apis/Google.Cloud.Container.V1/Google.Cloud.Container.V1/ClusterManagerClient.g.cs
+++ apis/Google.Cloud.Container.V1/Google.Cloud.Container.V1/ClusterManagerClient.g.cs
@@ -14,11 +14,6 @@
 
 // Generated code. DO NOT EDIT!
 
-// Ignore obsolete members within this file.
-// This is currently a hand-written addition to the generated file, until the
-// generator includes it automatically.
-#pragma warning disable CS0612 // Type or member is obsolete
-
 using gax = Google.Api.Gax;
 using gaxgrpc = Google.Api.Gax.Grpc;
 using proto = Google.Protobuf;
