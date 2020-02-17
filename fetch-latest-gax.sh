16b83c392964d239799e3a2e92e29164c472e90b
Jon Skeet <jonskeet@google.com>
2020-02-17 07:52:04 +0000

    Stop building a local copy of GAX

--- fetch-latest-gax.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Fetching and building GAX"
-
-git clone -q https://github.com/googleapis/gax-dotnet.git
-(cd gax-dotnet && dotnet pack -v quiet -c Release -o $PWD/nuget Gax.sln)
