3c900edda011b2dd092cd691ab274b977455bff0
Jon Skeet <jonskeet@google.com>
2020-02-19 07:51:25 +0000

    Remove obsolete BigQueryResults constructor

--- apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryResults.cs
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2/BigQueryResults.cs
@@ -104,24 +104,6 @@ namespace Google.Cloud.BigQuery.V2
         /// <param name="response">The response to a GetQueryResults API call. Must not be null.</param>
         /// <param name="tableReference">A reference to the table containing the results.
         /// May be null. (For example, script queries don't store results in tables.)</param>
-        /// <param name="options">Options to use when listing rows. May be null.</param>
-        [Obsolete("Please use the constructor accepting a GetQueryResultsOptions instead")]
-        public BigQueryResults(BigQueryClient client, GetQueryResultsResponse response, TableReference tableReference, ListRowsOptions options)
-            : this(client, response, tableReference, options?.ToGetQueryResultsOptions())
-        {
-        }
-
-        /// <summary>
-        /// Constructs a new set of results.
-        /// </summary>
-        /// <remarks>
-        /// This is public to allow tests to construct instances for production code to consume;
-        /// production code should not normally construct instances itself.
-        /// </remarks>
-        /// <param name="client">The client to use for further operations. Must not be null.</param>
-        /// <param name="response">The response to a GetQueryResults API call. Must not be null.</param>
-        /// <param name="tableReference">A reference to the table containing the results.
-        /// May be null. (For example, script queries don't store results in tables.)</param>
         /// <param name="options">Options to use when fetching results. May be null.</param>
         public BigQueryResults(BigQueryClient client, GetQueryResultsResponse response, TableReference tableReference, GetQueryResultsOptions options)
         {
