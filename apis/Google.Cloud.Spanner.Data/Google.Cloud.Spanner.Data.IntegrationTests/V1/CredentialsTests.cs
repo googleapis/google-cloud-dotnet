108e5924c8169608d2ff4d6f733dffd0e77625fe
Amanda Tarafa Mas <atarafamas@google.com>
2020-02-20 10:43:11 +0000

    Adds more channel/call credential clash tests, including a possible workaround showcase, which doesn't work.

--- apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.IntegrationTests/V1/CredentialsTests.cs
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.IntegrationTests/V1/CredentialsTests.cs
@@ -19,6 +19,7 @@ using Grpc.Auth;
 using Grpc.Core;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Google.Cloud.Spanner.V1.IntegrationTests
@@ -42,7 +43,7 @@ namespace Google.Cloud.Spanner.V1.IntegrationTests
         #region Sanity check tests, just to make sure that the credentials are as described.
 
         [Fact]
-        public void WithSpannerAccess()
+        public void GoodChannel_NoCall()
         {
             var builder = new SpannerClientBuilder
             {
@@ -58,7 +59,7 @@ namespace Google.Cloud.Spanner.V1.IntegrationTests
         }
 
         [Fact]
-        public void WithouSpannerAccess()
+        public void BadChannel_NoCall()
         {
             var builder = new SpannerClientBuilder
             {
@@ -74,12 +75,76 @@ namespace Google.Cloud.Spanner.V1.IntegrationTests
             Assert.Equal(StatusCode.PermissionDenied, exception.StatusCode);
         }
 
+        [Fact]
+        public void NoChannel_GoodCall()
+        {
+            var builder = new SpannerClientBuilder
+            {
+                // Don't add a call credential to the channel credential,
+                // just the credential needed to establish the TSL connection.
+                ChannelCredentials = new SslCredentials()
+            };
+            var client = builder.Build();
+
+            // Per call, a call credential is needed.
+            var sessions = client.ListSessions(
+                new ListSessionsRequest { DatabaseAsDatabaseName = _fixture.DatabaseName },
+                CallSettings.FromCallCredentials(_withSpannerAccess.ToCallCredentials()));
+
+            // We really don't care whether we get sessions or not.
+            // Just ensure that the call gets to the server.
+            Assert.True(sessions.Count() >= 0);
+        }
+
+        [Fact]
+        public void NoChannel_BadCall()
+        {
+            var builder = new SpannerClientBuilder
+            {
+                // Don't add a call credential to the channel credential,
+                // just the credential needed to establish the TSL connection.
+                ChannelCredentials = new SslCredentials()
+            };
+            var client = builder.Build();
+
+            // Per call, a call credential is needed.
+            var sessions = client.ListSessions(
+                new ListSessionsRequest { DatabaseAsDatabaseName = _fixture.DatabaseName },
+                CallSettings.FromCallCredentials(_withoutSpannerAccess.ToCallCredentials()));
+
+            // We really don't care whether we get sessions or not.
+            // Just ensure that the call gets to the server.
+            var exception = Assert.Throws<RpcException>(() => sessions.Count());
+            Assert.Equal(StatusCode.PermissionDenied, exception.StatusCode);
+        }
+
+        [Fact]
+        public void NoChannel_NoCall()
+        {
+            var builder = new SpannerClientBuilder
+            {
+                // Don't add a call credential to the channel credential,
+                // just the credential needed to establish the TSL connection.
+                ChannelCredentials = new SslCredentials()
+            };
+            var client = builder.Build();
+
+            // Don't add a credential to the call either.
+            var sessions = client.ListSessions(
+                new ListSessionsRequest { DatabaseAsDatabaseName = _fixture.DatabaseName });
+
+            // We really don't care whether we get sessions or not.
+            // Just ensure that the call gets to the server.
+            var exception = Assert.Throws<RpcException>(() => sessions.Count());
+            Assert.Equal(StatusCode.Unauthenticated, exception.StatusCode);
+        }
+
         #endregion
 
         #region Tests that test for credential clashing
 
         [Fact]
-        public void GoodChannelCred_BadCallCred_ListOperation()
+        public void GoodChannel_BadCall_ListOperation()
         {
             var builder = new SpannerClientBuilder
             {
@@ -100,7 +165,7 @@ namespace Google.Cloud.Spanner.V1.IntegrationTests
         }
 
         [Fact]
-        public void BadChannelCred_GoodCallCred_ListOperation()
+        public void BadChannel_GoodCall_ListOperation()
         {
             var builder = new SpannerClientBuilder
             {
@@ -120,7 +185,7 @@ namespace Google.Cloud.Spanner.V1.IntegrationTests
         }
 
         [Fact]
-        public void GoodChannelCred_BadCallCred_CreateOperation()
+        public void GoodChannel_BadCall_CreateOperation()
         {
             var builder = new SpannerClientBuilder
             {
@@ -143,7 +208,7 @@ namespace Google.Cloud.Spanner.V1.IntegrationTests
         }
 
         [Fact]
-        public void BadChannelCred_GoodCallCred_CreateOperation()
+        public void BadChannel_GoodCall_CreateOperation()
         {
             var builder = new SpannerClientBuilder
             {
@@ -169,6 +234,64 @@ namespace Google.Cloud.Spanner.V1.IntegrationTests
             client.DeleteSession(
                 new DeleteSessionRequest { SessionName = session.SessionName },
                 CallSettings.FromCallCredentials(_withSpannerAccess.ToCallCredentials()));
+        }
+
+        #endregion
+
+        #region Possible workarounds (exploration)
+
+        [Fact]
+        public void BadChannel_GoodCall_TweakedInterceptors()
+        {
+            // We don't know in what order the credential interceptors are executed.
+            // If the order is call -> channel, we want the channel credential to only
+            // add itself if there's no call credential.
+            // If the order is channel -> call, we want the call credential to remove
+            // any existing credential, if any.
+
+            // Let's tweak the channel credential interceptor so that it only adds itself
+            // if there's no other credential (call credential) set.
+            var channelCredentialsInterceptor = GoogleAuthInterceptors.FromCredential(_withoutSpannerAccess);
+            var tweakedChannelCredentials = CallCredentials.FromInterceptor(new AsyncAuthInterceptor((context, metadata) =>
+            {
+                var existingCredential = metadata.FirstOrDefault(entrie => entrie.Key.Equals("authorization", StringComparison.OrdinalIgnoreCase));
+                // The problem is that the metadata received is always empty.
+                // I suppose that the changes made here will be merged with the "real" metadata.
+                if (existingCredential != null)
+                {
+                    return Task.FromResult(true);
+                }
+                return channelCredentialsInterceptor(context, metadata);
+            }));
+            var builder = new SpannerClientBuilder
+            {
+                ChannelCredentials = ChannelCredentials.Create(new SslCredentials(), tweakedChannelCredentials)
+            };
+            var client = builder.Build();
+
+            // Let's tweak the call credential interceptor so that it removes any other credential befor adding itself.
+            var callCredentialsInterceptor = GoogleAuthInterceptors.FromCredential(_withSpannerAccess);
+            var tweakedCallCredentials = CallCredentials.FromInterceptor(new AsyncAuthInterceptor((context, metadata) =>
+            {
+                var existingCredential = metadata.FirstOrDefault(entrie => entrie.Key.Equals("authorization", StringComparison.OrdinalIgnoreCase));
+                // The problem is that the metadata received is always empty.
+                // I suppose that the changes made here will be merged with the "real" metadata.
+                if (existingCredential != null)
+                {
+                    metadata.Remove(existingCredential);
+                }
+                return callCredentialsInterceptor(context, metadata);
+            }));
+
+            var sessions = client.ListSessions(
+                new ListSessionsRequest { DatabaseAsDatabaseName = _fixture.DatabaseName },
+                CallSettings.FromCallCredentials(tweakedCallCredentials));
+
+            // This should pass after the tweaking, because we "quarantee" that the
+            // call credential is the only one set... except tweaking does not work
+            // because we don't have access to the "real" metadata.
+            // So the test fails throwing a PermissionDenied RpcException.
+            Assert.True(sessions.Count() >= 0);
         }
 
         #endregion
