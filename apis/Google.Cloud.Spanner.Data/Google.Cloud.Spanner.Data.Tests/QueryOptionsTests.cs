33c056a7a3ce868a31b8153e7694ef4222d8f1d0
Shanika Kuruppu <skuruppu@google.com>
2020-02-25 16:20:54 +1100

    Added QueryOptions class and tests.
    It's a wrapper around Google.Cloud.Spanner.Data.V1.ExecuteSqlRequest.Types.QueryOptions
    to avoid exposing protos in the API.

---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/QueryOptionsTests.cs
@@ -0,0 +1,52 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Spanner.V1;
+using Xunit;
+
+namespace Google.Cloud.Spanner.Data.Tests
+{
+    public class QueryOptionsTests
+    {
+        [Fact]
+        public void FromProtoFromNullProto()
+        {
+            var queryOptions = QueryOptions.FromProto(null);
+            Assert.Null(queryOptions);
+        }
+
+        [Fact]
+        public void FromProtoFromEmptyProto()
+        {
+            var proto = new V1.ExecuteSqlRequest.Types.QueryOptions();
+            var queryOptions = QueryOptions.FromProto(proto);
+            Assert.Equal("", queryOptions.OptimizerVersion);
+        }
+
+        [Fact]
+        public void NoOptionsSet()
+        {
+            var queryOptions = new QueryOptions();
+            Assert.Equal("", queryOptions.OptimizerVersion);
+        }
+
+        [Fact]
+        public void SetAndGetOptimizerVersion()
+        {
+            var queryOptions = new QueryOptions();
+            queryOptions.OptimizerVersion = "latest";
+            Assert.Equal("latest", queryOptions.OptimizerVersion);
+        }
+    }
+}
---
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data/QueryOptions.cs
@@ -0,0 +1,63 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Spanner.V1;
+
+namespace Google.Cloud.Spanner.Data
+{
+    /// <summary>
+    /// Immutable class representing query options.
+    /// </summary>
+    public sealed class QueryOptions
+    {
+        /// <summary>
+        /// An option to control the selection of optimizer version.
+        ///
+        /// This parameter allows individual queries to pick different query
+        /// optimizer versions.
+        ///
+        /// Specifying "latest" as a value instructs Cloud Spanner to use the
+        /// latest supported query optimizer version. If not specified, Cloud Spanner
+        /// uses optimizer version set at the database level options. Any other
+        /// positive integer (from the list of supported optimizer versions)
+        /// overrides the default optimizer version for query execution.
+        /// </summary>
+        public string OptimizerVersion
+        {
+            get { return Proto.OptimizerVersion; }
+            set { Proto.OptimizerVersion = value; }
+        }
+
+        /// <summary>
+        /// The proto representation of the query options. Must not be mutated
+        /// or exposed publicly.
+        /// </summary>
+        internal V1.ExecuteSqlRequest.Types.QueryOptions Proto { get; }
+
+        private QueryOptions(V1.ExecuteSqlRequest.Types.QueryOptions proto) => Proto = proto;
+
+        /// <summary>
+        /// Creates query options without specifying any options.
+        /// </summary>
+        public QueryOptions() : this(new V1.ExecuteSqlRequest.Types.QueryOptions())
+        {
+        }
+
+        /// <summary>
+        /// Set query options from the proto.
+        /// </summary>
+        public static QueryOptions FromProto(
+            V1.ExecuteSqlRequest.Types.QueryOptions proto) => proto == null ? null : new QueryOptions(proto);
+    }
+}
