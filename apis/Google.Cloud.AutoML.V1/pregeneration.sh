4d068166bbac46fe67e698f914458aca6b6b7083
Jon Skeet <jonskeet@google.com>
2020-03-05 05:38:23 +0000

    Remove now-unnecessary patches for AutoML, which is now annotated

--- apis/Google.Cloud.AutoML.V1/pregeneration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-OLDPWD=$PWD
-
-# The patch file is relative to googleapis; it looks like patch files don't like to specify
-# parent directories.
-(cd $GOOGLEAPIS; git apply $OLDPWD/pregeneration.patch)
