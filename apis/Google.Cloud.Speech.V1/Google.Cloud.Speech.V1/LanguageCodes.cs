272e20c2fff05ba56e07390866f38acdb66ce40a
Jon Skeet <jonskeet@google.com>
2020-03-17 12:00:13 +0000

    wip

--- apis/Google.Cloud.Speech.V1/Google.Cloud.Speech.V1/LanguageCodes.cs
+++ apis/Google.Cloud.Speech.V1/Google.Cloud.Speech.V1/LanguageCodes.cs
@@ -1,4 +1,6 @@
-﻿// Copyright 2017, Google Inc. All rights reserved.
+// This is a no-op change
+
+// Copyright 2017, Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
