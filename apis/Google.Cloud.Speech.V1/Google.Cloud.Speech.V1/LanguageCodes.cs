f18ff6ad5dd76f0f155b941e115e37af7b4bbcaa
Jon Skeet <jonskeet@google.com>
2020-03-17 12:05:03 +0000

    Second try

--- apis/Google.Cloud.Speech.V1/Google.Cloud.Speech.V1/LanguageCodes.cs
+++ apis/Google.Cloud.Speech.V1/Google.Cloud.Speech.V1/LanguageCodes.cs
@@ -1,5 +1,3 @@
-// This is a no-op change
-
 // Copyright 2017, Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// This is a no-op change
 
 namespace Google.Cloud.Speech.V1
 {
