10752200b4913253787ce46c05100fe84755ad2b
Jon Skeet <jonskeet@google.com>
2020-05-05 07:27:20 +0100

    Finally (?) fix the autorelease build reporter
    - We weren't evaluating toolversions.sh from the right directory
    - The aliasing didn't work anyway (I didn't dive into why) so I'm now just replacing python3 in the script

--- .kokoro/autorelease.sh
+++ .kokoro/autorelease.sh
@@ -7,13 +7,19 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 
 cd $SCRIPT_DIR
 
-# The publish reporter script uses "python3" which doesn't exist on Windows.
-# We already know how to find it though, so let's just use an alias.
-alias python3=$(source toolversions.sh && echo $PYTHON3)
-
 # Make sure we have the most recent version of pip, then install the gcp-releasetool package
 python -m pip install --upgrade pip
 python -m pip install gcp-releasetool
-python -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
+python -m releasetool publish-reporter-script > /tmp/publisher-script
+
+# The publish reporter script uses "python3" which doesn't exist on Windows.
+# Work out what we should use instead.
+PYTHON3=$(source ../toolversions.sh && echo $PYTHON3)
+
+# Fix up the publish reporter script using $PYTHON3. We assume this won't
+# be harmful within sed - at the moment it's always "python", "py -3" or "python3".
+sed -i "s/python3/$PYTHON3/g" /tmp/publisher-script
+
+source /tmp/publisher-script
 
 ./release.sh
