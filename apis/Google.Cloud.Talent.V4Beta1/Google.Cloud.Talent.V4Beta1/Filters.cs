83818224a0ab8c3a490675ca5b0b9ef9669cfccb
yoshi-automation <yoshi-automation@google.com>
2020-06-04 22:51:22 -0700

    docs: Remove a broken link from the documentation.
    PiperOrigin-RevId: 314792023
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Jun 4 13:24:33 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 8070bcb140b06991a636d4ba7d665c5adab1675d
    Source-Link: https://github.com/googleapis/googleapis/commit/8070bcb140b06991a636d4ba7d665c5adab1675d

--- apis/Google.Cloud.Talent.V4Beta1/Google.Cloud.Talent.V4Beta1/Filters.cs
+++ apis/Google.Cloud.Talent.V4Beta1/Google.Cloud.Talent.V4Beta1/Filters.cs
@@ -1541,7 +1541,7 @@ namespace Google.Cloud.Talent.V4Beta1 {
     /// or territory. If this field is not set, application behavior is biased
     /// toward the United States by default.
     ///
-    /// See https://cldr.unicode.org/ and
+    /// See
     /// https://www.unicode.org/cldr/charts/30/supplemental/territory_information.html
     /// for details. Example: "CH" for Switzerland.
     /// Note that this filter is not applicable for Profile Search related queries.
