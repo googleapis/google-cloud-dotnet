ffce8cf16ae8a78edc2c15bfded18958218e37ed
Jon Skeet <jonskeet@google.com>
2020-07-14 08:45:57 +0100

    Use the correct NuGet API key based on the package being pushed

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -17,8 +17,11 @@ cd ..
 
 export GOOGLE_APPLICATION_CREDENTIALS="$KOKORO_KEYSTORE_DIR/73609_cloud-sharp-jenkins-compute-service-account"
 export REQUESTER_PAYS_CREDENTIALS="$KOKORO_KEYSTORE_DIR/73609_gcloud-devel-service-account"
-export DOCS_CREDENTIALS="$KOKORO_KEYSTORE_DIR/73713_docuploader_service_account"
-export NUGET_API_KEY="$(cat "$KOKORO_KEYSTORE_DIR"/73609_google-cloud-nuget-api-key)"
+
+PYTHON3=$(source toolversions.sh && echo $PYTHON3)
+DOCS_CREDENTIALS="$KOKORO_KEYSTORE_DIR/73713_docuploader_service_account"
+GOOGLE_CLOUD_NUGET_API_KEY="$(cat "$KOKORO_KEYSTORE_DIR"/73609_google-cloud-nuget-api-key)"
+GOOGLE_APIS_PACKAGES_NUGET_API_KEY="$(cat "$KOKORO_KEYSTORE_DIR"/73609_google-apis-nuget-api-key)"
 
 COMMITTISH=$COMMITTISH_OVERRIDE
 if [[ $COMMITTISH_OVERRIDE = "" ]]
@@ -51,7 +54,28 @@ then
   echo "Pushing NuGet packages"
   # Push the changes to nuget.
   cd ./releasebuild/nuget
-  for pkg in *.nupkg; do dotnet nuget push -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY $pkg; done
+  for pkg in *.nupkg
+  do
+    # Work out just the package ID based on the filename.
+    pkg_id=$(echo $pkg | sed -r 's/^(.*)\.([0-9]+\.[0-9]+\.[0-9]+(-.*)?)\.nupkg$/\1/g')
+    # Work out the package owner based on apis.json and the package ID
+    default_package_owner=$([[ $pkg == Google.Cloud* ]] && echo google-cloud || echo google-api-packages)
+    package_owner=$PYTHON3 ../tools/getapifield.py ../apis/apis.json $pkg_id --default=$default_package_owner
+    # Work out the right NuGet API key based on the package owner
+    case "$package_owner" in
+      google-cloud)
+        pkg_nuget_api_key=$GOOGLE_CLOUD_NUGET_API_KEY
+        ;;
+      google-apis-packages)
+        pkg_nuget_api_key=$GOOGLE_APIS_PACKAGES_NUGET_API_KEY
+        ;;
+      *)
+       echo "No NuGet API key found for package owner $package_owner"
+       exit 1
+    esac
+    
+    dotnet nuget push -s https://api.nuget.org/v3/index.json -k $pkg_nuget_api_key $pkg
+  done
   cd ../..
 else
   echo "Skipping NuGet push"
