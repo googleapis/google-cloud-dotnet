f0a4f2db0d3fa614cba83cbb3344400dc3d2fa06
Amanda Tarafa Mas <atarafamas@google.com>
2020-07-17 10:04:38 +0100

    Second try

--- build.sh
+++ build.sh
@@ -120,7 +120,7 @@ fi
 # First build the analyzers, for use in everything else.
 log_build_action "(Start) build.sh"
 log_build_action "Available disk space"
-log_build_action $(df -h)
+df -h
 log_build_action "Building analyzers"
 
 dotnet publish -nologo -clp:NoSummary -v quiet -c Release -f netstandard1.3 tools/Google.Cloud.Tools.Analyzers
@@ -158,7 +158,7 @@ do
 done
 
 log_build_action "Available disk space"
-log_build_action $(df -h)
+df -h
 
 if [[ "$runtests" = true ]]
 then
