1a2216542fb8fdce7d2328bed147aaf0191e4675
Amanda Tarafa Mas <atarafamas@google.com>
2020-09-18 15:18:23 +0100

    Modifies cleantestdata.sh script to fetch secrets from new location.

--- .kokoro/cleantestdata.sh
+++ .kokoro/cleantestdata.sh
@@ -8,13 +8,16 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 cd $SCRIPT_DIR
 cd ..
 
+source $SCRIPT_DIR/populatesecrets.sh
+
+populate_all_secrets
+# Use the explicitly-provided credentials rather than relying on default compute credentials
+export GOOGLE_APPLICATION_CREDENTIALS="$SECRETS_LOCATION/cloud-sharp-jenkins-compute-service-account"
+
 # We don't really need the submodules, but it prevents some warnings in logs.
 echo "Cloning submodules"
 git submodule init
 git submodule update
-
-# Use the explicitly-provided credentials rather than relying on default compute credentials
-export GOOGLE_APPLICATION_CREDENTIALS="$KOKORO_KEYSTORE_DIR/73609_cloud-sharp-jenkins-compute-service-account"
 
 ./cleantestdata.sh
 
