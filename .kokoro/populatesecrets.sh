023da6dc17d8e22e3b14f93571856c86ad83fedf
Amanda Tarafa Mas <atarafamas@google.com>
2020-09-21 15:20:28 +0100

    Makes populatesecrets.sh support job specific and external secrets.

--- .kokoro/populatesecrets.sh
+++ .kokoro/populatesecrets.sh
@@ -7,17 +7,34 @@ declare -r SECRETS_LOCATION="${KOKORO_GFILE_DIR}/secret_manager"
 populate_all_secrets(){
     echo "Creating folder on disk for secrets: ${SECRETS_LOCATION}"
     mkdir -p ${SECRETS_LOCATION}
-    for key in $(echo ${COMMON_SECRETS} | sed "s/,/ /g")
+
+    populate_secrets "${COMMON_SECRETS}" "cloud-sharp-jenkins"
+    populate_secrets "${JOB_SECRETS}" "cloud-sharp-jenkins"
+    populate_secrets "${EXTERNAL_SECRETS}" "cloud-devrel-kokoro-resources"
+
+    # docuploader expects credentials on the Keystore path, so let's copy them there as well.
+    if [ -f "${SECRETS_LOCATION}/docuploader_service_account" ];
+    then
+        mkdir -p ${KOKORO_KEYSTORE_DIR}
+        cp "${SECRETS_LOCATION}/docuploader_service_account" "$KOKORO_KEYSTORE_DIR/73713_docuploader_service_account"
+    fi
+}
+
+populate_secrets(){
+    local env_var=$1
+    local project=$2
+    for key in $(echo ${env_var} | sed "s/,/ /g")
     do
       echo "Retrieving secret ${key}"
       gcloud secrets versions access latest \
-        --project cloud-sharp-jenkins \
+        --project $project \
         --secret $key > \
         "${SECRETS_LOCATION}/$key"
       if [[ $? == 0 ]]; then
         echo "Secret written to ${SECRETS_LOCATION}/${key}"
       else
         echo "Error retrieving secret ${key}"
+        exit 1
       fi
     done
 }
