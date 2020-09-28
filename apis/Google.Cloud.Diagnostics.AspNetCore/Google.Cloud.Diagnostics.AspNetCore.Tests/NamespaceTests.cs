09ec4891067284c98ef3932962785a73f96650a4
Amanda Tarafa Mas <atarafamas@google.com>
2020-09-28 16:54:16 +0100

    Adds tests for Diagnostics namespaces to be the correct ones.

--- apis/Google.Cloud.Diagnostics.AspNetCore/Google.Cloud.Diagnostics.AspNetCore.Tests/NamespaceTests.cs
+++ apis/Google.Cloud.Diagnostics.AspNetCore/Google.Cloud.Diagnostics.AspNetCore.Tests/NamespaceTests.cs
@@ -12,29 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Linq;
-using System.Reflection;
+using Google.Cloud.ClientTesting;
 using Xunit;
 
 namespace Google.Cloud.Diagnostics.AspNetCore.Tests
 {
+    // We have files organized in Logging, Trace and ErrorReporting folders
+    // but we really only have one namespace Google.Cloud.Diagnostics.Common.
+    // When using VisualStudio to add new files to these folders it's easy to
+    // forget to remove the folder names from the generated namespace.
+    // We have already merged some tests with the wrong namespace, which is not a problem.
+    // But if we ever merge production code in the wrong namespace then we have to carry
+    // that over until the next major version bump.
     public class NamespaceTests
     {
         [Fact]
-        public void Library_OnlyAspNetCoreNamespace()
-        {
-            var diagApsNetCoreAssembly = Assembly.GetAssembly(typeof(GoogleDiagnosticsWebHostBuilderExtensions));
-
-            // We have files organized in Logging, Trace and ErrorReporting folders
-            // but we really only have one namespace Google.Cloud.Diagnostics.Common.
-            // When using VisualStudio to add new files to these folders it's easy to
-            // forget to remove the folder names from the generated namespace.
-            // We have already merged some tests with the wrong namespace, which is not a problem.
-            // But if we ever merge production code in the wrong namespace then we have to carry
-            // that over until the next major version bump.
-
-            string ns = Assert.Single(diagApsNetCoreAssembly.GetTypes().Select(t => t.Namespace).Distinct());
-            Assert.Equal("Google.Cloud.Diagnostics.AspNetCore", ns);
-        }
+        public void Library_OnlyAspNetCoreNamespace() =>
+            CodeHealthTester.AssertOnlyAllowedNamespaces(
+                typeof(GoogleDiagnosticsWebHostBuilderExtensions), "Google.Cloud.Diagnostics.AspNetCore");
+        
+        [Fact]
+        public void Library_NoComponentSpecificNamespaces() =>
+            // Note: If this test fails, the other one in this file will fail as well
+            // but let's have the double protection.
+            CodeHealthTester.AssertNoDisallowedNamespaces(
+                typeof(GoogleDiagnosticsWebHostBuilderExtensions),
+                "Google.Cloud.Diagnostics.AspNetCore.ErrorReporting",
+                "Google.Cloud.Diagnostics.AspNetCore.Logging",
+                "Google.Cloud.Diagnostics.AspNetCore.Trace");
+        
     }
 }
