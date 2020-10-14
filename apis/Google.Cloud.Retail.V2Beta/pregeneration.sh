bcfa2e32f0f2af05c23e8faec1b5bd09ab78d78b
Jon Skeet <jonskeet@google.com>
2020-10-14 07:51:48 +0100

    Remove Google.Cloud.Retail.V2Beta
    Plans have changed around when client libraries will be needed, and
    there seems little point in merging autosynth commits in the meantime.

--- apis/Google.Cloud.Retail.V2Beta/pregeneration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Temporary fix for broken namespaces. This is being fixed internally.
-sed -i 's/Google\.Cloud\.Retail\.V2beta/Google.Cloud.Retail.V2Beta/g' $GOOGLEAPIS/google/cloud/retail/v2beta/*.proto
-
-# Fix resource names for location - we should use the common Location resource
-sed -i 's/retail\.googleapis\.com\/Location/locations\.googleapis.com\/Location/g' $GOOGLEAPIS/google/cloud/retail/v2beta/*.proto
-sed -i '31,34d' $GOOGLEAPIS/google/cloud/retail/v2beta/catalog.proto
