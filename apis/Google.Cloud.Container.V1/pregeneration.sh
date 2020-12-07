3ff0ae6a881cce945de9ba48b300ede2306d1f59
Jon Skeet <jonskeet@google.com>
2020-12-07 09:41:51 +0000

    chore: Remove generation tweaking scripts for Google.Cloud.Container.V1
    I've regenerated locally without these scripts present, and there are no differences.

--- apis/Google.Cloud.Container.V1/pregeneration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Fix links such as [zones](/compute/docs/zones) to include the full URL
-# Note: using | instead of / to avoid worrying about escaping.
-
-sed -i 's|](/|](https://cloud.google.com/|g' $GOOGLEAPIS/google/container/v1/*.proto
