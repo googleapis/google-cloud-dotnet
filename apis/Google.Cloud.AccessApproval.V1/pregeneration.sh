29531844f9e29834aff7281385005beee8326993
Jon Skeet <jonskeet@google.com>
2020-12-07 10:39:03 +0000

    chore: Add pre/post generation scripts for Google.Cloud.AccessApproval.V1
    This avoids naming collisions.

---
+++ apis/Google.Cloud.AccessApproval.V1/pregeneration.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Rename the AccessApproval service to AccessApprovalService.
+# This avoids naming collisions with AccessApprovalSettings, which
+# is both a message type in the protos and the settings class we
+# would otherwise generate for the service.
+#
+# It's not great to have to do this, but there are no nice alternatives.
+sed -i 's/^service AccessApproval /service AccessApprovalService /g' $GOOGLEAPIS/google/cloud/accessapproval/v1/accessapproval.proto
