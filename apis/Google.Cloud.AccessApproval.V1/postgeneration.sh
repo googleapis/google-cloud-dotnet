36e6eb6205b3229c20ff6338be872f6e89aded1d
Jon Skeet <jonskeet@google.com>
2020-12-09 10:21:24 +0000

    fix!: Fix the API service renaming for Google.Cloud.AccessApproval.V1
    BREAKING CHANGE: This renames the gRPC client classes.
    
    Fixes #5674

--- apis/Google.Cloud.AccessApproval.V1/postgeneration.sh
+++ apis/Google.Cloud.AccessApproval.V1/postgeneration.sh
@@ -4,3 +4,9 @@ set -e
 
 # Undo the changes made in pregeneration.sh
 git -C $GOOGLEAPIS checkout google/cloud/accessapproval/v1/
+
+# Fix up the generated client to use the right gRPC types
+sed -i s/AccessApprovalService.AccessApprovalServiceClient/AccessApproval.AccessApprovalClient/g Google.Cloud.AccessApproval.V1/AccessApprovalServiceClient.g.cs
+
+# Fix up unit test classes
+sed -i s/AccessApprovalService.AccessApprovalServiceClient/AccessApproval.AccessApprovalClient/g Google.Cloud.AccessApproval.V1.Tests/AccessApprovalServiceClientTest.g.cs
