048b0ac07484851cc1cd7d8631e8261b045a71ee
olavloite <koloite@gmail.com>
2021-01-19 15:50:18 +0100

    fix: parse WITH command as SELECT
    Parses WITH statements as queries and allows other whitespace
    characters than only ' '.
    
    Fixes #5857

--- apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/SpannerCommandTextBuilderTests.cs
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/SpannerCommandTextBuilderTests.cs
@@ -64,5 +64,92 @@ namespace Google.Cloud.Spanner.Data.Tests
             var builder = SpannerCommandTextBuilder.FromCommandText(ddlString);
             Assert.False(builder.IsDropDatabaseCommand);
         }
+
+        // https://cloud.google.com/spanner/docs/query-syntax
+        [Theory]
+        [InlineData("SELECT * FROM Albums")]
+        [InlineData(" SELECT * FROM Albums")]
+        [InlineData("\t\nSELECT * FROM Albums")]
+        [InlineData("SELECT  * FROM Albums")]
+        [InlineData("SELECT\t\n* FROM Albums")]
+        [InlineData("WITH AlbumsView AS (SELECT * FROM Albums) select Title from AlbumsView")]
+        [InlineData(" WITH AlbumsView AS (SELECT * FROM Albums) select Title from AlbumsView")]
+        [InlineData("\t\nWITH AlbumsView AS (SELECT * FROM Albums) select Title from AlbumsView")]
+        [InlineData(" WITH   AlbumsView AS (SELECT * FROM Albums) select Title from AlbumsView")]
+        [InlineData("WITH\t\nAlbumsView AS (SELECT * FROM Albums) select Title from AlbumsView")]
+        public void SelectCommand(string commandText)
+        {
+            var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
+            Assert.Equal(SpannerCommandType.Select, builder.SpannerCommandType);
+        }
+
+        // https://cloud.google.com/spanner/docs/dml-syntax#insert-statement
+        [Theory]
+        [InlineData("INSERT INTO Albums (AlbumId) VALUES (@id)")]
+        [InlineData(" INSERT INTO Albums (AlbumId) VALUES (@id)")]
+        [InlineData("\t\nINSERT\t\nINTO\t\nAlbums\t\n(AlbumId)t\nVALUESt\n(@id)")]
+        [InlineData("INSERT  INTO  Albums  (AlbumId) VALUES (@id)")]
+        [InlineData("INSERT\t\nINTO\t\nAlbums\t\n(AlbumId)t\nVALUESt\n(@id)")]
+        [InlineData("INSERT Albums (AlbumId) VALUES (@id)")]
+        [InlineData(" INSERT Albums (AlbumId) VALUES (@id)")]
+        [InlineData("\t\nINSERT\t\nAlbums\t\n(AlbumId)t\nVALUESt\n(@id)")]
+        [InlineData("INSERT  Albums  (AlbumId) VALUES (@id)")]
+        [InlineData("INSERT\t\nAlbums\t\n(AlbumId)t\nVALUESt\n(@id)")]
+        public void DmlInsertCommand(string commandText)
+        {
+            var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
+            Assert.Equal(SpannerCommandType.Dml, builder.SpannerCommandType);
+        }
+
+        // https://cloud.google.com/spanner/docs/dml-syntax#update-statement
+        [Theory]
+        [InlineData("UPDATE Albums SET Title=@title WHERE TRUE")]
+        [InlineData(" UPDATE Albums SET Title=@title WHERE TRUE")]
+        [InlineData("\t\nUPDATE\t\nAlbumst\nSETt\nTitle=@titlet\nWHEREt\nTRUE")]
+        [InlineData("UPDATE  Albums  SET Title=@title WHERE TRUE")]
+        [InlineData("UPDATE\t\nAlbums\t\nSETt\nTitle=@titlet\nWHEREt\nTRUE")]
+        public void DmlUpdateCommand(string commandText)
+        {
+            var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
+            Assert.Equal(SpannerCommandType.Dml, builder.SpannerCommandType);
+        }
+
+        // https://cloud.google.com/spanner/docs/dml-syntax#delete-statement
+        [Theory]
+        [InlineData("DELETE FROM Albums WHERE TRUE")]
+        [InlineData(" DELETE FROM Albums WHERE TRUE")]
+        [InlineData("\t\nDELETE FROM Albums WHERE TRUE")]
+        [InlineData("DELETE  FROM  Albums  WHERE  TRUE")]
+        [InlineData("DELETE\t\nFROM\t\nAlbumst\nWHEREt\nTRUE")]
+        [InlineData("DELETE Albums WHERE TRUE")]
+        [InlineData(" DELETE Albums WHERE TRUE")]
+        [InlineData("\t\nDELETE\t\nAlbumst\nWHEREt\nTRUE")]
+        [InlineData("DELETE  Albums  WHERE TRUE")]
+        [InlineData("DELETE\t\nAlbumst\nWHEREt\nTRUE")]
+        public void DmlDeleteCommand(string commandText)
+        {
+            var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
+            Assert.Equal(SpannerCommandType.Dml, builder.SpannerCommandType);
+        }
+
+        // https://cloud.google.com/spanner/docs/data-definition-language
+        [Theory]
+        [InlineData("CREATE TABLE FOO")]
+        [InlineData(" CREATE TABLE FOO")]
+        [InlineData("CREATE\t\nTABLE\t\nFOO")]
+        [InlineData("\t\nCREATE\t\nTABLE\t\nFOO")]
+        [InlineData("DROP TABLE FOO")]
+        [InlineData(" DROP TABLE FOO")]
+        [InlineData("DROP\t\nTABLE\t\nFOO")]
+        [InlineData("\t\nDROP\t\nTABLE\t\nFOO")]
+        [InlineData("ALTER TABLE FOO")]
+        [InlineData(" ALTER TABLE FOO")]
+        [InlineData("ALTER\t\nTABLE\t\nFOO")]
+        [InlineData("\t\nALTER\t\nTABLE\t\nFOO")]
+        public void DdlCommand(string commandText)
+        {
+            var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
+            Assert.Equal(SpannerCommandType.Ddl, builder.SpannerCommandType);
+        }
     }
 }
