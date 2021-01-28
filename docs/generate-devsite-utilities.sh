9f7ffa54728e807412b5c9ca83b53e39528ca10b
Jon Skeet <jonskeet@google.com>
2021-01-28 12:37:15 +0000

    Add script to generate utility package metadata
    This includes a script to be run from a new Kokoro job, controlled
    by environment variables - we don't need to run this on a regular
    basis, so it doesn't matter that it's a little clunky.

---
+++ docs/generate-devsite-utilities.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+
+source ../toolversions.sh
+
+install_docfx
+
+if [[ -z "$1" || -z "$2" ]]
+then
+  echo 'Please specify utility and version'
+  echo 'Known types: google-apis protobuf common-proto grpc gax'
+  exit 1
+fi
+
+if [[ "$3" != "" ]]
+then
+  declare -r SERVICE_ACCOUNT_JSON=$3
+else
+  declare -r SERVICE_ACCOUNT_JSON=""
+fi
+
+set -eu -o pipefail
+
+declare -r DEVSITE_STAGING_BUCKET=docs-staging-v2
+declare -r VERSION=$2
+
+# These are listed in the order of dependencies:
+# each type only depends on types listed earlier
+case $1 in
+  protobuf)
+    declare -r REPO=https://github.com/protocolbuffers/protobuf
+    declare -r TAG=v$VERSION
+    declare -r DEVSITE_PACKAGE=Google.Protobuf
+    declare -a PROJECTS=(
+      'csharp/src/Google.Protobuf'
+    )
+    declare -r TARGET_FRAMEWORK=netstandard2.0
+    declare -a XREFS=(
+    )
+    ;;
+  google-apis)
+    declare -r REPO=https://github.com/googleapis/google-api-dotnet-client
+    declare -r DEVSITE_PACKAGE=Google.Apis
+    declare -r TAG=v$VERSION
+    declare -a PROJECTS=(
+      'Src/Support/Google.Apis'
+      'Src/Support/Google.Apis.Auth'
+      'Src/Support/Google.Apis.Core'
+    )
+    declare -r TARGET_FRAMEWORK=netstandard2.0
+    declare -a XREFS=(
+    )
+    ;;
+  common-proto)
+    declare -r REPO=https://github.com/googleapis/gax-dotnet
+    declare -r TAG=Google.Api.CommonProtos-$VERSION
+    declare -r DEVSITE_PACKAGE=Google.Api.CommonProtos
+    declare -a PROJECTS=(
+      'Google.Api.CommonProtos'
+    )
+    declare -r TARGET_FRAMEWORK=net461
+    declare -a XREFS=(
+      'Google.Protobuf'
+    )
+    ;;
+  grpc)
+    declare -r REPO=https://github.com/grpc/grpc
+    # gRPC itself is on 1.x, even though Grpc.Core is on 2.x
+    # We work with the Grpc.Core version (which we use later in the metadata)
+    # so replace the first '2.' with '1.' here.
+    declare -r TAG=v${VERSION/2./1.}
+    declare -r DEVSITE_PACKAGE=Grpc.Core
+    declare -a PROJECTS=(
+      'src/csharp/Grpc.Auth'
+      'src/csharp/Grpc.Core'
+      'src/csharp/Grpc.Core.Api'
+    )
+    declare -r TARGET_FRAMEWORK=netstandard2.0
+    declare -a XREFS=(
+      'Google.Apis'
+    )
+    ;;
+  gax)
+    declare -r REPO=https://github.com/googleapis/gax-dotnet
+    declare -r TAG=Google.Api.Gax-$VERSION
+    declare -r DEVSITE_PACKAGE=Google.Api.Gax
+    declare -a PROJECTS=(
+      'Google.Api.Gax'
+      'Google.Api.Gax.Grpc'
+      'Google.Api.Gax.Grpc.Gcp'
+      'Google.Api.Gax.Grpc.GrpcCore'
+      'Google.Api.Gax.Grpc.GrpcNetClient'
+      'Google.Api.Gax.Grpc.Testing'
+      'Google.Api.Gax.Rest'
+      'Google.Api.Gax.Testing'
+    )
+    declare -r TARGET_FRAMEWORK=net461
+    declare -a XREFS=(
+      'Google.Protobuf'
+      'Google.Api.CommonProtos'
+      'Grpc.Core'
+      'Google.Apis'
+    )
+    ;;
+  *)
+    echo "Invalid utility $1"
+    exit 1
+esac
+
+rm -rf output
+mkdir output
+echo "Cloning $REPO"
+git clone -q $REPO -b $TAG --depth=1 -c advice.detachedHead=false output/repo
+
+PROJECTS_TMP=""
+for project in "${PROJECTS[@]}"
+do
+  PROJECTS_TMP="$PROJECTS_TMP output/repo/$project/*.csproj"
+done
+
+echo 'Running docfx metadata'
+$DOCFX metadata --logLevel Error -o output --property TargetFramework=$TARGET_FRAMEWORK $PROJECTS_TMP
+
+# This will have created an output/output directory. 
+# (For some reason docfx takes "output" and doubles it to output/output.)
+# We rename this to output/devsite/api so we can use output/devsite as the
+# "root" directory for docuploader.
+# (This will make it easier to add snippets in the future if we want to.)
+cd output
+mkdir devsite
+
+# It's unclear why we need to do this, but without it we regularly
+# get errors when trying the directory move.
+mv_attempts=10
+until [[ $mv_attempts == 0 ]]
+do
+  mv output/ devsite/api/ && break
+  mv_attempts=$((mv_attempts-1))
+  if [[ mv_attempts == 0 ]]
+  then
+    echo 'Failed to move output directory. Aborting.'
+    exit 1
+  else
+    echo "Retrying move after sleep. Attempts left: $mv_attempts"
+  fi
+  sleep 1
+done
+
+cd devsite
+
+echo 'Creating metadata file'
+
+# Make sure we have the most recent version of pip, then install the gcp-docuploader package
+python -m pip install -q --upgrade pip
+python -m pip install -q gcp-docuploader
+
+XREF_TMP=""
+for xref in "${XREFS[@]}"
+do
+  FULL_XREF="https://cloud.devsite.corp.google.com/dotnet/docs/reference/$xref/latest/xrefmap.yml"
+  XREF_TMP="$XREF_TMP --xrefs $FULL_XREF"
+done
+
+python -m docuploader create-metadata \
+  --name $DEVSITE_PACKAGE \
+  --version $VERSION \
+  --xref-services 'https://xref.docs.microsoft.com/query?uid={uid}' \
+  $XREF_TMP \
+  --language dotnet  
+
+if [[ $SERVICE_ACCOUNT_JSON != "" ]]
+then
+  python -m docuploader upload \
+    . \
+    --credentials $SERVICE_ACCOUNT_JSON \
+    --staging-bucket $DEVSITE_STAGING_BUCKET \
+    --destination-prefix docfx
+else
+  echo 'Service account JSON file not specified; skipping upload'
+fi
+
+echo 'Done'
