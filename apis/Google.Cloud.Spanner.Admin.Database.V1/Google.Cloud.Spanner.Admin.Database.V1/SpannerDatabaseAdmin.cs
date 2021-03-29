43a30b169862709ccd3faefae225905760316d28
yoshi-automation <yoshi-automation@google.com>
2021-03-29 00:47:02 -0700

    feat: add tagging request options
    PiperOrigin-RevId: 365498709
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Sun Mar 28 20:54:25 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 6ce40ff8faf68226782f507ca6b2d497a77044de
    Source-Link: https://github.com/googleapis/googleapis/commit/6ce40ff8faf68226782f507ca6b2d497a77044de

--- apis/Google.Cloud.Spanner.Admin.Database.V1/Google.Cloud.Spanner.Admin.Database.V1/SpannerDatabaseAdmin.cs
+++ apis/Google.Cloud.Spanner.Admin.Database.V1/Google.Cloud.Spanner.Admin.Database.V1/SpannerDatabaseAdmin.cs
@@ -3938,7 +3938,7 @@ namespace Google.Cloud.Spanner.Admin.Database.V1 {
     /// to. If this field is not specified, the restored database will use the same
     /// encryption configuration as the backup by default, namely
     /// [encryption_type][google.spanner.admin.database.v1.RestoreDatabaseEncryptionConfig.encryption_type]
-    /// = `USE_CONFIG_DEFAULT_OR_DATABASE_ENCRYPTION`.
+    /// = `USE_CONFIG_DEFAULT_OR_BACKUP_ENCRYPTION`.
     /// </summary>
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     public global::Google.Cloud.Spanner.Admin.Database.V1.RestoreDatabaseEncryptionConfig EncryptionConfig {
