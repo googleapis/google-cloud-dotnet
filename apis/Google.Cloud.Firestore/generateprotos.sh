de4ed2196e253c35f84c032e63b2cd27077acdbc
Jon Skeet <jonskeet@google.com>
2021-04-14 18:01:15 +0100

    Update Firestore proto generator script to use .g.cs extension

--- apis/Google.Cloud.Firestore/generateprotos.sh
+++ apis/Google.Cloud.Firestore/generateprotos.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Generates internal protos used by Google.Cloud.Firestore.
-# Requires that the googleapis repo has already been 
 
 set -e
 source ../../toolversions.sh
@@ -13,7 +12,7 @@ git clone https://github.com/googleapis/googleapis --depth 1 tmp
 
 $PROTOC \
   --csharp_out=Google.Cloud.Firestore \
-  --csharp_opt=internal_access=true \
+  --csharp_opt=internal_access=true,file_extension=.g.cs \
   -I $PROTOBUF_TOOLS_ROOT/tools \
   -I Google.Cloud.Firestore \
   -I tmp \
