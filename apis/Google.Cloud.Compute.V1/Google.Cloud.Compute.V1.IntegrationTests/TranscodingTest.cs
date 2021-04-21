95618833126650b42254cec309af8cba02e54ccc
Jon Skeet <jonskeet@google.com>
2021-04-21 10:17:25 +0100

    Add integration tests for Google.Cloud.Compute.V1

---
+++ apis/Google.Cloud.Compute.V1/Google.Cloud.Compute.V1.IntegrationTests/TranscodingTest.cs
@@ -0,0 +1,40 @@
+﻿// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Xunit;
+
+namespace Google.Cloud.Compute.V1.IntegrationTests
+{
+    /// <summary>
+    /// Integration tests focused on the GRPC Transcoding
+    /// </summary>
+    [Collection(nameof(ComputeFixture))]
+    public sealed class TranscodingTest
+    {
+        private readonly ComputeFixture _fixture;
+
+        public TranscodingTest(ComputeFixture fixture) => _fixture = fixture;
+
+        [Fact]
+        public void ListZones_MaxResults()
+        {
+            var client = ZonesClient.Create();
+            var allZones = client.List(_fixture.ProjectId);
+            Assert.InRange(allZones.Items.Count, 3, int.MaxValue);
+
+            var twoZones = client.List(new ListZonesRequest { Project = _fixture.ProjectId, MaxResults = 2 });
+            Assert.Equal(2, twoZones.Items.Count);
+        }
+    }
+}
