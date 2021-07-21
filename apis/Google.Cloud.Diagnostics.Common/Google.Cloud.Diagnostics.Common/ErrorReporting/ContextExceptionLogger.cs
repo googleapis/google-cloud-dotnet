4b88274cd4d86e214f99b8f27f4676b9cb48d025
Amanda Tarafa Mas <atarafamas@google.com>
2021-07-21 14:48:22 +0100

    feat: Adds trace context information to error log entries. Closes #5360

--- apis/Google.Cloud.Diagnostics.Common/Google.Cloud.Diagnostics.Common/ErrorReporting/ContextExceptionLogger.cs
+++ apis/Google.Cloud.Diagnostics.Common/Google.Cloud.Diagnostics.Common/ErrorReporting/ContextExceptionLogger.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Google.Api.Gax;
+using System;
 
 namespace Google.Cloud.Diagnostics.Common
 {
@@ -29,8 +29,27 @@ namespace Google.Cloud.Diagnostics.Common
         /// <param name="serviceName"> An identifier of the service, such as the name of the executable or job. May be null.</param>
         /// <param name="version">Represents the source code version that the developer provided. May be null.</param>
         /// <param name="options">The error reporting options. Can be null, if null default options will be used.</param>
+        /// <param name="serviceProvider">The service provider to obtain services from. May be null,
+        /// in which case some context information won't be added to the LogEntry.</param>
         /// <returns>An <see cref="IContextExceptionLogger"/> for the given options.</returns>
+        public static IContextExceptionLogger Create(
+            string projectId, string serviceName, string version,
+            ErrorReportingOptions options, IServiceProvider serviceProvider) =>
+            ErrorReportingContextExceptionLogger.Create(projectId, serviceName, version, serviceProvider, options);
+
+        /// <summary>
+        /// Creates an <see cref="IContextExceptionLogger"/>.
+        /// </summary>
+        /// <param name="projectId">The Google Cloud Platform project ID.  If unspecified and running
+        /// on GAE or GCE the project ID will be detected from the platform.</param>
+        /// <param name="serviceName"> An identifier of the service, such as the name of the executable or job. May be null.</param>
+        /// <param name="version">Represents the source code version that the developer provided. May be null.</param>
+        /// <param name="options">The error reporting options. Can be null, if null default options will be used.</param>
+        /// <returns>An <see cref="IContextExceptionLogger"/> for the given options.</returns>
+        /// <remarks>This method will call <see cref="Create(string, string, string, ErrorReportingOptions, IServiceProvider)"/>
+        /// passing a null value for the <see cref="IServiceProvider"/> parameter, which means that some context information
+        /// cannot be added to log entries because there are no services to obtain that information from.</remarks>
         public static IContextExceptionLogger Create(string projectId, string serviceName, string version, ErrorReportingOptions options) =>
-            ErrorReportingContextExceptionLogger.Create(projectId, serviceName, version, options);
+            Create(projectId, serviceName, version, options, null);
     }
 }
