12db9f0bacddb8d3d1a7dc2b02d961cb4451a351
Jon Skeet <jonskeet@google.com>
2021-08-04 17:13:25 +0100

    chore: Change compatibility test for generic constraints
    (I'll need to look closer into `IList<T>?` vs `IList<T>`, but that
    can come later.)

--- tools/Google.Cloud.Tools.VersionCompat.Tests/ClassMethodTest.cs
+++ tools/Google.Cloud.Tools.VersionCompat.Tests/ClassMethodTest.cs
@@ -84,11 +84,9 @@ namespace Google.Cloud.Tools.VersionCompat.Tests.ClassMethod
     namespace GenericConstraintChanged3.A { public class C { public void A<T, U>() where T : IList<T> where U : IList<T> { } } }
     namespace GenericConstraintChanged3.B { public class C { public void A<T, U>() where T : IList<U> where U : IList<T> { } } }
 
-// TODO: Shouldn't this be enable?
-#nullable disable
-    namespace GenericConstraintChanged4.A { public class C { public void A<T>() where T : IList<T> { } } }
-    namespace GenericConstraintChanged4.B { public class C { public void A<T>() where T : IList<T>? { } } }
-
+#nullable enable
+    namespace GenericConstraintChanged4.A { public class C { public void A<T>() where T : notnull, IList<T> { } } }
+    namespace GenericConstraintChanged4.B { public class C { public void A<T>() where T : IList<T> { } } }
 #nullable restore
 
     namespace MethodModiferChanged1.A { public abstract class C { public virtual int A() => 0; } }
