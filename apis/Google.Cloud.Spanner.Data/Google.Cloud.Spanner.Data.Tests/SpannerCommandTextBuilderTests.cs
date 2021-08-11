61938b6476fcbac26b40aeebc56519f444f9c24a
Amanda Tarafa Mas <atarafamas@google.com>
2021-08-11 15:45:19 +0100

    feat(Spanner): Support comments and statement hints in untyped commands. Alternative to #6848 Closes #6847

--- apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/SpannerCommandTextBuilderTests.cs
+++ apis/Google.Cloud.Spanner.Data/Google.Cloud.Spanner.Data.Tests/SpannerCommandTextBuilderTests.cs
@@ -77,6 +77,25 @@ namespace Google.Cloud.Spanner.Data.Tests
         [InlineData("\t\nWITH AlbumsView AS (SELECT * FROM Albums) select Title from AlbumsView")]
         [InlineData(" WITH   AlbumsView AS (SELECT * FROM Albums) select Title from AlbumsView")]
         [InlineData("WITH\t\nAlbumsView AS (SELECT * FROM Albums) select Title from AlbumsView")]
+        [InlineData("-- Single line comment\nSELECT * FROM Albums")]
+        [InlineData("# Single line comment\nSELECT * FROM Albums")]
+        [InlineData("/* Multi\nline\ncomment\n */\nSELECT * FROM Albums")]
+        [InlineData("/* Multi\n * line\n * comment\n */\nSELECT * FROM Albums")]
+        [InlineData("@{FORCE_INDEX=_BASE_TABLE} SELECT * FROM Albums")]
+        [InlineData("@{FORCE_INDEX=_BASE_TABLE} \n-- Single line comment\nSELECT * FROM Albums")]
+        [InlineData("@{FORCE_INDEX=_BASE_TABLE} \n# Single line comment\nSELECT * FROM Albums")]
+        [InlineData("@{FORCE_INDEX=_BASE_TABLE} /* Multi\nline\ncomment\n */\nSELECT * FROM Albums")]
+        [InlineData("@{FORCE_INDEX=_BASE_TABLE} /* Multi\n * line\n * comment\n */\nSELECT * FROM Albums")]
+        [InlineData("-- Single line comment\n@{FORCE_INDEX=_BASE_TABLE} \nSELECT * FROM Albums")]
+        [InlineData("# Single line comment\n@{FORCE_INDEX=_BASE_TABLE} \nSELECT * FROM Albums")]
+        [InlineData("/* Multi\nline\ncomment\n */\n@{FORCE_INDEX=_BASE_TABLE} \nSELECT * FROM Albums")]
+        [InlineData("/* Multi\n * line\n * comment\n */\n@{FORCE_INDEX=_BASE_TABLE} \nSELECT * FROM Albums")]
+        [InlineData("-- Single line comment\n@{FORCE_INDEX=_BASE_TABLE, OPTIMIZER_VERSION=1} \n\t \nSELECT * FROM Albums")]
+        [InlineData("-- Single line comment\n@{FORCE_INDEX=_BASE_TABLE, OPTIMIZER_VERSION=1} -- Another single line comment \n\t @{FORCE_INDEX=_BASE_TABLE, OPTIMIZER_VERSION=1} /* Multi\n * line\n * comment\n */\nSELECT * FROM Albums")]
+        [InlineData("-- Single line comment\n@{FORCE_INDEX=_BASE_TABLE, /* Multi\n * line\n * comment\n */ OPTIMIZER_VERSION=1} \n\t \nSELECT * FROM Albums")]
+        [InlineData("-- Single line comment\n@{FORCE_INDEX=_BASE_TABLE, -- Inner single line comment\n OPTIMIZER_VERSION=1} \n\t \nSELECT * FROM Albums")]
+        [InlineData("SELECT/*comment*/ * FROM Albums")]
+        [InlineData("SELECT--comment\n * FROM Albums")]
         public void SelectCommand(string commandText)
         {
             var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
@@ -95,6 +114,10 @@ namespace Google.Cloud.Spanner.Data.Tests
         [InlineData("\t\nINSERT\t\nAlbums\t\n(AlbumId)t\nVALUESt\n(@id)")]
         [InlineData("INSERT  Albums  (AlbumId) VALUES (@id)")]
         [InlineData("INSERT\t\nAlbums\t\n(AlbumId)t\nVALUESt\n(@id)")]
+        [InlineData("-- Single line comment\nINSERT INTO Albums (AlbumId) VALUES (@id)")]
+        [InlineData("# Single line comment\nINSERT INTO Albums (AlbumId) VALUES (@id)")]
+        [InlineData("/* Multi line comment */ INSERT INTO Albums (AlbumId) VALUES (@id)")]
+        [InlineData("@{FORCE_INDEX=_BASE_TABLE} INSERT INTO Albums (AlbumId) VALUES (@id)")]
         public void DmlInsertCommand(string commandText)
         {
             var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
@@ -108,6 +131,10 @@ namespace Google.Cloud.Spanner.Data.Tests
         [InlineData("\t\nUPDATE\t\nAlbumst\nSETt\nTitle=@titlet\nWHEREt\nTRUE")]
         [InlineData("UPDATE  Albums  SET Title=@title WHERE TRUE")]
         [InlineData("UPDATE\t\nAlbums\t\nSETt\nTitle=@titlet\nWHEREt\nTRUE")]
+        [InlineData("-- Single line comment\nUPDATE Albums SET Title=@title WHERE TRUE")]
+        [InlineData("# Single line comment\nUPDATE Albums SET Title=@title WHERE TRUE")]
+        [InlineData("/* Multi line comment */ UPDATE Albums SET Title=@title WHERE TRUE")]
+        [InlineData("@{FORCE_INDEX=_BASE_TABLE} UPDATE Albums SET Title=@title WHERE TRUE")]
         public void DmlUpdateCommand(string commandText)
         {
             var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
@@ -126,6 +153,10 @@ namespace Google.Cloud.Spanner.Data.Tests
         [InlineData("\t\nDELETE\t\nAlbumst\nWHEREt\nTRUE")]
         [InlineData("DELETE  Albums  WHERE TRUE")]
         [InlineData("DELETE\t\nAlbumst\nWHEREt\nTRUE")]
+        [InlineData("-- Single line comment\nDELETE FROM Albums WHERE TRUE")]
+        [InlineData("# Single line comment\nDELETE FROM Albums WHERE TRUE")]
+        [InlineData("/* Multi line comment */ DELETE FROM Albums WHERE TRUE")]
+        [InlineData("@{FORCE_INDEX=_BASE_TABLE} DELETE FROM Albums WHERE TRUE")]
         public void DmlDeleteCommand(string commandText)
         {
             var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
@@ -146,10 +177,23 @@ namespace Google.Cloud.Spanner.Data.Tests
         [InlineData(" ALTER TABLE FOO")]
         [InlineData("ALTER\t\nTABLE\t\nFOO")]
         [InlineData("\t\nALTER\t\nTABLE\t\nFOO")]
+        [InlineData("-- Single line comment\nCREATE TABLE FOO")]
+        [InlineData("# Single line comment\nCREATE TABLE FOO")]
+        [InlineData("/* Multi line comment */ CREATE TABLE FOO")]
         public void DdlCommand(string commandText)
         {
             var builder = SpannerCommandTextBuilder.FromCommandText(commandText);
             Assert.Equal(SpannerCommandType.Ddl, builder.SpannerCommandType);
         }
+
+        [Theory]
+        [InlineData("-- never ending line")]
+        [InlineData("# never ending line")]
+        [InlineData("/* never ending block")]
+        [InlineData("@ never ending hint")]
+        [InlineData("@ never ending hint /* } */")]
+        [InlineData("-- just comments and hints and white spaces\n #comment\n @}       ")]
+        public void InvalidCommand(string commandText) =>
+            Assert.Throws<ArgumentException>(() => SpannerCommandTextBuilder.FromCommandText(commandText));
     }
 }
