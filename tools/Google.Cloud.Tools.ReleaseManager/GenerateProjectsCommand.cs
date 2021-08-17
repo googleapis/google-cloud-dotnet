c3d0225f2e4424785ef06e0183a61d91819c380b
Jon Skeet <jonskeet@google.com>
2021-08-17 07:50:57 +0100

    Update to GAX 3.5.0

--- tools/Google.Cloud.Tools.ReleaseManager/GenerateProjectsCommand.cs
+++ tools/Google.Cloud.Tools.ReleaseManager/GenerateProjectsCommand.cs
@@ -63,7 +63,7 @@ namespace Google.Cloud.Tools.ReleaseManager
         private const string ProjectVersionValue = "project";
         private const string DefaultVersionValue = "default";
         private const string GrpcPackage = "Grpc.Core";
-        private const string DefaultGaxVersion = "3.4.0";
+        private const string DefaultGaxVersion = "3.5.0";
         private const string GrpcVersion = "2.38.1";
         private static readonly Dictionary<string, string> DefaultPackageVersions = new Dictionary<string, string>
         {
