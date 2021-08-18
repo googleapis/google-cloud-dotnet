1ef3d691114a6c2e20e2f074a8125f3f38e050fd
Jon Skeet <jonskeet@google.com>
2021-08-18 12:08:26 +0100

    Implement batch releases in release manager
    This consists of creating a config file matching BatchReleaseConfig, and running the batch-release command
    
    A config has to have exactly one "criterion", of which two are available currently:
    
    - SpecifiedApis: An array of the package IDs to release; useful when bulk-updating stale APIs
    - KnownCommits: Only release the APIs which have the *exact* set of commits in their history, with an optional history override. This is useful for mass changes such as version bumps or generator changes
    
    Current downside of batch releasing: if release PRs are created for APIs which are adjacent in the API catalog, that will cause conflicts when merging. Options are being considered for how to addres this.

---
+++ tools/Google.Cloud.Tools.ReleaseManager/BatchRelease/SpecifiedApisCriterion.cs
@@ -0,0 +1,47 @@
+﻿// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Tools.Common;
+using LibGit2Sharp;
+using System.Collections.Generic;
+
+namespace Google.Cloud.Tools.ReleaseManager.BatchRelease
+{
+    internal sealed class SpecifiedApisCriterion : IBatchCriterion
+    {
+        private readonly HashSet<string> _apis;
+
+        internal SpecifiedApisCriterion(IEnumerable<string> apis)
+        {
+            _apis = new HashSet<string>(apis);
+        }
+
+        public IEnumerable<ReleaseProposal> GetProposals(ApiCatalog catalog)
+        {
+            var root = DirectoryLayout.DetermineRootDirectory();
+            using var repo = new Repository(root);
+
+            foreach (var api in catalog.Apis)
+            {
+                if (!_apis.Contains(api.Id))
+                {
+                    continue;
+                }
+                var newVersion = api.StructuredVersion.AfterIncrement();
+
+                yield return ReleaseProposal.CreateFromHistory(repo, api.Id, newVersion);
+            }
+        }
+    }
+}
