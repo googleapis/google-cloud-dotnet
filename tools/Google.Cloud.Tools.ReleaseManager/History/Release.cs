04bae474a558400d8f8a94f2906d8e4409b22875
Jon Skeet <jonskeet@google.com>
2021-08-18 12:02:30 +0100

    Refactor history update to be more widely accessible

--- tools/Google.Cloud.Tools.ReleaseManager/History/Release.cs
+++ tools/Google.Cloud.Tools.ReleaseManager/History/Release.cs
@@ -26,12 +26,56 @@ namespace Google.Cloud.Tools.ReleaseManager.History
         public IReadOnlyList<GitCommit> Commits { get; }
         public DateTime ReleaseDate { get; }
 
+        private Release(StructuredVersion version, DateTimeOffset? releaseDate, IEnumerable<GitCommit> commits) =>
+            (Version, ReleaseDate, Commits) = (version, (releaseDate ?? DateTimeOffset.Now).UtcDateTime, commits.ToList().AsReadOnly());
+
         public Release(StructuredVersion version, Commit tagCommit, IEnumerable<GitCommit> commits)
+            : this(version, tagCommit?.GetDate(), commits)
         {
-            Version = version;
-            Commits = commits.ToList().AsReadOnly();
-            // Assume that if a release isn't tagged yet, it's because we're releasing it today.
-            ReleaseDate = tagCommit?.GetDate().UtcDateTime ?? DateTime.Today;
+        }
+
+        public Release(StructuredVersion version, Tag tag, IEnumerable<GitCommit> commits)
+            : this(version, tag?.GetDate(), commits)
+        {
+        }
+
+        public static IEnumerable<Release> LoadReleases(Repository repo, ApiCatalog catalog, ApiMetadata api)
+        {
+            var id = api.Id;
+            var commitPredicate = GitHelpers.CreateCommitPredicate(repo, catalog, api);
+
+            List<Release> releases = new List<Release>();
+            StructuredVersion currentVersion = StructuredVersion.FromString(api.Version);
+            Commit currentTagCommit = null;
+
+            // "Pending" as in "haven't been yielded in a release yet"
+            List<GitCommit> pendingCommits = new List<GitCommit>();
+
+            var tagPrefix = $"{id}-";
+            var versionsCommitId = repo.Tags
+                .Where(tag => tag.FriendlyName.StartsWith(tagPrefix))
+                .ToDictionary(tag => tag.Target.Id, tag => tag.FriendlyName.Substring(tagPrefix.Length));
+
+            foreach (var commit in repo.Head.Commits)
+            {
+                if (commitPredicate(commit))
+                {
+                    pendingCommits.Add(new GitCommit(commit));
+                }
+                if (versionsCommitId.TryGetValue(commit.Id, out string version) && !version.StartsWith("0."))
+                {
+                    yield return new Release(currentVersion, currentTagCommit, pendingCommits);
+                    // Release constructor clones the list, so we're safe to clear it.
+                    pendingCommits.Clear();
+                    currentTagCommit = commit;
+                    currentVersion = StructuredVersion.FromString(version);
+                }
+            }
+
+            if (pendingCommits.Count != 0)
+            {
+                yield return new Release(currentVersion, currentTagCommit, pendingCommits);
+            }
         }
     }
 }
