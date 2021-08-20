4d51dc2744f38de582684f57c0902e2537147e1f
yoshi-automation <yoshi-automation@google.com>
2021-08-19 22:44:13 -0700

    build: change links to make them absolute
    PiperOrigin-RevId: 391801304
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Aug 19 11:14:01 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 84b7c63c66a06faccd4c46b920fb531126784f30
    Source-Link: https://github.com/googleapis/googleapis/commit/84b7c63c66a06faccd4c46b920fb531126784f30

--- apis/Google.Cloud.DataCatalog.V1/Google.Cloud.DataCatalog.V1/DataCatalogClient.g.cs
+++ apis/Google.Cloud.DataCatalog.V1/Google.Cloud.DataCatalog.V1/DataCatalogClient.g.cs
@@ -675,7 +675,7 @@ namespace Google.Cloud.DataCatalog.V1
         /// <param name="query">
         /// Optional. The query string with a minimum of 3 characters and specific syntax.
         /// For more information, see
-        /// [Data Catalog search syntax](/data-catalog/docs/how-to/search-reference).
+        /// [Data Catalog search syntax](https://cloud.google.com/data-catalog/docs/how-to/search-reference).
         /// 
         /// An empty query string returns all data assets (in the specified scope)
         /// that you have access to.
@@ -732,7 +732,7 @@ namespace Google.Cloud.DataCatalog.V1
         /// <param name="query">
         /// Optional. The query string with a minimum of 3 characters and specific syntax.
         /// For more information, see
-        /// [Data Catalog search syntax](/data-catalog/docs/how-to/search-reference).
+        /// [Data Catalog search syntax](https://cloud.google.com/data-catalog/docs/how-to/search-reference).
         /// 
         /// An empty query string returns all data assets (in the specified scope)
         /// that you have access to.
