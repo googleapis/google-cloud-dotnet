74464a4bb5843bc02cd2f57dea2e623633ca310e
Amanda Tarafa Mas <atarafamas@google.com>
2021-09-29 00:57:13 +0100

    fix: Improve performance of the timed buffer.

--- apis/Google.Cloud.Diagnostics.Common/Google.Cloud.Diagnostics.Common/TimedBufferingConsumer.cs
+++ apis/Google.Cloud.Diagnostics.Common/Google.Cloud.Diagnostics.Common/TimedBufferingConsumer.cs
@@ -14,8 +14,10 @@
 
 using Google.Api.Gax;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Google.Cloud.Diagnostics.Common
 {
@@ -23,19 +25,21 @@ namespace Google.Cloud.Diagnostics.Common
     /// A <see cref="IFlushableConsumer{T}"/> that will automatically flush the buffer after a 
     /// given amount of time.
     /// </summary>
-    internal class TimedBufferingConsumer<T> : FlushableConsumerBase<T>
+    internal class TimedBufferingConsumer<T> : IFlushableConsumer<T>
     {
-        /// <summary>
-        /// The buffered items. This is not readonly as it is replaced when the buffer is flushed.
-        /// </summary>
-        private List<T> _items = new List<T>();
+        private const int RetryAttempts = 10;
+
+        /// <summary>The consumer to flush to.</summary>
+        private readonly IConsumer<T> _consumer;
+
+        private BlockingCollection<T> _receivedItems = new BlockingCollection<T>();
 
         /// <summary>The timer to automatically flush the buffer.</summary>
         private IThreadingTimer _timer;
 
         internal TimedBufferingConsumer(IConsumer<T> consumer, TimeSpan waitTime, IThreadingTimer timer)
-            : base(consumer)
         {
+            _consumer = GaxPreconditions.CheckNotNull(consumer, nameof(consumer));
             _timer = GaxPreconditions.CheckNotNull(timer, nameof(timer));
             // Initialize the timer to flush ever wait time interval.
             _timer.Initialize((e) => {
@@ -61,26 +65,79 @@ namespace Google.Cloud.Diagnostics.Common
             => new TimedBufferingConsumer<T>(consumer, waitTime, new SimpleThreadingTimer());
 
         /// <inheritdoc />
-        public override void Dispose()
+        public void Receive(IEnumerable<T> items)
+        {
+            GaxPreconditions.CheckNotNull(items, nameof(items));
+            using var enumerator = items.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                Receive(enumerator, RetryAttempts);
+            }
+        }
+
+        /// <inheritdoc />
+        public Task ReceiveAsync(IEnumerable<T> items, CancellationToken cancellationToken)
+        {
+            // TODO: Should we do something with cancellationToken, like check for cancellation
+            // on every iteration?
+            Receive(items);
+            return Task.CompletedTask;
+        }
+
+        private void Receive(IEnumerator<T> remainingItems, int attemptsRemaining)
+        {
+            try
+            {
+                do
+                {
+                    // We very explicitly use _receivedItems here instead of a local
+                    // reference. On Flush, _receivedItems will be assigned a new
+                    // collection and we want to use the new collection as soon
+                    // as possible.
+                    // Note that it's still possible that
+                    // 1. We read _receivedItems.
+                    // 2. We lose execution and a Flush starts.
+                    // 3. Flush swaps _receivedItems.
+                    // 4. Flush marks the old items (the one we are referencing) as CompleteAdding.
+                    // 5. We gain execution and execute Add on a completed collection, which fails.
+                    // That's why we catch InvalidOperationException and simply reattempt just
+                    // where we left off.
+                    // Also note that if we regain execution in between 3 and 4, that's fine.
+                    // Our call to Add will happen before the call to CompleteAdding, so it will
+                    // succeed before the collection is marked as complete.
+                    _receivedItems.Add(remainingItems.Current);
+                    attemptsRemaining = RetryAttempts;
+                }
+                while (remainingItems.MoveNext());
+            }
+            catch (InvalidOperationException) when (attemptsRemaining > 0)
+            {
+                Interlocked.MemoryBarrier();
+                Receive(remainingItems, --attemptsRemaining);
+            }
+        }
+
+        /// <inheritdoc />
+        public void Flush()
+        {
+            BlockingCollection<T> flushableItems = Interlocked.Exchange(ref _receivedItems, new BlockingCollection<T>());
+            flushableItems.CompleteAdding();
+            _consumer.Receive(flushableItems);
+        }
+
+        /// <inheritdoc />
+        public async Task FlushAsync(CancellationToken cancellationToken = default)
+        {
+            BlockingCollection<T> flushableItems = Interlocked.Exchange(ref _receivedItems, new BlockingCollection<T>());
+            flushableItems.CompleteAdding();
+            await _consumer.ReceiveAsync(flushableItems, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
         {
             _timer.Dispose();
             Flush();
-        }
-
-        /// <inheritdoc />
-        protected override bool ReceiveWithSemaphoreHeld(IEnumerable<T> items)
-        {
-            GaxPreconditions.CheckNotNull(items, nameof(items));
-            _items.AddRange(items);
-            return false;
-        }
-
-        /// <inheritdoc />
-        protected override IEnumerable<T> GetAndResetItemsWithSemaphoreHeld()
-        {
-            IList<T> old = _items;
-            _items = new List<T>();
-            return old;
         }
     }
 }
