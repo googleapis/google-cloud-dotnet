507248da1a0ce584a7f8ba71638a8126f9795e9a
Jon Skeet <jonskeet@google.com>
2021-10-08 08:31:24 +0100

    Remove previous prototyping that modified the protos and generated code
    Everything required (other than the code in OperationNewCode) is now generated.

--- apis/Google.Cloud.Compute.V1/midmicrogeneration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Update the proto with LROs, after the gRPC layer has been generated.
-dotnet run -p Google.Cloud.Compute.V1.ModifyProto \
-   $GOOGLEAPIS_DISCOVERY/google/cloud/compute/v1/compute.proto
--- apis/Google.Cloud.Compute.V1/postgeneration.sh
+++ apis/Google.Cloud.Compute.V1/postgeneration.sh
@@ -9,7 +9,3 @@ git -C $GOOGLEAPIS_DISCOVERY checkout google/cloud/compute/v1
 # We have a hand-written piece at the moment for the adapter.
 sed -i 's/using gaxgrpccore = Google.Api.Gax.Grpc.GrpcCore;//g' Google.Cloud.Compute.V1/*.g.cs
 sed -i 's/=> gaxgrpccore::GrpcCoreAdapter.Instance/=> ComputeRestAdapter.ComputeAdapter/g' Google.Cloud.Compute.V1/*.g.cs
-
-# Update the generated clients
-dotnet run -p Google.Cloud.Compute.V1.ModifyGeneratedClients \
-   Google.Cloud.Compute.V1
