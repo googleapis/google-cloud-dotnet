db2e28f5d8b1d79d83f1515680b54a4f84c74e9d
yoshi-automation <yoshi-automation@google.com>
2021-10-18 22:55:53 -0700

    docs: fix docstring formatting
    Committer: @parthea
    PiperOrigin-RevId: 404008335
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Oct 18 11:09:42 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 4dd603675ff73897600a5a94ec42606a6f83f907
    Source-Link: https://github.com/googleapis/googleapis/commit/4dd603675ff73897600a5a94ec42606a6f83f907

--- apis/Google.Cloud.ServiceControl.V1/Google.Cloud.ServiceControl.V1/Distribution.g.cs
+++ apis/Google.Cloud.ServiceControl.V1/Google.Cloud.ServiceControl.V1/Distribution.g.cs
@@ -64,10 +64,10 @@ namespace Google.Cloud.ServiceControl.V1 {
   /// points. It contains the size of the population of sample points plus
   /// additional optional information:
   ///
-  ///   - the arithmetic mean of the samples
-  ///   - the minimum and maximum of the samples
-  ///   - the sum-squared-deviation of the samples, used to compute variance
-  ///   - a histogram of the values of the sample points
+  /// * the arithmetic mean of the samples
+  /// * the minimum and maximum of the samples
+  /// * the sum-squared-deviation of the samples, used to compute variance
+  /// * a histogram of the values of the sample points
   /// </summary>
   public sealed partial class Distribution : pb::IMessage<Distribution>
   #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
