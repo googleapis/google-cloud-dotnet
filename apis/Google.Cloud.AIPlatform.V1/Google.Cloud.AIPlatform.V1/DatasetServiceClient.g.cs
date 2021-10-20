a2589a5f4d9971388c0fd5a193905d2bdb5be9c8
yoshi-automation <yoshi-automation@google.com>
2021-10-20 00:26:44 -0700

    docs: fix typo in alert.proto
    PiperOrigin-RevId: 402570091
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Oct 12 08:32:52 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: f0d5f7fffd7538fb84718de6e0dd66474926f009
    Source-Link: https://github.com/googleapis/googleapis/commit/f0d5f7fffd7538fb84718de6e0dd66474926f009

--- apis/Google.Cloud.AIPlatform.V1/Google.Cloud.AIPlatform.V1/DatasetServiceClient.g.cs
+++ apis/Google.Cloud.AIPlatform.V1/Google.Cloud.AIPlatform.V1/DatasetServiceClient.g.cs
@@ -332,9 +332,6 @@ namespace Google.Cloud.AIPlatform.V1
     /// The service that handles the CRUD of Vertex AI Dataset and its child
     /// resources.
     /// Service for managing datasets.
-    /// Service for managing datasets.
-    /// Service for managing datasets.
-    /// Service for managing datasets.
     /// </remarks>
     public abstract partial class DatasetServiceClient
     {
@@ -1758,9 +1755,6 @@ namespace Google.Cloud.AIPlatform.V1
     /// <remarks>
     /// The service that handles the CRUD of Vertex AI Dataset and its child
     /// resources.
-    /// Service for managing datasets.
-    /// Service for managing datasets.
-    /// Service for managing datasets.
     /// Service for managing datasets.
     /// </remarks>
     public sealed partial class DatasetServiceClientImpl : DatasetServiceClient
