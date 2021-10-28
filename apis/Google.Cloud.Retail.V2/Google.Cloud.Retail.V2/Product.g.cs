deb7506c70c971fb33e4cdbd75826840b17bd422
yoshi-automation <yoshi-automation@google.com>
2021-10-27 22:53:04 -0700

    docs: fix docstring formatting
    Committer: @parthea
    PiperOrigin-RevId: 405938813
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Wed Oct 27 11:36:29 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 37924239ed9e96535a025474c750db37e1b88c41
    Source-Link: https://github.com/googleapis/googleapis/commit/37924239ed9e96535a025474c750db37e1b88c41

--- apis/Google.Cloud.Retail.V2/Google.Cloud.Retail.V2/Product.g.cs
+++ apis/Google.Cloud.Retail.V2/Google.Cloud.Retail.V2/Product.g.cs
@@ -527,7 +527,7 @@ namespace Google.Cloud.Retail.V2 {
     /// * The key must be a UTF-8 encoded string with a length limit of 128
     ///   characters.
     /// * For indexable attribute, the key must match the pattern:
-    ///   [a-zA-Z0-9][a-zA-Z0-9_]*. For example, key0LikeThis or KEY_1_LIKE_THIS.
+    ///   `[a-zA-Z0-9][a-zA-Z0-9_]*`. For example, key0LikeThis or KEY_1_LIKE_THIS.
     /// </summary>
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     public pbc::MapField<string, global::Google.Cloud.Retail.V2.CustomAttribute> Attributes {
