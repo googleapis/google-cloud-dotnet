205c6e437ca9baabd65441ec721900fca17f1f29
Jon Skeet <jonskeet@google.com>
2021-11-10 08:48:19 +0000

    chore: Catch up by regenerating
    These were changes either due to OwlBot not copying gapic metadata before, or due to the lack of OwlBot configurations

--- apis/Google.Analytics.Data.V1Beta/Google.Analytics.Data.V1Beta/Data.g.cs
+++ apis/Google.Analytics.Data.V1Beta/Google.Analytics.Data.V1Beta/Data.g.cs
@@ -1027,7 +1027,7 @@ namespace Google.Analytics.Data.V1Beta {
     /// would like within the allowed character set. For example if a
     /// `dimensionExpression` concatenates `country` and `city`, you could call
     /// that dimension `countryAndCity`. Dimension names that you choose must match
-    /// the regular expression "^[a-zA-Z0-9_]$".
+    /// the regular expression `^[a-zA-Z0-9_]$`.
     ///
     /// Dimensions are referenced by `name` in `dimensionFilter`, `orderBys`,
     /// `dimensionExpression`, and `pivots`.
@@ -2075,7 +2075,7 @@ namespace Google.Analytics.Data.V1Beta {
     /// within the allowed character set. For example if `expression` is
     /// `screenPageViews/sessions`, you could call that metric's name =
     /// `viewsPerSession`. Metric names that you choose must match the regular
-    /// expression "^[a-zA-Z0-9_]$".
+    /// expression `^[a-zA-Z0-9_]$`.
     ///
     /// Metrics are referenced by `name` in `metricFilter`, `orderBys`, and metric
     /// `expression`.
