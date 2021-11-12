492dd329e63193dc97b20f4b3c8749a4794aa452
Jon Skeet <jonskeet@google.com>
2021-11-12 16:39:20 +0000

    chore: Remove now-unnecessary proto changes for Compute
    (The extraneous "small" proto and corresponding files have been
    moved.)

--- apis/Google.Cloud.Compute.V1/postgeneration.sh
+++ apis/Google.Cloud.Compute.V1/postgeneration.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# Undo the changes made in pregeneration.sh
-git -C $GOOGLEAPIS checkout google/cloud/compute/v1
-
 # Fix up Grpc.Core parts
 # We have a hand-written piece at the moment for the adapter.
 sed -i 's/using gaxgrpccore = Google.Api.Gax.Grpc.GrpcCore;//g' Google.Cloud.Compute.V1/*.g.cs
