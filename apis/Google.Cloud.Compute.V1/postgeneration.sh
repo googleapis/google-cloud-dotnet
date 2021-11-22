f5558b2c7b79fd78311deaf1fbb40e79f65fe6b5
Jon Skeet <jonskeet@google.com>
2021-11-22 15:06:02 +0000

    chore: Run the enum constant generator as part of the Compute build

--- apis/Google.Cloud.Compute.V1/postgeneration.sh
+++ apis/Google.Cloud.Compute.V1/postgeneration.sh
@@ -6,3 +6,7 @@ set -e
 # We have a hand-written piece at the moment for the adapter.
 sed -i 's/using gaxgrpccore = Google.Api.Gax.Grpc.GrpcCore;//g' Google.Cloud.Compute.V1/*.g.cs
 sed -i 's/=> gaxgrpccore::GrpcCoreAdapter.Instance/=> ComputeRestAdapter.ComputeAdapter/g' Google.Cloud.Compute.V1/*.g.cs
+
+# Generate enum constants
+dotnet build Google.Cloud.Compute.V1.EnumConstantGenerator
+dotnet run --no-build -p Google.Cloud.Compute.V1.EnumConstantGenerator > Google.Cloud.Compute.V1/ComputeEnumConstants.g.cs
