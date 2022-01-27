601c51c6bcc8f5264a7a5436d40aec659abbffe9
Jon Skeet <jonskeet@google.com>
2022-01-27 14:54:04 +0000

    Pass the service config file into the generator
    A few notes:
    
    - This requires the latest (unreleased at the time of writing) version of the generator
    - It currently fails for Google.Cloud.Iap.V1 as there's an additional API in the service config file; we're working out what to do about that
    - I don't know why quite a few APIs didn't have a service config file in apis.json, but I've fixed them. A couple genuinely don't have service config files - I've used "none" to make it clear that's deliberate. (Generation will fail if the value doesn't exist at all.)
    - The APIs which change the service names mid-generation need to change the service config file too
    
    With a manual fix for IAP, and using the latest (unreleased generator, generation itself succeeds with this change. I have not validated that the results build or are correct, however.

--- apis/Google.Cloud.Bigtable.V2/postgeneration.sh
+++ apis/Google.Cloud.Bigtable.V2/postgeneration.sh
@@ -6,6 +6,7 @@ set -e
 # (Do this first so that if we have any failures in the remaining steps)
 git -C $GOOGLEAPIS checkout google/bigtable/v2/bigtable.proto
 git -C $GOOGLEAPIS checkout google/bigtable/v2/bigtable_grpc_service_config.json
+git -C $GOOGLEAPIS checkout google/bigtable/v2/bigtable_v2.yaml
 
 # Fix up the generated client for using Grpc.Gcp and other tweaks
 sed -i -r -f BigtableServiceApiClient.sed Google.Cloud.Bigtable.V2/BigtableServiceApiClient.g.cs
