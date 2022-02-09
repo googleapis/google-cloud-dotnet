cfa9befc2cb7db20f490faa4a92416daa24e1f9e
Jon Skeet <jonskeet@google.com>
2022-02-09 08:26:15 +0000

    chore: Set up submodules when building docs
    This avoids a lot of pointless warnings in logs

--- .kokoro/builddocs.sh
+++ .kokoro/builddocs.sh
@@ -8,6 +8,10 @@ SCRIPT_DIR=$(dirname "$SCRIPT")
 cd $SCRIPT_DIR
 cd ..
 
+echo "Cloning submodules"
+git submodule init
+git submodule update
+
 # Build the libraries w/o running unit tests.
 ./build.sh --notests
 
