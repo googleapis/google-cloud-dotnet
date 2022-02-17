812492939a7589f12ba6f4b317fc07db81b08af8
Amanda Tarafa Mas <atarafamas@google.com>
2022-02-17 12:01:12 +0000

    chore: Regenerate Grafeas with updated postprocessor.

--- apis/Grafeas.V1/Grafeas.V1.GeneratedSnippets/GrafeasClient.BatchCreateNotesResourceNamesSnippet.g.cs
+++ apis/Grafeas.V1/Grafeas.V1.GeneratedSnippets/GrafeasClient.BatchCreateNotesResourceNamesSnippet.g.cs
@@ -14,7 +14,7 @@
 
 // Generated code. DO NOT EDIT!
 
-namespace Grafeas.V1.Snippets
+namespace GrafeasV1.Snippets
 {
     // [START containeranalysis_v1_generated_Grafeas_BatchCreateNotes_sync_flattened_resourceNames]
     using Grafeas.V1;
