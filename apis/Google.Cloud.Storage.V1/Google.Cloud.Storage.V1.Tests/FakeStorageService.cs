a8f9523a10e5218ae6230f7abfda47ad1a1ea9be
anshuldavid <anshuldavid@google.com>
2022-03-25 18:12:00 +0530

    Review comments implemented

--- apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.Tests/FakeStorageService.cs
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.Tests/FakeStorageService.cs
@@ -1,9 +1,19 @@
-﻿using Google.Apis.Storage.v1;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using Google.Apis.Requests;
-using Google.Apis.Util;
+using Google.Apis.Storage.v1;
 using Google.Cloud.ClientTesting;
 using System.Net;
 using System.Net.Http;
@@ -12,24 +22,6 @@ namespace Google.Cloud.Storage.V1.Tests
 {
     class FakeStorageService : StorageService
     {
-        // TODO: Vary this content based on the actual error code?
-        private const string SampleErrorContent =
-                @"{
-                    ""error"": {
-                        ""errors"": [
-                            {
-                                ""domain"": ""global"",
-                                ""reason"": ""required"",
-                                ""message"": ""Login Required"",
-                                ""locationType"": ""header"",
-                                ""location"": ""Authorization""
-                            }
-                        ],
-                        ""code"": 401,
-                        ""message"": ""Login Required""
-                    }
-                }";
-
         private readonly ReplayingMessageHandler handler;
 
         public FakeStorageService() : base(new Initializer
@@ -44,7 +36,6 @@ namespace Google.Cloud.Storage.V1.Tests
 
         public void ExpectRequest<TResponse>(ClientServiceRequest<TResponse> request, TResponse response)
         {
-            //MaybeDisablePrettyPrint(request);
             var httpRequest = request.CreateRequest();
             string responseContent = SerializeObject(response);
             handler.ExpectRequest(httpRequest.RequestUri, httpRequest.Content?.ReadAsStringAsync()?.Result, responseContent);
@@ -52,23 +43,9 @@ namespace Google.Cloud.Storage.V1.Tests
 
         public void ExpectRequest<TResponse>(ClientServiceRequest<TResponse> request, HttpStatusCode statusCode)
         {
-            // TODO: Validate that this isn't a success status code (given that we're returning an error response).
-
-            //MaybeDisablePrettyPrint(request);
             var httpRequest = request.CreateRequest();
-            var responseMessage = new HttpResponseMessage(statusCode)
-            {
-                Content = new StringContent(SampleErrorContent)
-            };
+            var responseMessage = new HttpResponseMessage(statusCode);
             handler.ExpectRequest(httpRequest.RequestUri, httpRequest.Content?.ReadAsStringAsync()?.Result, responseMessage);
-        }
-
-        private void MaybeDisablePrettyPrint(dynamic request)
-        {
-            // Almost all our tests using FakeService will have prettyPrint=false, and the requests will all
-            // be derived from the generic ServiceBaseRequest which has a PrettyPrint property.
-            // We use ??= so that if the request already has PrettyPrint set, we don't change that.
-            request.PrettyPrint ??= false;
         }
 
         public void Verify() => handler.Verify();
