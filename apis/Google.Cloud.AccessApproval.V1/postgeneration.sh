bcfb063f59641d1a3048cf131329a304ef0a18ee
Jon Skeet <jonskeet@google.com>
2022-04-19 19:36:14 +0100

    Fix Access Approval post-generation script for GAX v4

--- apis/Google.Cloud.AccessApproval.V1/postgeneration.sh
+++ apis/Google.Cloud.AccessApproval.V1/postgeneration.sh
@@ -7,6 +7,7 @@ git -C $GOOGLEAPIS checkout google/cloud/accessapproval/v1/
 
 # Fix up the generated client to use the right gRPC types
 sed -i s/AccessApprovalService.AccessApprovalServiceClient/AccessApproval.AccessApprovalClient/g Google.Cloud.AccessApproval.V1/AccessApprovalServiceClient.g.cs
+sed -i s/AccessApprovalService.Descriptor/AccessApproval.Descriptor/g Google.Cloud.AccessApproval.V1/AccessApprovalServiceClient.g.cs
 
 # Fix up unit test classes
 sed -i s/AccessApprovalService.AccessApprovalServiceClient/AccessApproval.AccessApprovalClient/g Google.Cloud.AccessApproval.V1.Tests/AccessApprovalServiceClientTest.g.cs
