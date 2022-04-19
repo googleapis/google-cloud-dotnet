32a0e8027fe8340f7df936881e4f0cf3d7b1f9bb
Jon Skeet <jonskeet@google.com>
2022-04-19 15:59:02 +0100

    Changes to Google.Cloud.Compute.V1 for GAX v4

--- apis/Google.Cloud.Compute.V1/postgeneration.sh
+++ apis/Google.Cloud.Compute.V1/postgeneration.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-# Fix up Grpc.Core parts
-# We have a hand-written piece at the moment for the adapter.
-sed -i 's/using gaxgrpccore = Google.Api.Gax.Grpc.GrpcCore;//g' Google.Cloud.Compute.V1/*.g.cs
-sed -i 's/=> gaxgrpccore::GrpcCoreAdapter.Instance/=> ComputeRestAdapter.ComputeAdapter/g' Google.Cloud.Compute.V1/*.g.cs
+# Until we have transports in the generator itself, manually fix this up.
+sed -i 's/ApiTransports.Grpc/ApiTransports.Rest/g' Google.Cloud.Compute.V1/*.g.cs
 
 # Generate enum constants
 dotnet build Google.Cloud.Compute.V1.EnumConstantGenerator
