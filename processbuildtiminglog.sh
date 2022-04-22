63da23efa119e1d92a7d2cee6f8be7972be375ea
Jon Skeet <jonskeet@google.com>
2022-04-22 16:43:02 +0100

    chore: Use --project in dotnet run

--- processbuildtiminglog.sh
+++ processbuildtiminglog.sh
@@ -3,4 +3,4 @@
 echo ----------------
 echo Build timing log
 echo ----------------
-dotnet run -p tools/Google.Cloud.Tools.ProcessBuildTimingLog -- build_timing_log.txt
+dotnet run --project tools/Google.Cloud.Tools.ProcessBuildTimingLog -- build_timing_log.txt
