d4aa849c51d4df55e99b4b15e4aeaf37fdea6b48
anshuldavid <anshuldavid@google.com>
2022-04-28 23:50:26 +0530

    Fixing  namespace

--- apis/Google.Cloud.Datastore.V1/Google.Cloud.Datastore.V1.Tests/FilterTest.cs
+++ apis/Google.Cloud.Datastore.V1/Google.Cloud.Datastore.V1.Tests/FilterTest.cs
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Xunit;
 using static Google.Cloud.Datastore.V1.PropertyFilter.Types;
 
