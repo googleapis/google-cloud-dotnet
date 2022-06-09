bd76069d7b4b522bbc1603e33fcabea379485186
Jon Skeet <jonskeet@google.com>
2022-06-09 05:50:36 +0100

    chore: move gRPC channel counting into Bigtable integration tests
    This is the only call site for GrpcInfo.EnableSubchannelCounting, and without it we don't need the Grpc.Core dependency in Google.Cloud.ClientTesting.

--- apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.IntegrationTests/GrpcInfo.cs
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.IntegrationTests/GrpcInfo.cs
@@ -19,7 +19,7 @@ using System;
 using System.Linq;
 using System.Threading;
 
-namespace Google.Cloud.ClientTesting
+namespace Google.Cloud.Bigtable.V2.IntegrationTests
 {
     /// <summary>
     /// Exposes information about the underlying gRPC layer.
