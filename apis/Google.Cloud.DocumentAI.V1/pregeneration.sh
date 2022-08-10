d6b2d0b24bb5b36f4c80753ceceb1da677e8b3a7
Amanda Tarafa Mas <atarafamas@google.com>
2022-08-10 16:54:00 +0100

    build: Update location dependencies in Document AI. - Updates preprocessing script to remove the resource definition for the documentai.googleapis.com/Location type without relying on it not being the last resource definition. - Add Google.Cloud.Location dependencies as Location mixins are being generated now. - This change apply to both Document AI V1 and V1Beta3 (the CL for V1Beta3 is coming).

--- apis/Google.Cloud.DocumentAI.V1/pregeneration.sh
+++ apis/Google.Cloud.DocumentAI.V1/pregeneration.sh
@@ -4,4 +4,16 @@ set -e
 
 # Delete the resource definition for the documentai.googleapis.com/Location type.
 # It has the same pattern as the common location type, so we want to use that instead.
-sed -i -z '/option (google.api.resource_definition) = {\n type: "documentai.googleapis.com\/Location"/I,+2 d' $GOOGLEAPIS/google/cloud/documentai/v1/*.proto
+
+proto_files=$(dir $GOOGLEAPIS/google/cloud/documentai/v1/*.proto)
+
+for proto_file in $proto_files
+do
+  index=$(grep -n -h -- 'type: "documentai.googleapis.com\/Location"' $proto_file | sed 's/:.*//')
+  if [[ $index != "" ]]
+  then
+    start="$(($index-1))"
+    end="$(($index+2))"
+    sed -i ""$start","$end"d" $proto_file
+  fi
+done
--- apis/Google.Cloud.DocumentAI.V1Beta3/pregeneration.sh
+++ apis/Google.Cloud.DocumentAI.V1Beta3/pregeneration.sh
@@ -4,7 +4,16 @@ set -e
 
 # Delete the resource definition for the documentai.googleapis.com/Location type.
 # It has the same pattern as the common location type, so we want to use that instead.
-# Note that takes advantage of the fact that there's another resource definition
-# immediately after it - so it removes the first line of *that* resource definition,
-# but leaves the first line of the location definition, because sed doesn't backtrack.
-sed -i '/ type: "documentai.googleapis.com\/Location"/I,+3 d' $GOOGLEAPIS/google/cloud/documentai/v1beta3/*.proto
+
+proto_files=$(dir $GOOGLEAPIS/google/cloud/documentai/v1beta3/*.proto)
+
+for proto_file in $proto_files
+do
+  index=$(grep -n -h -- 'type: "documentai.googleapis.com\/Location"' $proto_file | sed 's/:.*//')
+  if [[ $index != "" ]]
+  then
+    start="$(($index-1))"
+    end="$(($index+2))"
+    sed -i ""$start","$end"d" $proto_file
+  fi
+done
