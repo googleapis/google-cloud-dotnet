ec2a936b1514990eb8c1f785a42ffe06fb38d4bb
Jon Skeet <jonskeet@google.com>
2022-09-01 12:30:15 +0100

    chore: Remove now-unnecessary pregeneration change for Google.Cloud.NetworkSecurity.V1Beta1

--- apis/Google.Cloud.NetworkSecurity.V1Beta1/pregeneration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Add a minimalist comment to the NetworkSecurity service so it will generate.
-sed -i 's/service NetworkSecurity/\/\/ Network Security service.\nservice NetworkSecurity/g' \
-  $GOOGLEAPIS/google/cloud/networksecurity/v1beta1/network_security.proto
