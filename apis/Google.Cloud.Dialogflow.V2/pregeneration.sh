b3301e281b2e8f6d55fef1e42947f9e8a419bb80
Jon Skeet <jonskeet@google.com>
2022-09-01 12:21:20 +0100

    chore: Remove now-unnecessary Dialogflow.V2 preprocessing

--- apis/Google.Cloud.Dialogflow.V2/pregeneration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Fix comments to avoid unnecessary broken links and XML escaping in format strings.
-
-dotnet run --project Google.Cloud.Dialogflow.V2.PatchProtos -- $GOOGLEAPIS/google/cloud/dialogflow/v2
