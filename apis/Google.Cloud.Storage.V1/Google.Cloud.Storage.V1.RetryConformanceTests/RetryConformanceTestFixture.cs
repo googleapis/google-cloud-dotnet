17c0f1838a587cc0fed935a71e693d155a08a9c5
Jon Skeet <jonskeet@google.com>
2022-11-24 15:11:08 +0000

    chore: Minor tweaks for the Storage retry conformance tests

--- apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.RetryConformanceTests/RetryConformanceTestFixture.cs
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.RetryConformanceTests/RetryConformanceTestFixture.cs
@@ -12,13 +12,12 @@
 // See the License for the specific language governing permissions and 
 // limitations under the License.
 
-using Google.Cloud.ClientTesting;
-using System.Threading;
-using System;
-using Xunit;
-using System.IO;
 using Google.Cloud.Storage.V1.Tests.Conformance;
+using System;
+using System.IO;
 using System.Net.Http;
+using System.Threading;
+using Xunit;
 
 namespace Google.Cloud.Storage.V1.RetryConformanceTests;
 
