406b22ab78f26c6016eb368aaddd9bdc6a57b271
Jon Skeet <jonskeet@google.com>
2022-11-25 13:59:39 +0000

    tests: Check all GAPIC clients can be created successfully
    This will prevent failures of HTTP configuration for REGAPIC clients.

--- tools/Google.Cloud.Tools.Common/GeneratorType.cs
+++ tools/Google.Cloud.Tools.Common/GeneratorType.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2019 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@ namespace Google.Cloud.Tools.Common
     public enum GeneratorType
     {
         None,
-        Gapic,
         Micro,
         Proto
     }
