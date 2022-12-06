77d085ccff6e74775e290eb4bd4dc46839880a50
Jon Skeet <jonskeet@google.com>
2022-12-06 09:11:31 +0000

    chore: Regenerate all APIs
    This has picked up the rest-numeric-enums changes for packages which are regenerated by OwlBot

--- apis/Google.Cloud.DocumentAI.V1/Google.Cloud.DocumentAI.V1/PackageApiMetadata.g.cs
+++ apis/Google.Cloud.DocumentAI.V1/Google.Cloud.DocumentAI.V1/PackageApiMetadata.g.cs
@@ -29,6 +29,7 @@ namespace Google.Cloud.DocumentAI.V1
     {
         /// <summary>The <see cref="gaxgrpc::ApiMetadata"/> for services in this package.</summary>
         internal static gaxgrpc::ApiMetadata ApiMetadata { get; } = new gaxgrpc::ApiMetadata("Google.Cloud.DocumentAI.V1", GetFileDescriptors)
+            .WithRequestNumericEnumJsonEncoding(true)
             .WithHttpRuleOverrides(new scg::Dictionary<string, proto::ByteString>
             {
                 {
