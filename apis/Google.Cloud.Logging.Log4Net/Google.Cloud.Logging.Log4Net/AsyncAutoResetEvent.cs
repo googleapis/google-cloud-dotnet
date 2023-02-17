89047c8c36ae0cfb4b4755f2998f90d3f942dd88
Jon Skeet <jonskeet@google.com>
2023-02-17 10:48:08 +0000

    chore: Fix style nits in Log4Net appender

--- apis/Google.Cloud.Logging.Log4Net/Google.Cloud.Logging.Log4Net/AsyncAutoResetEvent.cs
+++ apis/Google.Cloud.Logging.Log4Net/Google.Cloud.Logging.Log4Net/AsyncAutoResetEvent.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// Copyright 2016 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ namespace Google.Cloud.Logging.Log4Net
 {
     internal sealed class AsyncAutoResetEvent
     {
-        private SemaphoreSlim _sem = new SemaphoreSlim(0, 1);
+        private readonly SemaphoreSlim _sem = new SemaphoreSlim(0, 1);
 
         public void Set()
         {
