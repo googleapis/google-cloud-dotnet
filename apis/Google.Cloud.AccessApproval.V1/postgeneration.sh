3fb2f43c22b9557ce3fe4e59e109ba6d17a81c18
Jon Skeet <jonskeet@google.com>
2023-02-28 11:21:55 +0000

    chore: Fix snippet metadata for renamed AccessApproval services

--- apis/Google.Cloud.AccessApproval.V1/postgeneration.sh
+++ apis/Google.Cloud.AccessApproval.V1/postgeneration.sh
@@ -11,3 +11,8 @@ sed -i s/AccessApprovalService.Descriptor/AccessApproval.Descriptor/g Google.Clo
 
 # Fix up the metadata
 sed -i 's/"AccessApprovalService": {/"AccessApproval": {/g' gapic_metadata.json
+
+# Fix up the snippet metadata (carefully)
+sed -i 's/v1.AccessApprovalService/v1.AccessApproval/g' Google.Cloud.AccessApproval.V1.GeneratedSnippets/snippet_metadata_google.cloud.accessapproval.v1.json
+sed -i 's/generated_AccessApprovalService/generated_AccessApproval/g' Google.Cloud.AccessApproval.V1.GeneratedSnippets/snippet_metadata_google.cloud.accessapproval.v1.json
+sed -i 's/"shortName": "AccessApprovalService"/"shortName": "AccessApproval"/g' Google.Cloud.AccessApproval.V1.GeneratedSnippets/snippet_metadata_google.cloud.accessapproval.v1.json
