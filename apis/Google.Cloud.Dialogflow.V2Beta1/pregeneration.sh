98c67f92bd26bb7fb4a818a22cf147b3fc1d7a13
Jon Skeet <jonskeet@google.com>
2023-02-28 10:53:29 +0000

    chore: Remove Google.Cloud.Dialogflow.V2Beta1 namespace tweaking
    This is now fixed in the published protos.

--- apis/Google.Cloud.Dialogflow.V2Beta1/pregeneration.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Correct the version number from V2beta1 to V2Beta1 in the namespace
-sed -i 's/option csharp_namespace = "Google.Cloud.Dialogflow.V2beta1";/option csharp_namespace = "Google.Cloud.Dialogflow.V2Beta1";/g' $GOOGLEAPIS/google/cloud/dialogflow/v2beta1/*.proto
