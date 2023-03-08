ffd3d2503498ca2dafcb9aded897cd5b1b34cd29
Jon Skeet <jonskeet@google.com>
2023-03-08 10:20:43 +0000

    chore: Remove regeneration customization for Google.Cloud.BigQuery.Logging.V1

--- apis/Google.Cloud.BigQuery.Logging.V1/pregeneration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Add the C# namespace option to the proto, just before the Go option
-sed -i '/^option go_package/i option csharp_namespace="Google.Cloud.BigQuery.Logging.V1";' $GOOGLEAPIS/google/cloud/bigquery/logging/v1/*.proto
