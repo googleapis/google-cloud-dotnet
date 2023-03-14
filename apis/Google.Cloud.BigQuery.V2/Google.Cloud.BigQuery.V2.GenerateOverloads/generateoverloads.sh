9ab29d285acc7c365ffc388a1db094e1b395ac3e
Jon Skeet <jonskeet@google.com>
2023-03-14 16:23:21 +0000

    chore: Modify the BigQuery generator to separate manual and generated code
    This moves all generated code into .g.cs files.
    The generator is simpler, and it's easier to tell generated code apart from hand-written code now.
    
    Generation is in the next commit

--- apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.GenerateOverloads/generateoverloads.sh
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.GenerateOverloads/generateoverloads.sh
@@ -2,12 +2,15 @@
 
 declare -r CLIENT=../Google.Cloud.BigQuery.V2/BigQueryClient
 
-dotnet run -- Methods/DatasetCrud.xml $CLIENT.DatasetCrud.cs
-dotnet run -- Methods/DatasetLabels.xml $CLIENT.DatasetLabels.cs
-dotnet run -- Methods/InsertData.xml $CLIENT.InsertData.cs
-dotnet run -- Methods/JobCrud.xml $CLIENT.JobCrud.cs
-dotnet run -- Methods/ModelCrud.xml $CLIENT.ModelCrud.cs
-dotnet run -- Methods/Queries.xml $CLIENT.Queries.cs
-dotnet run -- Methods/RoutineCrud.xml $CLIENT.RoutineCrud.cs
-dotnet run -- Methods/ServiceAccount.xml $CLIENT.ServiceAccount.cs
-dotnet run -- Methods/TableCrud.xml $CLIENT.TableCrud.cs
+# Remove all previously-generated code.
+rm ../Google.Cloud.BigQuery.V2/*.g.cs
+
+dotnet run -- Methods/DatasetCrud.xml $CLIENT.DatasetCrud.g.cs
+dotnet run -- Methods/DatasetLabels.xml $CLIENT.DatasetLabels.g.cs
+dotnet run -- Methods/InsertData.xml $CLIENT.InsertData.g.cs
+dotnet run -- Methods/JobCrud.xml $CLIENT.JobCrud.g.cs
+dotnet run -- Methods/ModelCrud.xml $CLIENT.ModelCrud.g.cs
+dotnet run -- Methods/Queries.xml $CLIENT.Queries.g.cs
+dotnet run -- Methods/RoutineCrud.xml $CLIENT.RoutineCrud.g.cs
+dotnet run -- Methods/ServiceAccount.xml $CLIENT.ServiceAccount.g.cs
+dotnet run -- Methods/TableCrud.xml $CLIENT.TableCrud.g.cs
