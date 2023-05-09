7256cea8c48cf9285cff28a1e4162638e97294a8
Jon Skeet <jonskeet@google.com>
2023-05-09 10:04:49 +0100

    chore: Remove now-unnecessary AIPlatform generation mods

--- apis/Google.Cloud.AIPlatform.V1/pregeneration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Rename resource types that would otherwise cause naming conflicts
-
-sed -i 's/"datalabeling.googleapis.com\/Dataset"/"datalabeling.googleapis.com\/DataLabelingDataset"/g' $GOOGLEAPIS/google/cloud/aiplatform/v1/*.proto
-sed -i 's/"automl.googleapis.com\/Dataset"/"automl.googleapis.com\/AutoMLDataset"/g' $GOOGLEAPIS/google/cloud/aiplatform/v1/*.proto
-sed -i 's/"automl.googleapis.com\/Model"/"automl.googleapis.com\/AutoMLModel"/g' $GOOGLEAPIS/google/cloud/aiplatform/v1/*.proto
