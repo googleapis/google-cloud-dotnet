020471ed41b3695f7a11a3ad1e4730344a1e0508
Jon Skeet <jonskeet@google.com>
2023-06-02 15:12:43 +0100

    chore: Update Logging method name disambiguation
    This was initially done before the change to AIP-136 had been
    discussed. This commit brings the names in line with AIP-136.

--- apis/Google.Cloud.Logging.V2/postgeneration.sh
+++ apis/Google.Cloud.Logging.V2/postgeneration.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Fix the gRPC generated code (for the grpc::Method instances) so it refers to the right server-side RPC
-sed -i 's/"BeginUpdateBucket"/"UpdateBucketAsync"/g' Google.Cloud.Logging.V2/LoggingConfigGrpc.g.cs
-sed -i 's/"BeginCreateBucket"/"CreateBucketAsync"/g' Google.Cloud.Logging.V2/LoggingConfigGrpc.g.cs
+sed -i 's/"UpdateBucketLongRunning"/"UpdateBucketAsync"/g' Google.Cloud.Logging.V2/LoggingConfigGrpc.g.cs
+sed -i 's/"CreateBucketLongRunning"/"CreateBucketAsync"/g' Google.Cloud.Logging.V2/LoggingConfigGrpc.g.cs
 
 # Undo the changes made in pregeneration.sh
 git -C $GOOGLEAPIS checkout google/logging/v2
--- apis/Google.Cloud.Logging.V2/pregeneration.sh
+++ apis/Google.Cloud.Logging.V2/pregeneration.sh
@@ -5,8 +5,7 @@ set -e
 # LoggingConfig has methods UpdateBucket and UpdateBucketAsync,
 # and CreateBucket and CreateBucketAsync... which causes conflicts
 # in both GAPIC and gRPC-generated code.
-# For now, rename the "XyzAsync" versions to "BeginXyz".
-# We'll revisit this naming before the next release.
-# (AIPs will be updated to prohibit this and suggest a different name.)
-sed -i 's/rpc UpdateBucketAsync/rpc BeginUpdateBucket/g' $GOOGLEAPIS/google/logging/v2/logging_config.proto
-sed -i 's/rpc CreateBucketAsync/rpc BeginCreateBucket/g' $GOOGLEAPIS/google/logging/v2/logging_config.proto
+# We rename the "XyzAsync" versions to "XyzLongRunning", in line with
+# https://google.aip.dev/136#rpc-name
+sed -i 's/rpc UpdateBucketAsync/rpc UpdateBucketLongRunning/g' $GOOGLEAPIS/google/logging/v2/logging_config.proto
+sed -i 's/rpc CreateBucketAsync/rpc CreateBucketLongRunning/g' $GOOGLEAPIS/google/logging/v2/logging_config.proto
