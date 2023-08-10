39751ec29b2c1aec495c06730b5bccead249be05
Hemanshv <hemanshverma@google.com>
2023-08-10 16:39:01 +0530

    chore: test release

--- .kokoro/autorelease.sh
+++ .kokoro/autorelease.sh
@@ -15,3 +15,15 @@ populate_all_secrets
 
 # Secrets are already populated, let's not do that again
 ./release.sh --skippopulatesecrets
+
+echo "Not really doing a release"
+
+# We use COMMITTISH_OVERRIDE in the real release, but
+# it's fine to use it here as a way of failing.
+if [[ "$COMMITTISH_OVERRIDE" != "" ]]
+then
+  echo "Failing! $COMMITTISH_OVERRIDE"
+  exit 1
+fi
+
+echo "Fake release succeeded!"
