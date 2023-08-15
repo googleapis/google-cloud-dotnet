a56565f43c94cc9230e0558b1255e48483c7f806
Jon Skeet <jonskeet@google.com>
2023-08-15 14:09:04 +0100

    chore: Replace Moq with NSubstitute in BigQuery tests

--- apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.Tests/CloudProjectTest.cs
+++ apis/Google.Cloud.BigQuery.V2/Google.Cloud.BigQuery.V2.Tests/CloudProjectTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2016 Google Inc. All Rights Reserved.
+// Copyright 2016 Google Inc. All Rights Reserved.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 using Google.Apis.Bigquery.v2;
 using Google.Apis.Bigquery.v2.Data;
-using Moq;
+using NSubstitute;
 using Xunit;
 using static Google.Apis.Bigquery.v2.Data.ProjectList;
 
@@ -33,7 +33,7 @@ namespace Google.Cloud.BigQuery.V2.Tests
                 Id = id,
                 ProjectReference = new ProjectReference { ProjectId = id }
             };
-            var project = new CloudProject(new Mock<BigQueryClient>().Object, data);
+            var project = new CloudProject(Substitute.For<BigQueryClient>(), data);
             Assert.Equal(name, project.FriendlyName);
             Assert.Equal(id, project.ProjectId);
             Assert.Equal(id, project.Reference.ProjectId);
