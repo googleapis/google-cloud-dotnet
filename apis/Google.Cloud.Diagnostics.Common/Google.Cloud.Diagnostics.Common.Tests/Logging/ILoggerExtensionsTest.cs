452ba4db1bddae08b9baf52d52aeaedb1e3a1bed
Jon Skeet <jonskeet@google.com>
2023-08-16 16:31:08 +0100

    chore: Use an in-memory logger instead of mocking

--- apis/Google.Cloud.Diagnostics.Common/Google.Cloud.Diagnostics.Common.Tests/Logging/ILoggerExtensionsTest.cs
+++ apis/Google.Cloud.Diagnostics.Common/Google.Cloud.Diagnostics.Common.Tests/Logging/ILoggerExtensionsTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2021 Google LLC
+// Copyright 2021 Google LLC
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,11 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Cloud.Diagnostics.Common.Tests.Logging;
 using Microsoft.Extensions.Logging;
-using Moq;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace Google.Cloud.Diagnostics.Common.Tests
@@ -26,194 +24,117 @@ namespace Google.Cloud.Diagnostics.Common.Tests
         [Fact]
         public void WithLabels_NoChangesInOriginalLogger()
         {
-            var loggerMock = new Mock<ILogger>(MockBehavior.Strict)
-                .SetupLog("Cutting hair");
+            var originalLogger = new MemoryLogger("Category");
 
-            var originalLogger = loggerMock.Object;
             var augmentedLogger = originalLogger.WithLabels(new KeyValuePair<string, string>("job_title", "hairdresser"));
-
             Assert.NotSame(originalLogger, augmentedLogger);
-            loggerMock.VerifyNoOtherCalls();
 
             originalLogger.LogInformation("Cutting hair");
 
-            loggerMock
-                .VerifyLog(Times.Once())
-                .VerifyNoOtherCalls();
+            var logs = originalLogger.ListLogEntries();
+            var entry = Assert.Single(logs);
+            Assert.Equal("Cutting hair", entry.Message);
+            Assert.Empty(entry.Scopes);
         }
 
         [Fact]
         public void WithLabels_BeginsScopeAndEndsScope()
         {
-            var scopeMock = GetScopeMock();
-
-            var loggerMock = new Mock<ILogger>(MockBehavior.Strict)
-                .SetupLog("Cutting hair")
-                .SetupBeginScope(scopeMock.Object, new KeyValuePair<string, string>("job_title", "hairdresser"));
-
-            var originalLogger = loggerMock.Object;
+            var originalLogger = new MemoryLogger("Category");
             var augmentedLogger = originalLogger.WithLabels(new KeyValuePair<string, string>("job_title", "hairdresser"));
 
             augmentedLogger.LogInformation("Cutting hair");
 
-            scopeMock.Verify(scope => scope.Dispose(), Times.Once());
-            loggerMock
-                .VerifyLog(Times.Once())
-                .VerifyBeginScope(Times.Once())
-                .VerifyNoOtherCalls();
+            var logs = originalLogger.ListLogEntries();
+            var entry = Assert.Single(logs);
+            Assert.Equal("Cutting hair", entry.Message);
+            AssertLabels(entry, "job_title:hairdresser");
         }
 
         [Fact]
         public void WithLabels_UseOriginalAfterAugmented()
         {
-            var scopeMock = GetScopeMock();
-
-            var loggerMock = new Mock<ILogger>(MockBehavior.Strict)
-                .SetupLog("Cutting hair")
-                .SetupBeginScope(scopeMock.Object, new KeyValuePair<string, string>("job_title", "hairdresser"));
-
-            var originalLogger = loggerMock.Object;
+            var originalLogger = new MemoryLogger("Category");
             var augmentedLogger = originalLogger.WithLabels(new KeyValuePair<string, string>("job_title", "hairdresser"));
 
             augmentedLogger.LogInformation("Cutting hair");
-            scopeMock.Verify(scope => scope.Dispose(), Times.Once());
-            originalLogger.LogInformation("Cutting hair");
+            originalLogger.LogInformation("Busy");
 
-            loggerMock
-                .VerifyLog(Times.Exactly(2))
-                .VerifyBeginScope(Times.Once())
-                .VerifyNoOtherCalls();
+            var logs = originalLogger.ListLogEntries();
+            Assert.Equal(2, logs.Count);
+            var augmentedEntry = logs[0];
+            Assert.Equal("Cutting hair", augmentedEntry.Message);
+            AssertLabels(augmentedEntry, "job_title:hairdresser");
+
+            var plainEntry = logs[1];
+            Assert.Equal("Busy", plainEntry.Message);
+            Assert.Empty(plainEntry.Scopes);
         }
 
         [Fact]
         public void WithAddedLabels()
         {
-            var scopeMock = GetScopeMock();
-
-            var loggerMock = new Mock<ILogger>(MockBehavior.Strict)
-                .SetupLog("Cutting hair")
-                .SetupBeginScope(scopeMock.Object,
-                    new KeyValuePair<string, string>("job_title", "hairdresser"),
-                    new KeyValuePair<string, string>("tool", "scissors"));
-
-            var originalLogger = loggerMock.Object;
+            var originalLogger = new MemoryLogger("Category");
             var oneLabelLogger = originalLogger.WithLabels(new KeyValuePair<string, string>("job_title", "hairdresser"));
             var twoLabelLogger = oneLabelLogger.WithAddedLabels(new KeyValuePair<string, string>("tool", "scissors"));
 
             Assert.NotSame(oneLabelLogger, twoLabelLogger);
-
             twoLabelLogger.LogInformation("Cutting hair");
 
-            scopeMock.Verify(scope => scope.Dispose(), Times.Once());
-
-            loggerMock
-                .VerifyLog(Times.Once())
-                .VerifyBeginScope(Times.Once())
-                .VerifyNoOtherCalls();
+            var logs = originalLogger.ListLogEntries();
+            var entry = Assert.Single(logs);
+            Assert.Equal("Cutting hair", entry.Message);
+            AssertLabels(entry, "job_title:hairdresser", "tool:scissors");
         }
 
         [Fact]
         public void WithAddedLabels_NoWithLabels()
         {
-            var scopeMock = GetScopeMock();
-
-            var loggerMock = new Mock<ILogger>(MockBehavior.Strict)
-                .SetupLog("Cutting hair")
-                .SetupBeginScope(scopeMock.Object, new KeyValuePair<string, string>("job_title", "hairdresser"));
-
-            var originalLogger = loggerMock.Object;
+            var originalLogger = new MemoryLogger("Category");
             var augmentedLogger = originalLogger.WithAddedLabels(new KeyValuePair<string, string>("job_title", "hairdresser"));
-
             augmentedLogger.LogInformation("Cutting hair");
 
-            scopeMock.Verify(scope => scope.Dispose(), Times.Once());
-            loggerMock
-                .VerifyLog(Times.Once())
-                .VerifyBeginScope(Times.Once())
-                .VerifyNoOtherCalls();
+            var logs = originalLogger.ListLogEntries();
+            var entry = Assert.Single(logs);
+            Assert.Equal("Cutting hair", entry.Message);
+            AssertLabels(entry, "job_title:hairdresser");
         }
 
         [Fact]
         public void WithAddedLabels_UseInitiallyAugmented()
         {
-            var oneLabelScopeMock = GetScopeMock();
-            var twoLabelsScopeMock = GetScopeMock();
-
-            var loggerMock = new Mock<ILogger>(MockBehavior.Strict)
-                .SetupLog("Cutting hair")
-                .SetupBeginScope(oneLabelScopeMock.Object, new KeyValuePair<string, string>("job_title", "hairdresser"))
-                .SetupBeginScope(twoLabelsScopeMock.Object,
-                    new KeyValuePair<string, string>("job_title", "hairdresser"),
-                    new KeyValuePair<string, string>("tool", "scissors"));
-
-            var originalLogger = loggerMock.Object;
+            var originalLogger = new MemoryLogger("Category");
             var oneLabelLogger = originalLogger.WithLabels(new KeyValuePair<string, string>("job_title", "hairdresser"));
             var twoLabelLogger = oneLabelLogger.WithAddedLabels(new KeyValuePair<string, string>("tool", "scissors"));
 
             twoLabelLogger.LogInformation("Cutting hair");
+            oneLabelLogger.LogInformation("Busy");
 
-            oneLabelScopeMock.Verify(scope => scope.Dispose(), Times.Never());
-            twoLabelsScopeMock.Verify(scope => scope.Dispose(), Times.Once());
+            var logs = originalLogger.ListLogEntries();
+            Assert.Equal(2, logs.Count);
+            var twoLabelEntry = logs[0];
+            Assert.Equal("Cutting hair", twoLabelEntry.Message);
+            AssertLabels(twoLabelEntry, "job_title:hairdresser", "tool:scissors");
 
-            oneLabelLogger.LogInformation("Cutting hair");
-
-            oneLabelScopeMock.Verify(scope => scope.Dispose(), Times.Once());
-            twoLabelsScopeMock.Verify(scope => scope.Dispose(), Times.Once());
-
-            loggerMock
-                .VerifyLog(Times.Exactly(2))
-                .VerifyBeginScope(Times.Exactly(2))
-                .VerifyNoOtherCalls();
+            var oneLabelEntry = logs[1];
+            Assert.Equal("Busy", oneLabelEntry.Message);
+            AssertLabels(oneLabelEntry, "job_title:hairdresser");
         }
 
-        private Mock<IDisposable> GetScopeMock()
+        private static void AssertLabels(TestLogEntry entry, params string[] labels)
         {
-            var scopeMock = new Mock<IDisposable>(MockBehavior.Strict);
-            scopeMock.Setup(scope => scope.Dispose());
-            return scopeMock;
-        }
-    }
+            var scope = Assert.Single(entry.Scopes);
+            var labellingScope = Assert.IsType<LabellingScopeState>(scope);
+            var actualLabels = labellingScope.Labels;
+            Assert.Equal(labels.Length, actualLabels.Count);
 
-    public static class ILoggerMockExtensions
-    {
-        public static Mock<ILogger> SetupBeginScope(this Mock<ILogger> loggerMock, IDisposable returnValue, params KeyValuePair<string, string>[] acceptedLabels)
-        {
-            loggerMock
-            .Setup(logger => logger.BeginScope(
-                It.Is<LabellingScopeState>(state => state.Labels.Count == acceptedLabels.Length && !state.Labels.Except(acceptedLabels).Any())))
-            .Returns(returnValue);
-
-            return loggerMock;
-        }
-
-        public static Mock<ILogger> SetupLog(this Mock<ILogger> loggerMock, string expectedFinalMessage)
-        {
-            // This is messy due to the type of the delegate passed as a formatter.
-            var invocationAction = new InvocationAction(invocation =>
+            foreach (var pair in labels)
             {
-                var state = invocation.Arguments[2];
-                var formatter = (Delegate) invocation.Arguments[4];
-                var message = formatter.DynamicInvoke(invocation.Arguments[2], invocation.Arguments[3]);
-                Assert.Equal(expectedFinalMessage, message);
-            });
-            loggerMock
-                .Setup(logger => logger.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), It.IsAny<Exception>(), It.IsAny<Func<It.IsAnyType, Exception, string>>()))
-                .Callback(invocationAction);
-
-            return loggerMock;
-        }
-
-        public static Mock<ILogger> VerifyBeginScope(this Mock<ILogger> loggerMock, Times times)
-        {
-            loggerMock.Verify(logger => logger.BeginScope(It.IsAny<object>()), times);
-
-            return loggerMock;
-        }
-
-        public static Mock<ILogger> VerifyLog(this Mock<ILogger> loggerMock, Times times)
-        {
-            loggerMock.Verify(logger => logger.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), null, It.IsAny<Func<It.IsAnyType, Exception, string>>()), times);
-            return loggerMock;
+                var bits = pair.Split(':');
+                var key = bits[0];
+                var value = bits[1];
+                Assert.Equal(value, actualLabels[key]);
+            }
         }
     }
 }
---
+++ apis/Google.Cloud.Diagnostics.Common/Google.Cloud.Diagnostics.Common.Tests/Logging/TestLogEntry.cs
@@ -0,0 +1,92 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+//
+// https://www.apache.org/licenses/LICENSE-2.0 
+//
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Google.Cloud.Diagnostics.Common.Tests.Logging;
+
+/// <summary>
+/// A very simplified representation of a log entry, aimed at testing.
+/// Adapted from https://github.com/GoogleCloudPlatform/functions-framework-dotnet/blob/main/src/Google.Cloud.Functions.Testing/TestLogEntry.cs
+/// </summary>
+public sealed class TestLogEntry
+{
+    private static readonly IReadOnlyList<object> EmptyScopeList = new List<object>().AsReadOnly();
+
+    /// <summary>
+    /// The category name of the log entry.
+    /// </summary>
+    public string CategoryName { get; }
+
+    /// <summary>
+    /// The level of the log entry.
+    /// </summary>
+    public LogLevel Level { get; }
+
+    /// <summary>
+    /// The exception in the log entry, or null if there was no exception.
+    /// </summary>
+    public Exception Exception { get; }
+
+    /// <summary>
+    /// The event ID of the log entry.
+    /// </summary>
+    public EventId EventId { get; }
+
+    /// <summary>
+    /// The message of the log entry.
+    /// </summary>
+    public string Message { get; }
+
+    /// <summary>
+    /// The scopes of the log entry. This is never null, but may be empty.
+    /// </summary>
+    public IReadOnlyList<object> Scopes { get; }
+
+    private TestLogEntry(string categoryName, LogLevel level, EventId eventId, string message, Exception exception, IReadOnlyList<object> scopes) =>
+        (CategoryName, Level, EventId, Message, Exception, Scopes) =
+        (categoryName, level, eventId, message, exception, scopes);
+
+    internal static TestLogEntry Create<TState>(
+        string categoryName, LogLevel logLevel, EventId eventId,
+        TState state, Exception exception, Func<TState, Exception, string> formatter,
+        IExternalScopeProvider scopeProvider)
+    {
+        var message = formatter(state, exception);
+        List<object> scopes = null;
+        scopeProvider.ForEachScope((scope, state) =>
+        {
+            if (scope is null)
+            {
+                return;
+            }
+            if (scopes is null)
+            {
+                scopes = new List<object>();
+            }
+            scopes.Add(scope);
+        }, state: (object) null);
+
+        return new TestLogEntry(categoryName, logLevel, eventId, message, exception, scopes?.AsReadOnly() ?? EmptyScopeList);
+    }
+
+    /// <summary>
+    /// Returns a diagnostic form of the log entry, in the form "[Level]: Message",
+    /// to simplify debugging.
+    /// </summary>
+    /// <returns>A diagnostic form of the log entry.</returns>
+    public override string ToString() => $"[{Level}]: {Message}";
+}
