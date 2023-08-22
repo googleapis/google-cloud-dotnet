d00edd27b6b1bbac5f0b0ea79f7c1e88e5074fa0
Jon Skeet <jonskeet@google.com>
2023-08-22 15:42:28 +0100

    chore: Final part of refactoring of release progress reporter
    The GitHub class was mostly very simple proxying methods, which didn't add much to GitHubClient.
    Likewise the PullRequestDetails class, which had most of the information required to make the calls (owner, repo etc) was only being used for data.
    
    By moving useful methods into PullRequestDetails, we end up with simpler code in PublishReporter.

--- tools/Google.Cloud.Tools.ReleaseProgressReporter/GitHub.cs
+++ tools/Google.Cloud.Tools.ReleaseProgressReporter/GitHub.cs
@@ -18,82 +18,10 @@ using Octokit;
 namespace Google.Cloud.Tools.ReleaseProgressReporter;
 
 /// <summary>
-/// Helper class to call GitHub APIs.
+/// Helper class for GitHub API activities that aren't simply exposed via <see cref="GitHubClient"/>.
 /// </summary>
-public class GitHub
-{
-    private readonly GitHubClient _githubClient;
-
-    public GitHub(string appId, string accessToken)
-    {
-        _githubClient = new GitHubClient(new ProductHeaderValue(appId))
-        {
-            Credentials = new Credentials(accessToken)
-        };
-    }
-
-    /// <summary>
-    /// Lists all the pull requests in the given repository.
-    /// </summary>
-    /// <param name="owner">The Owner of GitHub repository.</param>
-    /// <param name="repoName">The GitHub repository name.</param>
-    public async Task<IReadOnlyList<PullRequest>> ListPullRequestsAsync(string owner, string repoName) =>
-        await _githubClient.PullRequest.GetAllForRepository(owner, repoName);
-
-    /// <summary>
-    /// Get the specific pull request from the given GitHub repository.
-    /// </summary>
-    /// <param name="owner">The Owner of GitHub repository.</param>
-    /// <param name="repoName">The GitHub repository name.</param>
-    /// <param name="number">Pull request number.</param>
-    public async Task<PullRequest> GetPullRequestAsync(string owner, string repoName, int number) =>
-        await _githubClient.PullRequest.Get(owner, repoName, number);
-
-    /// <summary>
-    /// Creates a pull request on the given GitHub repository.
-    /// </summary>
-    /// <param name="owner">The Owner of GitHub repository.</param>
-    /// <param name="repoName">The GitHub repository name.</param>
-    /// <param name="head">The branch (or git ref) where your changes are implemented. Must not be null.</param>
-    /// <param name="baseBranch">The branch (or git ref) you want your changes pulled into. Must not be null.</param>
-    /// <param name="title">The title of the pull request.</param>
-    public async Task<PullRequest> CreatePullRequestAsync(string owner, string repoName, string head, string baseBranch, string title) =>
-        await _githubClient.PullRequest.Create(owner, repoName, new NewPullRequest(title, head, baseBranch));
-
-    /// <summary>
-    /// Adds a comment on the given pull request.
-    /// </summary>
-    /// <param name="owner">The Owner of GitHub repository.</param>
-    /// <param name="repoName">The GitHub repository name.</param>
-    /// <param name="number">Pull request identifier.</param>
-    /// <param name="comment">The comment to add on the pull request.</param>
-    public async Task<IssueComment> CreatePullRequestCommentAsync(string owner, string repoName, int number, string comment) =>
-        await _githubClient.Issue.Comment.Create(owner, repoName, number, comment);
-
-    /// <summary>
-    /// Adds the given label to the given pull request.
-    /// </summary>
-    /// <param name="owner">The Owner of GitHub repository.</param>
-    /// <param name="repoName">The GitHub repository name.</param>
-    /// <param name="number">Pull request identifier.</param>
-    /// <param name="labels">Labels to add on the pull request.</param>
-    public async Task<IReadOnlyList<Label>> AddIssueLabel(string owner, string repoName, int number, string[] labels) =>
-        await _githubClient.Issue.Labels.AddToIssue(owner, repoName, number, labels);
-
-    /// <summary>
-    /// Adds the new labels and remove the given old labels.
-    /// </summary>
-    /// <param name="repoName">The GitHub repository name.</param>
-    /// <param name="owner">The Owner of GitHub repository.</param>
-    /// <param name="number">Pull request identifier.</param>
-    /// <param name="newLabelsToAdd">New labels to add on the pull request.</param>
-    /// <param name="oldLablesToRemove">Old labels to remove from the pull request.</param>
-    public async Task<IReadOnlyList<Label>> UpdatePullLabelsAsync(string owner, string repoName, int number, string labelToAdd, string labelToRemove)
-    {
-        await _githubClient.Issue.Labels.RemoveFromIssue(owner, repoName, number, labelToRemove);
-        return await AddIssueLabel(owner, repoName, number, new string[] { labelToAdd });
-    }
-
+public static class GitHub
+{    
     /// <summary>
     /// Fetches a GitHub access token, requested from a JWT signed with a private key.
     /// </summary>
--- tools/Google.Cloud.Tools.ReleaseProgressReporter/PublishReporter.cs
+++ tools/Google.Cloud.Tools.ReleaseProgressReporter/PublishReporter.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and 
 // limitations under the License.
 
+using Octokit;
+
 namespace Google.Cloud.Tools.ReleaseProgressReporter;
 
 /// <summary>
@@ -19,12 +21,12 @@ namespace Google.Cloud.Tools.ReleaseProgressReporter;
 /// </summary>
 public class PublishReporter
 {
-    private readonly GitHub _gitHub;
+    private readonly GitHubClient _client;
     private readonly PullRequestDetails _pullRequest;
 
-    public PublishReporter(GitHub gitHub, PullRequestDetails pullRequest)
+    public PublishReporter(GitHubClient client, PullRequestDetails pullRequest)
     {
-        _gitHub = gitHub;
+        _client = client;
         _pullRequest = pullRequest;
     }
 
@@ -46,8 +48,8 @@ public class PublishReporter
         string message = string.IsNullOrEmpty(buildUrl) ?
             "The release build has started, but the build log URL could not be determined."
             : $"The release build has started; the log can be viewed [here]({buildUrl}).";
-
-        await _gitHub.CreatePullRequestCommentAsync(_pullRequest.Owner, _pullRequest.RepoName, _pullRequest.Id, message).ConfigureAwait(false);
+        
+        await _pullRequest.AddComment(_client, message);
     }
 
     /// <summary>
@@ -59,7 +61,8 @@ public class PublishReporter
             ? ("Release build succeeded.", "autorelease: published")
             : ("The release build failed! Please investigate!", "autorelease: failed");
         message += "\n" + details;
-        await _gitHub.CreatePullRequestCommentAsync(_pullRequest.Owner, _pullRequest.RepoName, _pullRequest.Id, message).ConfigureAwait(false);
-        await _gitHub.UpdatePullLabelsAsync(_pullRequest.Owner, _pullRequest.RepoName, _pullRequest.Id, label, labelToRemove: "autorelease: tagged").ConfigureAwait(false);
+        await _pullRequest.AddComment(_client, message);
+        await _pullRequest.AddLabel(_client, label);
+        await _pullRequest.RemoveLabel(_client, "autorelease: tagged");
     }
 }
