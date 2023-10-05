e4e303458298c3a63924690a4d9f3504d1c3ee86
Amanda Tarafa Mas <atarafamas@google.com>
2023-10-05 08:49:48 -0700

    chore: Clean up stale Firestore DBs used in tests.

--- apis/Google.Cloud.Firestore/Google.Cloud.Firestore.CleanTestData/Program.cs
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore.CleanTestData/Program.cs
@@ -12,9 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Api.Gax.ResourceNames;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Http;
+using Google.Cloud.Firestore.Admin.V1;
+using Newtonsoft.Json;
 using System;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Google.Cloud.Firestore.CleanTestData
 {
@@ -23,13 +32,36 @@ namespace Google.Cloud.Firestore.CleanTestData
         private static async Task Main(string[] args)
         {
             string project = Environment.GetEnvironmentVariable("FIRESTORE_TEST_PROJECT");
-            var db = FirestoreDb.Create(project);
+
+            var db = await FirestoreDb.CreateAsync(project);
+            var adminClient = await FirestoreAdminClient.CreateAsync();
+
+            // Use the REST API for operations not yet supported by Google.Cloud.Firestore.Admin.V1,
+            // currently DB deletion.
+            // TODO: Start using Google.Cloud.Firestore.Admin.V1 when it supports DB deletion.
+            string credentialsPath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+            string restApiUrlBase = "https://firestore.googleapis.com/v1";
+            string scope = "https://www.googleapis.com/auth/datastore";
+            GoogleCredential googleCredential = GoogleCredential.FromFile(credentialsPath).CreateScoped(scope);
+            HttpClient httpClient = new HttpClientFactory()
+                .CreateHttpClient(new CreateHttpClientArgs { Initializers = { googleCredential } });
+
             var collections = await db.ListRootCollectionsAsync().ToListAsync();
             foreach (var collection in collections)
             {
-                if (collection.Id.StartsWith("test-"))
+                if (collection.Id.StartsWith("test-", StringComparison.Ordinal))
                 {
                     await DeleteCollectionAsync(collection);
+                }
+            }
+
+            var deleteDatabaseEndpointTemplate = $"{restApiUrlBase}/projects/{project}/databases/";
+            var databases = (await adminClient.ListDatabasesAsync(ProjectName.FromProject(project))).Databases;
+            foreach (var database in databases)
+            {
+                if (database.DatabaseName.DatabaseId.StartsWith("db", StringComparison.Ordinal))
+                {
+                    await DeleteDatabaseAsync(database.DatabaseName.DatabaseId);
                 }
             }
 
@@ -49,6 +81,22 @@ namespace Google.Cloud.Firestore.CleanTestData
                     batch.Delete(doc);
                 }
                 await batch.CommitAsync();
+            }
+
+            async Task DeleteDatabaseAsync(string databaseId)
+            {
+                Console.WriteLine($"Attemting to delete {databaseId}");
+                try
+                {
+                    var deleteDatabaseEndpoint = new Uri($"{deleteDatabaseEndpointTemplate}{databaseId}");
+                    var response = await httpClient.DeleteAsync(deleteDatabaseEndpoint);
+                    response.EnsureSuccessStatusCode();
+                    Console.WriteLine($"Success deleting {databaseId}");
+                }
+                catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine($"Failure deleting {databaseId}: Not Found");
+                }
             }
         }
     }
