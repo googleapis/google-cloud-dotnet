0ed5bb4c998054cc6aaaaf6102b89babd2ef4551
Jon Skeet <jonskeet@google.com>
2023-10-09 14:38:54 +0100

    chore: Update Firestore database cleanup
    - Refactor slightly to use regular methods instead of everything being in Main
    - Use a file-scoped namespace
    - Only log deletion of top-level collections (to simplify understanding the output)
    - Delete indexes for test- collections in the default database

--- apis/Google.Cloud.Firestore/Google.Cloud.Firestore.CleanTestData/Program.cs
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore.CleanTestData/Program.cs
@@ -16,87 +16,107 @@ using Google.Api.Gax.ResourceNames;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Http;
 using Google.Cloud.Firestore.Admin.V1;
-using Newtonsoft.Json;
 using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using static System.Formats.Asn1.AsnWriter;
 
-namespace Google.Cloud.Firestore.CleanTestData
+namespace Google.Cloud.Firestore.CleanTestData;
+
+internal class Program
 {
-    internal class Program
+    private static async Task Main()
     {
-        private static async Task Main(string[] args)
+        string project = Environment.GetEnvironmentVariable("FIRESTORE_TEST_PROJECT");
+
+        await DeleteCollectionsAsync(project);
+        await DeleteIndexesAsync(project);
+        await DeleteDatabasesAsync(project);
+    }
+
+    private static async Task DeleteCollectionsAsync(string projectId)
+    {
+        var db = await FirestoreDb.CreateAsync(projectId);
+        var collections = await db.ListRootCollectionsAsync()
+            .Where(collection => collection.Id.StartsWith("test-", StringComparison.Ordinal))
+            .ToListAsync();
+        foreach (var collection in collections)
         {
-            string project = Environment.GetEnvironmentVariable("FIRESTORE_TEST_PROJECT");
+            // Log which collections we're deleting here rather than in DeleteCollectionAsync,
+            // as that's called recursively.
+            Console.WriteLine($"Deleting collection {collection.Id}");
+            await DeleteCollectionAsync(collection);
+        }
 
-            var db = await FirestoreDb.CreateAsync(project);
-            var adminClient = await FirestoreAdminClient.CreateAsync();
-
-            // Use the REST API for operations not yet supported by Google.Cloud.Firestore.Admin.V1,
-            // currently DB deletion.
-            // TODO: Start using Google.Cloud.Firestore.Admin.V1 when it supports DB deletion.
-            string credentialsPath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
-            string restApiUrlBase = "https://firestore.googleapis.com/v1";
-            string scope = "https://www.googleapis.com/auth/datastore";
-            GoogleCredential googleCredential = GoogleCredential.FromFile(credentialsPath).CreateScoped(scope);
-            HttpClient httpClient = new HttpClientFactory()
-                .CreateHttpClient(new CreateHttpClientArgs { Initializers = { googleCredential } });
-
-            var collections = await db.ListRootCollectionsAsync().ToListAsync();
-            foreach (var collection in collections)
+        async Task DeleteCollectionAsync(CollectionReference collection)
+        {
+            var allDocs = await collection.ListDocumentsAsync().ToListAsync();
+            // Note: one batch per collection is less efficient than filling the batch each time,
+            // but it's not a big problem.
+            var batch = db.StartBatch();
+            foreach (var doc in allDocs)
             {
-                if (collection.Id.StartsWith("test-", StringComparison.Ordinal))
+                foreach (var child in await doc.ListCollectionsAsync().ToListAsync())
                 {
-                    await DeleteCollectionAsync(collection);
+                    await DeleteCollectionAsync(child);
                 }
+                batch.Delete(doc);
             }
+            await batch.CommitAsync();
+        }
+    }
 
-            var deleteDatabaseEndpointTemplate = $"{restApiUrlBase}/projects/{project}/databases/";
-            var databases = (await adminClient.ListDatabasesAsync(ProjectName.FromProject(project))).Databases;
-            foreach (var database in databases)
+    private static async Task DeleteIndexesAsync(string projectId)
+    {
+        var adminClient = await FirestoreAdminClient.CreateAsync();
+        var indexes = await adminClient.ListIndexesAsync(new CollectionGroupName(projectId, "(default)", "-"))
+            .Where(index => index.IndexName.CollectionId.StartsWith("test-", StringComparison.Ordinal))
+            .ToListAsync();
+
+        foreach (var index in indexes)
+        {
+            Console.WriteLine($"Deleting index {index.Name}");
+            await adminClient.DeleteIndexAsync(index.IndexName);
+        }
+    }
+
+    private static async Task DeleteDatabasesAsync(string projectId)
+    {
+        var adminClient = await FirestoreAdminClient.CreateAsync();
+
+        // Use the REST API for operations not yet supported by Google.Cloud.Firestore.Admin.V1,
+        // currently DB deletion.
+        // TODO: Start using Google.Cloud.Firestore.Admin.V1 when it supports DB deletion.
+        string credentialsPath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+        string restApiUrlBase = "https://firestore.googleapis.com/v1";
+        string scope = "https://www.googleapis.com/auth/datastore";
+        GoogleCredential googleCredential = GoogleCredential.FromFile(credentialsPath).CreateScoped(scope);
+        HttpClient httpClient = new HttpClientFactory()
+            .CreateHttpClient(new CreateHttpClientArgs { Initializers = { googleCredential } });
+
+        var deleteDatabaseEndpointTemplate = $"{restApiUrlBase}/projects/{projectId}/databases/";
+        var databases = (await adminClient.ListDatabasesAsync(ProjectName.FromProject(projectId))).Databases
+            .Where(db => db.DatabaseName.DatabaseId.StartsWith("test-", StringComparison.Ordinal))
+            .ToList();
+        foreach (var database in databases)
+        {
+            await DeleteDatabaseAsync(database.DatabaseName.DatabaseId);
+        }
+
+        async Task DeleteDatabaseAsync(string databaseId)
+        {
+            Console.WriteLine($"Attempting to delete {databaseId}");
+            try
             {
-                if (database.DatabaseName.DatabaseId.StartsWith("db", StringComparison.Ordinal))
-                {
-                    await DeleteDatabaseAsync(database.DatabaseName.DatabaseId);
-                }
+                var deleteDatabaseEndpoint = new Uri($"{deleteDatabaseEndpointTemplate}{databaseId}");
+                var response = await httpClient.DeleteAsync(deleteDatabaseEndpoint);
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine($"Success deleting {databaseId}");
             }
-
-            async Task DeleteCollectionAsync(CollectionReference collection)
+            catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
             {
-                Console.WriteLine($"Deleting {collection.Id}");
-                var allDocs = await collection.ListDocumentsAsync().ToListAsync();
-                // Note: one batch per collection is less efficient than filling the batch each time,
-                // but it's not a big problem.
-                var batch = db.StartBatch();
-                foreach (var doc in allDocs)
-                {
-                    foreach (var child in await doc.ListCollectionsAsync().ToListAsync())
-                    {
-                        await DeleteCollectionAsync(child);
-                    }
-                    batch.Delete(doc);
-                }
-                await batch.CommitAsync();
-            }
-
-            async Task DeleteDatabaseAsync(string databaseId)
-            {
-                Console.WriteLine($"Attemting to delete {databaseId}");
-                try
-                {
-                    var deleteDatabaseEndpoint = new Uri($"{deleteDatabaseEndpointTemplate}{databaseId}");
-                    var response = await httpClient.DeleteAsync(deleteDatabaseEndpoint);
-                    response.EnsureSuccessStatusCode();
-                    Console.WriteLine($"Success deleting {databaseId}");
-                }
-                catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
-                {
-                    Console.WriteLine($"Failure deleting {databaseId}: Not Found");
-                }
+                Console.WriteLine($"Failure deleting {databaseId}: Not Found");
             }
         }
     }
