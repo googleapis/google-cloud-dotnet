d2fb21553371a9a6be1e2a31a35aada0b563f3cb
Jon Skeet <jonskeet@google.com>
2023-11-01 07:36:42 +0000

    chore: Update project generator to use xUnit 2.6.0

--- tools/Google.Cloud.Tools.ReleaseManager/GenerateProjectsCommand.cs
+++ tools/Google.Cloud.Tools.ReleaseManager/GenerateProjectsCommand.cs
@@ -107,7 +107,7 @@ namespace Google.Cloud.Tools.ReleaseManager
         {
             { "Google.Cloud.ClientTesting", ProjectVersionValue }, // Needed for all snippets and some other tests - easiest to just default
             { "Microsoft.NET.Test.Sdk", "17.7.2" },
-            { "xunit", "2.5.3" },
+            { "xunit", "2.6.0" },
             { "xunit.runner.visualstudio", "2.5.3" },
             { "Xunit.SkippableFact", "1.4.13" },
             { "NSubstitute", "5.1.0" },
