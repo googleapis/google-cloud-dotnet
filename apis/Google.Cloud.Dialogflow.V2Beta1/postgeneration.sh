82431c0204c89fbe8f6ceb453e5fa4ca834e7907
Jon Skeet <jonskeet@google.com>
2024-01-26 13:00:45 +0000

    chore: Update generator, protoc and gRPC versions
    This change renders five of our postgeneration scripts redundant.
    
    Fixes #10768
    
    (This anticipates a generator version of 1.4.24; that doesn't exist
    quite yet.)
    
    Once the generator is released and this is merged, we can regenerate
    everything, revert AI Platform changes (it still needs fixes) and
    then just not merge OwlBot PRs until we update versions there too.

--- apis/Google.Cloud.Dialogflow.V2Beta1/postgeneration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Ignore deprecation in the generated gRPC code
-sed -i 's/#pragma warning disable/#pragma warning disable 0612,/g' Google.Cloud.Dialogflow.V2Beta1/ParticipantGrpc.g.cs
--- apis/Google.Cloud.OsConfig.V1Alpha/postgeneration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Suppress deprecation warnings in generated gRPC code.
-# (If this becomes a common problem, we'll want a more robust fix.
-# It'll do for now.)
-
-sed -i 's/^#pragma warning disable 0414, 1591, 8981$/#pragma warning disable 0414, 1591, 8981, 0612/g' Google.Cloud.OsConfig.V1Alpha/OsconfigZonalServiceGrpc.g.cs
