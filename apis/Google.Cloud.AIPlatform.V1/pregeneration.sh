3107e37545c3d9a1315da36e289c79011e6051e9
Jon Skeet <jonskeet@google.com>
2024-01-30 16:43:06 +0000

    fix: Tweak generation for AI Platform so that the generated code builds

---
+++ apis/Google.Cloud.AIPlatform.V1/pregeneration.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Rename the Schema message to OpenApiSchema to avoid a conflict with the
+# namespace called Schema.
+sed -i 's/^\( *\)Schema/\1OpenApiSchema/g' $GOOGLEAPIS/google/cloud/aiplatform/v1/*.proto
+sed -i 's/^message Schema/message OpenApiSchema/g' $GOOGLEAPIS/google/cloud/aiplatform/v1/*.proto
+sed -i 's/map<string, Schema>/map<string, OpenApiSchema>/g' $GOOGLEAPIS/google/cloud/aiplatform/v1/*.proto
+
+# Remove the pre-deprecated field in QueryDeployedModelsResponse
+sed -i '/repeated DeployedModel deployed_models = 1 \[deprecated = true\]/d' \
+  $GOOGLEAPIS/google/cloud/aiplatform/v1/deployment_resource_pool_service.proto
