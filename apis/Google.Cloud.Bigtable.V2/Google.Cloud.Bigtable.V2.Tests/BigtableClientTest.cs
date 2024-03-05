222a1f22b66fb8f1ffdc57b847cc8930ffd04491
Jon Skeet <jonskeet@google.com>
2024-03-05 11:10:54 +0000

    chore: Remove now-invalid test
    We were validating that TableName wasn't null before, but it's not an optional field in SampleRowKeysRequest, so we don't validate it.

--- apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.Tests/BigtableClientTest.cs
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.Tests/BigtableClientTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2017 Google Inc. All rights reserved.
+// Copyright 2017 Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -741,13 +741,6 @@ namespace Google.Cloud.Bigtable.V2.Tests
             Assert.Throws<NotImplementedException>(() => client.SampleRowKeys(requestWithAppProfileId));
             Assert.NotEqual(appProfileIdOnClient, requestWithAppProfileId.AppProfileId);
             Assert.Equal(appProfileIdOnRequest, requestWithAppProfileId.AppProfileId);
-        }
-
-        [Fact]
-        public void SampleRowKeys_Validate_TableName()
-        {
-            var client = new MockBigtableClient();
-            Assert.Throws<ArgumentNullException>(() => client.SampleRowKeys((TableName)null));
         }
 
         private static BigtableClient CreateClient(string appProfileId) =>
