a01a21b28824e63891dcc2e914372ecdfea2d9ae
Jon Skeet <jonskeet@google.com>
2024-03-08 07:42:07 +0000

    chore: Remove workaround for Google.Apps.Events.Subscriptions.V1 generation
    The protos have now been fixed. Tested by regenerating: there are no changes to the generated code after the workaround has been removed.

--- apis/Google.Apps.Events.Subscriptions.V1/pregeneration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Temporary workaround for a method signature which redundantly includes pageToken and pageSize
-# See b/325289454
-sed -i 's/"page_size, page_token, filter"/"filter"/g' $GOOGLEAPIS/google/apps/events/subscriptions/v1/subscriptions_service.proto
