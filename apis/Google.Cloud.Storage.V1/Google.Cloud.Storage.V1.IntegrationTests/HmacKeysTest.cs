9d2bb1851cabb8bfd5644afb0932d7eb02001ca6
Amanda Tarafa Mas <atarafamas@google.com>
2024-04-18 15:56:39 -0700

    tests: Fix HMAC Key tests.
    Given that changes are only eventually consistent.

--- apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.IntegrationTests/HmacKeysTest.cs
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.IntegrationTests/HmacKeysTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2019 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,12 +24,19 @@ namespace Google.Cloud.Storage.V1.IntegrationTests
     [Collection(nameof(StorageFixture))]
     public class HmacKeysTest
     {
+        // https://cloud.google.com/storage/docs/authentication/hmackeys#restrictions
+        private static readonly TimeSpan s_hmacKeyCreationConsistencyDelay = TimeSpan.FromSeconds(30);
+        // https://cloud.google.com/storage/docs/consistency#strongly_consistent_operations
+        private static readonly TimeSpan s_hmacKeyStateChangeConsistencyDelay = TimeSpan.FromMinutes(3);
+
         private readonly StorageFixture _fixture;
 
         public HmacKeysTest(StorageFixture fixture) => _fixture = fixture;
 
         [Fact]
-        public void Lifecycle()
+        // This test is async because of the helpers for testing eventually consistent changes.
+        // We are testing sync production code though.
+        public async Task Lifecycle()
         {
             var client = _fixture.Client;
             var projectId = _fixture.ProjectId;
@@ -49,10 +56,13 @@ namespace Google.Cloud.Storage.V1.IntegrationTests
             Convert.FromBase64String(key.Secret);
 
             // We should now be able to find the key when listing it, either with no filter or filtering by the right email address.
-            var listed = Assert.Single(client.ListHmacKeys(projectId));
-            Assert.Equal(accessId, listed.AccessId);
-            listed = Assert.Single(client.ListHmacKeys(projectId, serviceAccountEmail));
-            Assert.Equal(accessId, listed.AccessId);
+            await _fixture.EventuallyAsync(s_hmacKeyCreationConsistencyDelay, () =>
+            {
+                var listed = Assert.Single(client.ListHmacKeys(projectId));
+                Assert.Equal(accessId, listed.AccessId);
+                listed = Assert.Single(client.ListHmacKeys(projectId, serviceAccountEmail));
+                Assert.Equal(accessId, listed.AccessId);
+            });
 
             // But not when filtering with the wrong email address.
             Assert.Empty(client.ListHmacKeys(projectId, alternativeServiceAccountEmail));
@@ -69,19 +79,27 @@ namespace Google.Cloud.Storage.V1.IntegrationTests
             Assert.Equal(fixme.State, updated.State);
 
             // We should be able to fetch the key and see that it's been updated
-            var fetched = client.GetHmacKey(projectId, accessId);
-            Assert.Equal(fixme.State, fetched.State);
+            // Because of eventual consistency, this can take up to 3 minutes, see
+            // https://cloud.google.com/storage/docs/consistency#strongly_consistent_operations
+            await _fixture.EventuallyAsync(s_hmacKeyStateChangeConsistencyDelay, () =>
+            {
+                var fetched = client.GetHmacKey(projectId, accessId);
+                Assert.Equal(fixme.State, fetched.State);
+            });
 
             // We should be able to delete the key
             client.DeleteHmacKey(projectId, accessId);
 
-            var deleted = client.GetHmacKey(projectId, accessId);
-            Assert.Equal(HmacKeyStates.Deleted, deleted.State);
+            await _fixture.EventuallyAsync(s_hmacKeyStateChangeConsistencyDelay, () =>
+            {
+                var deleted = client.GetHmacKey(projectId, accessId);
+                Assert.Equal(HmacKeyStates.Deleted, deleted.State);
 
-            // The deleted key should not show normally in list operations, but should show if we ask for deleted ones
-            Assert.Empty(client.ListHmacKeys(projectId));
-            Assert.Single(client.ListHmacKeys(projectId, options: new ListHmacKeysOptions { ShowDeletedKeys = true }),
-                k => k.AccessId == accessId);
+                // The deleted key should not show normally in list operations, but should show if we ask for deleted ones
+                Assert.Empty(client.ListHmacKeys(projectId));
+                Assert.Single(client.ListHmacKeys(projectId, options: new ListHmacKeysOptions { ShowDeletedKeys = true }),
+                    k => k.AccessId == accessId);
+            });
         }
 
         [Fact]
@@ -104,10 +122,14 @@ namespace Google.Cloud.Storage.V1.IntegrationTests
             Convert.FromBase64String(key.Secret);
 
             // We should now be able to find the key when listing it, either with no filter or filtering by the right email address.
-            var listed = Assert.Single(await client.ListHmacKeysAsync(projectId).ToListAsync());
-            Assert.Equal(accessId, listed.AccessId);
-            listed = Assert.Single(await client.ListHmacKeysAsync(projectId, serviceAccountEmail).ToListAsync());
-            Assert.Equal(accessId, listed.AccessId);
+            await _fixture.EventuallyAsync(s_hmacKeyCreationConsistencyDelay, async () =>
+            {
+                var listed = Assert.Single(await client.ListHmacKeysAsync(projectId).ToListAsync());
+                Assert.Equal(accessId, listed.AccessId);
+                listed = Assert.Single(await client.ListHmacKeysAsync(projectId, serviceAccountEmail).ToListAsync());
+                Assert.Equal(accessId, listed.AccessId);
+            });
+
             // But not when filtering with the wrong email address
             Assert.Empty(await client.ListHmacKeysAsync(projectId, alternativeServiceAccountEmail).ToListAsync());
 
@@ -123,18 +145,25 @@ namespace Google.Cloud.Storage.V1.IntegrationTests
             Assert.Equal(fixme.State, updated.State);
 
             // We should be able to fetch the key and see that it's been updated
-            var fetched = await client.GetHmacKeyAsync(projectId, accessId);
-            Assert.Equal(fixme.State, fetched.State);
+            await _fixture.EventuallyAsync(s_hmacKeyStateChangeConsistencyDelay, async () =>
+            {
+                var fetched = await client.GetHmacKeyAsync(projectId, accessId);
+                Assert.Equal(fixme.State, fetched.State);
+            });
 
             // We should be able to delete the key
             await client.DeleteHmacKeyAsync(projectId, accessId);
-            var deleted = await client.GetHmacKeyAsync(projectId, accessId);
-            Assert.Equal(HmacKeyStates.Deleted, deleted.State);
 
-            // The deleted key should not show normally in list operations, but should show if we ask for deleted ones
-            Assert.Empty(await client.ListHmacKeysAsync(projectId).ToListAsync());
-            Assert.Single(await client.ListHmacKeysAsync(projectId, options: new ListHmacKeysOptions { ShowDeletedKeys = true }).ToListAsync(),
-                k => k.AccessId == accessId);
+            await _fixture.EventuallyAsync(s_hmacKeyStateChangeConsistencyDelay, async () =>
+            {
+                var deleted = await client.GetHmacKeyAsync(projectId, accessId);
+                Assert.Equal(HmacKeyStates.Deleted, deleted.State);
+
+                // The deleted key should not show normally in list operations, but should show if we ask for deleted ones
+                Assert.Empty(await client.ListHmacKeysAsync(projectId).ToListAsync());
+                Assert.Single(await client.ListHmacKeysAsync(projectId, options: new ListHmacKeysOptions { ShowDeletedKeys = true }).ToListAsync(),
+                    k => k.AccessId == accessId);
+            });
         }
 
         private static string GetServiceAccountEmail()
