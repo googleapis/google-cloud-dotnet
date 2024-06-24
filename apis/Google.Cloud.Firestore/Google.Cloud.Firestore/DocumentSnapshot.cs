0bbb31d122b803c4832dbc18f6ba7a1a90dd1c35
Jon Skeet <jonskeet@google.com>
2024-06-24 10:06:13 +0100

    chore: Whitespace and using directives only
    This is primarily about BOMs, so that later meaningful changes don't end up with BOM changes intermingled.

--- apis/Google.Cloud.Firestore/Google.Cloud.Firestore/DocumentSnapshot.cs
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore/DocumentSnapshot.cs
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 using Google.Api.Gax;
-using Google.Cloud.Firestore.Converters;
 using Google.Cloud.Firestore.V1;
 using System;
 using System.Collections.Generic;
--- apis/Google.Cloud.Firestore/Google.Cloud.Firestore/Precondition.cs
+++ apis/Google.Cloud.Firestore/Google.Cloud.Firestore/Precondition.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2017, Google Inc. All rights reserved.
+// Copyright 2017, Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using Case = Google.Cloud.Firestore.V1.Precondition.ConditionTypeOneofCase;
 
 namespace Google.Cloud.Firestore
