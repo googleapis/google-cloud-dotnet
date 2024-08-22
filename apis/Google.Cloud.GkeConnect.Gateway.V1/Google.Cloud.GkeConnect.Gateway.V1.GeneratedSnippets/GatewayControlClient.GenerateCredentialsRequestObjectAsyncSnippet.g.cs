8b18d526af41f774a76448a82a4a81d5def38cb1
Jon Skeet <jonskeet@google.com>
2024-08-22 10:40:55 +0100

    chore: Generate Google.Cloud.GkeConnect.Gateway.V1

---
+++ apis/Google.Cloud.GkeConnect.Gateway.V1/Google.Cloud.GkeConnect.Gateway.V1.GeneratedSnippets/GatewayControlClient.GenerateCredentialsRequestObjectAsyncSnippet.g.cs
@@ -0,0 +1,51 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+namespace GoogleCSharpSnippets
+{
+    // [START connectgateway_v1_generated_GatewayControl_GenerateCredentials_async]
+    using Google.Cloud.GkeConnect.Gateway.V1;
+    using System.Threading.Tasks;
+
+    public sealed partial class GeneratedGatewayControlClientSnippets
+    {
+        /// <summary>Snippet for GenerateCredentialsAsync</summary>
+        /// <remarks>
+        /// This snippet has been automatically generated and should be regarded as a code template only.
+        /// It will require modifications to work:
+        /// - It may require correct/in-range values for request initialization.
+        /// - It may require specifying regional endpoints when creating the service client as shown in
+        ///   https://cloud.google.com/dotnet/docs/reference/help/client-configuration#endpoint.
+        /// </remarks>
+        public async Task GenerateCredentialsRequestObjectAsync()
+        {
+            // Create client
+            GatewayControlClient gatewayControlClient = await GatewayControlClient.CreateAsync();
+            // Initialize request argument(s)
+            GenerateCredentialsRequest request = new GenerateCredentialsRequest
+            {
+                Name = "",
+                ForceUseAgent = false,
+                Version = "",
+                KubernetesNamespace = "",
+                OperatingSystem = GenerateCredentialsRequest.Types.OperatingSystem.Unspecified,
+            };
+            // Make the request
+            GenerateCredentialsResponse response = await gatewayControlClient.GenerateCredentialsAsync(request);
+        }
+    }
+    // [END connectgateway_v1_generated_GatewayControl_GenerateCredentials_async]
+}
