ffcf4c656b8191de69f4e69b67e12b77875ad78d
Jon Skeet <jonskeet@google.com>
2024-09-17 12:44:19 +0100

    chore: Regenerate Google.Cloud.OracleDatabase.V1

--- apis/Google.Cloud.OracleDatabase.V1/Google.Cloud.OracleDatabase.V1/OracleDatabaseClient.g.cs
+++ apis/Google.Cloud.OracleDatabase.V1/Google.Cloud.OracleDatabase.V1/OracleDatabaseClient.g.cs
@@ -714,7 +714,7 @@ namespace Google.Cloud.OracleDatabase.V1
         });
 
         /// <summary>The service metadata associated with this client type.</summary>
-        public static gaxgrpc::ServiceMetadata ServiceMetadata { get; } = new gaxgrpc::ServiceMetadata(OracleDatabase.Descriptor, DefaultEndpoint, DefaultScopes, true, gax::ApiTransports.Grpc | gax::ApiTransports.Rest, PackageApiMetadata.ApiMetadata);
+        public static gaxgrpc::ServiceMetadata ServiceMetadata { get; } = new gaxgrpc::ServiceMetadata(OracleDatabase.Descriptor, DefaultEndpoint, DefaultScopes, true, gax::ApiTransports.Rest, PackageApiMetadata.ApiMetadata);
 
         internal static gaxgrpc::ChannelPool ChannelPool { get; } = new gaxgrpc::ChannelPool(ServiceMetadata);
 
