0d22595264777f3a6e3d076529fedb170a01e4da
Jon Skeet <jonskeet@google.com>
2024-12-09 09:41:07 +0000

    chore: Stop using git to undo changes in googleapis
    The "tweaks" should assume they can modify googleapis - the calling code should take a copy beforehand if necessary.

--- generator-input/tweaks/Google.Cloud.Logging.V2/postgeneration.sh
+++ generator-input/tweaks/Google.Cloud.Logging.V2/postgeneration.sh
@@ -7,6 +7,3 @@ cd $GENERATOR_OUTPUT_DIR/apis/Google.Cloud.Logging.V2
 # Fix the gRPC generated code (for the grpc::Method instances) so it refers to the right server-side RPC
 sed -i 's/"UpdateBucketLongRunning"/"UpdateBucketAsync"/g' Google.Cloud.Logging.V2/LoggingConfigGrpc.g.cs
 sed -i 's/"CreateBucketLongRunning"/"CreateBucketAsync"/g' Google.Cloud.Logging.V2/LoggingConfigGrpc.g.cs
-
-# Undo the changes made in pregeneration.sh
-git -C $GOOGLEAPIS checkout google/logging/v2
