28207716dabc9fce972a1a29e4c17cdabcad845b
Jon Skeet <jonskeet@google.com>
2025-03-20 08:11:53 +0000

    tools: Introduced --library-id option into the build command
    This is cleaner than treating an API path as a library ID. We
    probably want to move to have two separate commands eventually
    though: build-raw and build-configured.

---
+++ tools/Google.Cloud.Tools.ReleaseManager/ContainerCommands/ContainerOptions.cs
@@ -0,0 +1,24 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License"):
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Google.Cloud.Tools.ReleaseManager.ContainerCommands;
+
+internal static class ContainerOptions
+{
+    internal const string ApiPathOption = "api-path";
+    internal const string LibraryIdOption = "library-id";
+    internal const string RepoRootOption = "repo-root";
+    internal const string GeneratorOutputOption = "generator-output";
+    internal const string TestOption = "test";
+}
