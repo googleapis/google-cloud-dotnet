a274f86080f0976e47f99dcc642ef26bb4f077df
Jon Skeet <jonskeet@google.com>
2025-04-23 16:00:10 +0100

    tools: Provide the Bigtable client generator in the 1PP container
    This allows generation to take place with no network access.

--- generator-input/tweaks/Google.Cloud.Bigtable.V2/postgeneration.sh
+++ generator-input/tweaks/Google.Cloud.Bigtable.V2/postgeneration.sh
@@ -4,8 +4,17 @@ set -e
 
 BIGTABLE_OUTPUT=$GENERATOR_OUTPUT_DIR/apis/Google.Cloud.Bigtable.V2
 
-# Generate BigtableClient
-dotnet run --project Google.Cloud.Bigtable.V2.GenerateClient -- \
-  $BIGTABLE_OUTPUT/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.csproj \
-  BigtableServiceApiClient \
-  BigtableClient
+# Generate BigtableClient, either using a pre-built version of our client
+# generator if we're running in a container, or building-and-running otherwise.
+if [ -d /app/Google.Cloud.Bigtable.V2.GenerateClient ]
+then
+  dotnet /app/Google.Cloud.Bigtable.V2.GenerateClient/Google.Cloud.Bigtable.V2.GenerateClient.dll \
+    $BIGTABLE_OUTPUT/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.csproj \
+    BigtableServiceApiClient \
+    BigtableClient
+else
+  dotnet run --project Google.Cloud.Bigtable.V2.GenerateClient -- \
+    $BIGTABLE_OUTPUT/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.csproj \
+    BigtableServiceApiClient \
+    BigtableClient
+fi
