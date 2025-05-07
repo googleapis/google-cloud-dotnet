4301a6c0d1ed55da14ab9417d9bdfad3a8eed973
Google Cloud SDK Librarian <cloud-sdk-librarian-dotnet-robot@google.com>
2025-05-07 03:30:16 -0400

    feat: Initial generation for Google.Cloud.ModelArmor.V1

---
+++ apis/Google.Cloud.ModelArmor.V1/Google.Cloud.ModelArmor.V1.GeneratedSnippets/ModelArmorClient.DeleteTemplateRequestObjectAsyncSnippet.g.cs
@@ -0,0 +1,48 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+namespace GoogleCSharpSnippets
+{
+    // [START modelarmor_v1_generated_ModelArmor_DeleteTemplate_async]
+    using Google.Cloud.ModelArmor.V1;
+    using System.Threading.Tasks;
+
+    public sealed partial class GeneratedModelArmorClientSnippets
+    {
+        /// <summary>Snippet for DeleteTemplateAsync</summary>
+        /// <remarks>
+        /// This snippet has been automatically generated and should be regarded as a code template only.
+        /// It will require modifications to work:
+        /// - It may require correct/in-range values for request initialization.
+        /// - It may require specifying regional endpoints when creating the service client as shown in
+        ///   https://cloud.google.com/dotnet/docs/reference/help/client-configuration#endpoint.
+        /// </remarks>
+        public async Task DeleteTemplateRequestObjectAsync()
+        {
+            // Create client
+            ModelArmorClient modelArmorClient = await ModelArmorClient.CreateAsync();
+            // Initialize request argument(s)
+            DeleteTemplateRequest request = new DeleteTemplateRequest
+            {
+                TemplateName = TemplateName.FromProjectLocationTemplate("[PROJECT]", "[LOCATION]", "[TEMPLATE]"),
+                RequestId = "",
+            };
+            // Make the request
+            await modelArmorClient.DeleteTemplateAsync(request);
+        }
+    }
+    // [END modelarmor_v1_generated_ModelArmor_DeleteTemplate_async]
+}
