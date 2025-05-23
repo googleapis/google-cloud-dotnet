2ea24fd625bab6e49152da759a0816da19b0ade3
Jon Skeet <jonskeet@google.com>
2025-05-23 12:38:31 +0100

    tools: Implement ApiAnalyzer.MiniIndexEntry.Load
    This now means the "configure" container command works again.

--- tools/Google.Cloud.Tools.ReleaseManager/ApiAnalyzer.cs
+++ tools/Google.Cloud.Tools.ReleaseManager/ApiAnalyzer.cs
@@ -12,15 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Api;
 using Google.Cloud.Tools.Common;
 using Google.Protobuf;
+using Google.Protobuf.Reflection;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
+using System.Text;
 using YamlDotNet.Serialization;
+using IOPath = System.IO.Path; // For disambiguation with the Path property
 
 namespace Google.Cloud.Tools.ReleaseManager;
 
@@ -90,10 +94,7 @@ internal sealed class ApiAnalyzer
         return api;
     }
 
-    // This is internal so that it's available to GenerateApisCommand, for unconfigured
-    // APIs. This is somewhat experimental - if we want this long-term, we should probably
-    // move it elsewhere.
-    internal static Api.Service ParseServiceConfigYaml(string path)
+    internal static Service ParseServiceConfigYaml(string path)
     {
         if (path is null)
         {
@@ -113,7 +114,7 @@ internal sealed class ApiAnalyzer
             // deserialize straight from YAML to a Service...
             json = json.Replace("\"true\"", "true").Replace("\"false\"", "false");
             var parser = new JsonParser(JsonParser.Settings.Default.WithIgnoreUnknownFields(true));
-            return parser.Parse<Api.Service>(json);
+            return parser.Parse<Service>(json);
         }
     }
 
@@ -247,6 +248,25 @@ internal sealed class ApiAnalyzer
     /// </summary>
     private sealed class MiniIndexEntry
     {
+        private static readonly ImmutableDictionary<string, string> MixinDependencies = new Dictionary<string, string>()
+        {
+            { "google.cloud.location.Locations", "Google.Cloud.Location" },
+            { "google.iam.v1.IAMPolicy","Google.Cloud.Iam.V1" },
+            { "google.longrunning.Operations", "Google.LongRunning" }
+        }.ToImmutableDictionary();
+
+        private static readonly ExtensionRegistry s_registry = new ExtensionRegistry
+        {
+            ClientExtensions.DefaultHost,
+            ClientExtensions.MethodSignature,
+            ClientExtensions.OauthScopes,
+            FieldBehaviorExtensions.FieldBehavior,
+            AnnotationsExtensions.Http,
+            ResourceExtensions.Resource,
+            ResourceExtensions.ResourceDefinition,
+            ResourceExtensions.ResourceReference
+        };
+
         /// <summary>
         /// Directory within googleapis, e.g. google/cloud/functions/v2
         /// </summary>
@@ -263,14 +283,123 @@ internal sealed class ApiAnalyzer
         internal ImmutableList<string> MixinPackages { get; }
         internal ImmutableList<string> ImportDirectories { get; }
 
-        private MiniIndexEntry()
+        private MiniIndexEntry(string path, string serviceConfigFile, Service serviceConfig, ImmutableList<FileDescriptor> protoFiles)
         {
+            Path = path;
+            Title = serviceConfig.Title;
+            CSharpNamespace = DeriveCSharpNamespace(protoFiles);
+            DocumentationUri = serviceConfig.Publishing?.DocumentationUri;
+            Description = serviceConfig.Documentation?.Summary?.Replace('\n', ' ') ?? "<UNKNOWN - NO SERVICE CONFIG DOCUMENTATION SUMMARY>";
+            ServiceName = serviceConfig.Name;
+            ServiceConfigFile = serviceConfigFile;
+
+            MixinPackages = MixinDependencies
+                .Join(serviceConfig.Apis, mixin => mixin.Key, api => api.Name, (mixin, api) => mixin.Value)
+                .ToImmutableList();
+            ImportDirectories = protoFiles
+                .SelectMany(file => file.Dependencies)
+                .Select(import => IOPath.GetDirectoryName(import.Name).Replace('\\', '/'))
+                .Distinct()
+                .OrderBy(import => import, StringComparer.Ordinal)
+                .ToImmutableList();
+        }
+
+        private static string DeriveCSharpNamespace(ImmutableList<FileDescriptor> protoFiles)
+        {
+            // We want to enforce two rules:
+            // - All namespaces have a common prefix (effectively representing the "root" package for the API).
+            // - All files in the same protobuf package have the same namespace.
+            // These are validated separately. The rules don't fully enforce "sensible" names, but they're
+            // enough to catch simple errors.
+
+            var allNamespaces = protoFiles.Select(GetCSharpNamespace).Distinct().OrderBy(ns => ns.Length).ToImmutableList();
+            if (allNamespaces.Count == 0)
+            {
+                throw new UserErrorException("No namespaces available. Empty set of protos?");
+            }
+            var shortestNamespace = allNamespaces[0];
+            // We expect all namespaces to have a common prefix - which should be the first entry in our list, as it's ordered
+            // by length. For most APIs we'll only have a single entry anyway, but if there are APIs with subpackages of protos,
+            // those should end up in their own namespace.
+            foreach (var ns in allNamespaces)
+            {
+                if (!ns.StartsWith(shortestNamespace, StringComparison.Ordinal))
+                {
+                    throw new UserErrorException($"Inconsistent namespaces: {ns} is inconsistent with {shortestNamespace}");
+                }
+            }
+
+            // Per-package checking
+            var namespacesByProtoPackage = protoFiles.GroupBy(fd => fd.Package, GetCSharpNamespace);
+            foreach (var group in namespacesByProtoPackage)
+            {
+                if (group.Distinct(StringComparer.Ordinal).Count() != 1)
+                {
+                    throw new UserErrorException($"Multiple namespaces for package '{group.Key}': {string.Join(", ", group)}");
+                }
+            }
+
+            // Looks like it's probably reasonable.
+            return shortestNamespace;
+
+            static string GetCSharpNamespace(FileDescriptor desc)
+            {
+                // Order of looking for C# namespace: "csharp_namespace" option, proto package.
+                // TODO: Also look for the "metadata" proto annotation: Metadata.PackageName
+                string ns = desc?.GetOptions()?.CsharpNamespace;
+                if (!string.IsNullOrEmpty(ns))
+                {
+                    return ns;
+                }
+                // As a fallback, capitalize the first character of each part of the proto package.
+                return string.Join(".", desc.Package.Split('.').Select(x => ToUpperCamelCase(x)));
+            }
         }
 
         internal static MiniIndexEntry Load(ProtobufCompiler protoc, string googleapis, string apiPath)
         {
-            // TODO: Implement this by running protoc etc.
-            return null;
+            protoc.ValidateConfiguration();
+            var descriptorFile = IOPath.GetTempFileName();
+            protoc.Execute(googleapis, IOPath.Combine(googleapis, apiPath), $"--descriptor_set_out={descriptorFile}", "--include_imports");
+            FileDescriptorSet descriptorSet = FileDescriptorSet.Parser.ParseFrom(File.ReadAllBytes(descriptorFile));
+            var descriptorByteStrings = descriptorSet.File.Select(proto => proto.ToByteString());
+            // We've included imports in order to get a complete file descriptor set, but we only want ones within our API
+            // path. The FileDescriptor.Name property gives us the path to the proto; we need to add a "/" to our
+            // apiPath to make sure we don't get descriptors from peer directories.
+            // (For example, if we're looking for "a/b/v1" we don't want "a/b/v1beta/x.proto".)
+            var prefix = apiPath + "/";
+            var apiDescriptors = FileDescriptor.BuildFromByteStrings(descriptorByteStrings, s_registry)
+                .Where(d => d.Name.StartsWith(prefix))
+                .ToImmutableList();
+            var (serviceConfig, serviceConfigFile) = LoadServiceConfig(googleapis, apiPath);
+            return new(apiPath, serviceConfig, serviceConfigFile, apiDescriptors);
         }
+
+        private static (string, Service) LoadServiceConfig(string googleapis, string apiPath)
+        {
+            var yamlPath = Directory.GetFiles(IOPath.Combine(googleapis, apiPath), "*.yaml")
+                .Where(path => !IOPath.GetFileName(path).EndsWith("gapic.yaml"))
+                .FirstOrDefault();
+            if (yamlPath is null)
+            {
+                throw new UserErrorException($"No service config file found for API path '{apiPath}'");
+            }
+            return (IOPath.GetFileName(yamlPath), ParseServiceConfigYaml(yamlPath));
+        }
+
+        // Copied from https://github.com/googleapis/gapic-generator-csharp/blob/main/Google.Api.Generator/Utils/SystemExtensions.cs.
+        // We can move it somewhere more common if we need to...
+        private static char MaybeForceCase(char c, bool? toUpper) =>
+            toUpper is bool upper ? upper ? char.ToUpperInvariant(c) : char.ToLowerInvariant(c) : c;
+
+        private static string Camelizer(string s, bool firstUpper, bool forceAllChars) =>
+            s.Aggregate((upper: (bool?) firstUpper, prev: '\0', sb: new StringBuilder()), (acc, c) =>
+                !char.IsLetterOrDigit(c) ?
+                    (acc.sb.Length > 0 ? true : firstUpper, c, acc.sb) :
+                    (char.IsDigit(c) ? true : forceAllChars ? (bool?) false : null, c,
+                        acc.sb.Append(MaybeForceCase(c, char.IsLower(acc.prev) && char.IsUpper(c) ? true : acc.upper))),
+                acc => acc.sb.ToString());
+
+        private static string ToUpperCamelCase(string input, bool forceAllChars = false) => Camelizer(input, firstUpper: true, forceAllChars);
     }
 }
