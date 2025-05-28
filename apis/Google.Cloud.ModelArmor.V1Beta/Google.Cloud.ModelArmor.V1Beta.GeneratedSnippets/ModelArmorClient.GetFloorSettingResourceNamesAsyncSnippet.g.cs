07c03caa78bf94ca46fb8fa7ba77f3aa469f19df
Google Cloud SDK Librarian <cloud-sdk-librarian-dotnet-robot@google.com>
2025-05-28 13:48:07 +0000

    feat: Initial generation for Google.Cloud.ModelArmor.V1Beta

---
+++ apis/Google.Cloud.ModelArmor.V1Beta/Google.Cloud.ModelArmor.V1Beta.GeneratedSnippets/ModelArmorClient.GetFloorSettingResourceNamesAsyncSnippet.g.cs
@@ -0,0 +1,44 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+namespace GoogleCSharpSnippets
+{
+    // [START modelarmor_v1beta_generated_ModelArmor_GetFloorSetting_async_flattened_resourceNames]
+    using Google.Cloud.ModelArmor.V1Beta;
+    using System.Threading.Tasks;
+
+    public sealed partial class GeneratedModelArmorClientSnippets
+    {
+        /// <summary>Snippet for GetFloorSettingAsync</summary>
+        /// <remarks>
+        /// This snippet has been automatically generated and should be regarded as a code template only.
+        /// It will require modifications to work:
+        /// - It may require correct/in-range values for request initialization.
+        /// - It may require specifying regional endpoints when creating the service client as shown in
+        ///   https://cloud.google.com/dotnet/docs/reference/help/client-configuration#endpoint.
+        /// </remarks>
+        public async Task GetFloorSettingResourceNamesAsync()
+        {
+            // Create client
+            ModelArmorClient modelArmorClient = await ModelArmorClient.CreateAsync();
+            // Initialize request argument(s)
+            FloorSettingName name = FloorSettingName.FromProjectLocation("[PROJECT]", "[LOCATION]");
+            // Make the request
+            FloorSetting response = await modelArmorClient.GetFloorSettingAsync(name);
+        }
+    }
+    // [END modelarmor_v1beta_generated_ModelArmor_GetFloorSetting_async_flattened_resourceNames]
+}
