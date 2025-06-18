673bd7ec0d8587db35892813b10dfba4d54fd5e4
Jon Skeet <jonskeet@google.com>
2025-06-18 10:35:14 +0000

    tests: Build the Docker image at the start of run-container-tests.sh
    (At this point it really makes sense for it to be separate from the
    other integration tests.)

--- run-container-tests.sh
+++ run-container-tests.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# Note: we could alternatively include this in runintegrationtests.sh
+set -e
+
+if [ "$TEST_LIBRARIAN_DOTNET_DOCKER_IMAGE" == "" ]
+then
+  export TEST_LIBRARIAN_DOTNET_DOCKER_IMAGE="test-dotnet-image:$(date -u +%Y%m%dt%H%M%S)z"
+  echo "Building Docker image $TEST_LIBRARIAN_DOTNET_DOCKER_IMAGE"
+  docker build -q -t $TEST_LIBRARIAN_DOTNET_DOCKER_IMAGE -f Dockerfile.generator .
+else
+  echo "Using existing image $TEST_LIBRARIAN_DOTNET_DOCKER_IMAGE"
+fi
 
 dotnet test tools/Google.Cloud.Tools.ReleaseManager.Tests
 dotnet test tools/Google.Cloud.Tools.ReleaseManager.IntegrationTests
