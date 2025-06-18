51496d216c8a6cbbcf7f2d03ddf18b09fc441ce8
Amanda Tarafa Mas <atarafamas@google.com>
2025-06-18 16:54:48 -0700

    feat: Add option to pass the dotnet command.
    Have build raw command use this option.

--- tools/Google.Cloud.Tools.ReleaseManager/ContainerCommands/BuildRawCommand.cs
+++ tools/Google.Cloud.Tools.ReleaseManager/ContainerCommands/BuildRawCommand.cs
@@ -23,6 +23,7 @@ namespace Google.Cloud.Tools.ReleaseManager.ContainerCommands;
 /// Builds code generated for an API, without any other context (no handwritten code etc). Expected options:
 /// - generator-output: the directory containing the results of a previous generate command; required
 /// - api-path: (as provided to generate-raw) e.g. google/cloud/functions/v2; required
+/// - dotnet-path: the path to the dotnet command; optional
 ///
 /// This builds the result of "raw" generation, without any additional configuration etc.
 /// </summary>
@@ -32,6 +33,9 @@ public class BuildRawCommand : IContainerCommand
     {
         var generatorOutput = options.RequireOption(options.GeneratorOutput);
         var apiPath = options.RequireOption(options.ApiPath);
+        // This option is optional for command callers, but we can request it as required from
+        // Options because Options should set a default value for it.
+        var dotnetPath = options.RequireOption(options.DotnetPath);
 
         var apiRoot = Path.Combine(generatorOutput, apiPath);
         if (!Directory.Exists(apiRoot))
@@ -51,8 +55,8 @@ public class BuildRawCommand : IContainerCommand
 
         var psi = new ProcessStartInfo
         {
-            FileName = "/usr/bin/dotnet",
-            WorkingDirectory = apiRoot
+            FileName = dotnetPath,
+            WorkingDirectory = apiRoot,
         };
         var processArguments = new List<string> { "build", "-nologo", "-clp:NoSummary", "-v", "quiet", solution };
         processArguments.ForEach(psi.ArgumentList.Add);
