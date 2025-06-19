8cb2c2cb7fc3ae6c0d345f8a7120ba0f27649cdb
Amanda Tarafa Mas <atarafamas@google.com>
2025-06-19 00:46:02 -0700

    tests: Test BuildLibraryCommand.
    Closes googleapis/librarian#310

---
+++ tools/Google.Cloud.Tools.ReleaseManager.IntegrationTests/ContainerCommands/BuildLibraryCommandTest.cs
@@ -0,0 +1,99 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License"):
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.Tools.ReleaseManager.ContainerCommands;
+using System.IO;
+using Xunit;
+
+namespace Google.Cloud.Tools.ReleaseManager.IntegrationTests.ContainerCommands;
+
+[Collection(nameof(ContainerCommandFixture))]
+public class BuildLibraryCommandTest
+{
+    private readonly ContainerCommandFixture _fixture;
+
+    public BuildLibraryCommandTest(ContainerCommandFixture fixture) => _fixture = fixture;
+
+    [Fact]
+    public void SinglePackage()
+    {
+        var context = _fixture.CreateContext();
+        var catalog = context.CreateApiCatalog("Google.Test.V1", "Google.Test.V2");
+        var codeRepo = context.CreateCodeRepo(catalog);
+        codeRepo.AddScriptMocks();
+
+        var command = new BuildLibraryCommand();
+        var options = ContainerOptions.FromArgs(
+            $"--repo-root={codeRepo.Directory}",
+            "--library-id=Google.Test.V1");
+        Assert.Equal(0, command.Execute(options));
+
+        codeRepo.AssertExist("build.sh.result.txt");
+        Assert.Equal(new string[] { "build-start", "--notests", "Google.Test.V1", "build-end" }, File.ReadAllLines(Path.Combine(codeRepo.Directory, "build.sh.result.txt")));
+    }
+
+    [Fact]
+    public void SinglePackageWithTests()
+    {
+        var context = _fixture.CreateContext();
+        var catalog = context.CreateApiCatalog("Google.Test.V1", "Google.Test.V2");
+        var codeRepo = context.CreateCodeRepo(catalog);
+        codeRepo.AddScriptMocks();
+
+        var command = new BuildLibraryCommand();
+        var options = ContainerOptions.FromArgs(
+            $"--repo-root={codeRepo.Directory}",
+            "--library-id=Google.Test.V1",
+            "--test=true");
+        Assert.Equal(0, command.Execute(options));
+
+        codeRepo.AssertExist("build.sh.result.txt");
+        Assert.Equal(new string[] { "build-start", "Google.Test.V1", "build-end" }, File.ReadAllLines(Path.Combine(codeRepo.Directory, "build.sh.result.txt")));
+    }
+
+    [Fact]
+    public void AllPackages()
+    {
+        var context = _fixture.CreateContext();
+        var catalog = context.CreateApiCatalog("Google.Test.V1", "Google.Test.V2");
+        var codeRepo = context.CreateCodeRepo(catalog);
+        codeRepo.AddScriptMocks();
+
+        var command = new BuildLibraryCommand();
+        var options = ContainerOptions.FromArgs(
+            $"--repo-root={codeRepo.Directory}");
+        Assert.Equal(0, command.Execute(options));
+
+        codeRepo.AssertExist("build.sh.result.txt");
+        Assert.Equal(new string[] { "build-start", "--notests", "Google.Test.V1", "Google.Test.V2", "build-end" }, File.ReadAllLines(Path.Combine(codeRepo.Directory, "build.sh.result.txt")));
+    }
+
+    [Fact]
+    public void AllPackagesWithTests()
+    {
+        var context = _fixture.CreateContext();
+        var catalog = context.CreateApiCatalog("Google.Test.V1", "Google.Test.V2");
+        var codeRepo = context.CreateCodeRepo(catalog);
+        codeRepo.AddScriptMocks();
+
+        var command = new BuildLibraryCommand();
+        var options = ContainerOptions.FromArgs(
+            $"--repo-root={codeRepo.Directory}",
+            "--test=true");
+        Assert.Equal(0, command.Execute(options));
+
+        codeRepo.AssertExist("build.sh.result.txt");
+        Assert.Equal(new string[] { "build-start", "Google.Test.V1", "Google.Test.V2", "build-end" }, File.ReadAllLines(Path.Combine(codeRepo.Directory, "build.sh.result.txt")));
+    }
+}
