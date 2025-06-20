7137f7c570ed8ce53e7a6a600bdfe94aee96aca4
Jon Skeet <jonskeet@google.com>
2025-06-20 10:27:19 +0100

    tests: Add Docker-based tests for integration-test-library
    It's unfortunate that we need quite a lot of real-repo files, and
    currently we have to specify those everywhere. We could potentially
    have some "named file collections" or similar, so you could just
    specify "copy this set to /repo" but that can come later.

---
+++ tools/Google.Cloud.Tools.ReleaseManager.IntegrationTests/ContainerCommands/DockerTests/IntegrationTestLibrary-FlakyTest/repo/apis/Google.Test.V2/Google.Test.V2.IntegrationTests/FlakyTest.cs
@@ -0,0 +1,32 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License"):
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.IO;
+using Xunit;
+
+namespace Google.Test.V2.IntegrationTests;
+
+public class FlakyTest
+{
+    [Fact]
+    public void FailOnce()
+    {
+        if (File.Exists("/test/repo/failure-indicator.txt"))
+        {
+            return;
+        }
+        File.WriteAllText("/test/repo/failure-indicator.txt", "This test has failed once");
+        Assert.Fail("Failure that will pass on retry");
+    }
+}
