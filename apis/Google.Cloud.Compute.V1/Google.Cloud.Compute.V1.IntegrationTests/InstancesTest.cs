49e6127d7236ebdec1648517f71e450d416afcc6
Jon Skeet <jonskeet@google.com>
2025-07-08 11:22:48 +0100

    tests: skip a single Compute test in restricted environments
    See b/430220541 for more details.

--- apis/Google.Cloud.Compute.V1/Google.Cloud.Compute.V1.IntegrationTests/InstancesTest.cs
+++ apis/Google.Cloud.Compute.V1/Google.Cloud.Compute.V1.IntegrationTests/InstancesTest.cs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Google.Cloud.ClientTesting;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -28,9 +29,10 @@ namespace Google.Cloud.Compute.V1.IntegrationTests
         public InstancesTest(ComputeFixture fixture, ITestOutputHelper output) =>
             (_fixture, _output) = (fixture, output);
 
-        [Fact]
+        [SkippableFact]
         public void CreateInstance()
         {
+            TestEnvironment.SkipOnRestrictedEnvironment();
             string projectId = _fixture.ProjectId;
             string zone = _fixture.Zone;
             string machineType =
