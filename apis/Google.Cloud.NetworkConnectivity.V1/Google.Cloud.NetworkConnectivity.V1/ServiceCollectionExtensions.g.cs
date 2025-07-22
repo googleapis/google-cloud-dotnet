0b8255256fe9ca3c7d1c91cb27c4ba5f75462e9f
Google Cloud SDK Librarian <cloud-sdk-librarian-dotnet-robot@google.com>
2025-07-22 07:07:57 +0000

    regen: Regenerate Google.Cloud.NetworkConnectivity.V1 at API commit deb6327
    feat: add Network Connectivity Center APIs for Internal Range service
    
    
    PiperOrigin-RevId: 785376390
    Source-Link: https://github.com/googleapis/googleapis/commit/deb6327efce102408fa665b99af3887b02b4b27c

--- apis/Google.Cloud.NetworkConnectivity.V1/Google.Cloud.NetworkConnectivity.V1/ServiceCollectionExtensions.g.cs
+++ apis/Google.Cloud.NetworkConnectivity.V1/Google.Cloud.NetworkConnectivity.V1/ServiceCollectionExtensions.g.cs
@@ -99,6 +99,42 @@ namespace Microsoft.Extensions.DependencyInjection
             });
 
         /// <summary>
+        /// Adds a singleton <see cref="gcnv::InternalRangeServiceClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddInternalRangeServiceClient(this IServiceCollection services, sys::Action<gcnv::InternalRangeServiceClientBuilder> action = null) =>
+            services.AddSingleton(provider =>
+            {
+                gcnv::InternalRangeServiceClientBuilder builder = new gcnv::InternalRangeServiceClientBuilder();
+                action?.Invoke(builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
+        /// Adds a singleton <see cref="gcnv::InternalRangeServiceClient"/> to <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">
+        /// The service collection to add the client to. The services are used to configure the client when requested.
+        /// </param>
+        /// <param name="action">
+        /// An optional action to invoke on the client builder. This is invoked before services from
+        /// <paramref name="services"/> are used.
+        /// </param>
+        public static IServiceCollection AddInternalRangeServiceClient(this IServiceCollection services, sys::Action<sys::IServiceProvider, gcnv::InternalRangeServiceClientBuilder> action) =>
+            services.AddSingleton(provider =>
+            {
+                gcnv::InternalRangeServiceClientBuilder builder = new gcnv::InternalRangeServiceClientBuilder();
+                action?.Invoke(provider, builder);
+                return builder.Build(provider);
+            });
+
+        /// <summary>
         /// Adds a singleton <see cref="gcnv::PolicyBasedRoutingServiceClient"/> to <paramref name="services"/>.
         /// </summary>
         /// <param name="services">
