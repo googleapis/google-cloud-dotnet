2988e7da8f8844861cd0c01bfccf390ea3f7d65b
Google Cloud SDK Librarian <cloud-sdk-librarian-dotnet-robot@google.com>
2025-07-24 07:08:32 +0000

    regen: Regenerate Google.Cloud.DiscoveryEngine.V1 at API commit 1791a77
    feat: add SessionService
    docs: A comment for field `filter` in message `.google.cloud.discoveryengine.v1.ListSessionsRequest` is changed
    docs: A comment for field `session` in message `.google.cloud.discoveryengine.v1.SearchRequest` is changed
    docs: A comment for field `query` in message `.google.cloud.discoveryengine.v1.Session` is changed
    docs: A comment for field `answer` in message `.google.cloud.discoveryengine.v1.Session` is changed
    docs: A comment for field `query_id` in message `.google.cloud.discoveryengine.v1.Query` is changed
    
    
    PiperOrigin-RevId: 786299564
    Source-Link: https://github.com/googleapis/googleapis/commit/1791a77dfb0580ae2bbde3840105d2096968cc13

--- apis/Google.Cloud.DiscoveryEngine.V1/Google.Cloud.DiscoveryEngine.V1/ConversationalSearchService.g.cs
+++ apis/Google.Cloud.DiscoveryEngine.V1/Google.Cloud.DiscoveryEngine.V1/ConversationalSearchService.g.cs
@@ -11813,11 +11813,23 @@ namespace Google.Cloud.DiscoveryEngine.V1 {
     public const int FilterFieldNumber = 4;
     private string filter_ = "";
     /// <summary>
-    /// A filter to apply on the list results. The supported features are:
-    /// user_pseudo_id, state.
+    /// A comma-separated list of fields to filter by, in EBNF grammar.
+    /// The supported fields are:
+    /// * `user_pseudo_id`
+    /// * `state`
+    /// * `display_name`
+    /// * `starred`
+    /// * `is_pinned`
+    /// * `labels`
+    /// * `create_time`
+    /// * `update_time`
     ///
-    /// Example:
+    /// Examples:
     /// "user_pseudo_id = some_id"
+    /// "display_name = \"some_name\""
+    /// "starred = true"
+    /// "is_pinned=true AND (NOT labels:hidden)"
+    /// "create_time > \"1970-01-01T12:00:00Z\""
     /// </summary>
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     [global::System.CodeDom.Compiler.GeneratedCode("protoc", null)]
