0e88f6be1bf372fec30c0ce782f30a7e6624f9b6
Jon Skeet <jonskeet@google.com>
2025-11-07 09:16:10 +0000

    chore: remove Google.Cloud.Storage.V2
    This has never been released, and would require significant work
    before we *did* release it. We currently have no plans to use this
    API in .NET.

--- generator-input/tweaks/Google.Cloud.Storage.V2/pregeneration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Add the csharp_namespace option somewhere (anywhere) before the first protobuf element.
-# Eventually this will addressed in the googleapis repo.
-sed -i '20 i option csharp_namespace = "Google.Cloud.Storage.V2";' $GOOGLEAPIS/google/storage/v2/*.proto
