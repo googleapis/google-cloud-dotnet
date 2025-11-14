20652dbd59d6eeb66c8265781fb6becb9c7f5aa6
Google Cloud SDK Librarian <cloud-sdk-librarian-dotnet-robot@google.com>
2025-11-14 08:10:01 +0000

    regen: Regenerate Google.Shopping.Merchant.IssueResolution.V1 at API commit 66ed09c
    chore: removing parameter before client library generation
    
    
    PiperOrigin-RevId: 831886184
    Source-Link: https://github.com/googleapis/googleapis/commit/66ed09cfd237e5acd4b3eb43f73e963b3f87c563

--- apis/Google.Shopping.Merchant.IssueResolution.V1/Google.Shopping.Merchant.IssueResolution.V1.Snippets/IssueResolutionServiceClientSnippets.g.cs
+++ apis/Google.Shopping.Merchant.IssueResolution.V1/Google.Shopping.Merchant.IssueResolution.V1.Snippets/IssueResolutionServiceClientSnippets.g.cs
@@ -128,7 +128,6 @@ namespace GoogleCSharpSnippets
                 LanguageCode = "",
                 TimeZone = "",
                 Payload = new RenderIssuesRequestPayload(),
-                ProductIdBase64UrlEncoded = false,
             };
             // Make the request
             RenderProductIssuesResponse response = issueResolutionServiceClient.RenderProductIssues(request);
@@ -149,7 +148,6 @@ namespace GoogleCSharpSnippets
                 LanguageCode = "",
                 TimeZone = "",
                 Payload = new RenderIssuesRequestPayload(),
-                ProductIdBase64UrlEncoded = false,
             };
             // Make the request
             RenderProductIssuesResponse response = await issueResolutionServiceClient.RenderProductIssuesAsync(request);
