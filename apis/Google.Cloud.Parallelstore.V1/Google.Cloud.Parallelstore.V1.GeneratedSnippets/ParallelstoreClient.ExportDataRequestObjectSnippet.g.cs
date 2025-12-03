0c0e4f358ed266684d2faac0f7b0a4b0dda4edc4
Google Cloud SDK Librarian <cloud-sdk-librarian-dotnet-robot@google.com>
2025-12-03 08:14:50 +0000

    regen: Regenerate Google.Cloud.Parallelstore.V1 at API commit 02f73f5
    feat: add transfer metadata options proto definition
    docs: update tickets component number
    
    
    PiperOrigin-RevId: 839132650
    Source-Link: https://github.com/googleapis/googleapis/commit/02f73f5309229c62662217036d5ea533577b31b4

--- apis/Google.Cloud.Parallelstore.V1/Google.Cloud.Parallelstore.V1.GeneratedSnippets/ParallelstoreClient.ExportDataRequestObjectSnippet.g.cs
+++ apis/Google.Cloud.Parallelstore.V1/Google.Cloud.Parallelstore.V1.GeneratedSnippets/ParallelstoreClient.ExportDataRequestObjectSnippet.g.cs
@@ -42,6 +42,7 @@ namespace GoogleCSharpSnippets
                 DestinationGcsBucket = new DestinationGcsBucket(),
                 RequestId = "",
                 ServiceAccountAsServiceAccountName = ServiceAccountName.FromProjectServiceAccount("[PROJECT]", "[SERVICE_ACCOUNT]"),
+                MetadataOptions = new TransferMetadataOptions(),
             };
             // Make the request
             Operation<ExportDataResponse, ExportDataMetadata> response = parallelstoreClient.ExportData(request);
