07e3520d4090746125e91329921e426a38ca48d1
Google Cloud SDK Librarian <cloud-sdk-librarian-dotnet-robot@google.com>
2025-12-24 08:07:12 +0000

    feat: Initial generation for Google.Cloud.ApiRegistry.V1Beta

---
+++ apis/Google.Cloud.ApiRegistry.V1Beta/Google.Cloud.ApiRegistry.V1Beta.GeneratedSnippets/CloudApiRegistryClient.GetMcpServerResourceNamesSnippet.g.cs
@@ -0,0 +1,43 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated code. DO NOT EDIT!
+
+namespace GoogleCSharpSnippets
+{
+    // [START cloudapiregistry_v1beta_generated_CloudApiRegistry_GetMcpServer_sync_flattened_resourceNames]
+    using Google.Cloud.ApiRegistry.V1Beta;
+
+    public sealed partial class GeneratedCloudApiRegistryClientSnippets
+    {
+        /// <summary>Snippet for GetMcpServer</summary>
+        /// <remarks>
+        /// This snippet has been automatically generated and should be regarded as a code template only.
+        /// It will require modifications to work:
+        /// - It may require correct/in-range values for request initialization.
+        /// - It may require specifying regional endpoints when creating the service client as shown in
+        ///   https://cloud.google.com/dotnet/docs/reference/help/client-configuration#endpoint.
+        /// </remarks>
+        public void GetMcpServerResourceNames()
+        {
+            // Create client
+            CloudApiRegistryClient cloudApiRegistryClient = CloudApiRegistryClient.Create();
+            // Initialize request argument(s)
+            McpServerName name = McpServerName.FromProjectLocationApiNamespaceMcpServer("[PROJECT]", "[LOCATION]", "[API_NAMESPACE]", "[MCP_SERVER]");
+            // Make the request
+            McpServer response = cloudApiRegistryClient.GetMcpServer(name);
+        }
+    }
+    // [END cloudapiregistry_v1beta_generated_CloudApiRegistry_GetMcpServer_sync_flattened_resourceNames]
+}
