c0b8c67d7e4cd4f412c8f28b3ab0c397246b22d1
Amanda Tarafa Mas <atarafamas@google.com>
2026-01-26 09:18:09 -0800

    tests(Storage): Skip URL signing tests.
    See b/478856508.

--- apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.IntegrationTests/UrlSignerTest.V4SignerTest.cs
+++ apis/Google.Cloud.Storage.V1/Google.Cloud.Storage.V1.IntegrationTests/UrlSignerTest.V4SignerTest.cs
@@ -33,69 +33,69 @@ namespace Google.Cloud.Storage.V1.IntegrationTests
 
             private static string GetTestName([CallerMemberName] string methodName = null) => $"{nameof(V4SignerTest)}.{methodName}";
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task DeleteTest() => await _fixture.FinishDelayTest(GetTestName());
             private void DeleteTest_InitDelayTest() => DeleteTest_Common(_fixture, SigningVersion.V4);
 
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task GetTest() => await _fixture.FinishDelayTest(GetTestName());
             private void GetTest_InitDelayTest() => GetTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task GetBucketTest() => await _fixture.FinishDelayTest(GetTestName());
             private void GetBucketTest_InitDelayTest() => GetBucketTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task GetObjectWithSpacesTest() => await _fixture.FinishDelayTest(GetTestName());
             private void GetObjectWithSpacesTest_InitDelayTest() => GetObjectWithSpacesTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task GetWithCustomerSuppliedEncryptionKeysTest() => await _fixture.FinishDelayTest(GetTestName());
             private void GetWithCustomerSuppliedEncryptionKeysTest_InitDelayTest() => GetWithCustomerSuppliedEncryptionKeysTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task GetWithCustomHeadersTest() => await _fixture.FinishDelayTest(GetTestName());
             private void GetWithCustomHeadersTest_InitDelayTest() => GetWithCustomHeadersTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task GetWithVirtualHostedStyleTest() => await _fixture.FinishDelayTest(GetTestName());
             private void GetWithVirtualHostedStyleTest_InitDelayTest() => GetWithVirtualHostedStyleTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task GetWithHttpTest() => await _fixture.FinishDelayTest(GetTestName());
             private void GetWithHttpTest_InitDelayTest() => GetWithHttpTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task HeadTest() => await _fixture.FinishDelayTest(GetTestName());
             private void HeadTest_InitDelayTest() => HeadTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task HeadWithGetMethodSignedURLTest() => await _fixture.FinishDelayTest(GetTestName());
             private void HeadWithGetMethodSignedURLTest_InitDelayTest() => HeadWithGetMethodSignedURLTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task PutTest() => await _fixture.FinishDelayTest(GetTestName());
             private void PutTest_InitDelayTest() => PutTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task PutWithCustomerSuppliedEncryptionKeysTest() => await _fixture.FinishDelayTest(GetTestName());
             private void PutWithCustomerSuppliedEncryptionKeysTest_InitDelayTest() => PutWithCustomerSuppliedEncryptionKeysTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task PutWithCustomHeadersTest() => await _fixture.FinishDelayTest(GetTestName());
             private void PutWithCustomHeadersTest_InitDelayTest() => PutWithCustomHeadersTest_Common(_fixture, SigningVersion.V4);
 
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task ResumableUploadTest() => await _fixture.FinishDelayTest(GetTestName());
             private void ResumableUploadTest_InitDelayTest() => ResumableUploadTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task ResumableUploadResumeTest() => await _fixture.FinishDelayTest(GetTestName());
             private void ResumableUploadResumeTest_InitDelayTest() => ResumableUploadResumeTest_Common(_fixture, SigningVersion.V4);
 
-            [Fact]
+            [Fact(Skip = "b/478856508")]
             public async Task ResumableUploadWithCustomerSuppliedEncryptionKeysTest() => await _fixture.FinishDelayTest(GetTestName());
             private void ResumableUploadWithCustomerSuppliedEncryptionKeysTest_InitDelayTest() => ResumableUploadWithCustomerSuppliedEncryptionKeysTest_Common(_fixture, SigningVersion.V4);
         }
