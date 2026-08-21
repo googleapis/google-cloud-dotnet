ca6f1ba43067afcc60855b647147cd50791949a5
Eric Evans <ericfevans@google.com>
2026-08-21 11:43:32 -0700

    test(VertexAI.Extensions): Update comments

--- apis/Google.Cloud.VertexAI.Extensions/Google.Cloud.VertexAI.Extensions.IntegrationTests/VertexAIExtensionsTest.cs
+++ apis/Google.Cloud.VertexAI.Extensions/Google.Cloud.VertexAI.Extensions.IntegrationTests/VertexAIExtensionsTest.cs
@@ -67,7 +67,7 @@ public class VertexAIExtensionsTest
         }
     }
 
-    [Fact(Skip = "Image generation no longer available for Prediction API b/549774591")]
+    [Fact(Skip = "b/549774591")]
     public async Task AsIImageGenerator_GenerateImage()
     {
         IImageGenerator generator = await new PredictionServiceClientBuilder()
