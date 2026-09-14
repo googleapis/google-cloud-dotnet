dd49c91d0026ab3cf1c12f17eae508587541afe5
Google Cloud SDK Librarian <cloud-sdk-librarian-dotnet-robot@google.com>
2026-09-14 16:10:33 +0000

    regen: Regenerate Google.Cloud.DiscoveryEngine.V1 at API commit d3e4b45
    feat: add SearchAddonSpec and SearchRequest.search_addon_spec, letting callers disable individual search add-ons (semantic, KPI personalization, generative answer) under configurable pricing
    feat: add CustomRankingParams and SearchRequest.custom_ranking_params
    feat: add SearchRequest.entity for customers running multiple entities, domains, sites or regions
    feat: add SearchRequest.num_results_per_data_store and SearchRequest.DataStoreSpec.num_results to cap results per data store
    feat: add SearchRequest.SearchAsYouTypeSpec.Field, fields and score_threshold for per-request Search As You Type scoring
    feat: add SearchResponse.applied_controls, reporting which serving controls were applied
    feat: add SearchResponse.SearchResult.RetrievalSignals and retrieval_signals, exposing retrieval source and semantic relevance score
    feat: add SearchResponse.SearchResult.RankSignals.precomputed_expression_values
    feat: add discoveryengine.readwrite, discoveryengine.assist.readwrite and discoveryengine.serving.readwrite OAuth scopes to SearchService
    docs: Multi-turn Search is no longer restricted to private GA; the allowlisting note is removed from SearchRequest and SearchRequest.session
    docs: clarify SearchRequest.geo_search_query_detection_field_names, SearchRequest.ranking_expression, ImageQuery.image_bytes and the media ranking expression list
    
    
    PiperOrigin-RevId: 981167290
    Source-Link: https://github.com/googleapis/googleapis/commit/d3e4b45d316b4fb8ad120d04882cd7b1065fc333

--- apis/Google.Cloud.DiscoveryEngine.V1/Google.Cloud.DiscoveryEngine.V1.GeneratedSnippets/SearchServiceClient.SearchLiteRequestObjectAsyncSnippet.g.cs
+++ apis/Google.Cloud.DiscoveryEngine.V1/Google.Cloud.DiscoveryEngine.V1.GeneratedSnippets/SearchServiceClient.SearchLiteRequestObjectAsyncSnippet.g.cs
@@ -80,7 +80,11 @@ namespace GoogleCSharpSnippets
                 OneBoxPageSize = 0,
                 RelevanceScoreSpec = new SearchRequest.Types.RelevanceScoreSpec(),
                 RankingExpressionBackend = SearchRequest.Types.RankingExpressionBackend.Unspecified,
+                SearchAddonSpec = new SearchRequest.Types.SearchAddonSpec(),
                 PageCategories = { "", },
+                CustomRankingParams = new SearchRequest.Types.CustomRankingParams(),
+                NumResultsPerDataStore = 0,
+                Entity = "",
                 RelevanceFilterSpec = new SearchRequest.Types.RelevanceFilterSpec(),
             };
             // Make the request
