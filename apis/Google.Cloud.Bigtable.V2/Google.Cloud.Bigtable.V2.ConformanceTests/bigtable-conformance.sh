3ff2a468ae69094a7be94dcba9e2a63a435b2382
Amanda Tarafa Mas <atarafamas@google.com>
2026-09-23 09:46:51 -0700

    tests(Bigtable): Fix conformance tests failures.
    See b/563376582.

--- apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.ConformanceTests/bigtable-conformance.sh
+++ apis/Google.Cloud.Bigtable.V2/Google.Cloud.Bigtable.V2.ConformanceTests/bigtable-conformance.sh
@@ -34,8 +34,7 @@ cd cloud-bigtable-clients-test/tests
 # Cookie, RetryInfo, ExecuteQuery, ReverseScans and FeatureGap are known failures of new features that we don't yet support.
 # CloseClient we don't support as expected, but we support it in a valid manner.
 # For the others we have issues to investigate, see comments in b/372509076 .
-# For the TestMutateRows_NoRetry_* see b/563376582.
-eval "go test -v -proxy_addr=:7238 -skip _Retry_WithRoutingCookie\|_Retry_WithRetryInfo\|_CloseClient\|_ReverseScans\|TestFeatureGap\|TestExecuteQuery\|TestReadRows_Retry_LastScannedRow_Reverse\|TestReadRow_Generic_DeadlineExceeded\|TestMutateRows_NoRetry_MissingResponseEntry\|TestMutateRows_NoRetry_ErrorNotificationParity"
+eval "go test -v -proxy_addr=:7238 -skip _Retry_WithRoutingCookie\|_Retry_WithRetryInfo\|_CloseClient\|_ReverseScans\|TestFeatureGap\|TestExecuteQuery\|TestReadRows_Retry_LastScannedRow_Reverse\|TestReadRow_Generic_DeadlineExceeded"
 returnCode=$?
 popd
 
